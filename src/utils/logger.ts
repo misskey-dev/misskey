ebe3225e0c8f1b28ba15929fff8be3fe4396c31a
syuilo <syuilotan@yahoo.co.jp>
2016-12-29 21:14:47 +0900

    Add overload

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -10,9 +10,12 @@ function toLevelColor(level: LogLevel): chalk.ChalkStyle {
 	}
 }
 
+export function log(message: string): void;
 export function log(level: LogLevel, message: string): void;
 export function log(level: LogLevel, message: string, domain: string): void;
-export function log(level: LogLevel, message: string, domain?: string): void {
+export function log(x: string | LogLevel, message?: string, domain?: string): void {
+	const level = typeof message == 'undefined' ? 'Info' : x as LogLevel;
+	message = typeof message == 'undefined' ? x : message;
 	if (typeof domain == 'string') {
 		log(level, `[${domain}] ${message}`);
 	} else {
