6b1ddc51efa693af6682eb894d7837201902dad9
Aya Morisawa <AyaMorisawa4869@gmail.com>
2016-12-31 03:36:22 +0900

    Add missing type annotation

--- src/utils/machineInfo.ts
+++ src/utils/machineInfo.ts
@@ -2,7 +2,7 @@ import * as os from 'os';
 import Logger from './logger';
 
 export default class MachineInfo {
-	static show() {
+	static show(): void {
 		const totalmem = (os.totalmem() / 1024 / 1024 / 1024).toFixed(1);
 		const freemem = (os.freemem() / 1024 / 1024 / 1024).toFixed(1);
 		let logger = new Logger('Machine');
