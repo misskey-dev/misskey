cdd26a5aaab6ca45617b6b840dac330b6d0804b9
Aya Morisawa <AyaMorisawa4869@gmail.com>
2017-01-03 04:35:32 +0900

    Update MachineInfo

--- src/utils/machineInfo.ts
+++ src/utils/machineInfo.ts
@@ -6,7 +6,10 @@ export default class MachineInfo {
 		const totalmem = (os.totalmem() / 1024 / 1024 / 1024).toFixed(1);
 		const freemem = (os.freemem() / 1024 / 1024 / 1024).toFixed(1);
 		let logger = new Logger('Machine');
-		logger.info(os.hostname());
+		logger.info(`Hostname: ${os.hostname()}`);
+		logger.info(`Platform: ${process.platform}`);
+		logger.info(`Architecture: ${process.arch}`);
+		logger.info(`Node.js: ${process.version}`);
 		logger.info(`CPU: ${os.cpus().length}core`);
 		logger.info(`MEM: ${totalmem}GB (available: ${freemem}GB)`);
 	}
