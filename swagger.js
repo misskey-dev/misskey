972f0e7f41fc133f1ae7ad2d9fdd87e5dd7cbac0
Tosuke <tasukeprg@gmail.com>
2017-01-04 13:56:20 +0900

    Add host and schemes to swaggerJSON

--- swagger.js
+++ swagger.js
@@ -1,5 +1,8 @@
+'use strict'
+
 const swaggerJSDoc = require('swagger-jsdoc');
 const fs = require('fs');
+const yaml = require('js-yaml');
 
 const apiRoot = './src/api/endpoints';
 const files = [
@@ -23,6 +26,8 @@ var options = {
       title: 'Misskey API',
       version: 'aoi',
     },
+    host: 'api.misskey.xyz',
+    schemes: ['https'],
     consumes: [
       'application/x-www-form-urlencoded'
     ],
@@ -33,6 +38,12 @@ var options = {
   apis: []
 };
 options.apis = files.map(c => {return `${apiRoot}/${c}`;});
+
+if(fs.existsSync('.config/config.yml')){
+  var config = yaml.safeLoad(fs.readFileSync('./.config/config.yml', 'utf8'));
+  options.swaggerDefinition.host = config.url;
+  options.swaggerDefinition.schemes = config.https.enable ? ['https'] : ['http'];
+}
 
 var swaggerSpec = swaggerJSDoc(options);
 swaggerSpec.definitions.Error = errorDefinition;
