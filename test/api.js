888f31a9bb5635f0d5d906f88a819a2e2657c895
syuilo <syuilotan@yahoo.co.jp>
2017-01-17 09:32:28 +0900

    [WIP] test

--- test/api.js
+++ test/api.js
@@ -7,7 +7,34 @@ process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../built/server');
 const should = chai.should();
 
 chai.use(chaiHttp);
+
+const server = require('../built/api/server');
+
+describe('API', () => {
+	describe('posts/create', () => {
+		it('simple', done => {
+			const post = {
+				text: 'Hi'
+			};
+			chai.request(server)
+				.post('/posts/create')
+				.send(post)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					done();
+				});
+		});
+
+		it('reply', () => {
+
+		});
+
+		it('repost', () => {
+
+		});
+	});
+});
