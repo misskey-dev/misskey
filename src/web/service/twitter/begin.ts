6f7cf99f19f93d3b32c5c772ce029274a429aee3
syuilo <syuilotan@yahoo.co.jp>
2017-01-21 08:42:44 +0900

    [Server] Fix bug

--- src/web/service/twitter/begin.ts
+++ src/web/service/twitter/begin.ts
@@ -3,17 +3,19 @@ import * as express from 'express';
 const Twitter = require('twitter');
 import config from '../../../conf';
 
-const client = new Twitter({
-	consumer_key: config.twitter.consumer_key,
-	consumer_secret: config.twitter.consumer_secret
-});
-
 module.exports = (req: express.Request, res: express.Response) => {
-	client.post('oauth/request_token', {
-		oauth_callback: config.url + '/tw/cb'
-	}, (x, y, z) => {
-		console.log(x);
-		console.log(y);
-		console.log(z);
-	});
+	if (config.twitter) {
+		const client = new Twitter({
+			consumer_key: config.twitter.consumer_key,
+			consumer_secret: config.twitter.consumer_secret
+		});
+
+		client.post('oauth/request_token', {
+			oauth_callback: config.url + '/tw/cb'
+		}, (x, y, z) => {
+			console.log(x);
+			console.log(y);
+			console.log(z);
+		});
+	}
 };
