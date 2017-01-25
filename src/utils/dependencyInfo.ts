5aa7ab3936b76c9ee411c8a73ff9fa276f9e6780
otofune <otofune@gmail.com>
2017-01-25 20:22:45 +0900

    [utils] dependencyInfo: update MongoDB regexp

--- src/utils/dependencyInfo.ts
+++ src/utils/dependencyInfo.ts
@@ -9,7 +9,7 @@ export default class {
 	}
 
 	showAll(): void {
-		this.show('MongoDB', 'mongo --version', x => x.match(/^MongoDB shell version: (.*)\r?\n$/));
+		this.show('MongoDB', 'mongo --version', x => x.match(/^MongoDB shell version:? (.*)\r?\n/));
 		this.show('Redis', 'redis-server --version', x => x.match(/v=([0-9\.]*)/));
 		this.show('GraphicsMagick', 'gm -version', x => x.match(/^GraphicsMagick ([0-9\.]*) .*/));
 	}
