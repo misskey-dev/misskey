c9b4a9e5e920eebfe6ca91c07f2e89d9368e545e
syuilo <syuilotan@yahoo.co.jp>
2017-02-17 14:03:17 +0900

    [Client:Mobile] Update desogn

--- src/web/app/mobile/script.js
+++ src/web/app/mobile/script.js
@@ -11,6 +11,9 @@ const route = require('./router.ls');
  * Boot
  */
 boot(me => {
+	// http://qiita.com/junya/items/3ff380878f26ca447f85
+	document.body.setAttribute('ontouchstart', '');
+
 	// Register mixins
 	mixins(me);
 
