4dc96b87ddb9d2146116a54c7031d1682b236b18
syuilo <syuilotan@yahoo.co.jp>
2017-02-18 16:53:46 +0900

    :v:

---
+++ src/web/app/common/scripts/loading.js
@@ -0,0 +1,21 @@
+const NProgress = require('nprogress');
+NProgress.configure({
+	trickleSpeed: 500,
+	showSpinner: false
+});
+
+const root = document.getElementsByTagName('html')[0];
+
+module.exports = {
+	start: () => {
+		root.classList.add('progress');
+		NProgress.start();
+	},
+	done: () => {
+		root.classList.remove('progress');
+		NProgress.done();
+	},
+	set: val => {
+		NProgress.set(val);
+	}
+};
