b8bddb85e519ae47feb367e3b7067f7958105f6c
Aya Morisawa <AyaMorisawa4869@gmail.com>
2017-02-27 16:31:12 +0900

    Fix typo: arleady -> already

--- src/api/endpoints/following/create.js
+++ src/api/endpoints/following/create.js
@@ -52,7 +52,7 @@ module.exports = (params, user) =>
 		return rej('user not found');
 	}
 
-	// Check arleady following
+	// Check already following
 	const exist = await Following.findOne({
 		follower_id: follower._id,
 		followee_id: followee._id,
