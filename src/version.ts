45b2b2b1965be2aa33aa38665d161a1551d522c0
syuilo <syuilotan@yahoo.co.jp>
2017-03-18 02:12:27 +0900

    Fix

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 const getVersion = new Promise<string>(async resolve => {
-	const package = require('../package.json');
+	const ぱっけーじ = require('../package.json');
 
-	const version = package.version;
+	const version = ぱっけーじ.version;
 
 	resolve(version);
 });
