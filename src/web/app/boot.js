88041dbd94d60034991714b57b98859587cdb5a4
syuilo <syuilotan@yahoo.co.jp>
2017-05-18 14:34:13 +0900

    Refactoring

--- src/web/app/boot.js
+++ src/web/app/boot.js
@@ -37,7 +37,13 @@ const [head] = document.getElementsByTagName('head');
 if (isMobile) {
 	const meta = document.createElement('meta');
 	meta.setAttribute('name', 'viewport');
-	meta.setAttribute('content', 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no');
+	meta.setAttribute('content', [
+		['width', 'device-width'],
+		['initial-scale', '1'],
+		['minimum-scale', '1'],
+		['maximum-scale', '1'],
+		['user-scalable', 'no']
+	].map(x => x.join('=')).join(','));
 	head.appendChild(meta);
 }
 
