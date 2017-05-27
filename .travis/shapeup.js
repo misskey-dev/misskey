8d5adfbdd77b925fd2b8d0761c2d2b1b84e7bf3d
otofune <otofune@gmail.com>
2017-05-28 04:25:06 +0900

    [travis] shapeup script will indent package.json with tabs
    original package.json indented with tabs, but shapeup indent with 4
    space.

--- .travis/shapeup.js
+++ .travis/shapeup.js
@@ -7,7 +7,7 @@ fs.readFile(filename, (err, data) => {
     if (err) process.exit(2)
     const object = JSON.parse(data)
     delete object.devDependencies
-    fs.writeFile(filename, JSON.stringify(object, null, 4) + '\n', err => {
+    fs.writeFile(filename, JSON.stringify(object, null, '\t') + '\n', err => {
         if (err) process.exit(3)
     })
 })
