2e3a8d4e367fbaf4ab91a474fe1c63e836e99a1e
syuilo <syuilotan@yahoo.co.jp>
2017-06-07 00:10:01 +0900

    Better banner

--- webpack/plugins/banner.ts
+++ webpack/plugins/banner.ts
@@ -1,6 +1,9 @@
 import * as webpack from 'webpack';
 
 export default version => new webpack.BannerPlugin({
-	banner: `Misskey v${version} - built at ${new Date()} | (c) syuilo 2014-2017
-		hash:[hash], chunkhash:[chunkhash], name:[name], filebase:[filebase], query:[query], file:[file]`
+	banner:
+		`Misskey v${version} | MIT Licensed, (c) syuilo 2014-2017\n` +
+		'https://github.com/syuilo/misskey\n' +
+		`built at ${new Date()}\n` +
+		'hash:[hash], chunkhash:[chunkhash]'
 });
