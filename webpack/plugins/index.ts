866d5428bcb06b4978f8039fe7197c560559d1aa
syuilo <syuilotan@yahoo.co.jp>
2017-10-22 15:01:02 +0900

    Use uglifyjs-webpack-plugin instead of uglify-es-webpack-plugin
    Because uglify-es-webpack-plugin is now deprecated

--- webpack/plugins/index.ts
+++ webpack/plugins/index.ts
@@ -2,13 +2,11 @@ const StringReplacePlugin = require('string-replace-webpack-plugin');
 
 import constant from './const';
 import hoist from './hoist';
-//import minify from './minify';
+import minify from './minify';
 import banner from './banner';
 
-/*
 const env = process.env.NODE_ENV;
 const isProduction = env === 'production';
-*/
 
 export default version => {
 	const plugins = [
@@ -16,11 +14,11 @@ export default version => {
 		new StringReplacePlugin(),
 		hoist()
 	];
-/*
+
 	if (isProduction) {
 		plugins.push(minify());
 	}
-*/
+
 	plugins.push(banner(version));
 
 	return plugins;
