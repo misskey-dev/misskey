f14fbfa3b67c51eaab4c0b273cc45c541758f615
syuilo <syuilotan@yahoo.co.jp>
2017-11-11 01:00:14 +0900

    Fix bug

--- src/common/get-notification-summary.ts
+++ src/common/get-notification-summary.ts
@@ -1,4 +1,3 @@
-import { INotification } from '../api/models/notification';
 import getPostSummary from './get-post-summary';
 import getReactionEmoji from './get-reaction-emoji';
 
@@ -6,22 +5,22 @@ import getReactionEmoji from './get-reaction-emoji';
  * 通知を表す文字列を取得します。
  * @param notification 通知
  */
-export default function(notification: INotification & any): string {
+export default function(notification: any): string {
 	switch (notification.type) {
 		case 'follow':
-			return `${notification.notifier.name}にフォローされました`;
+			return `${notification.user.name}にフォローされました`;
 		case 'mention':
-			return `言及されました: ${notification.notifier.name}「${getPostSummary(notification.post)}」`;
+			return `言及されました: ${notification.user.name}「${getPostSummary(notification.post)}」`;
 		case 'reply':
-			return `返信されました: ${notification.notifier.name}「${getPostSummary(notification.post)}」`;
+			return `返信されました: ${notification.user.name}「${getPostSummary(notification.post)}」`;
 		case 'repost':
-			return `Repostされました: ${notification.notifier.name}「${getPostSummary(notification.post)}」`;
+			return `Repostされました: ${notification.user.name}「${getPostSummary(notification.post)}」`;
 		case 'quote':
-			return `引用されました: ${notification.notifier.name}「${getPostSummary(notification.post)}」`;
+			return `引用されました: ${notification.user.name}「${getPostSummary(notification.post)}」`;
 		case 'reaction':
-			return `リアクションされました: ${notification.notifier.name} <${getReactionEmoji(notification.reaction)}>「${getPostSummary(notification.post)}」`;
+			return `リアクションされました: ${notification.user.name} <${getReactionEmoji(notification.reaction)}>「${getPostSummary(notification.post)}」`;
 		case 'poll_vote':
-			return `投票されました: ${notification.notifier.name}「${getPostSummary(notification.post)}」`;
+			return `投票されました: ${notification.user.name}「${getPostSummary(notification.post)}」`;
 		default:
 			return `<不明な通知タイプ: ${notification.type}>`;
 	}
