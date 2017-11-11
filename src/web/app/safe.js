8abaf7ab1b110a9142c4725d58f35fd8373b8450
syuilo <syuilotan@yahoo.co.jp>
2017-11-11 22:58:39 +0900

    Add missing whitespace

--- src/web/app/safe.js
+++ src/web/app/safe.js
@@ -9,6 +9,6 @@ if (!('fetch' in window)) {
 		'お使いのブラウザが古いためMisskeyを動作させることができません。' +
 		'バージョンを最新のものに更新するか、別のブラウザをお試しください。' +
 		'\n\n' +
-		'Your browser seems outdated.' +
+		'Your browser seems outdated. ' +
 		'To run Misskey, please update your browser to latest version or try other browsers.');
 }
