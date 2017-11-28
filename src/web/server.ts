66e76d9aa271415fb272822a1092224a07d455ec
syuilo <syuilotan@yahoo.co.jp>
2017-11-28 14:07:00 +0900

    Send 404 status correctly

--- src/web/server.ts
+++ src/web/server.ts
@@ -39,6 +39,9 @@ app.get('/apple-touch-icon.png', (req, res) => res.sendFile(`${__dirname}/assets
 app.use('/assets', express.static(`${__dirname}/assets`, {
 	maxAge: ms('7 days')
 }));
+app.use('/assets', (req, res) => {
+	res.sendStatus(404);
+});
 
 /**
  * ServiceWroker
