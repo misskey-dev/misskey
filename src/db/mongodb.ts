59c64ba7d91be1fbedba3c7188150e0f81c76818
syuilo <syuilotan@yahoo.co.jp>
2017-12-31 14:38:41 +0900

    Improve readability

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -1,8 +1,8 @@
 import config from '../conf';
 
 const uri = config.mongodb.user && config.mongodb.pass
-? `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`
-: `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
+	? `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`
+	: `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
 
 /**
  * monk
