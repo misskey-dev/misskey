acfa2ef0288f80b6b972d41c582355306c28ebf5
syuilo <syuilotan@yahoo.co.jp>
2018-02-15 18:39:05 +0900

    wip

--- src/web/app/common/views/components/index.ts
+++ src/web/app/common/views/components/index.ts
@@ -7,6 +7,7 @@ import nav from './nav.vue';
 import postHtml from './post-html';
 import reactionIcon from './reaction-icon.vue';
 import time from './time.vue';
+import images from './images.vue';
 
 Vue.component('mk-signin', signin);
 Vue.component('mk-signup', signup);
@@ -15,3 +16,4 @@ Vue.component('mk-nav', nav);
 Vue.component('mk-post-html', postHtml);
 Vue.component('mk-reaction-icon', reactionIcon);
 Vue.component('mk-time', time);
+Vue.component('mk-images', images);
