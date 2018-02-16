d19897c8c763ad26b15743796e99215b5710ebd4
こぴなたみぽ <syuilotan@yahoo.co.jp>
2018-02-16 15:52:28 +0900

    wip

---
+++ src/web/app/mobile/views/pages/notification.vue
@@ -0,0 +1,31 @@
+<template>
+<mk-ui :func="fn" func-icon="%fa:check%">
+	<span slot="header">%fa:R bell%%i18n:mobile.tags.mk-notifications-page.notifications%</span>
+	<mk-notifications @fetched="onFetched"/>
+</mk-ui>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Progress from '../../../common/scripts/loading';
+
+export default Vue.extend({
+	mounted() {
+		document.title = 'Misskey | %i18n:mobile.tags.mk-notifications-page.notifications%';
+		document.documentElement.style.background = '#313a42';
+
+		Progress.start();
+	},
+	methods: {
+		fn() {
+			const ok = window.confirm('%i18n:mobile.tags.mk-notifications-page.read-all%');
+			if (!ok) return;
+
+			this.$root.$data.os.api('notifications/mark_as_read_all');
+		},
+		onFetched() {
+			Progress.done();
+		}
+	}
+});
+</script>
