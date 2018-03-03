521b8557382e693ecf3e32ab539203d29dc557b0
syuilo <Syuilotan@yahoo.co.jp>
2018-03-03 11:27:05 +0900

    Update drive.vue

--- src/web/app/mobile/views/pages/drive.vue
+++ src/web/app/mobile/views/pages/drive.vue
@@ -2,7 +2,7 @@
 <mk-ui>
 	<span slot="header">
 		<template v-if="folder">%fa:R folder-open%{{ folder.name }}</template>
-		<template v-if="file"><mk-file-type-icon class="icon" :type="file.type"/>{{ file.name }}</template>
+		<template v-if="file"><mk-file-type-icon data-icon :type="file.type"/>{{ file.name }}</template>
 		<template v-if="!folder && !file">%fa:cloud%%i18n:mobile.tags.mk-drive-page.drive%</template>
 	</span>
 	<template slot="func"><button @click="fn">%fa:ellipsis-h%</button></template>
