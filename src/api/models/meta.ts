7310435584963d782c209163c65048d2cdeb4289
syuilo <syuilotan@yahoo.co.jp>
2018-03-28 13:16:52 +0900

    wip

--- src/api/models/meta.ts
+++ src/api/models/meta.ts
@@ -1,7 +1,8 @@
 import db from '../../db/mongodb';
 
-export default db.get('meta') as any; // fuck type definition
+const Meta = db.get<IMeta>('meta');
+export default Meta;
 
 export type IMeta = {
-	top_image: string;
+	broadcasts: any[];
 };
