cba73d6bc1eeb44ad2beac2566812daa9ec32751
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-04-01 20:07:04 +0900

    Implement account public key endpoint

---
+++ src/server/activitypub/with-user.ts
@@ -0,0 +1,23 @@
+import parseAcct from '../../common/user/parse-acct';
+import User from '../../models/user';
+
+export default (redirect, respond) => async (req, res, next) => {
+	const { username, host } = parseAcct(req.params.user);
+	if (host !== null) {
+		return res.sendStatus(422);
+	}
+
+	const user = await User.findOne({
+		usernameLower: username.toLowerCase(),
+		host: null
+	});
+	if (user === null) {
+		return res.sendStatus(404);
+	}
+
+	if (username !== user.username) {
+		return res.redirect(redirect(user.username));
+	}
+
+	return respond(user, req, res, next);
+}
