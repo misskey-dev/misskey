8afdb5aef91bea931c1ab7ecfd9f5ba1e977652c
syuilo <syuilotan@yahoo.co.jp>
2018-04-03 01:22:23 +0900

    Fix

--- src/server/activitypub/post.ts
+++ src/server/activitypub/post.ts
@@ -1,37 +1,29 @@
 import * as express from 'express';
 import context from '../../remote/activitypub/renderer/context';
 import render from '../../remote/activitypub/renderer/note';
-import parseAcct from '../../acct/parse';
 import Post from '../../models/post';
 import User from '../../models/user';
 
 const app = express();
 app.disable('x-powered-by');
 
-app.get('/@:user/:post', async (req, res, next) => {
+app.get('/posts/:post', async (req, res, next) => {
 	const accepted = req.accepts(['html', 'application/activity+json', 'application/ld+json']);
 	if (!(['application/activity+json', 'application/ld+json'] as any[]).includes(accepted)) {
 		return next();
 	}
 
-	const { username, host } = parseAcct(req.params.user);
-	if (host !== null) {
-		return res.sendStatus(422);
-	}
-
-	const user = await User.findOne({
-		usernameLower: username.toLowerCase(),
-		host: null
+	const post = await Post.findOne({
+		_id: req.params.post
 	});
-	if (user === null) {
+	if (post === null) {
 		return res.sendStatus(404);
 	}
 
-	const post = await Post.findOne({
-		_id: req.params.post,
-		userId: user._id
+	const user = await User.findOne({
+		_id: post.userId
 	});
-	if (post === null) {
+	if (user === null) {
 		return res.sendStatus(404);
 	}
 
