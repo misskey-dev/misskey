4b507ed349496fbd7fafc7913bd299763fed827c
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-04-03 17:18:06 +0900

    Implement remote account unfollow

--- src/remote/activitypub/act/undo/unfollow.ts
+++ src/remote/activitypub/act/undo/unfollow.ts
@@ -1,24 +1,11 @@
-import FollowedLog from '../../../../models/followed-log';
-import Following from '../../../../models/following';
-import FollowingLog from '../../../../models/following-log';
-import User from '../../../../models/user';
+import queue from '../../../../queue';
 
-export default async (resolver, { $id }) => {
-	const following = await Following.findOneAndDelete({ _id: $id });
-	if (following === null) {
-		return;
-	}
-
-	await Promise.all([
-		User.update({ _id: following.followerId }, { $inc: { followingCount: -1 } }),
-		User.findOne({ _id: following.followerId }).then(({ followingCount }) => FollowingLog.insert({
-			userId: following.followerId,
-			count: followingCount - 1
-		})),
-		User.update({ _id: following.followeeId }, { $inc: { followersCount: -1 } }),
-		User.findOne({ _id: following.followeeId }).then(({ followersCount }) => FollowedLog.insert({
-			userId: following.followeeId,
-			count: followersCount - 1
-		})),
-	]);
-};
+export default ({ $id }) => new Promise((resolve, reject) => {
+	queue.create('http', { type: 'unfollow', id: $id }).save(error => {
+		if (error) {
+			reject(error);
+		} else {
+			resolve();
+		}
+	});
+});
