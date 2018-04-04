5bd1451b610c134c056991e05327990180cbb8d5
syuilo <syuilotan@yahoo.co.jp>
2018-04-04 23:22:48 +0900

    wip

--- src/queue/processors/http/deliver.ts
+++ src/queue/processors/http/deliver.ts
@@ -1,17 +1,7 @@
 import * as kue from 'kue';
 
-import Channel from '../../models/channel';
-import Following from '../../models/following';
-import ChannelWatching from '../../models/channel-watching';
-import Post, { pack } from '../../models/post';
-import User, { isLocalUser } from '../../models/user';
-import stream, { publishChannelStream } from '../../publishers/stream';
-import context from '../../remote/activitypub/renderer/context';
-import renderCreate from '../../remote/activitypub/renderer/create';
-import renderNote from '../../remote/activitypub/renderer/note';
-import request from '../../remote/request';
+import request from '../../../remote/request';
 
 export default async (job: kue.Job, done): Promise<void> => {
-
-	request(user, following.follower[0].account.inbox, create);
-}
+	await request(job.data.user, job.data.to, job.data.content);
+};
