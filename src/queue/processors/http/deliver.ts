866797a0058a0eac1e5c1d7f6cdeeab845a505ae
syuilo <syuilotan@yahoo.co.jp>
2018-04-07 06:23:38 +0900

    Fix bug

--- src/queue/processors/http/deliver.ts
+++ src/queue/processors/http/deliver.ts
@@ -7,7 +7,7 @@ export default async (job: kue.Job, done): Promise<void> => {
 		await request(job.data.user, job.data.to, job.data.content);
 		done();
 	} catch (res) {
-		if (res.statusCode >= 300 && res.statusCode < 400) {
+		if (res.statusCode >= 400 && res.statusCode < 500) {
 			// HTTPステータスコード4xxはクライアントエラーであり、それはつまり
 			// 何回再送しても成功することはないということなのでエラーにはしないでおく
 			done();
