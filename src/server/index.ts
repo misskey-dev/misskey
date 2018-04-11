0f994692435d7be4f94492294113db3b5dbc17a1
syuilo <syuilotan@yahoo.co.jp>
2018-04-12 05:54:54 +0900

    HSTS
    Co-Authored-By: tamaina <tamaina@hotmail.co.jp>

--- src/server/index.ts
+++ src/server/index.ts
@@ -41,6 +41,17 @@ app.use((req, res, next) => {
 	next();
 });
 
+/**
+ * HSTS
+ * 6month(15552000sec)
+ */
+if (config.url.startsWith('https')) {
+	app.use((req, res, next) => {
+		res.header('strict-transport-security', 'max-age=15552000; preload');
+		next();
+	});
+}
+
 // Drop request when without 'Host' header
 app.use((req, res, next) => {
 	if (!req.headers['host']) {
