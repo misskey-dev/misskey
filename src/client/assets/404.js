9717451e2bd0362ea351a0d631df956c17cae40c
syuilo <syuilotan@yahoo.co.jp>
2018-04-11 11:48:04 +0900

    Update recovery scripts

--- src/client/assets/404.js
+++ src/client/assets/404.js
@@ -1,5 +1,11 @@
 const yn = window.confirm(
-	'サーバー上に存在しないスクリプトがリクエストされました。お使いのMisskeyのバージョンが古いことが原因の可能性があります。Misskeyを更新しますか？');
+	'サーバー上に存在しないスクリプトがリクエストされました。お使いのMisskeyのバージョンが古いことが原因の可能性があります。Misskeyを更新しますか？\n\nA script that does not exist on the server was requested. It may be caused by an old version of Misskey you’re using. Do you want to delete the cache?');
+
+const langYn = window.confirm('また、言語を日本語に設定すると解決する場合があります。日本語に設定しますか？\n\nAlso, setting the language to Japanese may solve the problem. Would you like to set it to Japanese?');
+
+if (langYn) {
+	localStorage.setItem('lang', 'ja');
+}
 
 if (yn) {
 	// Clear cache (serive worker)
@@ -16,6 +22,4 @@ if (yn) {
 	localStorage.removeItem('v');
 
 	location.reload(true);
-} else {
-	alert('問題が解決しない場合はサーバー管理者までお問い合せください。');
 }
