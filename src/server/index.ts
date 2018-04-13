16920caf9cf5340f8e1ea3b40ff145967fc72c0a
syuilo <syuilotan@yahoo.co.jp>
2018-04-13 14:07:42 +0900

    Revert "Use http2"
    This reverts commit 645481c2e8d13123c74271fa86291f10b99b9a55.

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@
  */
 
 import * as fs from 'fs';
+import * as http from 'http';
 import * as http2 from 'http2';
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
@@ -48,7 +49,7 @@ function createServer() {
 		});
 		return http2.createSecureServer(certs, app.callback());
 	} else {
-		return http2.createServer(app.callback());
+		return http.createServer(app.callback());
 	}
 }
 
