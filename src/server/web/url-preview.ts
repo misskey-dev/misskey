fde2782a9eb5dcd7a706bee2401ce44c891c3653
syuilo <syuilotan@yahoo.co.jp>
2018-04-14 01:45:44 +0900

    Cache url preview

--- src/server/web/url-preview.ts
+++ src/server/web/url-preview.ts
@@ -5,6 +5,10 @@ module.exports = async (ctx: Koa.Context) => {
 	const summary = await summaly(ctx.query.url);
 	summary.icon = wrap(summary.icon);
 	summary.thumbnail = wrap(summary.thumbnail);
+
+	// Cache 7days
+	ctx.set('Cache-Control', 'max-age=604800, immutable');
+
 	ctx.body = summary;
 };
 
