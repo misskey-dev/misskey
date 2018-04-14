298e4fe566e77ee45754d1fa065b452ef23b65ad
syuilo <syuilotan@yahoo.co.jp>
2018-04-15 01:36:16 +0900

    wip

--- src/client/app/common/views/components/forkit.vue
+++ src/client/app/common/views/components/forkit.vue
@@ -1,5 +1,5 @@
 <template>
-<a class="a" href="https://github.com/syuilo/misskey" target="_blank" title="%i18n:@open-github-link%" aria-label="%i18n:@open-github-link%">
+<a class="a" href="https://github.com/syuilo/misskey" target="_blank" title="View source on Github">
 	<svg width="80" height="80" viewBox="0 0 250 250" aria-hidden="aria-hidden">
 		<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
 		<path class="octo-arm" d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor"></path>
