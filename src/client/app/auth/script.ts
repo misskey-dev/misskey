51ed3a6fadac232dfa306884b02a6f2bed2d4e09
syuilo <syuilotan@yahoo.co.jp>
2018-04-16 06:31:56 +0900

    Fix #1483

--- src/client/app/auth/script.ts
+++ src/client/app/auth/script.ts
@@ -2,6 +2,8 @@
  * Authorize Form
  */
 
+import VueRouter from 'vue-router';
+
 // Style
 import './style.styl';
 
@@ -12,14 +14,17 @@ import Index from './views/index.vue';
 /**
  * init
  */
-init(async (launch) => {
+init(launch => {
 	document.title = 'Misskey | アプリの連携';
 
-	// Launch the app
-	const [app] = launch();
+	// Init router
+	const router = new VueRouter({
+		mode: 'history',
+		routes: [
+			{ path: '/:token', component: Index },
+		]
+	});
 
-	// Routing
-	app.$router.addRoutes([
-		{ path: '/:token', component: Index },
-	]);
+	// Launch the app
+	launch(router);
 });
