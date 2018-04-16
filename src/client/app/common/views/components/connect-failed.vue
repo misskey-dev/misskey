77bac02fc032554f144f4bf3dd9ad0cd67c26672
syuilo <syuilotan@yahoo.co.jp>
2018-04-16 15:39:35 +0900

    :v:

--- src/client/app/common/views/components/connect-failed.vue
+++ src/client/app/common/views/components/connect-failed.vue
@@ -3,9 +3,9 @@
 	<img src="data:image/jpeg;base64,%base64:/assets/error.jpg%" alt=""/>
 	<h1>%i18n:@title%</h1>
 	<p class="text">
-		{{ '%i18n:!@description%'.substr(0, '%i18n:!@description%'.indexOf('{')) }}
+		<span>{{ '%i18n:!@description%'.substr(0, '%i18n:!@description%'.indexOf('{')) }}</span>
 		<a @click="reload">{{ '%i18n:!@description%'.match(/\{(.+?)\}/)[1] }}</a>
-		{{ '%i18n:!@description%'.substr('%i18n:!@description%'.indexOf('}') + 1) }}
+		<span>{{ '%i18n:!@description%'.substr('%i18n:!@description%'.indexOf('}') + 1) }}</span>
 	</p>
 	<button v-if="!troubleshooting" @click="troubleshooting = true">%i18n:@troubleshoot%</button>
 	<x-troubleshooter v-if="troubleshooting"/>
@@ -32,7 +32,7 @@ export default Vue.extend({
 	},
 	methods: {
 		reload() {
-			location.reload();
+			location.reload(true);
 		}
 	}
 });
