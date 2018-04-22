3a767f29bec4da6e22608191a189f01d3ccb2d5b
syuilo <syuilotan@yahoo.co.jp>
2018-04-22 17:34:25 +0900

    Fix #1532

--- src/client/app/desktop/views/components/media-image.vue
+++ src/client/app/desktop/views/components/media-image.vue
@@ -31,7 +31,7 @@ export default Vue.extend({
 			const xp = mouseX / this.$el.offsetWidth * 100;
 			const yp = mouseY / this.$el.offsetHeight * 100;
 			this.$el.style.backgroundPosition = xp + '% ' + yp + '%';
-			this.$el.style.backgroundImage = 'url("' + this.image.url + '?thumbnail")';
+			this.$el.style.backgroundImage = `url("${this.image.url}")`;
 		},
 
 		onMouseleave() {
