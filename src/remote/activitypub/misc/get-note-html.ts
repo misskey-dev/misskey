8c46e5b3d91d95ae119f8722cd0af85c8b0f12c3
syuilo <syuilotan@yahoo.co.jp>
2018-05-07 16:31:33 +0900

    投稿のレンダリングを改善

--- src/remote/activitypub/misc/get-note-html.ts
+++ src/remote/activitypub/misc/get-note-html.ts
@@ -11,7 +11,12 @@ export default function(note: INote) {
 	if (note.poll != null) {
 		const url = `${config.url}/notes/${note._id}`;
 		// TODO: i18n
-		html += `<p>【投票】<br />${url}</p>`;
+		html += `<p><a href="${url}">【Misskeyで投票を見る】</a></p>`;
+	}
+
+	if (note.renoteId != null) {
+		const url = `${config.url}/notes/${note.renoteId}`;
+		html += `<p>RE: <a href="${url}">${url}</a></p>`;
 	}
 
 	return html;
