7af9ad9869f58b8681eccce02cd39c793c5e6ba7
syuilo <syuilotan@yahoo.co.jp>
2018-05-13 16:26:11 +0900

    Better error handling

--- src/queue/processors/http/deliver.ts
+++ src/queue/processors/http/deliver.ts
@@ -14,7 +14,7 @@ export default async (job: kue.Job, done): Promise<void> => {
 			done();
 		} else {
 			console.warn(`deliver failed: ${res.statusMessage}`);
-			done(new Error(res.statusMessage));
+			done(res);
 		}
 	}
 };
