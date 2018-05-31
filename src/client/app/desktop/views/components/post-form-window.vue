111dbdcd7f269c8ff9e266fedd9d18b60faea88a
syuilo <syuilotan@yahoo.co.jp>
2018-05-31 16:38:05 +0900

    Fix

--- src/client/app/desktop/views/components/post-form-window.vue
+++ src/client/app/desktop/views/components/post-form-window.vue
@@ -1,21 +1,23 @@
 <template>
-<mk-window ref="window" is-modal @closed="$destroy">
-	<span slot="header">
-		<span :class="$style.icon" v-if="geo">%fa:map-marker-alt%</span>
+<mk-window class="mk-post-form-window" ref="window" is-modal @closed="$destroy">
+	<span slot="header" class="mk-post-form-window--header">
+		<span class="icon" v-if="geo">%fa:map-marker-alt%</span>
 		<span v-if="!reply">%i18n:@note%</span>
 		<span v-if="reply">%i18n:@reply%</span>
-		<span :class="$style.count" v-if="media.length != 0">{{ '%i18n:@attaches%'.replace('{}', media.length) }}</span>
-		<span :class="$style.count" v-if="uploadings.length != 0">{{ '%i18n:@uploading-media%'.replace('{}', uploadings.length) }}<mk-ellipsis/></span>
+		<span class="count" v-if="media.length != 0">{{ '%i18n:@attaches%'.replace('{}', media.length) }}</span>
+		<span class="count" v-if="uploadings.length != 0">{{ '%i18n:@uploading-media%'.replace('{}', uploadings.length) }}<mk-ellipsis/></span>
 	</span>
 
-	<mk-note-preview v-if="reply" :class="$style.notePreview" :note="reply"/>
-	<mk-post-form ref="form"
-		:reply="reply"
-		@posted="onPosted"
-		@change-uploadings="onChangeUploadings"
-		@change-attached-media="onChangeMedia"
-		@geo-attached="onGeoAttached"
-		@geo-dettached="onGeoDettached"/>
+	<div class="mk-post-form-window--body">
+		<mk-note-preview v-if="reply" class="notePreview" :note="reply"/>
+		<mk-post-form ref="form"
+			:reply="reply"
+			@posted="onPosted"
+			@change-uploadings="onChangeUploadings"
+			@change-attached-media="onChangeMedia"
+			@geo-attached="onGeoAttached"
+			@geo-dettached="onGeoDettached"/>
+	</div>
 </mk-window>
 </template>
 
@@ -56,21 +58,33 @@ export default Vue.extend({
 });
 </script>
 
-<style lang="stylus" module>
-.icon
-	margin-right 8px
+<style lang="stylus" scoped>
+root(isDark)
+	.mk-post-form-window--header
+		.icon
+			margin-right 8px
 
-.count
-	margin-left 8px
-	opacity 0.8
+		.count
+			margin-left 8px
+			opacity 0.8
 
-	&:before
-		content '('
+			&:before
+				content '('
 
-	&:after
-		content ')'
+			&:after
+				content ')'
 
-.notePreview
-	margin 16px 22px 0 22px
+	.mk-post-form-window--body
+		.notePreview
+			if isDark
+				margin 16px 22px 0 22px
+			else
+				margin 16px 22px
+
+.mk-post-form-window[data-darkmode]
+	root(true)
+
+.mk-post-form-window:not([data-darkmode])
+	root(false)
 
 </style>
