fcd437c89fd429a61e774a59b97c28f16676419d
syuilo <syuilotan@yahoo.co.jp>
2018-06-15 09:56:59 +0900

    Fix bug

--- src/server/api/endpoints/drive.ts
+++ src/server/api/endpoints/drive.ts
@@ -1,34 +1,32 @@
-/**
- * Module dependencies
- */
 import DriveFile from '../../../models/drive-file';
 
 /**
  * Get drive information
- *
- * @param {any} params
- * @param {any} user
- * @return {Promise<any>}
  */
 module.exports = (params, user) => new Promise(async (res, rej) => {
 	// Calculate drive usage
-	const usage = ((await DriveFile
-		.aggregate([
-			{ $match: { 'metadata.userId': user._id } },
-			{
-				$project: {
-					length: true
-				}
-			},
-			{
-				$group: {
-					_id: null,
-					usage: { $sum: '$length' }
-				}
+	const usage = await DriveFile
+		.aggregate([{
+			$match: {
+				'metadata.userId': user._id,
+				'metadata.deletedAt': { $exists: false }
 			}
-		]))[0] || {
-			usage: 0
-		}).usage;
+		}, {
+			$project: {
+				length: true
+			}
+		}, {
+			$group: {
+				_id: null,
+				usage: { $sum: '$length' }
+			}
+		}])
+		.then((aggregates: any[]) => {
+			if (aggregates.length > 0) {
+				return aggregates[0].usage;
+			}
+			return 0;
+		});
 
 	res({
 		capacity: user.driveCapacity,
