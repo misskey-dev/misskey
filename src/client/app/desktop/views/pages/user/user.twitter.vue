cdd9585cb8f39bbe7aca3179038e985b7eeaccbf
syuilo <syuilotan@yahoo.co.jp>
2018-06-24 15:55:41 +0900

    :art:

--- src/client/app/desktop/views/pages/user/user.twitter.vue
+++ src/client/app/desktop/views/pages/user/user.twitter.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="adsvaidqfznoartcbplullnejvxjphcn">
-	<p>%fa:B twitter%<a :href="`https://twitter.com/${user.twitter.screenName}`" target="_blank">@{{ user.twitter.screenName }}</a></p>
+	<span>%fa:B twitter%<a :href="`https://twitter.com/${user.twitter.screenName}`" target="_blank">@{{ user.twitter.screenName }}</a></span>
 </div>
 </template>
 
@@ -16,6 +16,11 @@ export default Vue.extend({
 .adsvaidqfznoartcbplullnejvxjphcn
 	padding 32px
 	background #1a94f2
+	border-radius 6px
 	color #fff
+
+	a
+		margin-left 8px
+		color #fff
 
 </style>
