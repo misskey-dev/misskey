118db9b267c451a164ceaabfdb7ca16d4834e9e2
syuilo <syuilotan@yahoo.co.jp>
2018-07-06 20:27:48 +0900

    wip

--- src/server/web/docs.ts
+++ src/server/web/docs.ts
@@ -2,16 +2,65 @@
  * Docs
  */
 
+import * as fs from 'fs';
+import * as path from 'path';
 import ms = require('ms');
 import * as Router from 'koa-router';
 import * as send from 'koa-send';
 import { Context } from 'cafy';
+import * as glob from 'glob';
+import * as yaml from 'js-yaml';
 import ObjectContext from 'cafy/built/types/object';
 import config from '../../config';
-import generateVars from '../../client/docs/vars';
 import I18n from '../../build/i18n';
+import { fa } from '../../build/fa';
+import { licenseHtml } from '../../build/license';
+const constants = require('../../const.json');
 
 const docs = `${__dirname}/../../client/docs/`;
+
+async function genVars(lang: string): Promise<{ [key: string]: any }> {
+	const vars = {} as { [key: string]: any };
+
+	vars['lang'] = lang;
+
+	const endpoints = glob.sync('./built/server/api/endpoints/**/*.js');
+	vars['endpoints'] = endpoints.map(ep => require('../../../' + ep)).filter(x => x.meta).map(x => x.meta.name);
+
+	const entities = glob.sync('./src/client/docs/api/entities/**/*.yaml');
+	vars['entities'] = entities.map(x => {
+		const _x = yaml.safeLoad(fs.readFileSync(x, 'utf-8')) as any;
+		return _x.name;
+	});
+
+	const docs = glob.sync('./src/client/docs/**/*.*.pug');
+	vars['docs'] = {};
+	docs.forEach(x => {
+		const [, name, lang] = x.match(/docs\/(.+?)\.(.+?)\.pug$/);
+		if (vars['docs'][name] == null) {
+			vars['docs'][name] = {
+				name,
+				title: {}
+			};
+		}
+		vars['docs'][name]['title'][lang] = fs.readFileSync(x, 'utf-8').match(/^h1 (.+?)\r?\n/)[1];
+	});
+
+	vars['kebab'] = (string: string) => string.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/\s+/g, '-').toLowerCase();
+
+	vars['config'] = config;
+
+	vars['copyright'] = constants.copyright;
+
+	vars['facss'] = fa.dom.css();
+
+	vars['license'] = licenseHtml;
+
+	const i18n = new I18n(lang);
+	vars['i18n'] = (key: string) => i18n.get(null, key);
+
+	return vars;
+}
 
 // WIP type
 const parseEPDefParam = (key: string, param: Context) => {
@@ -19,6 +68,46 @@ const parseEPDefParam = (key: string, param: Context) => {
 		name: key,
 		type: param.getType()
 	}, param.data);
+};
+
+const parseParam = (param: any) => {
+	const id = param.type.match(/^id\((.+?)\)|^id/);
+	const entity = param.type.match(/^entity\((.+?)\)/);
+	const isObject = /^object/.test(param.type);
+	const isDate = /^date/.test(param.type);
+	const isArray = /\[\]$/.test(param.type);
+	if (id) {
+		param.kind = 'id';
+		param.type = 'string';
+		param.entity = id[1];
+		if (isArray) {
+			param.type += '[]';
+		}
+	}
+	if (entity) {
+		param.kind = 'entity';
+		param.type = 'object';
+		param.entity = entity[1];
+		if (isArray) {
+			param.type += '[]';
+		}
+	}
+	if (isObject) {
+		param.kind = 'object';
+	}
+	if (isDate) {
+		param.kind = 'date';
+		param.type = 'string';
+		if (isArray) {
+			param.type += '[]';
+		}
+	}
+
+	if (param.optional) {
+		param.type += '?';
+	}
+
+	return param;
 };
 
 const sortParams = (params: Array<{name: string}>) => {
@@ -33,7 +122,7 @@ const sortParams = (params: Array<{name: string}>) => {
 };
 
 // WIP type
-const extractDefs = (params: Context[]) => {
+const extractEPDefs = (params: Context[]) => {
 	let defs: any[] = [];
 
 	params.forEach(param => {
@@ -44,7 +133,26 @@ const extractDefs = (params: Context[]) => {
 				params: sortParams(Object.keys(props).map(k => parseEPDefParam(k, props[k])))
 			});
 
-			const childDefs = extractDefs(Object.keys(props).map(k => props[k]));
+			const childDefs = extractEPDefs(Object.keys(props).map(k => props[k]));
+
+			defs = defs.concat(childDefs);
+		}
+	});
+
+	return sortParams(defs);
+};
+
+const extractDefs = (params: any[]) => {
+	let defs: any[] = [];
+
+	params.forEach(param => {
+		if (param.def) {
+			defs.push({
+				name: param.defName,
+				params: sortParams(param.def.map((p: any) => parseParam(p)))
+			});
+
+			const childDefs = extractDefs(param.def);
 
 			defs = defs.concat(childDefs);
 		}
@@ -68,6 +176,7 @@ router.get('/*/api/endpoints/*', async ctx => {
 	const ep = require('../../../built/server/api/endpoints/' + ctx.params[1]).meta;
 
 	const vars = {
+		title: ep.name,
 		endpoint: ep.name,
 		url: {
 			host: config.api_url,
@@ -76,26 +185,24 @@ router.get('/*/api/endpoints/*', async ctx => {
 		desc: ep.desc,
 		// @ts-ignore
 		params: sortParams(Object.keys(ep.params).map(k => parseEPDefParam(k, ep.params[k]))),
-		paramDefs: extractDefs(Object.keys(ep.params).map(k => ep.params[k])),
+		paramDefs: extractEPDefs(Object.keys(ep.params).map(k => ep.params[k])),
 	};
 
-	const commonVars = await generateVars();
-
-	const i18n = new I18n(lang);
-
-	await ctx.render('../../../../src/client/docs/api/endpoints/view', Object.assign({}, vars, {
-		lang,
-		title: ep.name,
-		kebab: (string: string) => string.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/\s+/g, '-').toLowerCase(),
-		i18n: (key: string) => i18n.get(null, key),
-		common: commonVars
-	}));
+	await ctx.render('../../../../src/client/docs/api/endpoints/view', Object.assign(await genVars(lang), vars));
 });
 
-router.get('*', async ctx => {
-	await send(ctx, `${ctx.params[0]}.html`, {
-		root: docs
-	});
+router.get('/*/api/entities/*', async ctx => {
+	const lang = ctx.params[0];
+	const entity = ctx.params[1];
+
+	const x = yaml.safeLoad(fs.readFileSync(path.resolve('./src/client/docs/api/entities/' + entity + '.yaml'), 'utf-8')) as any;
+
+	await ctx.render('../../../../src/client/docs/api/entities/view', Object.assign(await genVars(lang), {
+		name: x.name,
+		desc: x.desc,
+		props: sortParams(x.props.map((p: any) => parseParam(p))),
+		propDefs: extractDefs(x.props)
+	}));
 });
 
 export default router;
