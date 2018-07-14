e0ccec7d55cfa352bc081a2ba3c8eccd17e334b9
tamaina <tamaina@hotmail.co.jp>
2018-07-14 14:19:20 +0900

    wip

---
+++ src/client/app/common/views/components/user-lists.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="mk-user-lists">
+	<div class="create">
+		<button class="ui" @click="add">%fa:plus% %i18n:@create-list%</button>
+	</div>
+	<a v-for="list in lists" :key="list.id" @click="navigate(list)">{{ list.title }}</a>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	data() {
+		return {
+			fetching: true,
+			lists: []
+		};
+	},
+	mounted() {
+		(this as any).api('users/lists/list').then(lists => {
+			this.fetching = false;
+			this.lists = lists;
+		});
+	},
+	methods: {
+		add() {
+			(this as any).apis.input({
+				title: '%i18n:@enter-list-name%',
+			}).then(async title => {
+				const list = await (this as any).api('users/lists/create', {
+					title
+				});
+
+				this.$router.push('/i/lists/' + list.id);
+			});
+		}
+	}
+});
+</script>
+
+<style lang="stylus" scoped>
+@import '~const.styl'
+
+root(isDark)
+	> .create
+		position -webkit-sticky
+		position sticky
+		top 0
+		left 0
+		z-index 1
+		width 100%
+		padding 8px
+		background isDark ? #282c37 : #f7f7f7
+		box-shadow 0 0px 2px rgba(0,0,0,0.2)
+
+		> button
+			width 100%
+
+	> a
+		display block
+		padding 16px 24px
+		border-bottom solid 1px isDark ? #1c2023 : #eee
+		text-decoration none
+
+		&:hover
+			background-color isDark ? #1e2129 : #fafafa
+
+.mk-user-lists[data-darkmode]
+	root(true)
+
+.mk-user-lists:not([data-darkmode])
+	root(false)
+</style>
--- src/client/app/desktop/views/components/user-lists-window.vue
+++ src/client/app/desktop/views/components/user-lists-window.vue
@@ -1,69 +1,24 @@
 <template>
 <mk-window ref="window" is-modal width="450px" height="500px" @closed="$destroy">
 	<span slot="header">%fa:list% %i18n:@title%</span>
-
-	<div class="xkxvokkjlptzyewouewmceqcxhpgzprp" :data-darkmode="$store.state.device.darkmode">
-		<button class="ui" @click="add">%i18n:@create-list%</button>
-		<a v-for="list in lists" :key="list.id" @click="choice(list)">{{ list.title }}</a>
-	</div>
+	<mk-user-lists :class="$style.content" @navigate="navigate"/>
 </mk-window>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
-	data() {
-		return {
-			fetching: true,
-			lists: []
-		};
-	},
-	mounted() {
-		(this as any).api('users/lists/list').then(lists => {
-			this.fetching = false;
-			this.lists = lists;
-		});
-	},
 	methods: {
-		add() {
-			(this as any).apis.input({
-				title: 'リスト名',
-			}).then(async title => {
-				const list = await (this as any).api('users/lists/create', {
-					title
-				});
-
-				this.$emit('choosen', list);
-			});
-		},
-		choice(list) {
-			this.$emit('choosen', list);
-		},
-		close() {
-			(this as any).$refs.window.close();
+		navigate(list) {
+			this.$router.push(`/i/lists/${list.id}`);
+			(this as any).close(this);
 		}
 	}
 });
 </script>
 
-<style lang="stylus" scoped>
-
-root(isDark)
-	padding 16px
-
-	> button
-		margin-bottom 16px
-
-	> a
-		display block
-		padding 16px
-		border solid 1px isDark ? #1c2023 : #eee
-		border-radius 4px
-
-.xkxvokkjlptzyewouewmceqcxhpgzprp[data-darkmode]
-	root(true)
-
-.xkxvokkjlptzyewouewmceqcxhpgzprp:not([data-darkmode])
-	root(false)
-
+<style lang="stylus" module>
+.content
+	height 100%
+	overflow auto
 </style>
