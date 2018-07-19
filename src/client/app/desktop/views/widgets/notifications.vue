66c106722c658fc1faf5bfd31b898dd42b995b26
Aya Morisawa <AyaMorisawa4869@gmail.com>
2018-07-19 16:40:40 +0900

    Hide settings button
    It is unimplemented

--- src/client/app/desktop/views/widgets/notifications.vue
+++ src/client/app/desktop/views/widgets/notifications.vue
@@ -2,7 +2,7 @@
 <div class="mkw-notifications">
 	<mk-widget-container :show-header="!props.compact">
 		<template slot="header">%fa:R bell%%i18n:@title%</template>
-		<button slot="func" title="%i18n:@settings%" @click="settings">%fa:cog%</button>
+		<!-- <button slot="func" title="%i18n:@settings%" @click="settings">%fa:cog%</button> -->
 
 		<mk-notifications :class="$style.notifications"/>
 	</mk-widget-container>
