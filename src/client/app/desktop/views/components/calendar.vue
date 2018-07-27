df5a7c7e0c3ed65ce3beaf28f0e5befe9c37ccdf
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-07-27 18:28:06 +0900

    Update calendar.vue

--- src/client/app/desktop/views/components/calendar.vue
+++ src/client/app/desktop/views/components/calendar.vue
@@ -35,7 +35,7 @@ import Vue from 'vue';
 const eachMonthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function isLeapYear(year) {
-	return !(year % (year % 25 ? 4 : 16));
+	return !(year & (year % 25 ? 3 : 5));
 }
 
 export default Vue.extend({
