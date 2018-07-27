08e1c87fa602f470e7e19d7c162597ddc6980162
syuilo <syuilotan@yahoo.co.jp>
2018-07-27 18:50:15 +0900

    oops

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -2,7 +2,7 @@ import http from './processors/http';
 import { ILocalUser } from '../models/user';
 
 export function createHttpJob(data: any) {
-	return http(data, () => {});
+	return http({ data }, () => {});
 }
 
 export function deliver(user: ILocalUser, content: any, to: any) {
