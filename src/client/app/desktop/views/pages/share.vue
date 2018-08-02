385454506250721fdeadc30c94bc0ba3c349137d
gutfuckllc <40531868+gutfuckllc@users.noreply.github.com>
2018-08-01 23:27:17 -0400

    Bettered share.vue, improved formatting

--- src/client/app/desktop/views/pages/share.vue
+++ src/client/app/desktop/views/pages/share.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="pptjhabgjtt7kwskbfv4y3uml6fpuhmr">
-	<h1>%i18n:@share-with-left%%i18n:common.name%%i18n:@share-with-right%</h1>
+	<h1>{{'%i18n:@share-with%'.split("{}")[0] + '%i18n:common.name%' + '%i18n:@share-with%'.split("{}")[1]}}</h1>
 	<div>
 		<mk-signin v-if="!$store.getters.isSignedIn"/>
 		<mk-post-form v-else-if="!posted" :initial-text="text" :instant="true" @posted="posted = true"/>
 		<p v-if="posted" class="posted">%fa:check%</p>
 	</div>
-	<button v-if="posted" class="ui button" @click="close">%i18n:@close%</button>
+	<button v-if="posted" class="ui button" @click="close">%i18n:common.close%</button>
 </div>
 </template>
 
