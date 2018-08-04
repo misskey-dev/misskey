4ba6e1c2b2ce8baf28567a8faf9d1d0b45a89cc2
syuilo <syuilotan@yahoo.co.jp>
2018-08-04 11:39:59 +0900

    Fix bug

--- src/mfm/html.ts
+++ src/mfm/html.ts
@@ -1,5 +1,6 @@
 const { lib: emojilib } = require('emojilib');
-const JSDOM = require('jsdom');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 import config from '../config';
 import { INote } from '../models/note';
 import { TextElement } from './parse';
