a5a1f3c3711b526dadd06bf1f27092b0795223e4
mei23 <m@m544.net>
2018-08-10 20:06:52 +0900

    Fix Powered by <b>Misskey</b>.

--- src/client/app/desktop/views/pages/welcome.vue
+++ src/client/app/desktop/views/pages/welcome.vue
@@ -18,7 +18,7 @@
 				<div class="about">
 					<h1 v-if="name != 'Misskey'">{{ name }}</h1>
 					<h1 v-else><img :src="$store.state.device.darkmode ? 'assets/title.dark.svg' : 'assets/title.light.svg'" :alt="name"></h1>
-					<p class="powerd-by" v-if="name != 'Misskey'">%i18n:@powered-by-misskey%</p>
+					<p class="powerd-by" v-if="name != 'Misskey'" v-html="'%i18n:@powered-by-misskey%'"></p>
 					<p class="desc" v-html="description || '%i18n:common.about%'"></p>
 					<a ref="signup" @click="signup">📦 %i18n:@signup%</a>
 				</div>
