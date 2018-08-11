bfea4925d6181efcfe79244fbb0ac214590624ae
mei23 <m@m544.net>
2018-08-12 04:27:32 +0900

    Fix ActivityPub attachment url

--- src/remote/activitypub/renderer/image.ts
+++ src/remote/activitypub/renderer/image.ts
@@ -3,6 +3,6 @@ import { IDriveFile } from '../../../models/drive-file';
 
 export default (file: IDriveFile) => ({
 	type: 'Image',
-	url: `${config.drive_url}/${file._id}`,
+	url: file.metadata.url || `${config.drive_url}/${file._id}`,
 	sensitive: file.metadata.isSensitive
 });
