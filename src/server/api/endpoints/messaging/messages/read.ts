be68f42220c1dbf4ce0fd7f57edb4c21ef70f1b2
syuilo <syuilotan@yahoo.co.jp>
2018-08-11 21:01:39 +0900

    Implement messaging/messages/read

---
+++ src/server/api/endpoints/messaging/messages/read.ts
@@ -0,0 +1,43 @@
+import $ from 'cafy'; import ID from '../../../../../misc/cafy-id';
+import Message from '../../../../../models/messaging-message';
+import { ILocalUser } from '../../../../../models/user';
+import read from '../../../common/read-messaging-message';
+import getParams from '../../../get-params';
+
+export const meta = {
+	desc: {
+		ja: '指定した自分宛てのメッセージを既読にします。',
+		en: 'Mark as read a message of messaging.'
+	},
+
+	requireCredential: true,
+
+	kind: 'messaging-write',
+
+	params: {
+		messageId: $.type(ID).note({
+			desc: {
+				ja: '既読にするメッセージのID',
+				en: 'The ID of a message that you want to mark as read'
+			}
+		})
+	}
+};
+
+export default (params: any, user: ILocalUser) => new Promise(async (res, rej) => {
+	const [ps, psErr] = getParams(meta, params);
+	if (psErr) throw psErr;
+
+	const message = await Message.findOne({
+		_id: ps.messageId,
+		recipientId: user._id
+	});
+
+	if (message == null) {
+		return rej('message not found');
+	}
+
+	read(user._id, message.userId, message);
+
+	res();
+});
