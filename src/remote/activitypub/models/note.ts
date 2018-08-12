eef4611966acda8eba2e2bdfe0b186c7f27b48b4
mei23 <m@m544.net>
2018-08-12 18:56:29 +0900

    Fix ActivityPub followers/specified detection

--- src/remote/activitypub/models/note.ts
+++ src/remote/activitypub/models/note.ts
@@ -69,12 +69,13 @@ export async function createNote(value: any, resolver?: Resolver, silent = false
 	if (!note.to.includes('https://www.w3.org/ns/activitystreams#Public')) {
 		if (note.cc.includes('https://www.w3.org/ns/activitystreams#Public')) {
 			visibility = 'home';
+		} else if (note.to.includes(`${actor.uri}/followers`)) {	// TODO: person.followerと照合するべき？
+			visibility = 'followers';
 		} else {
 			visibility = 'specified';
 			visibleUsers = await Promise.all(note.to.map(uri => resolvePerson(uri)));
 		}
 	}
-	if (note.cc.length == 0) visibility = 'followers';
 	//#endergion
 
 	// 添付メディア
