ec07112f94d3e00a0b5c0fdfcb6e56f46b7e1adb
syuilo <syuilotan@yahoo.co.jp>
2018-08-14 16:53:57 +0900

    Fix bug

--- src/client/app/desktop/views/pages/admin/admin.dashboard.vue
+++ src/client/app/desktop/views/pages/admin/admin.dashboard.vue
@@ -1,10 +1,12 @@
 <template>
 <div>
 	<h1>%i18n:@dashboard%</h1>
-	<p><b>%i18n:@all-users%</b>: <span>{{ stats.usersCount | number }}</span></p>
-	<p><b>%i18n:@original-users%</b>: <span>{{ stats.originalUsersCount | number }}</span></p>
-	<p><b>%i18n:@all-notes%</b>: <span>{{ stats.notesCount | number }}</span></p>
-	<p><b>%i18n:@original-notes%</b>: <span>{{ stats.originalNotesCount | number }}</span></p>
+	<div v-if="stats">
+		<p><b>%i18n:@all-users%</b>: <span>{{ stats.usersCount | number }}</span></p>
+		<p><b>%i18n:@original-users%</b>: <span>{{ stats.originalUsersCount | number }}</span></p>
+		<p><b>%i18n:@all-notes%</b>: <span>{{ stats.notesCount | number }}</span></p>
+		<p><b>%i18n:@original-notes%</b>: <span>{{ stats.originalNotesCount | number }}</span></p>
+	</div>
 </div>
 </template>
 
