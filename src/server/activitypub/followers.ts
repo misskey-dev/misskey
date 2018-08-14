0986301788a40663ced0aa9d6d8a97b099b622b1
mei23 <m@m544.net>
2018-08-14 20:13:32 +0900

    Implement ActivityPub Followers/Following/Outbox

---
+++ src/server/activitypub/followers.ts
@@ -0,0 +1,80 @@
+import * as mongo from 'mongodb';
+import * as Koa from 'koa';
+import config from '../../config';
+import $ from 'cafy'; import ID from '../../misc/cafy-id';
+import User from '../../models/user';
+import Following from '../../models/following';
+import pack from '../../remote/activitypub/renderer';
+import renderOrderedCollection from '../../remote/activitypub/renderer/ordered-collection';
+import renderOrderedCollectionPage from '../../remote/activitypub/renderer/ordered-collection-page';
+import renderFollowUser from '../../remote/activitypub/renderer/follow-user';
+
+export default async (ctx: Koa.Context) => {
+	const userId = new mongo.ObjectID(ctx.params.user);
+
+	// Get 'cursor' parameter
+	const [cursor = null, cursorErr] = $.type(ID).optional.get(ctx.request.query.cursor);
+
+	// Get 'page' parameter
+	const pageErr = !$.str.optional.or(['true', 'false']).ok(ctx.request.query.page);
+	const page: boolean = ctx.request.query.page === 'true';
+
+	// Validate parameters
+	if (cursorErr || pageErr) {
+		ctx.status = 400;
+		return;
+	}
+
+	// Verify user
+	const user = await User.findOne({
+		_id: userId,
+		host: null
+	});
+
+	if (user === null) {
+		ctx.status = 404;
+		return;
+	}
+
+	const limit = 10;
+	const partOf = `${config.url}/users/${userId}/followers`;
+
+	if (page) {
+		// Construct query
+		const query = {
+			followeeId: user._id
+		} as any;
+
+		// カーソルが指定されている場合
+		if (cursor) {
+			query._id = {
+				$lt: cursor
+			};
+		}
+
+		// Get followers
+		const followings = await Following
+			.find(query, {
+				limit: limit + 1,
+				sort: { _id: -1 }
+			});
+
+		// 「次のページ」があるかどうか
+		const inStock = followings.length === limit + 1;
+		if (inStock) followings.pop();
+
+		const renderedFollowers = await Promise.all(followings.map(following => renderFollowUser(following.followerId)));
+		const rendered = renderOrderedCollectionPage(
+			`${partOf}?page=true${cursor ? `&cursor=${cursor}` : ''}`,
+			user.followersCount, renderedFollowers, partOf,
+			null,
+			inStock ? `${partOf}?page=true&cursor=${followings[followings.length - 1]._id}` : null
+		);
+
+		ctx.body = pack(rendered);
+	} else {
+		// index page
+		const rendered = renderOrderedCollection(partOf, user.followersCount, `${partOf}?page=true`, null);
+		ctx.body = pack(rendered);
+	}
+};
