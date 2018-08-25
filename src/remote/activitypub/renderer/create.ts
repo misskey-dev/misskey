ffcb2f755c2d20e62272212905c7d2ca795e4c3d
mei23 <m@m544.net>
2018-08-25 14:12:44 +0900

    Send actor in CreateNote, Announce

--- src/remote/activitypub/renderer/create.ts
+++ src/remote/activitypub/renderer/create.ts
@@ -1,4 +1,12 @@
-export default (object: any) => ({
-	type: 'Create',
-	object
-});
+import config from '../../../config';
+import { INote } from '../../../models/note';
+
+export default (object: any, note: INote) => {
+	return {
+		id: `${config.url}/notes/${note._id}/activity`,
+		actor: `${config.url}/users/${note.userId}`,
+		type: 'Create',
+		published: note.createdAt.toISOString(),
+		object
+	};
+};
