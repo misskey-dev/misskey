68a7661f08b851999bb0c7deff1d7553af904fb4
mei23 <m@m544.net>
2018-08-25 14:23:51 +0900

    Create Note activity にも toとcc

--- src/remote/activitypub/renderer/create.ts
+++ src/remote/activitypub/renderer/create.ts
@@ -2,11 +2,16 @@ import config from '../../../config';
 import { INote } from '../../../models/note';
 
 export default (object: any, note: INote) => {
-	return {
+	const activity = {
 		id: `${config.url}/notes/${note._id}/activity`,
 		actor: `${config.url}/users/${note.userId}`,
 		type: 'Create',
 		published: note.createdAt.toISOString(),
 		object
-	};
+	} as any;
+
+	if (object.to) activity.to = object.to;
+	if (object.cc) activity.cc = object.cc;
+
+	return activity;
 };
