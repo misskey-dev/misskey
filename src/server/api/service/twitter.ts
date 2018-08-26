bfed1475bbdd1941bb9b12e5ad772ab0f5840bb4
Aya Morisawa <AyaMorisawa4869@gmail.com>
2018-08-26 13:55:39 +0900

    Use endsWith for readability

--- src/server/api/service/twitter.ts
+++ src/server/api/service/twitter.ts
@@ -14,7 +14,7 @@ function getUserToken(ctx: Koa.Context) {
 
 function compareOrigin(ctx: Koa.Context) {
 	function normalizeUrl(url: string) {
-		return url[url.length - 1] === '/' ? url.substr(0, url.length - 1) : url;
+		return url.endsWith('/') ? url.substr(0, url.length - 1) : url;
 	}
 
 	const referer = ctx.headers['referer'];
