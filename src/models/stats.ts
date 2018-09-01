52aa64fcb6bef627da82c7d1515a5798a0d53684
Aya Morisawa <AyaMorisawa4869@gmail.com>
2018-09-01 19:12:07 +0900

    Fix drop index

--- src/models/stats.ts
+++ src/models/stats.ts
@@ -2,7 +2,12 @@ import * as mongo from 'mongodb';
 import db from '../db/mongodb';
 
 const Stats = db.get<IStats>('stats');
-Stats.dropIndex({ date: -1 } as any); // 後方互換性のため
+
+// 後方互換性のため
+Stats.dropIndex({ date: -1 } as any).catch((e: mongo.MongoError) => {
+	if (e.code !== 27) throw e;
+});
+
 Stats.createIndex({ span: -1, date: -1 }, { unique: true });
 export default Stats;
 
