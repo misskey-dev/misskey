2c135fa2f6980ab9550c8c50d0615b28fff558ca
Aya Morisawa <AyaMorisawa4869@gmail.com>
2018-09-01 16:12:50 +0900

    Not fallback to native locale

--- locales/index.js
+++ locales/index.js
@@ -6,12 +6,8 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 
 const langs = ['de-DE', 'en-US', 'fr-FR', 'ja-JP', 'ja-KS', 'pl-PL', 'es-ES'];
-const nativeLang = 'ja-JP';
 
 const loadLocale = lang => yaml.safeLoad(fs.readFileSync(`${__dirname}/${lang}.yml`, 'utf-8'));
-const nativeLocale = loadLocale(nativeLang);
-const fallbackToNativeLocale = locale => Object.assign({}, nativeLocale, locale);
-const makeLocale = lang => lang == nativeLang ? nativeLocale : fallbackToNativeLocale(loadLocale(lang));
-const locales = langs.map(lang => ({ [lang]: makeLocale(lang) }));
+const locales = langs.map(lang => ({ [lang]: loadLocale(lang) }));
 
 module.exports = locales.reduce((a, b) => ({ ...a, ...b }));
