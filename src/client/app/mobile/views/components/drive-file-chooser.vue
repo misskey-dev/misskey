ef630195fa6a86ae54eaf866b38c5379dd79242e
syuilo <syuilotan@yahoo.co.jp>
2018-09-02 18:23:47 +0900

    モバイルのドライブダイアログがおかしいのを修正 & ダークモード対応

--- src/client/app/mobile/views/components/drive-file-chooser.vue
+++ src/client/app/mobile/views/components/drive-file-chooser.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="mk-drive-file-chooser">
+<div class="cdxzvcfawjxdyxsekbxbfgtplebnoneb">
 	<div class="body">
 		<header>
 			<h1>%i18n:@select-file%<span class="count" v-if="files.length > 0">({{ files.length }})</span></h1>
 			<button class="close" @click="cancel">%fa:times%</button>
 			<button v-if="multiple" class="ok" @click="ok">%fa:check%</button>
 		</header>
-		<mk-drive ref="browser"
+		<mk-drive class="drive" ref="browser"
 			:select-file="true"
 			:multiple="multiple"
 			@change-selection="onChangeSelection"
@@ -46,7 +46,7 @@ export default Vue.extend({
 </script>
 
 <style lang="stylus" scoped>
-.mk-drive-file-chooser
+root(isDark)
 	position fixed
 	z-index 20000
 	top 0
@@ -59,10 +59,11 @@ export default Vue.extend({
 	> .body
 		width 100%
 		height 100%
-		background #fff
+		background isDark ? #282c37 : #fff
 
 		> header
-			border-bottom solid 1px #eee
+			border-bottom solid 1px isDark ? #1b1f29 : #eee
+			color isDark ? #fff : #111
 
 			> h1
 				margin 0
@@ -90,9 +91,15 @@ export default Vue.extend({
 				line-height 42px
 				width 42px
 
-		> .mk-drive
+		> .drive
 			height calc(100% - 42px)
 			overflow scroll
 			-webkit-overflow-scrolling touch
+
+.cdxzvcfawjxdyxsekbxbfgtplebnoneb[data-darkmode]
+	root(true)
+
+.cdxzvcfawjxdyxsekbxbfgtplebnoneb:not([data-darkmode])
+	root(false)
 
 </style>
