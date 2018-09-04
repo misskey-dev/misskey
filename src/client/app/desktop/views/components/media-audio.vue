2555e23b107bb2c38e068e5f2f9da568a2adb375
tamaina <tamaina@hotmail.co.jp>
2018-09-04 20:01:15 +0900

    wip

---
+++ src/client/app/desktop/views/components/media-audio.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="abunaiaudionankasirankedoichioux" v-if="audio.isSensitive && hide" @click="hide = false">
+	<div>
+		<b>%fa:exclamation-triangle% %i18n:@sensitive%</b>
+		<span>%i18n:@click-to-show%</span>
+	</div>
+</div>
+<div class="komeijiokayusabanomisoniohitashi" v-else>
+	<audio class="audio"
+		:src="audio.url"
+		:title="audio.name"
+		controls
+		ref="audio"
+		v-if="inlinePlayable" />
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	props: {
+		audio: {
+			type: Object,
+			required: true
+		},
+		inlinePlayable: {
+			default: false
+		},
+		hide: {
+			type: Boolean,
+			default: true
+		}
+	},
+	computed: {
+		imageStyle(): any {
+			return {
+				'background-image': `url(${this.audio.url})`
+			};
+		}
+	}
+})
+</script>
+
+<style lang="stylus" scoped>
+.komeijiokayusabanomisoniohitashi
+	.audio
+		display block
+		width 100%
+		height 100%
+		border-radius 4px
+
+.abunaiaudionankasirankedoichioux
+	display flex
+	justify-content center
+	align-items center
+	background #111
+	color #fff
+
+	> div
+		display table-cell
+		text-align center
+		font-size 12px
+
+		> b
+			display block
+
+</style>
