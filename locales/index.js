7c5bc03492a475eac1008d67869cba8dcbc78a07
syuilo <syuilotan@yahoo.co.jp>
2018-09-05 12:53:08 +0900

    Update langs

--- locales/index.js
+++ locales/index.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-const langs = ['de-DE', 'en-US', 'fr-FR', 'ja-JP', 'ja-KS', 'pl-PL', 'es-ES'];
+const langs = ['de-DE', 'en-US', 'fr-FR', 'ja-JP', 'ja-KS', 'pl-PL', 'es-ES', 'nl-NL'];
 
 const loadLocale = lang => yaml.safeLoad(fs.readFileSync(`${__dirname}/${lang}.yml`, 'utf-8'));
 const locales = langs.map(lang => ({ [lang]: loadLocale(lang) }));
