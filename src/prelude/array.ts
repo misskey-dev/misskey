4954843f8227e8e5d6da236958924214b4b6f969
Aya Morisawa <AyaMorisawa4869@gmail.com>
2018-09-06 02:26:21 +0900

    Resolve #2629

--- src/prelude/array.ts
+++ src/prelude/array.ts
@@ -5,3 +5,7 @@ export function countIf<T>(f: (x: T) => boolean, xs: T[]): number {
 export function count<T>(x: T, xs: T[]): number {
 	return countIf(y => x === y, xs);
 }
+
+export function intersperse<T>(sep: T, xs: T[]): T[] {
+	return [].concat(...xs.map(x => [sep, x])).slice(1);
+}
