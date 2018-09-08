5a13c38a6d27535da21894ca8642d722873c017f
MeiMei <30769358+mei23@users.noreply.github.com>
2018-09-08 21:44:28 +0900

    インスタンス名がブラウザタイトルに反映されないのを修正 (#2668)
    * titleが反映されないのを修正
    
    * deckでtitleが反映されるように修正

--- src/client/app/config.ts
+++ src/client/app/config.ts
@@ -5,6 +5,7 @@ declare const _COPYRIGHT_: string;
 declare const _VERSION_: string;
 declare const _CODENAME_: string;
 declare const _ENV_: string;
+declare const _NAME_: string;
 
 const address = new URL(location.href);
 
@@ -20,3 +21,4 @@ export const copyright = _COPYRIGHT_;
 export const version = _VERSION_;
 export const codename = _CODENAME_;
 export const env = _ENV_;
+export const name = _NAME_;
