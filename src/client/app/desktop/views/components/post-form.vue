f5e143052f6267abf1616fb08fa640c16e21d271
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-09-10 08:42:59 +0900

    Update post-form.vue
    refs: https://github.com/syuilo/misskey/pull/2661#issuecomment-419579444

--- src/client/app/desktop/views/components/post-form.vue
+++ src/client/app/desktop/views/components/post-form.vue
@@ -45,7 +45,7 @@
 		<span v-if="visibility === 'specified'">%fa:envelope%</span>
 		<span v-if="visibility === 'private'">%fa:lock%</span>
 	</button>
-	<p class="text-count" :class="{ over: [...text.trim()].length > 1000 }">{{ 1000 - [...text.trim()].length }}</p>
+	<p class="text-count" :class="{ over: length(text.trim()) > 1000 }">{{ 1000 - length(text.trim()) }}</p>
 	<button :class="{ posting }" class="submit" :disabled="!canPost" @click="post">
 		{{ posting ? '%i18n:@posting%' : submitText }}<mk-ellipsis v-if="posting"/>
 	</button>
@@ -63,6 +63,7 @@ import MkVisibilityChooser from '../../../common/views/components/visibility-cho
 import parse from '../../../../../mfm/parse';
 import { host } from '../../../config';
 import { erase } from '../../../../../prelude/array';
+impprt { length } from 'stringz';
 
 export default Vue.extend({
 	components: {
@@ -146,7 +147,7 @@ export default Vue.extend({
 		canPost(): boolean {
 			return !this.posting &&
 				(1 <= this.text.length || 1 <= this.files.length || this.poll || this.renote) &&
-				([...this.text.trim()].length <= 1000);
+				(length(this.text.trim()) <= 1000);
 		}
 	},
 
