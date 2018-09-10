98d31ff064c999c82344a5ba37aaf7cd2a411803
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-09-10 17:51:22 +0900

    Update post-form.vue
    refs: https://github.com/syuilo/misskey/pull/2661#discussion_r216242002

--- src/client/app/desktop/views/components/post-form.vue
+++ src/client/app/desktop/views/components/post-form.vue
@@ -202,7 +202,7 @@ export default Vue.extend({
 	methods: {
 	  trimmedLength(text: string) {
 			return length(text.trim());
-		}
+		},
 
 		addTag(tag: string) {
 			insertTextAtCursor(this.$refs.text, ` #${tag} `);
