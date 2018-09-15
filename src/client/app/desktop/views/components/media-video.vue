952a49f7496c5c6a6dae8998e8d42ea53281db2e
tamaina <tamaina@hotmail.co.jp>
2018-09-15 22:22:41 +0900

    revert デスクトップの動画埋め込みを廃止

--- src/client/app/desktop/views/components/media-video.vue
+++ src/client/app/desktop/views/components/media-video.vue
@@ -6,11 +6,19 @@
 	</div>
 </div>
 <div class="vwxdhznewyashiknzolsoihtlpicqepe" v-else>
+	<video class="video"
+		:src="video.url"
+		:title="video.name"
+		controls
+		@dblclick.prevent="onClick"
+		ref="video"
+		v-if="inlinePlayable" />
 	<a class="thumbnail"
 		:href="video.url"
 		:style="imageStyle"
 		@click.prevent="onClick"
-		:title="video.name">
+		:title="video.name"
+		v-else>
 		%fa:R play-circle%
 	</a>
 </div>
@@ -19,12 +27,14 @@
 <script lang="ts">
 import Vue from 'vue';
 import MkMediaVideoDialog from './media-video-dialog.vue';
-
 export default Vue.extend({
 	props: {
 		video: {
 			type: Object,
 			required: true
+		},
+		inlinePlayable: {
+			default: false
 		},
 		hide: {
 			type: Boolean,
@@ -57,32 +67,32 @@ export default Vue.extend({
 
 <style lang="stylus" scoped>
 .vwxdhznewyashiknzolsoihtlpicqepe
+	.video
+		display block
+		width 100%
+		height 100%
+		border-radius 4px
 	.thumbnail
 		display flex
 		justify-content center
 		align-items center
 		font-size 3.5em
-
 		cursor zoom-in
 		overflow hidden
 		background-position center
 		background-size cover
 		width 100%
 		height 100%
-
 .uofhebxjdgksfmltszlxurtjnjjsvioh
 	display flex
 	justify-content center
 	align-items center
 	background #111
 	color #fff
-
 	> div
 		display table-cell
 		text-align center
 		font-size 12px
-
-		> *
+		> b
 			display block
-
 </style>
