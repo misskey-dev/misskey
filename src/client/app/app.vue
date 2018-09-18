a9a2f4820bc02620917aa6b1703bc5cfff4c36c9
syuilo <syuilotan@yahoo.co.jp>
2018-09-19 08:36:06 +0900

    Add keyboard shortcut doc

--- src/client/app/app.vue
+++ src/client/app/app.vue
@@ -1,3 +1,24 @@
 <template>
-<router-view id="app"></router-view>
+<router-view id="app" v-hotkey.global="keymap"></router-view>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { url, lang } from './config';
+
+export default Vue.extend({
+	computed: {
+		keymap(): any {
+			return {
+				'h|slash': this.help
+			};
+		}
+	},
+
+	methods: {
+		help() {
+			window.open(`${url}/docs/${lang}/keyboard-shortcut`, '_blank');
+		}
+	}
+});
+</script>
