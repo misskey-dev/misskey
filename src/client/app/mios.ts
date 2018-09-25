9ca6a6bf064634bab6611f4e70d20a62fd3b48ed
syuilo <syuilotan@yahoo.co.jp>
2018-09-25 21:23:26 +0900

    サーバーへの接続が不安定な時にサインアウトしないように

--- src/client/app/mios.ts
+++ src/client/app/mios.ts
@@ -265,7 +265,7 @@ export default class MiOS extends EventEmitter {
 			// When success
 			.then(res => {
 				// When failed to authenticate user
-				if (res.status !== 200) {
+				if (res.status !== 200 && res.status < 500) {
 					return this.signout();
 				}
 
