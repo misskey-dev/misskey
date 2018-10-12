eb95f584bf3aa4cb2f4538e943fea0959222cb70
mei23 <m@m544.net>
2018-10-12 18:46:32 +0900

    Use punycode

--- src/server/api/service/v1.ts
+++ src/server/api/service/v1.ts
@@ -1,13 +1,15 @@
 import * as Router from 'koa-router';
 import User from '../../../models/user';
+import { toASCII } from 'punycode';
 
 // Init router
 const router = new Router();
 
 router.get('/v1/instance/peers', async ctx => {
-	const peers = await User.distinct('host', { host: { $ne: null } });
+	const peers = await User.distinct('host', { host: { $ne: null } }) as any as string[];
+	const punyCodes = peers.map(peer => toASCII(peer));
 	ctx.set('Cache-Control', 'public, max-age=3600');
-	ctx.body = peers;
+	ctx.body = punyCodes;
 });
 
 module.exports = router;
