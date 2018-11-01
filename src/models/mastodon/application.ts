d1b25a20c28a89c944b82869d7593dd34b307aa6
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-11-01 12:07:29 +0900

    Create application.ts

---
+++ src/models/mastodon/application.ts
@@ -0,0 +1,11 @@
+// !WIP: Don't use these functions
+// TODO: Implement Mastodon media attachment
+
+export type IMastodonApplication = {
+	name: string,
+	website?: string
+};
+
+export async function toMastodonApplication(): Promise<IMastodonApplication> {
+	return null;
+}
