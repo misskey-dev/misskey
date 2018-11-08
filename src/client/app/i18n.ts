067d48e33f03ba3368a97765731f3ae17734b02c
syuilo <syuilotan@yahoo.co.jp>
2018-11-08 21:01:18 +0900

    Update src/client/app/i18n.ts

--- src/client/app/i18n.ts
+++ src/client/app/i18n.ts
@@ -1,11 +1,10 @@
-import VueI18n from 'vue-i18n';
 import { lang, locale } from './config';
 
 export default function(scope: string) {
-	return new VueI18n({
+	return {
 		locale: lang,
 		messages: {
 			[lang]: locale[scope]
 		}
-	});
+	};
 }
