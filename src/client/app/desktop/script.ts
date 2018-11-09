34723686c887e0b0a2d6f4d93eaa183905110402
syuilo <syuilotan@yahoo.co.jp>
2018-11-09 15:09:39 +0900

    wip

--- src/client/app/desktop/script.ts
+++ src/client/app/desktop/script.ts
@@ -32,7 +32,10 @@ import MkFollow from '../common/views/pages/follow.vue';
 import Ctx from './views/components/context-menu.vue';
 import PostFormWindow from './views/components/post-form-window.vue';
 import RenoteFormWindow from './views/components/renote-form-window.vue';
+import MkChooseFileFromDriveWindow from './views/components/choose-file-from-drive-window.vue';
+import MkChooseFolderFromDriveWindow from './views/components/choose-folder-from-drive-window.vue';
 
+import { url } from '../config';
 import MiOS from '../mios';
 
 /**
@@ -68,7 +71,47 @@ init(async (launch) => {
 					});
 					if (o.cb) vm.$once('closed', o.cb);
 				}
-			}
+			},
+
+			$chooseDriveFiles(opts) {
+				return new Promise((res, rej) => {
+					const o = opts || {};
+
+					if (document.body.clientWidth > 800) {
+						const w = this.$root.new(MkChooseFileFromDriveWindow, {
+							title: o.title,
+							multiple: o.multiple,
+							initFolder: o.currentFolder
+						});
+						w.$once('selected', file => {
+							res(file);
+						});
+						document.body.appendChild(w.$el);
+					} else {
+						window['cb'] = file => {
+							res(file);
+						};
+
+						window.open(url + `/selectdrive?multiple=${o.multiple}`,
+							'choose_drive_window',
+							'height=500, width=800');
+					}
+				});
+			},
+
+			$chooseDriveFolder(opts) {
+				return new Promise((res, rej) => {
+					const o = opts || {};
+					const w = this.$root.new(MkChooseFolderFromDriveWindow, {
+						title: o.title,
+						initFolder: o.currentFolder
+					});
+					w.$once('selected', folder => {
+						res(folder);
+					});
+					document.body.appendChild(w.$el);
+				});
+			},
 		}
 	});
 
