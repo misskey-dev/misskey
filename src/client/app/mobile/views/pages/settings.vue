cf0b7e26b520ea7909fc78d7f7a7d86796e25e1f
syuilo <syuilotan@yahoo.co.jp>
2018-11-09 21:30:12 +0900

    Remove duplicated option

--- src/client/app/mobile/views/pages/settings.vue
+++ src/client/app/mobile/views/pages/settings.vue
@@ -25,7 +25,6 @@
 					<ui-switch v-model="showFullAcct">{{ $t('@.show-full-acct') }}</ui-switch>
 					<ui-switch v-model="showVia">{{ $t('@.show-via') }}</ui-switch>
 					<ui-switch v-model="useOsDefaultEmojis">{{ $t('@.use-os-default-emojis') }}</ui-switch>
-					<ui-switch v-model="useOsDefaultEmojis">{{ $t('@.use-os-default-emojis') }}</ui-switch>
 					<ui-switch v-model="iLikeSushi">{{ $t('@.i-like-sushi') }}</ui-switch>
 					<ui-switch v-model="disableAnimatedMfm">{{ $t('@.disable-animated-mfm') }}</ui-switch>
 					<ui-switch v-model="alwaysShowNsfw">{{ $t('@.always-show-nsfw') }} ({{ $t('@.this-setting-is-this-device-only') }})</ui-switch>
