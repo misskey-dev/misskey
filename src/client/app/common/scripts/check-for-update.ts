3826a820bbd593debec12d2eda05ef20bd9f714b
syuilo <syuilotan@yahoo.co.jp>
2018-11-11 19:13:10 +0900

    [Client] Fix i18n
    Closes #3192

--- src/client/app/common/scripts/check-for-update.ts
+++ src/client/app/common/scripts/check-for-update.ts
@@ -23,8 +23,8 @@ export default async function($root: any, force = false, silent = false) {
 
 		if (!silent) {
 			$root.$dialog({
-				title: '%i18n:common.update-available-title%',
-				text: '%i18n:common.update-available%'.replace('{newer}', newer).replace('{current}', current)
+				title: $root.$t('@.update-available-title'),
+				text: $root.$t('@.update-available', { newer, current })
 			});
 		}
 
