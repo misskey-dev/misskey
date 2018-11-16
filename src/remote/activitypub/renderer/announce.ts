0bf54b3ff6899f4eb5e2bb42c6466094c8f0b8c4
MeiMei <30769358+mei23@users.noreply.github.com>
2018-11-17 03:25:48 +0900

    Renote visibility (#3290)

--- src/remote/activitypub/renderer/announce.ts
+++ src/remote/activitypub/renderer/announce.ts
@@ -4,13 +4,26 @@ import { INote } from '../../../models/note';
 export default (object: any, note: INote) => {
 	const attributedTo = `${config.url}/users/${note.userId}`;
 
+	let to: string[] = [];
+	let cc: string[] = [];
+
+	if (note.visibility == 'public') {
+		to = ['https://www.w3.org/ns/activitystreams#Public'];
+		cc = [`${attributedTo}/followers`];
+	} else if (note.visibility == 'home') {
+		to = [`${attributedTo}/followers`];
+		cc = ['https://www.w3.org/ns/activitystreams#Public'];
+	} else {
+		return null;
+	}
+
 	return {
 		id: `${config.url}/notes/${note._id}/activity`,
 		actor: `${config.url}/users/${note.userId}`,
 		type: 'Announce',
 		published: note.createdAt.toISOString(),
-		to: ['https://www.w3.org/ns/activitystreams#Public'],
-		cc: [attributedTo, `${attributedTo}/followers`],
+		to,
+		cc,
 		object
 	};
 };
