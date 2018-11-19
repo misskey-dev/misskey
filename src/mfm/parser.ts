1a8e1c10f4be168cd2a0c1af24c0957b24d9eddc
syuilo <syuilotan@yahoo.co.jp>
2018-11-19 23:25:01 +0900

    wip

--- src/mfm/parser.ts
+++ src/mfm/parser.ts
@@ -8,7 +8,7 @@ type Node = {
 	props?: any;
 };
 
-function makeNode(name: string, children: Node[], props?: any): Node {
+function _makeNode(name: string, children?: Node[], props?: any): Node {
 	return {
 		name,
 		children,
@@ -16,26 +16,58 @@ function makeNode(name: string, children: Node[], props?: any): Node {
 	};
 }
 
-function text(none?: string) {
-	return (none ? P.noneOf(none) : P.any).map(x => makeNode('text', null, { text: x }));
+function makeNode(name: string, props?: any): Node {
+	return _makeNode(name, null, props);
+}
+
+function makeNodeWithChildren(name: string, children: Node[], props?: any): Node {
+	return _makeNode(name, children, props);
 }
 
 const mfm = P.createLanguage({
 	root: r => P.alt(
+		r.big,
 		r.bold,
+		r.url,
 		r.mention,
 		r.emoji,
-		text()
+		r.text
 	).many(),
+
+	text: () => P.any.map(x => makeNode('text', { text: x })),
+
+	//#region Big
+	big: r =>
+		P.regexp(/^\*\*\*([\s\S]+?)\*\*\*/, 1)
+		.map(x => makeNodeWithChildren('big', P.alt(
+			r.mention,
+			r.emoji,
+			r.text
+		).atLeast(1).tryParse(x))),
+	//#endregion
+
+	//#region Block code
+	blockCode: r =>
+		P.regexp(/^```([\s\S]+?)```/, 1)
+		.map(x => makeNode('blockCode', x)),
+	//#endregion
 
 	//#region Bold
 	bold: r =>
 		P.regexp(/^\*\*([\s\S]+?)\*\*/, 1)
-		.map(x => makeNode('bold', P.alt(
+		.map(x => makeNodeWithChildren('bold', P.alt(
 			r.mention,
 			r.emoji,
-			text()
+			r.text
 		).atLeast(1).tryParse(x))),
+	//#endregion
+
+	//#region Emoji
+	emoji: r =>
+		P.regexp(/^:([a-z0-9_+-]+):/i, 1)
+		.map(x => makeNode('emoji', {
+			name: x
+		})),
 	//#endregion
 
 	//#region Mention
@@ -44,26 +76,29 @@ const mfm = P.createLanguage({
 		.map(x => {
 			const { username, host } = parseAcct(x.substr(1));
 			const canonical = host != null ? `@${username}@${toUnicode(host)}` : x;
-			return makeNode('mention', null, {
+			return makeNode('mention', {
 				canonical, username, host
 			});
 		}),
 	//#endregion
 
-	//#region Emoji
-	emoji: r =>
-		P.regexp(/^:([a-z0-9_+-]+):/i, 1)
-		.map(x => makeNode('emoji', null, {
-			name: x
-		})),
-	//#endregion
-
-	//#region Block code
-	blockCode: r =>
-		P.regexp(/^```([\s\S]+?)```/, 1)
-		.map(x => makeNode('blockCode', null, x)),
+	//#region URL
+	url: r =>
+		P((input, i) => {
+			const text = input.substr(i);
+			const match = text.match(/^https?:\/\/[\w\/:%#@\$&\?!\(\)\[\]~\.,=\+\-]+/);
+			if (!match) return P.makeFailure(i, 'not a url');
+			let url = match[0];
+			const before = input[i - 1];
+			if (url.endsWith('.')) url = url.substr(0, url.lastIndexOf('.'));
+			if (url.endsWith(',')) url = url.substr(0, url.lastIndexOf(','));
+			if (url.endsWith(')') && before == '(') url = url.substr(0, url.lastIndexOf(')'));
+			return P.makeSuccess(i + url.length, url);
+		})
+		.map(x => makeNode('url', { url: x })),
 	//#endregion
 });
 
 console.log(mfm.root.tryParse('aaa**important text @foo bar**bbb'));
 console.log(mfm.root.tryParse('```\naaa```bbb\n```'));
+console.log(mfm.root.tryParse('foo https://example.com. bar'));
