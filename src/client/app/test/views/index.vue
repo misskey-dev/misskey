db9c2913cfa4a83d1da637ae79dd8112c5cb5f5d
syuilo <syuilotan@yahoo.co.jp>
2018-11-21 05:36:55 +0900

    Update index.vue

--- src/client/app/test/views/index.vue
+++ src/client/app/test/views/index.vue
@@ -11,6 +11,25 @@
 			</div>
 		</section>
 	</ui-card>
+
+	<ui-card>
+		<div slot="title">Dialog Generator</div>
+		<section class="fit-top">
+			<ui-select v-model="dialogType" placeholder="">
+				<option value="info">Info</option>
+				<option value="success">Success</option>
+				<option value="error">Error</option>
+			</ui-select>
+			<ui-input v-model="dialogTitle">
+				<span>Title</span>
+			</ui-input>
+			<ui-input v-model="dialogText">
+				<span>Text</span>
+			</ui-input>
+			<ui-switch v-model="dialogShowCancelButton">With cancel button</ui-switch>
+			<ui-button @click="showDialog">Show</ui-button>
+		</section>
+	</ui-card>
 </main>
 </template>
 
@@ -21,7 +40,22 @@ export default Vue.extend({
 	data() {
 		return {
 			mfm: '',
+			dialogType: 'success',
+			dialogTitle: '',
+			dialogText: 'Hello World!',
+			dialogShowCancelButton: false
 		};
+	},
+
+	methods: {
+		showDialog() {
+			this.$root.alert({
+				type: this.dialogType,
+				title: this.dialogTitle,
+				text: this.dialogText,
+				showCancelButton: this.dialogShowCancelButton
+			});
+		}
 	}
 });
 </script>
