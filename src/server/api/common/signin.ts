f2578539067ff00e16d9f98415e00287a397dd35
syuilo <syuilotan@yahoo.co.jp>
2018-11-28 05:27:34 +0900

    Update signin.ts

--- src/server/api/common/signin.ts
+++ src/server/api/common/signin.ts
@@ -4,19 +4,21 @@ import config from '../../../config';
 import { ILocalUser } from '../../../models/user';
 
 export default function(ctx: Koa.Context, user: ILocalUser, redirect = false) {
-	const expires = 1000 * 60 * 60 * 24 * 365; // One Year
-	ctx.cookies.set('i', user.token, {
-		path: '/',
-		domain: config.hostname,
-		// SEE: https://github.com/koajs/koa/issues/974
-		// When using a SSL proxy it should be configured to add the "X-Forwarded-Proto: https" header
-		secure: config.url.startsWith('https'),
-		httpOnly: false,
-		expires: new Date(Date.now() + expires),
-		maxAge: expires
-	});
-
 	if (redirect) {
+		//#region Cookie
+		const expires = 1000 * 60 * 60 * 24 * 365; // One Year
+		ctx.cookies.set('i', user.token, {
+			path: '/',
+			domain: config.hostname,
+			// SEE: https://github.com/koajs/koa/issues/974
+			// When using a SSL proxy it should be configured to add the "X-Forwarded-Proto: https" header
+			secure: config.url.startsWith('https'),
+			httpOnly: false,
+			expires: new Date(Date.now() + expires),
+			maxAge: expires
+		});
+		//#endregion
+
 		ctx.redirect(config.url);
 	} else {
 		ctx.status = 204;
