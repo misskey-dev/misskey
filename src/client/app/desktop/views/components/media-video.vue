cf4cecd4dff5b67a979ba0b4b06645c3690f9e5c
MeiMei <30769358+mei23@users.noreply.github.com>
2018-12-01 07:16:28 +0900

    Fix media video (#3452)
    * Fix: Always show nsfw
    
    * [Client] Add missing video play icon

--- src/client/app/desktop/views/components/media-video.vue
+++ src/client/app/desktop/views/components/media-video.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="uofhebxjdgksfmltszlxurtjnjjsvioh" v-if="video.isSensitive && hide" @click="hide = false">
+<div class="uofhebxjdgksfmltszlxurtjnjjsvioh" v-if="video.isSensitive && hide && !$store.state.device.alwaysShowNsfw" @click="hide = false">
 	<div>
 		<b><fa icon="exclamation-triangle"/> {{ $t('sensitive') }}</b>
 		<span>{{ $t('click-to-show') }}</span>
