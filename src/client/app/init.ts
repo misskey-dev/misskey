d2a10ff3abe8d314664fda09427c0a74d370b7a6
Co-authored-by: Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-12-09 02:56:45 +0900

    Eliminate duplicated condition on if-statement

--- src/client/app/init.ts
+++ src/client/app/init.ts
@@ -385,9 +385,11 @@ export default (callback: (launch: (router: VueRouter) => [Vue, MiOS]) => void, 
 			const shadow = '0 3px 8px rgba(0, 0, 0, 0.2)';
 			const shadowRight = '4px 0 4px rgba(0, 0, 0, 0.1)';
 			const shadowLeft = '-4px 0 4px rgba(0, 0, 0, 0.1)';
-			if (os.store.state.settings.useShadow) document.documentElement.style.setProperty('--shadow', shadow);
-			if (os.store.state.settings.useShadow) document.documentElement.style.setProperty('--shadowRight', shadowRight);
-			if (os.store.state.settings.useShadow) document.documentElement.style.setProperty('--shadowLeft', shadowLeft);
+			if (os.store.state.settings.useShadow) {
+				document.documentElement.style.setProperty('--shadow', shadow);
+				document.documentElement.style.setProperty('--shadowRight', shadowRight);
+				document.documentElement.style.setProperty('--shadowLeft', shadowLeft);
+			}
 			os.store.watch(s => {
 				return s.settings.useShadow;
 			}, v => {
