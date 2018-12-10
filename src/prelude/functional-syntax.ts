c13db73920cbee3a914aa7037e133dafed4dc1fe
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-12-11 01:38:17 +0900

    Create functional-syntax.ts

---
+++ src/prelude/functional-syntax.ts
@@ -0,0 +1,4 @@
+export function switchMap<T, TResult>(source: T, fallback?: TResult, ...cases: [T, TResult][]): TResult {
+	const result = cases.find(([x, ]: [T, TResult]) => source === x);
+	return result ? result[1] : fallback;
+}
