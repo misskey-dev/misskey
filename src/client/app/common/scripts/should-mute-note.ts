61204745484eefec32b33d95a113cc8bc0657e3a
Aya Morisawa <AyaMorisawa4869@gmail.com>
2018-12-10 17:08:48 +0900

    Use && and || to eliminate if-statement (#3559)
    Co-authored-by: Acid Chicken (硫酸鶏) <root@acid-chicken.com>

--- src/client/app/common/scripts/should-mute-note.ts
+++ src/client/app/common/scripts/should-mute-note.ts
@@ -2,27 +2,8 @@ export default function(me, settings, note) {
 	const isMyNote = note.userId == me.id;
 	const isPureRenote = note.renoteId != null && note.text == null && note.fileIds.length == 0 && note.poll == null;
 
-	if (settings.showMyRenotes === false) {
-		if (isMyNote && isPureRenote) {
-			return true;
-		}
-	}
-
-	if (settings.showRenotedMyNotes === false) {
-		if (isPureRenote && (note.renote.userId == me.id)) {
-			return true;
-		}
-	}
-
-	if (settings.showLocalRenotes === false) {
-		if (isPureRenote && (note.renote.user.host == null)) {
-			return true;
-		}
-	}
-
-	if (!isMyNote && note.text && settings.mutedWords.some(q => q.length > 0 && !q.some(word => !note.text.includes(word)))) {
-		return true;
-	}
-
-	return false;
+	return settings.showMyRenotes === false && isMyNote && isPureRenote ||
+		settings.showRenotedMyNotes === false && isPureRenote && note.renote.userId == me.id ||
+		settings.showLocalRenotes === false && isPureRenote && note.renote.user.host == null ||
+		!isMyNote && note.text && settings.mutedWords.some(q => q.length > 0 && !q.some(word => !note.text.includes(word)));
 }
