2455d07d172764387aa16a1aa9e0c0688537e616
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-12-11 10:32:33 +0900

    Update get-drive-file-url.ts

--- src/misc/get-drive-file-url.ts
+++ src/misc/get-drive-file-url.ts
@@ -2,28 +2,19 @@ import { IDriveFile } from '../models/drive-file';
 import config from '../config';
 
 export default function(file: IDriveFile, thumbnail = false): string {
-	if (file == null) return null;
-
-	if (file.metadata.withoutChunks) {
-		if (thumbnail) {
-			return file.metadata.thumbnailUrl || file.metadata.webpublicUrl || file.metadata.url;
-		} else {
-			return file.metadata.webpublicUrl || file.metadata.url;
-		}
-	} else {
-		if (thumbnail) {
-			return `${config.drive_url}/${file._id}?thumbnail`;
-		} else {
-			return `${config.drive_url}/${file._id}?web`;
-		}
-	}
+	return file && (file.metadata.withoutChunks ?
+		thumbnail ?
+			file.metadata.thumbnailUrl || file.metadata.webpublicUrl || file.metadata.url :
+			file.metadata.webpublicUrl || file.metadata.url :
+		thumbnail ?
+			`${config.drive_url}/${file._id}?thumbnail` :
+			`${config.drive_url}/${file._id}?web`);
 }
 
 export function getOriginalUrl(file: IDriveFile) {
-	if (file.metadata && file.metadata.url) {
+	if (file.metadata && file.metadata.url)
 		return file.metadata.url;
-	}
 
-	const accessKey = file.metadata ? file.metadata.accessKey : null;
+	const accessKey = file.metadata && file.metadata.accessKey;
 	return `${config.drive_url}/${file._id}${accessKey ? '?original=' + accessKey : ''}`;
 }
