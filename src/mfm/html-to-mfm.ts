086fd1529a2180542ffd084605185b7988938882
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-12-11 09:38:05 +0900

    Update html-to-mfm.ts

--- src/mfm/html-to-mfm.ts
+++ src/mfm/html-to-mfm.ts
@@ -16,27 +16,29 @@ export default function(html: string): string {
 			'');
 	}
 
-	function analyzeChildren(node: any): string {
-		return node.childNodes ? (node.childNodes as any[]).reduce((a: string, c: any) => a + analyze(c), '') : '';
-	}
-
 	function analyze(node: any): string {
 		return switchMap(node.nodeName, analyzeChildren(node),
 			['#text', node.value as string],
 			['br', '\n'],
-			['a', (() => {
-				const txt = getText(node);
-				const rel = node.attrs.find((x: any) => x.name == 'rel');
-				const href = node.attrs.find((x: any) => x.name == 'href');
-				const part = txt.split('@');
-
-				return (
-					(rel && rel.value.match('tag') !== null) || !href || href.value == txt ? txt : // ハッシュタグ / hrefがない / txtがURL
-					part.length && !part[0].length ? switchMap(part.length, '', // メンション
-						[2, `${txt}@${(new URL(href.value)).hostname}`], // ホスト名部分が省略されているので復元する
-						[3, txt]) :
-					`[${txt}](${href.value})`); // その他
-			})()],
+			['a', analyzeLink(node)],
 			['p', `\n\n${analyzeChildren(node)}`]);
+	}
+
+	function analyzeChildren(node: any): string {
+		return node.childNodes ? (node.childNodes as any[]).reduce((a: string, c: any) => a + analyze(c), '') : '';
+	}
+
+	function analyzeLink(node: any): string {
+		const txt = getText(node);
+		const rel = node.attrs.find((x: any) => x.name == 'rel');
+		const href = node.attrs.find((x: any) => x.name == 'href');
+		const part = txt.split('@');
+
+		return (
+			(rel && rel.value.match('tag') !== null) || !href || href.value == txt ? txt : // ハッシュタグ / hrefがない / txtがURL
+			part.length && !part[0].length ? switchMap(part.length, '', // メンション
+				[2, `${txt}@${(new URL(href.value)).hostname}`], // ホスト名部分が省略されているので復元する
+				[3, txt]) :
+			`[${txt}](${href.value})`); // その他
 	}
 }
