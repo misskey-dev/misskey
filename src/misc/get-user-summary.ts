8f4c598b9231de3d27129dff125c761f65f10fdd
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-12-11 11:14:35 +0900

    Update get-user-summary.ts

--- src/misc/get-user-summary.ts
+++ src/misc/get-user-summary.ts
@@ -7,12 +7,8 @@ import getUserName from './get-user-name';
  * @param user ユーザー
  */
 export default function(user: IUser): string {
-	let string = `${getUserName(user)} (@${getAcct(user)})\n` +
-		`${user.notesCount}投稿、${user.followingCount}フォロー、${user.followersCount}フォロワー\n`;
-
-	if (isLocalUser(user)) {
-		string += `場所: ${user.profile.location}、誕生日: ${user.profile.birthday}\n`;
-	}
-
-	return string + `「${user.description}」`;
+	return `${getUserName(user)} (@${getAcct(user)})
+${user.notesCount}投稿、${user.followingCount}フォロー、${user.followersCount}フォロワー
+${isLocalUser(user) ? `場所: ${user.profile.location}、誕生日: ${user.profile.birthday}
+` : ''}「${user.description}」`;
 }
