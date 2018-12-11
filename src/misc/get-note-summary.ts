8572f956202bddfddf704e0dfd864edd1c23ab19
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-12-11 10:59:48 +0900

    Update get-note-summary.ts

--- src/misc/get-note-summary.ts
+++ src/misc/get-note-summary.ts
@@ -3,48 +3,15 @@
  * @param {*} note (packされた)投稿
  */
 const summarize = (note: any): string => {
-	if (note.deletedAt) {
-		return '(削除された投稿)';
-	}
-
-	if (note.isHidden) {
-		return '(非公開の投稿)';
-	}
-
-	let summary = '';
-
-	// 本文
-	summary += note.text ? note.text : '';
-
-	// ファイルが添付されているとき
-	if ((note.files || []).length != 0) {
-		summary += ` (${note.files.length}つのファイル)`;
-	}
-
-	// 投票が添付されているとき
-	if (note.poll) {
-		summary += ' (投票)';
-	}
-
-	// 返信のとき
-	if (note.replyId) {
-		if (note.reply) {
-			summary += `\n\nRE: ${summarize(note.reply)}`;
-		} else {
-			summary += '\n\nRE: ...';
-		}
-	}
-
-	// Renoteのとき
-	if (note.renoteId) {
-		if (note.renote) {
-			summary += `\n\nRN: ${summarize(note.renote)}`;
-		} else {
-			summary += '\n\nRN: ...';
-		}
-	}
-
-	return summary.trim();
+	return (
+		note.deletedAt ? '(削除された投稿)' :
+		note.isHidden ? '(非公開の投稿)' :
+		[
+			[note.files && note.files.length, (x: any) => `(${x.files.length}つのファイル)`], // ファイルが添付されているとき
+			[note.poll, (x: any) => '(投票)'], // 投票が添付されているとき
+			[note.replyId, (x: any) => `RE: ${x.reply ? summarize(x.reply) : '...'}`, '\n\n'], // 返信のとき
+			[note.renoteId, (x: any) => `RN: ${x.renote ? summarize(x.renote) : '...'}`, '\n\n'] // Renoteのとき
+		].reduce((a: string, [p, c, s]) => p ? a ? a + (s || ' ') + c(note) : c(note) : a, note.text)); // 本文
 };
 
 export default summarize;
