67a9170ee40bc4e24d45ba9913be59df8516db8a
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-12-11 17:33:24 +0900

    Create 404.vue

---
+++ src/client/app/common/views/pages/404.vue
@@ -0,0 +1,56 @@
+<template>
+<figure>
+<img src="/assets/404.png" alt="">
+<figcaption>
+<h1><span>404</span></h1>
+<p><span>{{ $t('page-not-found') }}</span></p>
+</figcaption>
+</figure>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import i18n from '../../../i18n';
+
+export default Vue.extend({
+	i18n: i18n('common/views/pages/404.vue')
+})
+</script>
+
+<style lang="stylus" scoped>
+figure
+	align-items center
+	bottom 0
+	display flex
+	justify-content center
+	left 0
+	margin auto
+	position fixed
+	right 0
+	top 0
+
+	img
+		height 384px
+		image-rendering crisp-edges
+		width 256px
+
+	figcaption
+		margin 8px
+
+		h1,
+		p
+			color var(--text)
+			display flex
+			flex-flow column
+
+			*
+				position relative
+				width 100%
+
+@media (max-width: 767px)
+	figure
+		flex-flow column
+
+	figcaption
+		text-align center
+</style>
