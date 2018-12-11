1077172b77c81e2c8c66643899c659b795fe2afa
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-12-11 12:16:51 +0900

    Update get-note-summary.ts

--- src/misc/get-note-summary.ts
+++ src/misc/get-note-summary.ts
@@ -7,11 +7,12 @@ const summarize = (note: any): string => {
 		note.deletedAt ? '(削除された投稿)' :
 		note.isHidden ? '(非公開の投稿)' :
 		[
-			[note.files && note.files.length, (x: any) => `(${x.files.length}つのファイル)`], // ファイルが添付されているとき
-			[note.poll, (x: any) => '(投票)'], // 投票が添付されているとき
-			[note.replyId, (x: any) => `RE: ${x.reply ? summarize(x.reply) : '...'}`, '\n\n'], // 返信のとき
-			[note.renoteId, (x: any) => `RN: ${x.renote ? summarize(x.renote) : '...'}`, '\n\n'] // Renoteのとき
-		].reduce((a: string, [p, c, s]) => p ? a ? a + (s || ' ') + c(note) : c(note) : a, note.text)); // 本文
+			[note.files && note.files.length, () => `(${note.files.length}つのファイル)`], // ファイルが添付されているとき
+			[note.poll, () => '(投票)'], // 投票が添付されているとき
+			[note.replyId, () => `RE: ${note.reply ? summarize(note.reply) : '...'}`, '\n\n'], // 返信のとき
+			[note.renoteId, () => `RN: ${note.renote ? summarize(note.renote) : '...'}`, '\n\n'] // Renoteのとき
+		].reduce((a: string, [p, c, s]) => p ? a ? a + (s || ' ') + c() : c() : a,
+			note.text)); // 本文
 };
 
 export default summarize;
