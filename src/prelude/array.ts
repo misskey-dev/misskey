b65349137b22cd0756058b1ab0ad4a3825c61120
mei23 <m@m544.net>
2018-12-17 18:16:57 +0900

    Refactor prelude/array

--- src/prelude/array.ts
+++ src/prelude/array.ts
@@ -1,59 +1,118 @@
-export function countIf<T>(f: (x: T) => boolean, xs: T[]): number {
-	return xs.filter(f).length;
+
+/**
+ * Return the number of matched items from target array by condition
+ * @param condition Condition to match
+ * @param source Source array
+ */
+export function countIf<T>(condition: (x: T) => boolean, source: T[]): number {
+	return source.filter(condition).length;
 }
 
-export function count<T>(x: T, xs: T[]): number {
-	return countIf(y => x === y, xs);
+/**
+ * Return the number of matched items from target array by value
+ * @param value Value to match
+ * @param source Source array
+ */
+export function count<T>(value: T, source: T[]): number {
+	return countIf(y => value === y, source);
 }
 
-export function concat<T>(xss: T[][]): T[] {
-	return ([] as T[]).concat(...xss);
+/**
+ * Flatten 2d arrays
+ * @param source Source array of array
+ */
+export function concat<T>(source: T[][]): T[] {
+	return ([] as T[]).concat(...source);
 }
 
-export function intersperse<T>(sep: T, xs: T[]): T[] {
-	return concat(xs.map(x => [sep, x])).slice(1);
+/**
+ * Return a new array with interspersed
+ * @param source Source array
+ */
+export function intersperse<T>(sep: T, source: T[]): T[] {
+	return concat(source.map(s => [sep, s])).slice(1);
 }
 
-export function erase<T>(x: T, xs: T[]): T[] {
-	return xs.filter(y => x !== y);
+/**
+ * Return a new array with erasing matched item from source array
+ * @param value Value to match
+ * @param source Source array
+ */
+export function erase<T>(value: T, source: T[]): T[] {
+	return source.filter(s => value !== s);
 }
 
 /**
  * Finds the array of all elements in the first array not contained in the second array.
  * The order of result values are determined by the first array.
  */
+
+/**
+ * Finds the array of all elements in the first array not contained in the second array.
+ * The order of result values are determined by the first array.
+ * @param includes First array
+ * @param excludes Second array
+ */
 export function difference<T>(includes: T[], excludes: T[]): T[] {
 	return includes.filter(x => !excludes.includes(x));
 }
 
-export function unique<T>(xs: T[]): T[] {
-	return [...new Set(xs)];
+/**
+ * Return a new array with uniqued
+ * @param source Source array
+ */
+export function unique<T>(source: T[]): T[] {
+	return [...new Set(source)];
 }
 
-export function sum(xs: number[]): number {
-	return xs.reduce((a, b) => a + b, 0);
+/**
+ * Get sum of array numbers
+ * @param source Source array
+ */
+export function sum(source: number[]): number {
+	return source.reduce((a, b) => a + b, 0);
 }
 
-export function maximum(xs: number[]): number {
-	return Math.max(...xs);
+/**
+ * Get maximun number from array
+ * @param source Source array
+ */
+export function maximum(source: number[]): number {
+	return Math.max(...source);
 }
 
-export function groupBy<T>(f: (x: T, y: T) => boolean, xs: T[]): T[][] {
+/**
+ * Grouping array values by condition
+ * @param condition Condition to match
+ * @param source Source array
+ */
+export function groupBy<T>(condition: (x: T, y: T) => boolean, source: T[]): T[][] {
 	const groups = [] as T[][];
-	for (const x of xs) {
-		if (groups.length !== 0 && f(groups[groups.length - 1][0], x)) {
-			groups[groups.length - 1].push(x);
+	for (const s of source) {
+		if (groups.length !== 0 && condition(groups[groups.length - 1][0], s)) {
+			groups[groups.length - 1].push(s);
 		} else {
-			groups.push([x]);
+			groups.push([s]);
 		}
 	}
 	return groups;
 }
 
-export function groupOn<T, S>(f: (x: T) => S, xs: T[]): T[][] {
-	return groupBy((a, b) => f(a) === f(b), xs);
+/**
+ * Grouping array values by keySelecter
+ * @param keySelecter Grouping key selector
+ * @param source Source array
+ */
+export function groupOn<T, S>(keySelecter: (x: T) => S, source: T[]): T[][] {
+	return groupBy((a, b) => keySelecter(a) === keySelecter(b), source);
 }
 
+/**
+ * Compare values like version string
+ * true if first item less than second one
+ * @param xs First
+ * @param ys Second
+ */
 export function lessThan(xs: number[], ys: number[]): boolean {
 	for (let i = 0; i < Math.min(xs.length, ys.length); i++) {
 		if (xs[i] < ys[i]) return true;
@@ -62,14 +121,19 @@ export function lessThan(xs: number[], ys: number[]): boolean {
 	return xs.length < ys.length;
 }
 
-export function takeWhile<T>(f: (x: T) => boolean, xs: T[]): T[] {
-	const ys = [];
-	for (const x of xs) {
-		if (f(x)) {
-			ys.push(x);
+/**
+ * Return a new array with taking value from source array while condition match
+ * @param condition Condition to match
+ * @param source Source array
+ */
+export function takeWhile<T>(condition: (x: T) => boolean, source: T[]): T[] {
+	const results = [];
+	for (const s of source) {
+		if (condition(s)) {
+			results.push(s);
 		} else {
 			break;
 		}
 	}
-	return ys;
+	return results;
 }
