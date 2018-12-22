7d6aac3431a9a370a505972ecfa35c39b1e8e8d3
syuilo <syuilotan@yahoo.co.jp>
2018-12-23 03:27:26 +0900

    Fix space

--- src/client/app/desktop/views/components/post-form.vue
+++ src/client/app/desktop/views/components/post-form.vue
@@ -209,7 +209,7 @@ export default Vue.extend({
 
 		// ダイレクトへのリプライはリプライ先ユーザーを初期設定
 		if (this.reply && this.reply.visibility === 'specified') {
-			this.$root.api('users/show', {	userId: this.reply.userId }).then(user => {
+			this.$root.api('users/show', { userId: this.reply.userId }).then(user => {
 				this.visibleUsers.push(user);
 			});
 		}
