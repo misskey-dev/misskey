9740db868546cea4e86fd81153b5b49e1225e876
syuilo <syuilotan@yahoo.co.jp>
2019-01-17 09:24:20 +0900

    [MFM] Better hashtag parsing: Ignore double quotation
    Resolve #3886

--- test/mfm.ts
+++ test/mfm.ts
@@ -365,6 +365,14 @@ describe('MFM', () => {
 				]);
 			});
 
+			it('ignore double quote', () => {
+				const tokens = analyze('#foo"');
+				assert.deepStrictEqual(tokens, [
+					leaf('hashtag', { hashtag: 'foo' }),
+					text('"'),
+				]);
+			});
+
 			it('allow including number', () => {
 				const tokens = analyze('#foo123');
 				assert.deepStrictEqual(tokens, [
