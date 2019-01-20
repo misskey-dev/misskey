e62e7389a3a83c3232c63e53c6d3e0df7e1648e5
mei23 <m@m544.net>
2019-01-21 05:45:12 +0900

    Revert "APからには不要な処理を削除"
    This reverts commit 8b5d8af9b0cc4d4ad0cf21de59827ff21df99560.

--- src/services/note/polls/vote.ts
+++ src/services/note/polls/vote.ts
@@ -1,6 +1,11 @@
 import Vote from '../../../models/poll-vote';
 import Note, { INote } from '../../../models/note';
-import { IUser } from '../../../models/user';
+import Watching from '../../../models/note-watching';
+import watch from '../../../services/note/watch';
+import { publishNoteStream } from '../../../stream';
+import notify from '../../../notify';
+import createNote from '../../../services/note/create';
+import { isLocalUser, IUser } from '../../../models/user';
 
 export default (user: IUser, note: INote, choice: number) => new Promise(async (res, rej) => {
 	if (!note.poll.choices.some(x => x.id == choice)) return rej('invalid choice param');
@@ -33,4 +38,50 @@ export default (user: IUser, note: INote, choice: number) => new Promise(async (
 	await Note.update({ _id: note._id }, {
 		$inc: inc
 	});
+
+	publishNoteStream(note._id, 'pollVoted', {
+		choice: choice,
+		userId: user._id.toHexString()
+	});
+
+	// Notify
+	notify(note.userId, user._id, 'poll_vote', {
+		noteId: note._id,
+		choice: choice
+	});
+
+	// Fetch watchers
+	Watching
+		.find({
+			noteId: note._id,
+			userId: { $ne: user._id },
+			// 削除されたドキュメントは除く
+			deletedAt: { $exists: false }
+		}, {
+			fields: {
+				userId: true
+			}
+		})
+		.then(watchers => {
+			for (const watcher of watchers) {
+				notify(watcher.userId, user._id, 'poll_vote', {
+					noteId: note._id,
+					choice: choice
+				});
+			}
+		});
+
+	// ローカルユーザーが投票した場合この投稿をWatchする
+	if (isLocalUser(user) && user.settings.autoWatch !== false) {
+		watch(user._id, note);
+	}
+
+	// ローカルからリモートへの投票の場合リプライ送信
+	if (isLocalUser(user) && note._user.host != null) {
+		createNote(user, {
+			createdAt: new Date(),
+			text: choice.toString(),
+			reply: note,
+		});
+	}
 });
