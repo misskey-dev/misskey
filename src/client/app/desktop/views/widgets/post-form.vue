4ab96251f557a2a654833474807ed7168ae8989a
syuilo <syuilotan@yahoo.co.jp>
2019-01-23 19:45:14 +0900

    [Client] Resolve #3967

--- src/client/app/desktop/views/widgets/post-form.vue
+++ src/client/app/desktop/views/widgets/post-form.vue
@@ -179,6 +179,7 @@ export default define({
 			this.$root.api('notes/create', {
 				text: this.text == '' ? undefined : this.text,
 				fileIds: this.files.length > 0 ? this.files.map(f => f.id) : undefined,
+				visibility: this.$store.state.settings.defaultNoteVisibility
 			}).then(data => {
 				this.clear();
 			}).catch(err => {
