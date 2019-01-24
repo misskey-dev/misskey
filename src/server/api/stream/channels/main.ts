a9f5e959f20ba0550ef524fd18fd6479e7fbf3a6
mei23 <m@m544.net>
2019-01-24 21:11:14 +0900

    非公開投稿は通知/メンション通知しない

--- src/server/api/stream/channels/main.ts
+++ src/server/api/stream/channels/main.ts
@@ -19,6 +19,11 @@ export default class extends Channel {
 			switch (type) {
 				case 'notification': {
 					if (mutedUserIds.includes(body.userId)) return;
+					if (body.note && body.note.isHidden) return;
+					break;
+				}
+				case 'mention': {
+					if (body.isHidden) return;
 					break;
 				}
 			}
