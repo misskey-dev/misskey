c34a27853376efe38e1596558f300c836e724359
MeiMei <30769358+mei23@users.noreply.github.com>
2019-01-31 23:14:45 +0900

    おすすめのアンケートでミュートユーザーのものは表示しない (#4067)

--- src/server/api/endpoints/notes/polls/recommendation.ts
+++ src/server/api/endpoints/notes/polls/recommendation.ts
@@ -2,6 +2,7 @@ import $ from 'cafy';
 import Vote from '../../../../../models/poll-vote';
 import Note, { pack } from '../../../../../models/note';
 import define from '../../../define';
+import Mute from '../../../../../models/mute';
 
 export const meta = {
 	desc: {
@@ -37,6 +38,11 @@ export default define(meta, (ps, user) => new Promise(async (res, rej) => {
 
 	const nin = votes && votes.length != 0 ? votes.map(v => v.noteId) : [];
 
+	// ミュートしているユーザーを取得
+	const mutedUserIds = await Mute.find({
+		muterId: user._id
+	}).then(ms => ms.map(m => m.muteeId));
+
 	const notes = await Note
 		.find({
 			'_user.host': null,
@@ -44,7 +50,8 @@ export default define(meta, (ps, user) => new Promise(async (res, rej) => {
 				$nin: nin
 			},
 			userId: {
-				$ne: user._id
+				$ne: user._id,
+				$nin: mutedUserIds
 			},
 			poll: {
 				$exists: true,
