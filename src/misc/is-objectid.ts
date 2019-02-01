8dbaf06224ef656b1b2ff4bf7d4e617d20e132a8
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-02-01 18:42:18 +0900

    Use `is` syntax

--- src/misc/is-objectid.ts
+++ src/misc/is-objectid.ts
@@ -1,3 +1,5 @@
-export default function(x: any): boolean {
+import { ObjectID } from 'mongodb';
+
+export default function(x: any): x is ObjectID {
 	return x.hasOwnProperty('toHexString') || x.hasOwnProperty('_bsontype');
 }
