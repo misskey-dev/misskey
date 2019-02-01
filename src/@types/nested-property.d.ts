96a2b909c83e133394e1130c7d31ffef62182e8f
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-02-01 14:38:25 +0900

    Follow lint

--- src/@types/nested-property.d.ts
+++ src/@types/nested-property.d.ts
@@ -1,19 +1,19 @@
 declare module 'nested-property' {
-	interface HasNestedPropertyOptions {
-		own?: boolean
+	interface IHasNestedPropertyOptions {
+		own?: boolean;
 	}
 
-	interface IsInNestedPropertyOptions {
-		validPath?: boolean
+	interface IIsInNestedPropertyOptions {
+		validPath?: boolean;
 	}
 
 	export function set<T>(object: T, property: string, value: any): T;
 
 	export function get(object: object, property: string): any;
 
-	export function has(object: object, property: string, options?: HasNestedPropertyOptions): boolean;
+	export function has(object: object, property: string, options?: IHasNestedPropertyOptions): boolean;
 
-	export function hasOwn(object: object, property: string, options?: HasNestedPropertyOptions): boolean;
+	export function hasOwn(object: object, property: string, options?: IHasNestedPropertyOptions): boolean;
 
-	export function isIn(object: object, property: string, objectInPath: object, options?: IsInNestedPropertyOptions): boolean;
+	export function isIn(object: object, property: string, objectInPath: object, options?: IIsInNestedPropertyOptions): boolean;
 }
