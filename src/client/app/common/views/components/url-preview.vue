6c05a8c763b4cb3aca6cbd1ad9f08118363dcb85
mei23 <m@m544.net>
2019-02-03 03:58:52 +0900

    Fix: URLプレビューのthumbnailで()を含むURLを提示されると表示できない

--- src/client/app/common/views/components/url-preview.vue
+++ src/client/app/common/views/components/url-preview.vue
@@ -9,7 +9,7 @@
 </div>
 <div v-else class="mk-url-preview">
 	<a :class="{ mini, compact }" :href="url" target="_blank" :title="url" v-if="!fetching">
-		<div class="thumbnail" v-if="thumbnail" :style="`background-image: url(${thumbnail})`"></div>
+		<div class="thumbnail" v-if="thumbnail" :style="`background-image: url('${thumbnail}')`"></div>
 		<article>
 			<header>
 				<h1 :title="title">{{ title }}</h1>
@@ -185,6 +185,7 @@ export default Vue.extend({
 				this.title = info.title;
 				this.description = info.description;
 				this.thumbnail = info.thumbnail;
+				console.log(info.thumbnail);
 				this.icon = info.icon;
 				this.sitename = info.sitename;
 				this.fetching = false;
