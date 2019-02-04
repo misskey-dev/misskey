843796a22fae50b6f1c1dbb9e60fd4e95bdd9549
mei23 <m@m544.net>
2019-02-05 01:22:23 +0900

    Media Proxy

---
+++ src/server/proxy/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Media Proxy
+ */
+
+import * as Koa from 'koa';
+import * as cors from '@koa/cors';
+import * as Router from 'koa-router';
+import { proxyMedia } from './proxy-media';
+
+// Init app
+const app = new Koa();
+app.use(cors());
+
+// Init router
+const router = new Router();
+
+router.get('/:url*', proxyMedia);
+
+// Register router
+app.use(router.routes());
+
+module.exports = app;
