9373ec29e0c48a6c714d6bd062d45f276239ab7c
mei23 <m@m544.net>
2019-02-12 03:38:16 +0900

    Fix: anonymousでアクセスするとサスペンドユーザーが隠れない

--- src/server/api/common/get-hide-users.ts
+++ src/server/api/common/get-hide-users.ts
@@ -4,7 +4,7 @@ import User, { IUser } from '../../../models/user';
 import { unique } from '../../../prelude/array';
 
 export async function getHideUserIds(me: IUser) {
-	return me ? await getHideUserIdsById(me._id) : [];
+	return await getHideUserIdsById(me._id);
 }
 
 export async function getHideUserIdsById(meId?: mongo.ObjectID) {
