893795a31d77cdf43ddc27c6e805b95f5e5897f6
syuilo <syuilotan@yahoo.co.jp>
2019-02-20 22:05:34 +0900

    Update some logs

--- src/misc/show-machine-info.ts
+++ src/misc/show-machine-info.ts
@@ -1,15 +1,13 @@
 import * as os from 'os';
 import * as sysUtils from 'systeminformation';
-import Logger from "./logger";
+import Logger from './logger';
 
 export async function showMachineInfo(parentLogger: Logger) {
 	const logger = parentLogger.createSubLogger('machine');
 	logger.debug(`Hostname: ${os.hostname()}`);
-	logger.debug(`Platform: ${process.platform}`);
-	logger.debug(`Architecture: ${process.arch}`);
-	logger.debug(`CPU: ${os.cpus().length} core`);
+	logger.debug(`Platform: ${process.platform} Arch: ${process.arch}`);
 	const mem = await sysUtils.mem();
 	const totalmem = (mem.total / 1024 / 1024 / 1024).toFixed(1);
 	const availmem = (mem.available / 1024 / 1024 / 1024).toFixed(1);
-	logger.debug(`MEM: ${totalmem}GB (available: ${availmem}GB)`);
+	logger.debug(`CPU: ${os.cpus().length} core MEM: ${totalmem}GB (available: ${availmem}GB)`);
 }
