c0b6643ecadbe56d60658902a2027042d8b545d2
syuilo <syuilotan@yahoo.co.jp>
2019-02-22 09:25:18 +0900

    wip

--- src/server/api/common/getters.ts
+++ src/server/api/common/getters.ts
@@ -1,7 +1,7 @@
 import * as mongo from 'mongodb';
 import Note from '../../../models/note';
 import User, { isRemoteUser, isLocalUser } from '../../../models/user';
-import { ApiError } from '../error';
+import { IdentifiableError } from '../../../misc/identifiable-error';
 
 /**
  * Get valied note for API processing
@@ -13,11 +13,7 @@ export async function getValiedNote(noteId: mongo.ObjectID) {
 	});
 
 	if (note === null) {
-		throw new ApiError({
-			message: 'No such note.',
-			code: 'NO_SUCH_NOTE',
-			id: '9725d0ce-ba28-4dde-95a7-2cbb2c15de24'
-		});
+		throw new IdentifiableError('9725d0ce-ba28-4dde-95a7-2cbb2c15de24', 'No such note.');
 	}
 
 	return note;
@@ -32,11 +28,7 @@ export async function getUser(userId: mongo.ObjectID) {
 	});
 
 	if (user == null) {
-		throw new ApiError({
-			message: 'No such user.',
-			code: 'NO_SUCH_USER',
-			id: '15348ddd-432d-49c2-8a5a-8069753becff'
-		});
+		throw new IdentifiableError('15348ddd-432d-49c2-8a5a-8069753becff', 'No such user.');
 	}
 
 	return user;
