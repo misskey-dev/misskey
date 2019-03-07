69df556ff5ebf556069cbc649bc40a790632d3f5
syuilo <syuilotan@yahoo.co.jp>
2019-03-08 04:55:55 +0900

    Resolve #4448

--- src/index.ts
+++ src/index.ts
@@ -117,9 +117,6 @@ async function masterMain() {
 		await spawnWorkers(config.clusterLimit);
 	}
 
-	// start queue
-	require('./queue').default();
-
 	bootLogger.succ(`Now listening on port ${config.port} on ${config.url}`, null, true);
 }
 
@@ -129,6 +126,9 @@ async function masterMain() {
 async function workerMain() {
 	// start server
 	await require('./server').default();
+
+	// start job queue
+	require('./queue').default();
 
 	if (cluster.isWorker) {
 		// Send a 'ready' message to parent process
@@ -150,13 +150,9 @@ async function queueMain() {
 	bootLogger.succ('Misskey initialized');
 
 	// start processor
-	const queue = require('./queue').default();
+	require('./queue').default();
 
-	if (queue) {
-		bootLogger.succ('Queue started', null, true);
-	} else {
-		bootLogger.error('Queue not available');
-	}
+	bootLogger.succ('Queue started', null, true);
 }
 
 const runningNodejsVersion = process.version.slice(1).split('.').map(x => parseInt(x, 10));
