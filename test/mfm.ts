23547c030a3dbe81e6711dc1feb86dafc8d656fe
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-03-12 22:18:12 +0900

    Fix path

--- test/mfm.ts
+++ test/mfm.ts
@@ -12,7 +12,7 @@ import * as assert from 'assert';
 
 import { parse, parsePlain } from '../src/mfm/parse';
 import { toHtml } from '../src/mfm/toHtml';
-import { createTree as tree, createLeaf as leaf, MfmTree } from '../src/mfm/types';
+import { createTree as tree, createLeaf as leaf, MfmTree } from '../src/mfm/prelude';
 import { removeOrphanedBrackets } from '../src/mfm/language';
 
 function text(text: string): MfmTree {
