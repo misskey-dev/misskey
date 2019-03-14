1179920790f2d4926eeb17368c3895b5321936d6
MeiMei <30769358+mei23@users.noreply.github.com>
2019-03-14 15:16:07 +0900

    unFollowAll on suspend (#4490)
    * unFollowAll on suspend
    
    * use services
    
    * silent

--- src/server/api/endpoints/admin/suspend-user.ts
+++ src/server/api/endpoints/admin/suspend-user.ts
@@ -1,7 +1,9 @@
 import $ from 'cafy';
 import ID, { transform } from '../../../../misc/cafy-id';
 import define from '../../define';
-import User from '../../../../models/user';
+import User, { IUser } from '../../../../models/user';
+import Following from '../../../../models/following';
+import deleteFollowing from '../../../../services/following/delete';
 
 export const meta = {
 	desc: {
@@ -51,5 +53,25 @@ export default define(meta, async (ps) => {
 		}
 	});
 
+	unFollowAll(user);
+
 	return;
 });
+
+async function unFollowAll(follower: IUser) {
+	const followings = await Following.find({
+		followerId: follower._id
+	});
+
+	for (const following of followings) {
+		const followee = await User.findOne({
+			_id: following.followeeId
+		});
+
+		if (followee == null) {
+			throw `Cant find followee ${following.followeeId}`;
+		}
+
+		await deleteFollowing(follower, followee, true);
+	}
+}
