bf5d43054bcdf7f4085725746db9c781f5b04410
syuilo <syuilotan@yahoo.co.jp>
2019-03-15 13:09:05 +0900

    Fix bug

--- src/client/app/admin/views/queue.vue
+++ src/client/app/admin/views/queue.vue
@@ -4,7 +4,7 @@
 		<template #title><fa :icon="faTasks"/> {{ $t('title') }}</template>
 		<section class="wptihjuy">
 			<header><fa :icon="faPaperPlane"/> Deliver</header>
-			<ui-info warn v-if="latestStats.deliver.waiting > 0">The queue is jammed.</ui-info>
+			<ui-info warn v-if="latestStats && latestStats.deliver.waiting > 0">The queue is jammed.</ui-info>
 			<ui-horizon-group inputs v-if="latestStats" class="fit-bottom">
 				<ui-input :value="latestStats.deliver.activeSincePrevTick | number" type="text" readonly>
 					<span>Process</span>
@@ -31,7 +31,7 @@
 		</section>
 		<section class="wptihjuy">
 			<header><fa :icon="faInbox"/> Inbox</header>
-			<ui-info warn v-if="latestStats.inbox.waiting > 0">The queue is jammed.</ui-info>
+			<ui-info warn v-if="latestStats && latestStats.inbox.waiting > 0">The queue is jammed.</ui-info>
 			<ui-horizon-group inputs v-if="latestStats" class="fit-bottom">
 				<ui-input :value="latestStats.inbox.activeSincePrevTick | number" type="text" readonly>
 					<span>Process</span>
