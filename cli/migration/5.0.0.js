eece188e08a99827e9d8b594570632af83ecb8fd
syuilo <syuilotan@yahoo.co.jp>
2019-03-21 05:37:50 +0900

    Clean

--- cli/migration/5.0.0.js
+++
@@ -1,9 +0,0 @@
-const { default: DriveFile } = require('../../built/models/drive-file');
-
-DriveFile.update({}, {
-	$rename: {
-		'metadata.isMetaOnly': 'metadata.withoutChunks'
-	}
-}, {
-	multi: true
-});
