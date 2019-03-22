aa630dbb68f0a81549a094a73b67ec045f16c1af
syuilo <syuilotan@yahoo.co.jp>
2019-03-23 01:36:31 +0900

    Update fetch-meta.ts

--- src/misc/fetch-meta.ts
+++ src/misc/fetch-meta.ts
@@ -1,12 +1,11 @@
 import { Meta } from '../models/meta';
 import { Metas } from '../models';
 
-export default function(): Promise<Meta> {
-	return Metas.findOne().then(meta => {
-		if (meta) {
-			return meta;
-		} else {
-			return Metas.save({});
-		}
-	});
+export default async function(): Promise<Meta> {
+	const meta = await Metas.findOne();
+	if (meta) {
+		return meta;
+	} else {
+		return Metas.save({});
+	}
 }
