9392da86481db9e73ffa1b9c3f6b2027ce1d77c0
syuilo <syuilotan@yahoo.co.jp>
2019-03-29 20:51:39 +0900

    Update gen-id.ts

--- src/misc/gen-id.ts
+++ src/misc/gen-id.ts
@@ -1,6 +1,38 @@
-import { ulid } from 'ulid';
+// Misskey ID
+// 長さ8の[2000年1月1日からの経過ミリ秒をbase36でエンコードしたもの] + 長さ2の[ランダムな文字列]
 
-// v11 TODO
-export function genId(): string {
-	return ulid().toLowerCase();
+const CHARS = '0123456789abcdefghijklmnopqrstuvwxyz';
+const TIME2000 = 946684800000;
+
+function getTime(time: number) {
+	time = time - TIME2000;
+	if (time < 0) time = 0;
+	if (time === 0) {
+		return CHARS[0];
+	}
+
+	const n = CHARS.length;
+	let s = '';
+
+	while (time > 0) {
+		s = CHARS[time % n] + s;
+		time = Math.floor(time / n);
+	}
+
+	return s;
+}
+
+function getRandom() {
+	let str = '';
+
+	for (let i = 0; i < 2; i++) {
+		str += CHARS[Math.floor(Math.random() * CHARS.length)];
+	}
+
+	return str;
+}
+
+export function genId(date?: Date): string {
+	if (date && (date > new Date())) date = new Date();
+	return getTime(date ? date.getTime() : Date.now()).padStart(8, CHARS[0]) + getRandom();
 }
