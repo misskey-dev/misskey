2ce8e02b25ef8b34b1d2e5468baf04b50aa83a05
mei23 <m@m544.net>
2019-03-30 07:06:25 +0900

    ユーザーリストの user.description を single line 扱いで表示するように

--- src/client/app/common/views/components/user-list.vue
+++ src/client/app/common/views/components/user-list.vue
@@ -16,7 +16,7 @@
 					<p class="username">@{{ user | acct }}</p>
 				</div>
 				<div class="description" v-if="user.description" :title="user.description">
-					<mfm :text="user.description" :is-note="false" :author="user" :i="$store.state.i" :custom-emojis="user.emojis" :should-break="false"/>
+					<mfm :text="user.description" :is-note="false" :author="user" :i="$store.state.i" :custom-emojis="user.emojis" :should-break="false" :plain-text="true"/>
 				</div>
 			</div>
 		</div>
