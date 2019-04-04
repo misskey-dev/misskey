9547623bc1a4255b9b4ba61f4796c14e402d9698
syuilo <syuilotan@yahoo.co.jp>
2019-04-04 16:34:51 +0900

    存在しないユーザーにメンションできなくなっていた問題を修正

--- src/services/note/create.ts
+++ src/services/note/create.ts
@@ -600,10 +600,11 @@ async function extractMentionedUsers(user: User, tokens: ReturnType<typeof parse
 
 	const mentions = extractMentions(tokens);
 
-	let mentionedUsers =
-		erase(null, await Promise.all(mentions.map(m =>
-			resolveUser(m.username, m.host || user.host).catch(() => null)
-		)));
+	let mentionedUsers = await Promise.all(mentions.map(m =>
+		resolveUser(m.username, m.host || user.host).catch(() => null)
+	));
+
+	mentionedUsers = mentionedUsers.filter(x => x != null);
 
 	// Drop duplicate users
 	mentionedUsers = mentionedUsers.filter((u, i, self) =>
