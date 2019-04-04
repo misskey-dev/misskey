5022d167f65561022846df3b40a3bef44440a5cb
syuilo <syuilotan@yahoo.co.jp>
2019-04-05 01:00:19 +0900

    Add mute tests

---
+++ test/mute.ts
@@ -0,0 +1,102 @@
+/*
+ * Tests of mute
+ *
+ * How to run the tests:
+ * > mocha test/mute.ts --require ts-node/register
+ *
+ * To specify test:
+ * > mocha test/mute.ts --require ts-node/register -g 'test name'
+ *
+ * If the tests not start, try set following enviroment variables:
+ * TS_NODE_FILES=true and TS_NODE_TRANSPILE_ONLY=true
+ * for more details, please see: https://github.com/TypeStrong/ts-node/issues/754
+ */
+
+process.env.NODE_ENV = 'test';
+
+import * as assert from 'assert';
+import * as childProcess from 'child_process';
+import { async, signup, request, post, react } from './utils';
+
+describe('Mute', () => {
+	let p: childProcess.ChildProcess;
+
+	// alice mutes carol
+	let alice: any;
+	let bob: any;
+	let carol: any;
+
+	before(done => {
+		p = childProcess.spawn('node', [__dirname + '/../index.js'], {
+			stdio: ['inherit', 'inherit', 'ipc'],
+			env: { NODE_ENV: 'test' }
+		});
+		p.on('message', async message => {
+			if (message === 'ok') {
+				alice = await signup({ username: 'alice' });
+				bob = await signup({ username: 'bob' });
+				carol = await signup({ username: 'carol' });
+				done();
+			}
+		});
+	});
+
+	after(() => {
+		p.kill();
+	});
+
+	it('ミュート作成', async(async () => {
+		const res = await request('/mute/create', {
+			userId: carol.id
+		}, alice);
+
+		assert.strictEqual(res.status, 204);
+	}));
+
+	describe('Timeline', () => {
+		it('タイムラインにミュートしているユーザーの投稿が含まれない', async(async () => {
+			const aliceNote = await post(alice);
+			const bobNote = await post(bob);
+			const carolNote = await post(carol);
+
+			const res = await request('/notes/local-timeline', {}, alice);
+
+			assert.strictEqual(res.status, 200);
+			assert.strictEqual(Array.isArray(res.body), true);
+			assert.strictEqual(res.body.some(note => note.id === aliceNote.id), true);
+			assert.strictEqual(res.body.some(note => note.id === bobNote.id), true);
+			assert.strictEqual(res.body.some(note => note.id === carolNote.id), false);
+		}));
+
+		it('タイムラインにミュートしているユーザーの投稿のRenoteが含まれない', async(async () => {
+			const aliceNote = await post(alice);
+			const carolNote = await post(carol);
+			const bobNote = await post(bob, {
+				renoteId: carolNote.id
+			});
+
+			const res = await request('/notes/local-timeline', {}, alice);
+
+			assert.strictEqual(res.status, 200);
+			assert.strictEqual(Array.isArray(res.body), true);
+			assert.strictEqual(res.body.some(note => note.id === aliceNote.id), true);
+			assert.strictEqual(res.body.some(note => note.id === bobNote.id), false);
+			assert.strictEqual(res.body.some(note => note.id === carolNote.id), false);
+		}));
+	});
+
+	describe('Notification', () => {
+		it('通知にミュートしているユーザーの通知が含まれない(リアクション)', async(async () => {
+			const aliceNote = await post(alice);
+			await react(bob, aliceNote, 'like');
+			await react(carol, aliceNote, 'like');
+
+			const res = await request('/i/notifications', {}, alice);
+
+			assert.strictEqual(res.status, 200);
+			assert.strictEqual(Array.isArray(res.body), true);
+			assert.strictEqual(res.body.some(notification => notification.userId === bob.id), true);
+			assert.strictEqual(res.body.some(notification => notification.userId === carol.id), false);
+		}));
+	});
+});
