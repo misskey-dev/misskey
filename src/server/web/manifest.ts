5756e86b556c7234e425a2b42d079cf5ab56c834
syuilo <syuilotan@yahoo.co.jp>
2019-04-05 18:32:16 +0900

    Update manifest.ts

--- src/server/web/manifest.ts
+++ src/server/web/manifest.ts
@@ -1,10 +1,9 @@
 import * as Koa from 'koa';
 import * as manifest from '../../client/assets/manifest.json';
-import * as deepcopy from 'deepcopy';
 import fetchMeta from '../../misc/fetch-meta';
 
 module.exports = async (ctx: Koa.BaseContext) => {
-	const json = deepcopy(manifest);
+	const json = JSON.parse(JSON.stringify(manifest));
 
 	const instance = await fetchMeta();
 
