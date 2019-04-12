cebd0d67381aa5509fd119a7de3df1f64cf2c958
mei23 <m@m544.net>
2019-04-12 19:56:38 +0900

    Fix: Punycodeなインスタンスが重複登録される

--- src/services/register-or-fetch-instance-doc.ts
+++ src/services/register-or-fetch-instance-doc.ts
@@ -1,8 +1,11 @@
 import Instance, { IInstance } from '../models/instance';
 import federationChart from '../services/chart/federation';
+import { toDbHost } from '../misc/convert-host';
 
 export async function registerOrFetchInstanceDoc(host: string): Promise<IInstance> {
 	if (host == null) return null;
+
+	host = toDbHost(host);
 
 	const index = await Instance.findOne({ host });
 
