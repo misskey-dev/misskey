52e3bcfd2968ffdab8c124dc8dad9cb9e3e995ac
syuilo <syuilotan@yahoo.co.jp>
2019-04-17 00:33:02 +0900

    Meta を二重にデータベースに保存できないように

--- src/misc/fetch-meta.ts
+++ src/misc/fetch-meta.ts
@@ -1,6 +1,5 @@
 import { Meta } from '../models/entities/meta';
 import { Metas } from '../models';
-import { genId } from './gen-id';
 
 export default async function(): Promise<Meta> {
 	const meta = await Metas.findOne();
@@ -8,7 +7,7 @@ export default async function(): Promise<Meta> {
 		return meta;
 	} else {
 		return Metas.save({
-			id: genId(),
+			id: 'x'
 		} as Meta);
 	}
 }
