704aabd703de2be4d06b399929e6f33525526b57
tamaina <tamaina@hotmail.co.jp>
2019-04-18 03:32:45 +0900

    Use menu instead of prompt Fix #4540, Fix #342 (#4575)
    * Use menu instead prompt
    
    * fix
    
    * https://bit.ly/2U0JuVt
    
    * fix

--- src/client/app/mobile/views/pages/drive.vue
+++ src/client/app/mobile/views/pages/drive.vue
@@ -5,7 +5,7 @@
 		<template v-if="file"><mk-file-type-icon data-icon :type="file.type" style="margin-right:4px;"/>{{ file.name }}</template>
 		<template v-if="!folder && !file"><span style="margin-right:4px;"><fa icon="cloud"/></span>{{ $t('@.drive') }}</template>
 	</template>
-	<template #func><button @click="fn"><fa icon="ellipsis-h"/></button></template>
+	<template #func v-if="folder || (!folder && !file)"><button @click="openContextMenu" ref="contextSource"><fa icon="ellipsis-h"/></button></template>
 	<x-drive
 		ref="browser"
 		:init-folder="initFolder"
@@ -26,9 +26,12 @@
 import Vue from 'vue';
 import i18n from '../../../i18n';
 import Progress from '../../../common/scripts/loading';
+import XMenu from '../../../common/views/components/menu.vue';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default Vue.extend({
-	i18n: i18n(),
+	i18n: i18n('mobile/views/pages/drive.vue'),
 	components: {
 		XDrive: () => import('../components/drive.vue').then(m => m.default),
 	},
@@ -62,9 +65,6 @@ export default Vue.extend({
 			} else {
 				(this.$refs as any).browser.goRoot(true);
 			}
-		},
-		fn() {
-			(this.$refs as any).browser.openContextMenu();
 		},
 		onMoveRoot(silent) {
 			const title = `${this.$root.instanceName} Drive`;
@@ -104,6 +104,42 @@ export default Vue.extend({
 
 			this.file = file;
 			this.folder = null;
+		},
+		openContextMenu() {
+			this.$root.new(XMenu, {
+				items: [{
+					type: 'item',
+					text: this.$t('contextmenu.upload'),
+					icon: 'upload',
+					action: this.$refs.browser.selectLocalFile
+				}, {
+					type: 'item',
+					text: this.$t('contextmenu.url-upload'),
+					icon: faCloudUploadAlt,
+					action: this.$refs.browser.urlUpload
+				}, {
+					type: 'item',
+					text: this.$t('contextmenu.create-folder'),
+					icon: ['far', 'folder'],
+					action: this.$refs.browser.createFolder
+				}, ...(this.folder ? [{
+					type: 'item',
+					text: this.$t('contextmenu.rename-folder'),
+					icon: 'i-cursor',
+					action: this.$refs.browser.renameFolder
+				}, {
+					type: 'item',
+					text: this.$t('contextmenu.move-folder'),
+					icon: ['far', 'folder-open'],
+					action: this.$refs.browser.moveFolder
+				}, {
+					type: 'item',
+					text: this.$t('contextmenu.delete-folder'),
+					icon: faTrashAlt,
+					action: this.$refs.browser.deleteFolder
+				}] : [])],
+				source: this.$refs.contextSource,
+			});
 		}
 	}
 });
