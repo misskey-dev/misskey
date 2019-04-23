701ab6cc6826a2e272d614fae97e7058110f1e1f
syuilo <syuilotan@yahoo.co.jp>
2019-04-24 03:26:02 +0900

    Refactor

--- src/prelude/await-all.ts
+++ src/prelude/await-all.ts
@@ -7,16 +7,17 @@ type AwaitAll<T> = {
 export async function awaitAll<T>(obj: T): Promise<AwaitAll<T>> {
 	const target = {} as any;
 	const keys = Object.keys(obj);
-	const rawValues = Object.values(obj);
-	const retValues = ((values: any[]): any[] =>
-		values.map(value => {
-			if (!value || !value.constructor || value.constructor.name !== 'Object') return value;
-			return awaitAll(value);
-		})
-	)(rawValues);
-	const values = await Promise.all(retValues);
-	for (let i = 0; i < values.length; i++) {
-		target[keys[i]] = values[i];
+	const values = Object.values(obj);
+
+	const resolvedValues = await Promise.all(values.map(value =>
+		(value || !value.constructor || value.constructor.name !== 'Object')
+			? value
+			: awaitAll(value)
+	));
+
+	for (let i = 0; i < keys.length; i++) {
+		target[keys[i]] = resolvedValues[i];
 	}
+
 	return target;
 }
