e45aa0532c2606031e644aec01a8d40a327a39b6
MeiMei <30769358+mei23@users.noreply.github.com>
2019-04-25 04:08:03 +0900

    Noteが間接参照されたときはstreaming等にpublishしない (#4796)

--- src/remote/activitypub/models/note.ts
+++ src/remote/activitypub/models/note.ts
@@ -265,7 +265,7 @@ export async function resolveNote(value: string | IObject, resolver?: Resolver):
 	// リモートサーバーからフェッチしてきて登録
 	// ここでuriの代わりに添付されてきたNote Objectが指定されていると、サーバーフェッチを経ずにノートが生成されるが
 	// 添付されてきたNote Objectは偽装されている可能性があるため、常にuriを指定してサーバーフェッチを行う。
-	return await createNote(uri, resolver);
+	return await createNote(uri, resolver, true);
 }
 
 export async function extractEmojis(tags: ITag[], host_: string) {
