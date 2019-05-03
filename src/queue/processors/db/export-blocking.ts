a397c040fe08d56d8a5db2dc9c5a2c18fde131aa
MeiMei <30769358+mei23@users.noreply.github.com>
2019-05-03 18:34:45 +0900

    エクスポートファイルでは同一ハッシュチェックをしないように (#4841)

--- src/queue/processors/db/export-blocking.ts
+++ src/queue/processors/db/export-blocking.ts
@@ -79,7 +79,7 @@ export async function exportBlocking(job: Bull.Job, done: any): Promise<void> {
 	logger.succ(`Exported to: ${path}`);
 
 	const fileName = 'blocking-' + dateFormat(new Date(), 'yyyy-mm-dd-HH-MM-ss') + '.csv';
-	const driveFile = await addFile(user, path, fileName);
+	const driveFile = await addFile(user, path, fileName, null, null, true);
 
 	logger.succ(`Exported to: ${driveFile._id}`);
 	cleanup();
