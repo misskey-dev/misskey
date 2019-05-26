820f1ee7f77341b1ab152918898bb3414203ed0f
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2019-05-27 00:02:06 +0900

    i18n: ページ編集のカウンターの増加値

--- src/client/app/common/views/pages/page-editor/els/page-editor.el.counter.vue
+++ src/client/app/common/views/pages/page-editor/els/page-editor.el.counter.vue
@@ -5,7 +5,7 @@
 	<section style="padding: 0 16px 0 16px;">
 		<ui-input v-model="value.name"><template #prefix><fa :icon="faMagic"/></template><span>{{ $t('blocks._counter.name') }}</span></ui-input>
 		<ui-input v-model="value.text"><span>{{ $t('blocks._counter.text') }}</span></ui-input>
-		<ui-input v-model="value.inc" type="number"><span>{{ $t('blocks._counter.increment') }}</span></ui-input>
+		<ui-input v-model="value.inc" type="number"><span>{{ $t('blocks._counter.inc') }}</span></ui-input>
 	</section>
 </x-container>
 </template>
