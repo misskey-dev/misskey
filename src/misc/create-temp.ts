eac72a35768fd61672cad38be2aadf74a49e188e
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-06-04 21:51:08 +0900

    Fix ambiguous type

--- src/misc/create-temp.ts
+++ src/misc/create-temp.ts
@@ -1,7 +1,7 @@
 import * as tmp from 'tmp';
 
-export function createTemp(): Promise<[string, unknown]> {
-	return new Promise<[string, unknown]>((res, rej) => {
+export function createTemp() {
+	return new Promise<[string, () => void]>((res, rej) => {
 		tmp.file((e, path, fd, cleanup) => {
 			if (e) return rej(e);
 			res([path, cleanup]);
