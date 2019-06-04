8301a9d44e4a109a39b649c691be9ed4621f5cfb
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-06-04 20:44:25 +0900

    Use overload instead of generic types

--- src/mfm/parse.ts
+++ src/mfm/parse.ts
@@ -2,14 +2,18 @@ import { mfmLanguage } from './language';
 import { MfmForest } from './prelude';
 import { normalize } from './normalize';
 
-export function parse<T extends string | null>(source: T): T extends string ? MfmForest : null {
+export function parse(source: string): MfmForest;
+export function parse(source: null): null;
+export function parse(source: string | null) {
 	return typeof source === 'string' ?
 		normalize(mfmLanguage.root.tryParse(source)) :
-		null as any;
+		null;
 }
 
-export function parsePlain<T extends string | null>(source: T): T extends string ? MfmForest : null {
+export function parsePlain(source: string): MfmForest;
+export function parsePlain(source: null): null;
+export function parsePlain(source: string | null) {
 	return typeof source === 'string' ?
 		normalize(mfmLanguage.plain.tryParse(source)) :
-		null as any;
+		null;
 }
