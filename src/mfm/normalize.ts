b4d6f9c28e97ecc81f3770d4e141324d157da985
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-06-04 21:07:17 +0900

    Refactor mfm/normalize.ts

--- src/mfm/normalize.ts
+++ src/mfm/normalize.ts
@@ -1,23 +1,31 @@
 import * as A from '../prelude/array';
 import * as S from '../prelude/string';
-import { MfmForest, MfmTree } from './prelude';
+import { MfmForest, MfmForestOf, MfmTree, MfmTreeOf, TextNode } from './prelude';
 import { createTree, createLeaf } from '../prelude/tree';
 
-function isEmptyTextTree(t: MfmTree): boolean {
-	return t.node.type == 'text' && t.node.props.text === '';
+function isTextTree(t: MfmTree): t is MfmTreeOf<TextNode> {
+	return t.node.type == 'text';
 }
 
-function concatTextTrees(ts: MfmForest): MfmTree {
+function isTextForest(ts: MfmForest): ts is MfmForestOf<TextNode> {
+	return isTextTree(ts[0]);
+}
+
+function isEmptyTextTree(t: MfmTree): t is MfmTree<{ text: '' }> {
+	return isTextTree(t) && !!t.node.props.text.length;
+}
+
+function concatTextTrees(ts: MfmForestOf<TextNode>): MfmTreeOf<TextNode> {
 	return createLeaf({ type: 'text', props: { text: S.concat(ts.map(x => x.node.props.text)) } });
 }
 
 function concatIfTextTrees(ts: MfmForest): MfmForest {
-	return ts[0].node.type === 'text' ? [concatTextTrees(ts)] : ts;
+	return isTextForest(ts) ? [concatTextTrees(ts)] : ts;
 }
 
 function concatConsecutiveTextTrees(ts: MfmForest): MfmForest {
-	const us = A.concat(A.groupOn(t => t.node.type, ts).map(concatIfTextTrees));
-	return us.map(t => createTree(t.node, concatConsecutiveTextTrees(t.children)));
+	return A.concat(A.groupOn(t => t.node.type, ts).map(concatIfTextTrees))
+		.map(t => createTree(t.node, concatConsecutiveTextTrees(t.children)));
 }
 
 function removeEmptyTextNodes(ts: MfmForest): MfmForest {
