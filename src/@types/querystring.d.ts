f13ca7bf7ec28d1f32c804fb5ca20f4d22973180
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-06-04 22:25:19 +0900

    Create type definition for 'querystring'

---
+++ src/@types/querystring.d.ts
@@ -0,0 +1,23 @@
+declare module 'querystring' {
+	interface IStringifyOptions {
+		encodeURIComponent?: (str: string) => string;
+	}
+
+	interface IParseOptions {
+		maxKeys?: number;
+		decodeURIComponent?: (str: string) => string;
+	}
+
+	interface IParsedUrlQuery {
+		[key: string]: string | string[];
+	}
+
+	interface IParsedUrlQueryInput {
+		[key: string]: unknown;
+	}
+
+	function stringify(obj?: IParsedUrlQueryInput, sep?: string, eq?: string, options?: IStringifyOptions): string;
+	function parse(str: string, sep?: string, eq?: string, options?: ParseOptions): IParsedUrlQuery;
+	function escape(str: string): string;
+	function unescape(str: string): string;
+}
