9e997339fcc5b55b7ce256eff1a8776d857736d0
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-06-04 22:19:33 +0900

    Refactor prelude/await-all.ts

--- src/prelude/await-all.ts
+++ src/prelude/await-all.ts
@@ -1,12 +1,16 @@
-type Await<T> = T extends Promise<infer U> ? U : T;
+type Await<T> =
+	T extends Promise<infer U> ? U :
+	T extends PromiseLike<infer U> ? U : T;
 
 type AwaitAll<T> = {
 	[P in keyof T]: Await<T[P]>;
 };
 
 export async function awaitAll<T>(obj: T): Promise<AwaitAll<T>> {
-	const target = {} as unknown;
-	const keys = Object.keys(obj);
+	const target = {} as {
+		[P in keyof T]: Await<T[P]>;
+	};
+	const keys = Object.keys(obj) as (keyof typeof obj)[];
 	const values = Object.values(obj);
 
 	const resolvedValues = await Promise.all(values.map(value =>
