bde60dfe9c69279790cb970ef96d618bdff15c16
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-06-04 22:25:33 +0900

    Refactor prelude/url.ts

--- src/prelude/url.ts
+++ src/prelude/url.ts
@@ -3,7 +3,7 @@ import { stringify } from 'querystring';
 export function query(obj: {}): string {
 	return stringify(Object.entries(obj)
 		.filter(([, v]) => Array.isArray(v) ? v.length : v !== undefined)
-		.reduce((a, [k, v]) => (a[k] = v, a), {} as Record<string, unknown>));
+		.reduce<Record<string, unknown>>((a, [k, v]) => (a[k] = v, a), {}));
 }
 
 export function appendQuery(url: string, query: string): string {
