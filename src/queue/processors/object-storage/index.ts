6b2047ce9848dc692f76ea71801b052ebfea81bc
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-06-06 10:37:58 +0900

    Remove unnecessary type cast

--- src/queue/processors/object-storage/index.ts
+++ src/queue/processors/object-storage/index.ts
@@ -3,10 +3,10 @@ import deleteFile from './delete-file';
 
 const jobs = {
 	deleteFile,
-} as unknown;
+};
 
 export default function(q: Bull.Queue) {
 	for (const [k, v] of Object.entries(jobs)) {
-		q.process(k, v as unknown);
+		q.process(k, v);
 	}
 }
