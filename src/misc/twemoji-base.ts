aca6b98f1145bbde91af1ced0eb85e69e8c8d9a9
mei23 <m@m544.net>
2019-06-16 05:59:29 +0900

    Support Unicode 12.0 Emoji

--- src/misc/twemoji-base.ts
+++ src/misc/twemoji-base.ts
@@ -1,4 +1,4 @@
-export const twemojiBase = 'https://cdn.jsdelivr.net/npm/twemoji@11.3.0';
-// https://cdn.jsdelivr.net/npm/twemoji@11.3.0
-// https://cdnjs.cloudflare.com/ajax/libs/twemoji/11.3.0
+export const twemojiBase = 'https://cdn.jsdelivr.net/npm/twemoji@12.0.1';
+// https://cdn.jsdelivr.net/npm/twemoji@12.0.1
+// https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.1
 // https://twemoji.maxcdn.com
