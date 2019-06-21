98c6f1866e95ba8a193463024b5dcb41307ae3e1
mei23 <m@m544.net>
2019-06-21 13:55:12 +0900

    Fix code style

--- src/client/app/common/views/components/url-preview.vue
+++ src/client/app/common/views/components/url-preview.vue
@@ -113,8 +113,9 @@ export default Vue.extend({
 			return;
 		}
 
-		if (requestUrl.hostname === 'music.youtube.com')
+		if (requestUrl.hostname === 'music.youtube.com') {
 			requestUrl.hostname = 'youtube.com';
+		}
 
 		const requestLang = (lang || 'ja-JP').replace('ja-KS', 'ja-JP');
 
