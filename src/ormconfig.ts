5a349eb3766d84b9a9af46930e60dcf6c4ab0688
syuilo <syuilotan@yahoo.co.jp>
2019-06-29 15:31:54 +0900

    Update ormconfig.ts

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import config from './config';
+import { entities as charts } from './services/chart/entities';
 
 const json = {
 	type: 'postgres',
@@ -9,7 +10,10 @@ const json = {
 	password: config.db.pass,
 	database: config.db.db,
 	extra: config.db.extra,
-	entities: ['src/models/entities/*.ts'],
+	entities: [
+		'src/models/entities/*.ts',
+		...charts as any
+	],
 	migrations: ['migration/*.ts'],
 	cli: {
 		migrationsDir: 'migration'
