f17e229c1e390eb98f167c918dde87dbbfcaacf2
syuilo <syuilotan@yahoo.co.jp>
2019-07-03 03:58:45 +0900

    Resolve #5072

--- src/client/app/admin/views/db.vue
+++ src/client/app/admin/views/db.vue
@@ -5,6 +5,14 @@
 		<section v-if="tables">
 			<div v-for="table in Object.keys(tables)"><b>{{ table }}</b> {{ tables[table].count | number }} {{ tables[table].size | bytes }}</div>
 		</section>
+		<section>
+			<header><fa :icon="faBroom"/> {{ $t('vacuum') }}</header>
+			<ui-info>{{ $t('vacuum-info') }}</ui-info>
+			<ui-switch v-model="fullVacuum">FULL</ui-switch>
+			<ui-switch v-model="analyzeVacuum">ANALYZE</ui-switch>
+			<ui-button @click="vacuum()"><fa :icon="faBroom"/> {{ $t('vacuum') }}</ui-button>
+			<ui-info warn>{{ $t('vacuum-exclamation') }}</ui-info>
+		</section>
 	</ui-card>
 </div>
 </template>
@@ -12,7 +20,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import i18n from '../../i18n';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faDatabase, faBroom } from '@fortawesome/free-solid-svg-icons';
 
 export default Vue.extend({
 	i18n: i18n('admin/views/db.vue'),
@@ -20,7 +28,9 @@ export default Vue.extend({
 	data() {
 		return {
 			tables: null,
-			faDatabase
+			fullVacuum: true,
+			analyzeVacuum: true,
+			faDatabase, faBroom
 		};
 	},
 
@@ -32,6 +42,18 @@ export default Vue.extend({
 		fetch() {
 			this.$root.api('admin/get-table-stats').then(tables => {
 				this.tables = tables;
+			});
+		},
+
+		vacuum() {
+			this.$root.api('admin/vacuum', {
+				full: this.fullVacuum,
+				analyze: this.analyzeVacuum,
+			}).then(() => {
+				this.$root.dialog({
+					type: 'success',
+					splash: true
+				});
 			});
 		},
 	}
