b34c1379e9aac1c8612a62afa849a48069d19d74
syuilo <Syuilotan@yahoo.co.jp>
2019-07-14 03:18:45 +0900

    Resolve #3238

--- src/server/api/endpoints/admin/silence-user.ts
+++ src/server/api/endpoints/admin/silence-user.ts
@@ -2,6 +2,7 @@ import $ from 'cafy';
 import { ID } from '../../../../misc/cafy-id';
 import define from '../../define';
 import { Users } from '../../../../models';
+import { insertModerationLog } from '../../../../services/insert-moderation-log';
 
 export const meta = {
 	desc: {
@@ -25,7 +26,7 @@ export const meta = {
 	}
 };
 
-export default define(meta, async (ps) => {
+export default define(meta, async (ps, me) => {
 	const user = await Users.findOne(ps.userId as string);
 
 	if (user == null) {
@@ -38,5 +39,9 @@ export default define(meta, async (ps) => {
 
 	await Users.update(user.id, {
 		isSilenced: true
+	});
+
+	insertModerationLog(me, 'silence', {
+		targetId: user.id,
 	});
 });
