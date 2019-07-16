d9ed340be7790d0ef1c929032b446502ef675071
mei23 <m@m544.net>
2019-07-17 02:42:22 +0900

    アカウント削除でもDelete activity

--- src/server/api/endpoints/i/delete-account.ts
+++ src/server/api/endpoints/i/delete-account.ts
@@ -3,6 +3,7 @@ import * as bcrypt from 'bcryptjs';
 import define from '../../define';
 import { Users, UserProfiles } from '../../../../models';
 import { ensure } from '../../../../prelude/ensure';
+import { doPostSuspend } from '../../../../services/suspend-user';
 
 export const meta = {
 	requireCredential: true,
@@ -25,6 +26,9 @@ export default define(meta, async (ps, user) => {
 	if (!same) {
 		throw new Error('incorrect password');
 	}
+
+	// 物理削除する前にDelete activityを送信する
+	await doPostSuspend(user).catch(e => {});
 
 	await Users.delete(user.id);
 });
