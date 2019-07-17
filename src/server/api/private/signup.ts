f1a7ab639b1ff17327040a041a6151648e79c388
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2019-07-18 00:00:03 +0900

    Resolve #5117 (#5171)
    * Resolve #5117
    
    * 出来立てのユーザーがストリームコネクションを持ってるはずがない

--- src/server/api/private/signup.ts
+++ src/server/api/private/signup.ts
@@ -5,7 +5,7 @@ import generateUserToken from '../common/generate-native-user-token';
 import config from '../../../config';
 import { fetchMeta } from '../../../misc/fetch-meta';
 import * as recaptcha from 'recaptcha-promise';
-import { Users, RegistrationTickets } from '../../../models';
+import { Users, Signins, RegistrationTickets } from '../../../models';
 import { genId } from '../../../misc/gen-id';
 import { usersChart } from '../../../services/chart';
 import { User } from '../../../models/entities/user';
@@ -136,6 +136,16 @@ export default async (ctx: Koa.BaseContext) => {
 	});
 
 	usersChart.update(account, true);
+
+	// Append signin history
+	await Signins.save({
+		id: genId(),
+		createdAt: new Date(),
+		userId: account.id,
+		ip: ctx.ip,
+		headers: ctx.headers,
+		success: true
+	});
 
 	const res = await Users.pack(account, account, {
 		detail: true,
