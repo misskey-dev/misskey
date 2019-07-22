e332ceac054a5d1180f69ef800fbc77d8acd9998
mei23 <m@m544.net>
2019-07-22 18:55:36 +0900

    nyaize

--- src/client/app/common/scripts/post-form.ts
+++ src/client/app/common/scripts/post-form.ts
@@ -9,6 +9,7 @@ import i18n from '../../i18n';
 import { erase, unique } from '../../../../prelude/array';
 import extractMentions from '../../../../misc/extract-mentions';
 import { formatTimeString } from '../../../../misc/format-time-string';
+import { nyaize } from '../../../../misc/nyaize';
 
 export default (opts) => ({
 	i18n: i18n(),
@@ -467,7 +468,7 @@ export default (opts) => ({
 				createdAt: new Date().toISOString(),
 				userId: this.$store.state.i.id,
 				user: this.$store.state.i,
-				text: this.text == '' ? undefined : this.text,
+				text: this.text === '' ? undefined : this.$store.state.i.isCat ? nyaize(this.text) : this.text,
 				visibility: this.visibility,
 				localOnly: this.localOnly,
 				fileIds: this.files.length > 0 ? this.files.map(f => f.id) : undefined,
