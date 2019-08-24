340d920f6c1b0c9bc76f0eea21fbc31abded4601
mei23 <m@m544.net>
2019-08-24 11:42:17 +0900

    fix

--- src/client/app/common/views/components/media-list.vue
+++ src/client/app/common/views/components/media-list.vue
@@ -36,7 +36,7 @@ export default Vue.extend({
 		//#region for Safari bug
 		if (this.$refs.grid) {
 			this.$refs.grid.style.height = this.$refs.grid.clientHeight ? `${this.$refs.grid.clientHeight}px`
-				: (this.$store.state.device.inDeckMode ? '128px' : this.$root.isMobile ? '173px' : '287px');
+				: this.$store.state.device.inDeckMode ? '128px' : this.$root.isMobile ? '173px' : '287px';
 		}
 		//#endregion
 	},
