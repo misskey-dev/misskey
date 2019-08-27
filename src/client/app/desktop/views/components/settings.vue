3678efb5a5d97bf1b876531939b9960e0c6d92fa
Aya Morisawa <AyaMorisawa4869@gmail.com>
2019-08-28 00:45:41 +0900

    Assign URL to each page of settings

--- src/client/app/desktop/views/components/settings.vue
+++ src/client/app/desktop/views/components/settings.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="mk-settings">
 	<div class="nav" :class="{ inWindow }">
-		<p :class="{ active: page == 'profile' }" @mousedown="page = 'profile'"><fa icon="user" fixed-width/>{{ $t('@._settings.profile') }}</p>
-		<p :class="{ active: page == 'appearance' }" @mousedown="page = 'appearance'"><fa icon="palette" fixed-width/>{{ $t('@._settings.appearance') }}</p>
-		<p :class="{ active: page == 'behavior' }" @mousedown="page = 'behavior'"><fa icon="desktop" fixed-width/>{{ $t('@._settings.behavior') }}</p>
-		<p :class="{ active: page == 'notification' }" @mousedown="page = 'notification'"><fa :icon="['far', 'bell']" fixed-width/>{{ $t('@._settings.notification') }}</p>
-		<p :class="{ active: page == 'drive' }" @mousedown="page = 'drive'"><fa icon="cloud" fixed-width/>{{ $t('@.drive') }}</p>
-		<p :class="{ active: page == 'hashtags' }" @mousedown="page = 'hashtags'"><fa icon="hashtag" fixed-width/>{{ $t('@._settings.tags') }}</p>
-		<p :class="{ active: page == 'muteAndBlock' }" @mousedown="page = 'muteAndBlock'"><fa icon="ban" fixed-width/>{{ $t('@._settings.mute-and-block') }}</p>
-		<p :class="{ active: page == 'apps' }" @mousedown="page = 'apps'"><fa icon="puzzle-piece" fixed-width/>{{ $t('@._settings.apps') }}</p>
-		<p :class="{ active: page == 'security' }" @mousedown="page = 'security'"><fa icon="unlock-alt" fixed-width/>{{ $t('@._settings.security') }}</p>
-		<p :class="{ active: page == 'api' }" @mousedown="page = 'api'"><fa icon="key" fixed-width/>API</p>
-		<p :class="{ active: page == 'other' }" @mousedown="page = 'other'"><fa icon="cogs" fixed-width/>{{ $t('@._settings.other') }}</p>
+		<p :class="{ active: page == 'profile' }" @mousedown="go('profile')"><fa icon="user" fixed-width/>{{ $t('@._settings.profile') }}</p>
+		<p :class="{ active: page == 'appearance' }" @mousedown="go('appearance')"><fa icon="palette" fixed-width/>{{ $t('@._settings.appearance') }}</p>
+		<p :class="{ active: page == 'behavior' }" @mousedown="go('behavior')"><fa icon="desktop" fixed-width/>{{ $t('@._settings.behavior') }}</p>
+		<p :class="{ active: page == 'notification' }" @mousedown="go('notification')"><fa :icon="['far', 'bell']" fixed-width/>{{ $t('@._settings.notification') }}</p>
+		<p :class="{ active: page == 'drive' }" @mousedown="go('drive')"><fa icon="cloud" fixed-width/>{{ $t('@.drive') }}</p>
+		<p :class="{ active: page == 'hashtags' }" @mousedown="go('hashtags')"><fa icon="hashtag" fixed-width/>{{ $t('@._settings.tags') }}</p>
+		<p :class="{ active: page == 'muteAndBlock' }" @mousedown="go('muteAndBlock')"><fa icon="ban" fixed-width/>{{ $t('@._settings.mute-and-block') }}</p>
+		<p :class="{ active: page == 'apps' }" @mousedown="go('apps')"><fa icon="puzzle-piece" fixed-width/>{{ $t('@._settings.apps') }}</p>
+		<p :class="{ active: page == 'security' }" @mousedown="go('security')"><fa icon="unlock-alt" fixed-width/>{{ $t('@._settings.security') }}</p>
+		<p :class="{ active: page == 'api' }" @mousedown="go('api')"><fa icon="key" fixed-width/>API</p>
+		<p :class="{ active: page == 'other' }" @mousedown="go('other')"><fa icon="cogs" fixed-width/>{{ $t('@._settings.other') }}</p>
 	</div>
 	<div class="pages">
 		<x-settings :page="page"/>
@@ -45,6 +45,12 @@ export default Vue.extend({
 			page: this.initialPage || 'profile',
 		};
 	},
+	methods: {
+		go(page: string) {
+			this.page = page;
+			this.$router.push(`/i/settings/${page}`);
+		}
+	}
 });
 </script>
 
