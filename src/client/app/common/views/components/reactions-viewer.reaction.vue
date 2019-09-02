f187df3933ec18e815f5e05c375645105bad052c
syuilo <Syuilotan@yahoo.co.jp>
2019-09-03 06:20:04 +0900

    Update reactions-viewer.reaction.vue

--- src/client/app/common/views/components/reactions-viewer.reaction.vue
+++ src/client/app/common/views/components/reactions-viewer.reaction.vue
@@ -173,6 +173,14 @@ export default Vue.extend({
 	border-radius 4px
 	cursor pointer
 
+	&, *
+		-webkit-touch-callout none
+		-webkit-user-select none
+		-khtml-user-select none
+		-moz-user-select none
+		-ms-user-select none
+		user-select none
+
 	*
 		user-select none
 		pointer-events none
