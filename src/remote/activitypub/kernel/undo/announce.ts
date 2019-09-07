9ca4267f028083604b5925e3e0aff7650faa1253
mei23 <m@m544.net>
2019-09-07 20:13:19 +0900

    Use activity id

--- src/remote/activitypub/kernel/undo/announce.ts
+++ src/remote/activitypub/kernel/undo/announce.ts
@@ -1,34 +1,14 @@
-import config from '../../../../config';
 import { Notes } from '../../../../models';
-import { Note } from '../../../../models/entities/note';
 import { IRemoteUser } from '../../../../models/entities/user';
 import { IAnnounce, getApId } from '../../type';
 import deleteNote from '../../../../services/note/delete';
 
 export const undoAnnounce = async (actor: IRemoteUser, activity: IAnnounce): Promise<void> => {
-	const targetUri = getApId(activity.object);
+	const uri = getApId(activity);
 
-	let note: Note | undefined;
-
-	if (targetUri.startsWith(config.url + '/')) {
-		// Announce target is local
-		const targetNoteId = targetUri.split('/').pop();
-
-		note = await Notes.findOne({
-			renoteId: targetNoteId
-		});
-	} else {
-		// Announce target is remote
-		const targetNote = await Notes.findOne({
-			uri: targetUri
-		});
-
-		if (!targetNote) return;
-
-		note = await Notes.findOne({
-			renoteId: targetNote.id
-		});
-	}
+	const note = await Notes.findOne({
+		uri
+	});
 
 	if (!note) return;
 
