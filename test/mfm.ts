fdcc234d10313de2175003b7489f83264372b6b6
mei23 <m@m544.net>
2019-09-15 18:43:05 +0900

    Fix: Keycap Number Sign が表示できない

--- test/mfm.ts
+++ test/mfm.ts
@@ -639,6 +639,13 @@ describe('MFM', () => {
 					text('/bar'),
 				]);
 			});
+
+			it('ignore single Keycap Number Sign (U+0023 + U+FE0F + U+20E3)', () => {
+				const tokens = parse('#️⃣');
+				assert.deepStrictEqual(tokens, [
+					leaf('emoji', { emoji: '#️⃣' })
+				]);
+			});
 		});
 
 		describe('quote', () => {
