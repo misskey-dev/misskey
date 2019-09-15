6487941920ec14c1f4c212ee921c5d5ce2148708
tamaina <tamaina@hotmail.co.jp>
2019-09-15 21:21:25 +0900

    PWA Fix

--- src/client/app/sw.js
+++ src/client/app/sw.js
@@ -4,13 +4,51 @@
 
 import composeNotification from './common/scripts/compose-notification';
 
+// eslint-disable-next-line no-undef
+const version = _VERSION_
+const cacheName = `mk-cache-${version}`
+
 // インストールされたとき
 self.addEventListener('install', ev => {
 	console.info('installed');
 
-	ev.waitUntil(Promise.all([
-		self.skipWaiting(), // Force activate
-	]));
+  ev.waitUntil(
+		caches.open(cacheName)
+			.then(cache => {
+				return cache.addAll([
+					"/",
+					`/assets/desktop.${version}.js`,
+					`/assets/mobile.${version}.js`,
+					"/assets/error.jpg"
+				]);
+			})
+			.then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', ev => {
+	ev.waitUntil(
+		caches.keys()
+			.then(cacheNames => Promise.all(
+				cacheNames
+					.filter((v) => v !== cacheName)
+					.map(name => caches.delete(name))
+			))
+			.then(() => self.clients.claim())
+	);
+});
+
+self.addEventListener('fetch', ev => {
+	if (ev.request.method !== 'GET') return
+	ev.respondWith(
+		caches.match(ev.request)
+			.then(response => {
+				return response || fetch(ev.request);
+			})
+			.catch(() => {
+				return caches.match("/");
+			})
+	);
 });
 
 // プッシュ通知を受け取ったとき
