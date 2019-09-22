9497581df96f432659807ad3940103bee36e0538
mei23 <m@m544.net>
2019-09-22 14:36:11 +0900

    Resolve syuilo#5447

--- src/server/web/index.ts
+++ src/server/web/index.ts
@@ -146,7 +146,7 @@ router.get('/@:user.json', async ctx => {
 
 //#region for crawlers
 // User
-router.get('/@:user', async (ctx, next) => {
+router.get(['/@:user', '/@:user/*'], async (ctx, next) => {
 	const { username, host } = parseAcct(ctx.params.user);
 	const user = await Users.findOne({
 		usernameLower: username.toLowerCase(),
