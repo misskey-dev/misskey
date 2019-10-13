c228b8e7c7e2419b8d15d4ec58e608a85a02c0aa
mei23 <m@m544.net>
2019-10-14 04:53:46 +0900

    default instance name

--- src/client/app/common/views/pages/explore.vue
+++ src/client/app/common/views/pages/explore.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
 	<div class="localfedi7" v-if="meta && stats && tag == null" :style="{ backgroundImage: meta.bannerUrl ? `url(${meta.bannerUrl})` : null }">
-		<header>{{ $t('explore', { host: meta.name }) }}</header>
+		<header>{{ $t('explore', { host: meta.name || 'Misskey' }) }}</header>
 		<div>{{ $t('users-info', { users: num(stats.originalUsersCount) }) }}</div>
 	</div>
 
