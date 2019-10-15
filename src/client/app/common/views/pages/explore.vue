0bc7792964fd0c3bff49cf162b47d5e2d7971f0b
mei23 <m@m544.net>
2019-10-16 01:17:06 +0900

    とりあえずリモートの人気のユーザーはリモートのみにするように

--- src/client/app/common/views/pages/explore.vue
+++ src/client/app/common/views/pages/explore.vue
@@ -91,7 +91,7 @@ export default Vue.extend({
 			} },
 			popularUsersF: { endpoint: 'users', limit: 10, params: {
 				state: 'alive',
-				origin: 'combined',
+				origin: 'remote',
 				sort: '+follower',
 			} },
 			recentlyUpdatedUsersF: { endpoint: 'users', limit: 10, params: {
