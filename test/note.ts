a02c08732756253f3008d88f069976c7378f82bf
rinsuki <428rinsuki+git@gmail.com>
2019-10-25 07:08:05 +0900

    fix?

--- test/note.ts
+++ test/note.ts
@@ -18,7 +18,7 @@ import * as assert from 'assert';
 import * as childProcess from 'child_process';
 import { async, signup, request, post, uploadFile, launchServer } from './utils';
 import { Note } from '../src/models/entities/note';
-import { initDb } from '../src/db/postgre';
+const initDb = require('../built/db/postgre.js').initDb;
 
 describe('Note', () => {
 	let p: childProcess.ChildProcess;
