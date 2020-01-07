4c1c34b6d6edd92c457eb84cf42a4027413fbfee
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2020-01-07 23:12:13 +0900

    Resolve #5688

--- src/misc/detect-mine.ts
+++ src/misc/detect-mine.ts
@@ -9,7 +9,7 @@ export async function detectMine(path: string) {
 			.on('error', rej)
 			.once('data', (buffer: Buffer) => {
 				readable.destroy();
-				const type = fileType(buffer);
+				const type = await FileType.fromBuffer(buffer);
 				if (type) {
 					if (type.mime == 'application/xml' && checkSvg(path)) {
 						res(['image/svg+xml', 'svg']);
