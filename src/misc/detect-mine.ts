2de1e27016db4e39a872e10350caae1ca17cc872
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2020-01-07 23:14:39 +0900

    あああああ

--- src/misc/detect-mine.ts
+++ src/misc/detect-mine.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
-import fileType = require('file-type');
 import checkSvg from '../misc/check-svg';
+const FileType = require('file-type')；
 
 export async function detectMine(path: string) {
 	return new Promise<[string, string | null]>((res, rej) => {
