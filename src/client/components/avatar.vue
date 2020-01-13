ec77ec5c4f18e2bfd9ae6db2415c8ef4e810820e
syuilo <Syuilotan@yahoo.co.jp>
2020-01-14 01:55:48 +0900

    wip

--- src/client/components/avatar.vue
+++ src/client/components/avatar.vue
@@ -1,16 +1,16 @@
 <template>
-	<span class="mk-avatar" :title="user | acct" v-if="disableLink && !disablePreview" v-user-preview="user.id" @click="onClick" v-once>
-		<span class="inner" :style="icon"></span>
-	</span>
-	<span class="mk-avatar" :title="user | acct" v-else-if="disableLink && disablePreview" @click="onClick" v-once>
-		<span class="inner" :style="icon"></span>
-	</span>
-	<router-link class="mk-avatar" :to="user | userPage" :title="user | acct" :target="target" v-else-if="!disableLink && !disablePreview" v-user-preview="user.id" v-once>
-		<span class="inner" :style="icon"></span>
-	</router-link>
-	<router-link class="mk-avatar" :to="user | userPage" :title="user | acct" :target="target" v-else-if="!disableLink && disablePreview" v-once>
-		<span class="inner" :style="icon"></span>
-	</router-link>
+<span class="mk-avatar" :class="{ cat }" :title="user | acct" v-if="disableLink && !disablePreview" v-user-preview="user.id" @click="onClick" v-once>
+	<span class="inner" :style="icon"></span>
+</span>
+<span class="mk-avatar" :class="{ cat }" :title="user | acct" v-else-if="disableLink && disablePreview" @click="onClick" v-once>
+	<span class="inner" :style="icon"></span>
+</span>
+<router-link class="mk-avatar" :class="{ cat }" :to="user | userPage" :title="user | acct" :target="target" v-else-if="!disableLink && !disablePreview" v-user-preview="user.id" v-once>
+	<span class="inner" :style="icon"></span>
+</router-link>
+<router-link class="mk-avatar" :class="{ cat }" :to="user | userPage" :title="user | acct" :target="target" v-else-if="!disableLink && disablePreview" v-once>
+	<span class="inner" :style="icon"></span>
+</router-link>
 </template>
 
 <script lang="ts">
@@ -37,6 +37,9 @@ export default Vue.extend({
 		}
 	},
 	computed: {
+		cat(): boolean {
+			return this.user.isCat && this.$store.state.settings.circleIcons;
+		},
 		url(): string {
 			return this.$store.state.device.disableShowingAnimatedImages
 				? getStaticImageUrl(this.user.avatarUrl)
@@ -44,8 +47,14 @@ export default Vue.extend({
 		},
 		icon(): any {
 			return {
+				backgroundColor: this.user.avatarColor,
 				backgroundImage: `url(${this.url})`,
 			};
+		}
+	},
+	mounted() {
+		if (this.user.avatarColor) {
+			this.$el.style.color = this.user.avatarColor;
 		}
 	},
 	methods: {
@@ -64,6 +73,28 @@ export default Vue.extend({
 	flex-shrink: 0;
 	overflow: hidden;
 	border-radius: 100%;
+
+	&.cat {
+		&:before, &:after {
+			background: #df548f;
+			border: solid 4px currentColor;
+			box-sizing: border-box;
+			content: '';
+			display: inline-block;
+			height: 50%;
+			width: 50%;
+		}
+
+		&:before {
+			border-radius: 0 75% 75%;
+			transform: rotate(37.5deg) skew(30deg);
+		}
+
+		&:after {
+			border-radius: 75% 0 75% 75%;
+			transform: rotate(-37.5deg) skew(-30deg);
+		}
+	}
 	
 	.inner {
 		background-position: center center;
