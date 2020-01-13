a801488bfd51adde824b157ffbbcd83f8f0fde1f
syuilo <Syuilotan@yahoo.co.jp>
2020-01-14 06:45:54 +0900

    wip

--- src/client/pages/instance/users.vue
+++ src/client/pages/instance/users.vue
@@ -1,27 +1,41 @@
 <template>
-<section class="_section mk-instance-users">
-	<div class="title"><fa :icon="faUsers"/> {{ $t('users') }}</div>
-	<div class="content padHalf">
-		<x-pagination :pagination="pagination" #default="{items}" class="users" ref="users" :auto-margin="false">
-			<button class="user _button" v-for="(user, i) in items" :key="user.id" :data-index="i" @click="show(user)">
-				<mk-avatar :user="user" class="avatar"/>
-				<div class="body">
-					<mk-user-name :user="user" class="name"/>
-					<mk-acct :user="user" class="acct"/>
-				</div>
-			</button>
-		</x-pagination>
-	</div>
-	<div class="footer">
-		<x-button inline primary @click="addUser()"><fa :icon="faPlus"/> {{ $t('addUser') }}</x-button>
-	</div>
-</section>
+<div class="mk-instance-users">
+	<section class="_section lookup">
+		<div class="title"><fa :icon="faSearch"/> {{ $t('lookup') }}</div>
+		<div class="content">
+			<x-input class="target" v-model="target" type="text" @enter="showUser()" style="margin-top: 0;">
+				<span>{{ $t('usernameOrUserId') }}</span>
+			</x-input>
+			<x-button @click="showUser()" primary><fa :icon="faSearch"/> {{ $t('lookup') }}</x-button>
+		</div>
+	</section>
+
+	<section class="_section users">
+		<div class="title"><fa :icon="faUsers"/> {{ $t('users') }}</div>
+		<div class="content padHalf">
+			<x-pagination :pagination="pagination" #default="{items}" class="users" ref="users" :auto-margin="false">
+				<button class="user _button" v-for="(user, i) in items" :key="user.id" :data-index="i" @click="show(user)">
+					<mk-avatar :user="user" class="avatar"/>
+					<div class="body">
+						<mk-user-name :user="user" class="name"/>
+						<mk-acct :user="user" class="acct"/>
+					</div>
+				</button>
+			</x-pagination>
+		</div>
+		<div class="footer">
+			<x-button inline primary @click="addUser()"><fa :icon="faPlus"/> {{ $t('addUser') }}</x-button>
+		</div>
+	</section>
+</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { faPlus, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faUsers, faSearch } from '@fortawesome/free-solid-svg-icons';
+import parseAcct from '../../../misc/acct/parse';
 import XButton from '../../components/ui/button.vue';
+import XInput from '../../components/ui/input.vue';
 import XPagination from '../../components/ui/pagination.vue';
 import MkUserModerateDialog from '../../components/user-moderate-dialog.vue';
 
@@ -34,6 +48,7 @@ export default Vue.extend({
 
 	components: {
 		XButton,
+		XInput,
 		XPagination,
 	},
 
@@ -44,11 +59,48 @@ export default Vue.extend({
 				limit: 10,
 				offsetMode: true
 			},
-			faPlus, faUsers
+			target: '',
+			faPlus, faUsers, faSearch
 		}
 	},
 
 	methods: {
+		/** テキストエリアのユーザーを解決する */
+		fetchUser() {
+			return new Promise((res) => {
+				const usernamePromise = this.$root.api('users/show', parseAcct(this.target));
+				const idPromise = this.$root.api('users/show', { userId: this.target });
+				let _notFound = false;
+				const notFound = () => {
+					if (_notFound) {
+						this.$root.dialog({
+							type: 'error',
+							text: this.$t('noSuchUser')
+						});
+					} else {
+						_notFound = true;
+					}
+				};
+				usernamePromise.then(res).catch(e => {
+					if (e.code === 'NO_SUCH_USER') {
+						notFound();
+					}
+				});
+				idPromise.then(res).catch(e => {
+					notFound();
+				});
+			});
+		},
+
+		/** テキストエリアから処理対象ユーザーを設定する */
+		async showUser() {
+			const user = await this.fetchUser();
+			this.$root.api('admin/show-user', { userId: user.id }).then(info => {
+				this.show(user, info);
+			});
+			this.target = '';
+		},
+
 		async addUser() {
 			const { canceled: canceled1, result: username } = await this.$root.dialog({
 				title: this.$t('username'),
@@ -99,51 +151,53 @@ export default Vue.extend({
 @import '../../theme';
 
 .mk-instance-users {
-	> .content {
-		max-height: 300px;
-		overflow: auto;
-		
-		> .users {
-			> .user {
-				display: flex;
-				width: 100%;
-				box-sizing: border-box;
-				text-align: left;
-				align-items: center;
-				padding: 8px 16px;
-				border-radius: var(--radius);
+	> .users {
+		> .content {
+			max-height: 300px;
+			overflow: auto;
+			
+			> .users {
+				> .user {
+					display: flex;
+					width: 100%;
+					box-sizing: border-box;
+					text-align: left;
+					align-items: center;
+					padding: 8px 16px;
+					border-radius: var(--radius);
 
-				@media (max-width: 500px) {
-					padding: 8px;
-				}
-
-				&:hover {
-					background: rgba(0, 0, 0, 0.03);
-
-					@media (prefers-color-scheme: dark) {
-						background: rgba(255, 255, 255, 0.03);
-					}
-				}
-
-				> * {
-					pointer-events: none;
-				}
-
-				> .avatar {
-					width: 50px;
-					height: 50px;
-				}
-
-				> .body {
-					padding: 8px;
-
-					> .name {
-						display: block;
-						font-weight: bold;
+					@media (max-width: 500px) {
+						padding: 8px;
 					}
 
-					> .acct {
-						opacity: 0.5;
+					&:hover {
+						background: rgba(0, 0, 0, 0.03);
+
+						@media (prefers-color-scheme: dark) {
+							background: rgba(255, 255, 255, 0.03);
+						}
+					}
+
+					> * {
+						pointer-events: none;
+					}
+
+					> .avatar {
+						width: 50px;
+						height: 50px;
+					}
+
+					> .body {
+						padding: 8px;
+
+						> .name {
+							display: block;
+							font-weight: bold;
+						}
+
+						> .acct {
+							opacity: 0.5;
+						}
 					}
 				}
 			}
