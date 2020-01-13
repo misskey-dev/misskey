3f18cafee9eee33f33541bfe8be2fa4df6e177af
syuilo <Syuilotan@yahoo.co.jp>
2020-01-13 23:36:07 +0900

    wip

--- src/client/pages/favorites.vue
+++ src/client/pages/favorites.vue
@@ -1,11 +1,14 @@
 <template>
 <div>
+	<portal to="icon"><fa :icon="faStar"/></portal>
+	<portal to="title">{{ $t('favorites') }}</portal>
 	<x-notes :pagination="pagination" :detail="true" :extract="items => items.map(item => item.note)" @before="before()" @after="after()"/>
 </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import Progress from '../scripts/loading';
 import XNotes from '../components/notes.vue';
 
@@ -27,7 +30,8 @@ export default Vue.extend({
 				limit: 10,
 				params: () => ({
 				})
-			}
+			},
+			faStar
 		};
 	},
 
