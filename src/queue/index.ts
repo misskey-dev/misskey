28a370079c6a0e5602b0d9a45ed767adaed0ad8a
mei23 <m@m544.net>
2020-01-19 20:27:41 +0900

    Fix #5728

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -27,9 +27,9 @@ function initializeQueue(name: string) {
 
 function renderError(e: Error): any {
 	return {
-		stack: e.stack,
-		message: e.message,
-		name: e.name
+		stack: e?.stack,
+		message: e?.message,
+		name: e?.name
 	};
 }
 
