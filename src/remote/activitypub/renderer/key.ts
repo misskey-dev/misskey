46aaf8fa9a30cbe1c1410e59e70caa9e202c4cf0
MeiMei <30769358+mei23@users.noreply.github.com>
2020-01-20 04:51:44 +0900

    AP Actorの鍵とkeyIdのフォーマットの変更 (#5733)
    * Node PKCS#8
    
    * keyIdを#main-key形式に

--- src/remote/activitypub/renderer/key.ts
+++ src/remote/activitypub/renderer/key.ts
@@ -1,10 +1,14 @@
 import config from '../../../config';
 import { ILocalUser } from '../../../models/entities/user';
 import { UserKeypair } from '../../../models/entities/user-keypair';
+import { createPublicKey } from 'crypto';
 
-export default (user: ILocalUser, key: UserKeypair) => ({
-	id: `${config.url}/users/${user.id}/publickey`,
+export default (user: ILocalUser, key: UserKeypair, postfix?: string) => ({
+	id: `${config.url}/users/${user.id}${postfix || '/publickey'}`,
 	type: 'Key',
 	owner: `${config.url}/users/${user.id}`,
-	publicKeyPem: key.publicKey
+	publicKeyPem: createPublicKey(key.publicKey).export({
+		type: 'spki',
+		format: 'pem'
+	})
 });
