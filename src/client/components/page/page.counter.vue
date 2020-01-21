dc968b5b5da7f9fb3c8e07e58948b52fe32eb43d
syuilo <Syuilotan@yahoo.co.jp>
2020-01-22 04:20:03 +0900

    wip

---
+++ src/client/components/page/page.counter.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+	<x-button class="llumlmnx" @click="click()">{{ script.interpolate(value.text) }}</x-button>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import XButton from '../ui/button.vue';
+
+export default Vue.extend({
+	components: {
+		XButton
+	},
+	props: {
+		value: {
+			required: true
+		},
+		script: {
+			required: true
+		}
+	},
+	data() {
+		return {
+			v: 0,
+		};
+	},
+	watch: {
+		v() {
+			this.script.aiScript.updatePageVar(this.value.name, this.v);
+			this.script.eval();
+		}
+	},
+	methods: {
+		click() {
+			this.v = this.v + (this.value.inc || 1);
+		}
+	}
+});
+</script>
+
+<style lang="scss" scoped>
+.llumlmnx {
+	display: inline-block;
+	min-width: 300px;
+	max-width: 450px;
+	margin: 8px 0;
+}
+</style>
