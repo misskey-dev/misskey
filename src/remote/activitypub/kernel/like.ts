30db089f1d6fe9f93f0ca0738f3644d978a869e9
mei23 <m@m544.net>
2020-02-03 17:57:12 +0900

    リモートから自身の投稿へリアクションした場合にエラーにならないように

--- src/remote/activitypub/kernel/like.ts
+++ src/remote/activitypub/kernel/like.ts
@@ -9,6 +9,8 @@ export default async (actor: IRemoteUser, activity: ILike) => {
 	const note = await fetchNote(targetUri);
 	if (!note) return `skip: target note not found ${targetUri}`;
 
+	if (actor.id === note.userId) return `skip: cannot react to my note`;
+
 	await create(actor, note, activity._misskey_reaction || activity.content || activity.name);
 	return `ok`;
 };
