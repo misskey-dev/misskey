7be6501571ec257feca2c3986b0eaa18c7f6b8b5
MeiMei <30769358+mei23@users.noreply.github.com>
2020-02-07 21:37:24 +0900

    EmojiReaction => EmojiReact (#5877)

--- src/remote/activitypub/kernel/undo/index.ts
+++ src/remote/activitypub/kernel/undo/index.ts
@@ -34,6 +34,7 @@ export default async (actor: IRemoteUser, activity: IUndo): Promise<void> => {
 			break;
 		case 'Like':
 		case 'EmojiReaction':
+		case 'EmojiReact':
 			undoLike(actor, object as ILike);
 			break;
 		case 'Announce':
