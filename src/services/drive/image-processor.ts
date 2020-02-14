0b02cd581a0c2a8cca344c78f25a200cafb3c992
mei23 <m@m544.net>
2020-02-14 11:30:32 +0900

    サムネイルをJPEGで生成するように

--- src/services/drive/image-processor.ts
+++ src/services/drive/image-processor.ts
@@ -11,7 +11,11 @@ export type IImage = {
  *   with resize, remove metadata, resolve orientation, stop animation
  */
 export async function convertToJpeg(path: string, width: number, height: number): Promise<IImage> {
-	const data = await sharp(path)
+	return convertSharpToJpeg(await sharp(path), width, height);
+}
+
+export async function convertSharpToJpeg(sharp: sharp.Sharp, width: number, height: number): Promise<IImage> {
+	const data = await sharp
 		.resize(width, height, {
 			fit: 'inside',
 			withoutEnlargement: true
@@ -35,7 +39,11 @@ export async function convertToJpeg(path: string, width: number, height: number)
  *   with resize, remove metadata, resolve orientation, stop animation
  */
 export async function convertToWebp(path: string, width: number, height: number): Promise<IImage> {
-	const data = await sharp(path)
+	return convertSharpToWebp(await sharp(path), width, height);
+}
+
+export async function convertSharpToWebp(sharp: sharp.Sharp, width: number, height: number): Promise<IImage> {
+	const data = await sharp
 		.resize(width, height, {
 			fit: 'inside',
 			withoutEnlargement: true
@@ -58,7 +66,11 @@ export async function convertToWebp(path: string, width: number, height: number)
  *   with resize, remove metadata, resolve orientation, stop animation
  */
 export async function convertToPng(path: string, width: number, height: number): Promise<IImage> {
-	const data = await sharp(path)
+	return convertSharpToPng(await sharp(path), width, height);
+}
+
+export async function convertSharpToPng(sharp: sharp.Sharp, width: number, height: number): Promise<IImage> {
+	const data = await sharp
 		.resize(width, height, {
 			fit: 'inside',
 			withoutEnlargement: true
@@ -72,4 +84,24 @@ export async function convertToPng(path: string, width: number, height: number):
 		ext: 'png',
 		type: 'image/png'
 	};
+}
+
+/**
+ * Convert to PNG or JPEG
+ *   with resize, remove metadata, resolve orientation, stop animation
+ */
+export async function convertToPngOrJpeg(path: string, width: number, height: number): Promise<IImage> {
+	return convertSharpToPngOrJpeg(await sharp(path), width, height);
+}
+
+export async function convertSharpToPngOrJpeg(sharp: sharp.Sharp, width: number, height: number): Promise<IImage> {
+	const stats = await sharp.stats();
+	const metadata = await sharp.metadata();
+
+	// 不透明で300x300pxの範囲を超えていればJPEG
+	if (stats.isOpaque && ((metadata.width && metadata.width >= 300) || (metadata.height && metadata!.height >= 300))) {
+		return await convertSharpToJpeg(sharp, width, height);
+	} else {
+		return await convertSharpToPng(sharp, width, height);
+	}
 }
