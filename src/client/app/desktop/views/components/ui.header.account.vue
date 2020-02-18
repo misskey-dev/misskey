124934db75e32867ff40cf2a7164412903076890
rinsuki <428rinsuki+git@gmail.com>
2020-02-19 02:17:04 +0900

    deckモードを切り替えた時に変なところにいかないように

--- src/client/app/desktop/views/components/ui.header.account.vue
+++ src/client/app/desktop/views/components/ui.header.account.vue
@@ -170,7 +170,7 @@ export default Vue.extend({
 		},
 		toggleDeckMode() {
 			this.$store.commit('device/set', { key: 'deckMode', value: !this.$store.state.device.inDeckMode });
-			location.replace('/');
+			location.reload();
 		},
 	}
 });
