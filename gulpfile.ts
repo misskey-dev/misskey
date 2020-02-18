39275ce0723efaa62db580b742df110f44c1417d
rinsuki <428rinsuki+git@gmail.com>
2020-02-19 00:04:59 +0900

    サーバー周りのビルドを殺す

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,10 +3,10 @@
  */
 
 import * as gulp from 'gulp';
-import * as ts from 'gulp-typescript';
-const sourcemaps = require('gulp-sourcemaps');
+// import * as ts from 'gulp-typescript';
+// const sourcemaps = require('gulp-sourcemaps');
 import tslint from 'gulp-tslint';
-const stylus = require('gulp-stylus');
+// const stylus = require('gulp-stylus');
 import * as rimraf from 'rimraf';
 import * as chalk from 'chalk';
 import * as rename from 'gulp-rename';
@@ -25,30 +25,30 @@ if (isDebug) {
 	console.warn(chalk.yellow.bold('         built script will not be compressed.'));
 }
 
-gulp.task('build:ts', () => {
-	const tsProject = ts.createProject('./tsconfig.json');
+// gulp.task('build:ts', () => {
+// 	const tsProject = ts.createProject('./tsconfig.json');
 
-	return tsProject
-		.src()
-		.pipe(sourcemaps.init())
-		.pipe(tsProject())
-		.on('error', () => {})
-		.pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../built' }))
-		.pipe(gulp.dest('./built/'));
-});
+// 	return tsProject
+// 		.src()
+// 		.pipe(sourcemaps.init())
+// 		.pipe(tsProject())
+// 		.on('error', () => {})
+// 		.pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../built' }))
+// 		.pipe(gulp.dest('./built/'));
+// });
 
-gulp.task('build:copy:views', () =>
-	gulp.src('./src/server/web/views/**/*').pipe(gulp.dest('./built/server/web/views'))
-);
+// gulp.task('build:copy:views', () =>
+// 	gulp.src('./src/server/web/views/**/*').pipe(gulp.dest('./built/server/web/views'))
+// );
 
 gulp.task('build:copy:fonts', () =>
 	gulp.src('./node_modules/three/examples/fonts/**/*').pipe(gulp.dest('./built/client/assets/fonts/'))
 );
 
-gulp.task('build:copy', gulp.parallel('build:copy:views', 'build:copy:fonts', () =>
+gulp.task('build:copy', gulp.parallel(/* 'build:copy:views', */'build:copy:fonts', () =>
 	gulp.src([
-		'./src/const.json',
-		'./src/emojilist.json',
+		// './src/const.json',
+		// './src/emojilist.json',
 		'./src/server/web/views/**/*',
 		'./src/**/assets/**/*',
 		'!./src/client/app/**/assets/**/*'
@@ -120,12 +120,12 @@ gulp.task('copy:client', () =>
 			.pipe(gulp.dest('./built/client/assets/'))
 );
 
-gulp.task('doc', () =>
-	gulp.src('./src/docs/**/*.styl')
-		.pipe(stylus())
-		.pipe(cleanCSS())
-		.pipe(gulp.dest('./built/docs/assets/'))
-);
+// gulp.task('doc', () =>
+// 	gulp.src('./src/docs/**/*.styl')
+// 		.pipe(stylus())
+// 		.pipe(cleanCSS())
+// 		.pipe(gulp.dest('./built/docs/assets/'))
+// );
 
 gulp.task('build:client', gulp.parallel(
 	'build:client:script',
@@ -134,10 +134,10 @@ gulp.task('build:client', gulp.parallel(
 ));
 
 gulp.task('build', gulp.parallel(
-	'build:ts',
+	// 'build:ts',
 	'build:copy',
 	'build:client',
-	'doc'
+	// 'doc'
 ));
 
 gulp.task('default', gulp.task('build'));
