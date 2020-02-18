4448c8f2d00283bfcb937f6e218e4129fd651d8c
rinsuki <428rinsuki+git@gmail.com>
2020-02-18 23:55:27 +0900

    force use images.weserv.nl

--- src/client/app/common/scripts/get-static-image-url.ts
+++ src/client/app/common/scripts/get-static-image-url.ts
@@ -3,9 +3,7 @@ import * as url from '../../../../prelude/url';
 
 export function getStaticImageUrl(baseUrl: string): string {
 	const u = new URL(baseUrl);
-	const dummy = `${u.host}${u.pathname}`;	// 拡張子がないとキャッシュしてくれないCDNがあるので
-	return `${instanceUrl}/proxy/${dummy}?${url.query({
-		url: u.href,
-		static: '1'
+	return `https://images.weserv.nl/${url.query({
+		url: u.href
 	})}`;
 }
