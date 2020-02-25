4501dd33765b37ead914ca41b4babb6f81d55260
syuilo <Syuilotan@yahoo.co.jp>
2020-02-26 07:45:33 +0900

    Update count-same-renotes.ts

--- src/services/count-same-renotes.ts
+++ src/services/count-same-renotes.ts
@@ -1,14 +1,14 @@
 import { Notes } from '../models';
 
 export default async function(userId: string, renoteId: string, excludeNoteId: string | undefined): Promise<number> {
-	//指定したユーザーの指定したノートのリノートがいくつあるか数える
+	// 指定したユーザーの指定したノートのリノートがいくつあるか数える
 	const query = Notes.createQueryBuilder('note')
-		.where('note.userId = :userId', {userId})
-		.andWhere('note.renoteId = :renoteId', {renoteId})
+		.where('note.userId = :userId', { userId })
+		.andWhere('note.renoteId = :renoteId', { renoteId })
 
-	//指定した投稿を除く
+	// 指定した投稿を除く
 	if (excludeNoteId) {
-		query.andWhere('note.id != :excludeNoteId', {excludeNoteId})
+		query.andWhere('note.id != :excludeNoteId', { excludeNoteId })
 	}
 
 	return await query.getCount();
