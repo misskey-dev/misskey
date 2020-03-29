d8b7ea1f9c9a5dfdd7d5306b75bb9a421aa7f823
mei23 <m@m544.net>
2020-03-30 03:24:35 +0900

    Fix: 権限設定漏れ

--- src/server/api/endpoints/admin/get-table-stats.ts
+++ src/server/api/endpoints/admin/get-table-stats.ts
@@ -2,7 +2,8 @@ import define from '../../define';
 import { getConnection } from 'typeorm';
 
 export const meta = {
-	requireCredential: false as const,
+	requireCredential: true as const,
+	requireModerator: true,
 
 	desc: {
 		'en-US': 'Get table stats'
