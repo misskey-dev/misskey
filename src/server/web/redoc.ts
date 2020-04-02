9c5fa1493b09f8e09bdd9235a5aa2be0095dc4bd
tamaina <tamaina@hotmail.co.jp>
2020-04-03 01:57:00 +0900

    mem cache

--- src/server/web/redoc.ts
+++ src/server/web/redoc.ts
@@ -2,30 +2,40 @@
  *
  * This file is based on https://github.com/Redocly/redoc/blob/v2.0.0-rc.26/cli/index.ts
  *
- * Copyright (c) 2015-present, Rebilly, Inc. 
+ * Copyright (c) 2015-present, Rebilly, Inc.
  * Released under the MIT license
  * https://github.com/Redocly/redoc/blob/master/LICENSE
- * 
+ *
  */
-import * as Koa from 'koa';
+
 import * as React from 'react';
 import { renderToString } from 'react-dom/server';
 import { ServerStyleSheet } from 'styled-components';
-import { genOpenapiSpec } from '../api/openapi/gen-spec';
-
 import { createStore, Redoc } from 'redoc';
 
-module.exports = async (ctx: Koa.Context) => {
+import { genOpenapiSpec } from '../api/openapi/gen-spec';
+
+let cache: object;
+
+module.exports = async (ctx: any) => {
+	if (cache) {
+		ctx.body = cache;
+		return;
+	}
+
+	await ctx.render('redoc', await bundle());
+	cache = ctx.body;
+};
+
+async function bundle() {
 	const store = await createStore(genOpenapiSpec(), '/api.json');
 	const sheet = new ServerStyleSheet();
-	const html = renderToString(sheet.collectStyles(React.createElement(Redoc, { store })));
-	const css = sheet.getStyleTags();
-	const redocState = sanitizeJSONString(JSON.stringify(await store.toJS()));
-
-	await ctx.render('redoc', {
-	html, css, redocState
-	});
-};
+	return {
+		html: renderToString(sheet.collectStyles(React.createElement(Redoc, { store }))),
+		css: sheet.getStyleTags(),
+		redocState: sanitizeJSONString(JSON.stringify(await store.toJS()))
+	}
+}
 
 function sanitizeJSONString(str: string) {
 	return escapeClosingScriptTag(escapeUnicode(str));
