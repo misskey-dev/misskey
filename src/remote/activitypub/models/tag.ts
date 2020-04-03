99fc77b6787bf16e772d46493067de75ea219bb7
MeiMei <30769358+mei23@users.noreply.github.com>
2020-04-03 22:51:38 +0900

    APメンションはaudienceじゃなくてtagを参照するなど (#6128)
    * APメンションはaudienceじゃなくてtagを参照するなど
    
    * AP/tag/Mentionではurlじゃなくてuriを提示する
    
    * createPersonでaliasが入力された場合に対応
    
    * AP HTMLパースでMention/Hashtag判定にtagを使うように
    
    * fix
    
    * indent
    
    * use hashtag name
    
    * fix
    
    * URLエンコード不要だったら<>を使わないの条件が消えたたのを修正

--- src/remote/activitypub/models/tag.ts
+++ src/remote/activitypub/models/tag.ts
@@ -1,26 +1,18 @@
-import { IIcon } from './icon';
-import { IIdentifier } from './identifier';
+import { toArray } from '../../../prelude/array';
+import { IObject, isHashtag, IApHashtag } from '../type';
 
-/***
- * tag (ActivityPub)
- */
-export type ITag = {
-	id: string;
-	type: string;
-	name?: string;
-	value?: string;
-	updated?: Date;
-	icon?: IIcon;
-	identifier?: IIdentifier;
-};
-
-export function extractHashtags(tags: ITag[] | null | undefined): string[] {
+export function extractApHashtags(tags: IObject | IObject[] | null | undefined) {
 	if (tags == null) return [];
 
-	const hashtags = tags.filter(tag => tag.type === 'Hashtag' && typeof tag.name == 'string');
+	const hashtags = extractApHashtagObjects(tags);
 
 	return hashtags.map(tag => {
-		const m = tag.name ? tag.name.match(/^#(.+)/) : null;
+		const m = tag.name.match(/^#(.+)/);
 		return m ? m[1] : null;
-	}).filter(x => x != null) as string[];
+	}).filter((x): x is string => x != null);
+}
+
+export function extractApHashtagObjects(tags: IObject | IObject[] | null | undefined): IApHashtag[] {
+	if (tags == null) return [];
+	return toArray(tags).filter(isHashtag);
 }
