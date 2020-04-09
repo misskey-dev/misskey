d3c0f3c251e8371d78d953f32f7311a38f4a1bdb
MeiMei <30769358+mei23@users.noreply.github.com>
2020-04-09 23:42:23 +0900

    Use node-fetch instead of request (#6228)
    * requestをnode-fetchになど
    
    * format
    
    * fix error
    
    * t
    
    * Fix test

--- src/services/drive/s3.ts
+++ src/services/drive/s3.ts
@@ -1,32 +1,17 @@
 import * as S3 from 'aws-sdk/clients/s3';
-import config from '../../config';
 import { Meta } from '../../models/entities/meta';
-import { HttpsProxyAgent } from 'https-proxy-agent';
-import * as agentkeepalive from 'agentkeepalive';
-
-const httpsAgent = config.proxy
-	? new HttpsProxyAgent(config.proxy)
-	: new agentkeepalive.HttpsAgent({
-			freeSocketTimeout: 30 * 1000
-		});
+import { httpsAgent, httpAgent } from '../../misc/fetch';
 
 export function getS3(meta: Meta) {
-	const conf = {
+	return new S3({
 		endpoint: meta.objectStorageEndpoint || undefined,
-		accessKeyId: meta.objectStorageAccessKey,
-		secretAccessKey: meta.objectStorageSecretKey,
+		accessKeyId: meta.objectStorageAccessKey!,
+		secretAccessKey: meta.objectStorageSecretKey!,
 		region: meta.objectStorageRegion || undefined,
 		sslEnabled: meta.objectStorageUseSSL,
 		s3ForcePathStyle: !!meta.objectStorageEndpoint,
 		httpOptions: {
+			agent: meta.objectStorageUseSSL ? httpsAgent : httpAgent
 		}
-	} as S3.ClientConfiguration;
-
-	if (meta.objectStorageUseSSL) {
-		conf.httpOptions!.agent = httpsAgent;
-	}
-
-	const s3 = new S3(conf);
-
-	return s3;
+	});
 }
