68abd2987b0b274f3a98ab8268151b4e4350c85a
mei23 <m@m544.net>
2020-04-12 06:26:23 +0900

    コメント

--- src/misc/fetch.ts
+++ src/misc/fetch.ts
@@ -27,25 +27,42 @@ export async function getJson(url: string, accept = 'application/json, */*', tim
 	return await res.json();
 }
 
+/**
+ * Get http non-proxy agent
+ */
 const _http = new http.Agent({
 	keepAlive: true,
 	keepAliveMsecs: 30 * 1000,
 });
 
+/**
+ * Get https non-proxy agent
+ */
 const _https = new https.Agent({
 	keepAlive: true,
 	keepAliveMsecs: 30 * 1000,
 	lookup: cache.lookup,
 });
 
+/**
+ * Get http proxy or non-proxy agent
+ */
 export const httpAgent = config.proxy
 	? new HttpProxyAgent(config.proxy)
 	: _http;
 
+/**
+ * Get https proxy or non-proxy agent
+ */
 export const httpsAgent = config.proxy
 	? new HttpsProxyAgent(config.proxy)
 	: _https;
 
+/**
+ * Get agent by URL
+ * @param url URL
+ * @param bypassProxy Allways bypass proxy
+ */
 export function getAgentByUrl(url: URL, bypassProxy = false) {
 	if (bypassProxy || (config.proxyBypassHosts || []).includes(url.hostname)) {
 		return url.protocol == 'http:' ? _http : _https;
