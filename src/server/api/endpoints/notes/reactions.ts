04600f66801727645c19fe8a04cbc12e032ac07e
Xeltica <xeltica@gmail.com>
2020-04-18 11:45:20 +0900

    カスタム絵文字リアクションがプレビューされない不具合を修正

--- src/server/api/endpoints/notes/reactions.ts
+++ src/server/api/endpoints/notes/reactions.ts
@@ -79,7 +79,9 @@ export default define(meta, async (ps, user) => {
 	} as DeepPartial<NoteReaction>;
 
 	if (ps.type) {
-		query.reaction = ps.type;
+		const suffix = '@.:';
+		const type = ps.type.endsWith(suffix) ? ps.type.slice(0, ps.type.length - suffix.length) + ':' : ps.type;
+		query.reaction = type;
 	}
 
 	const reactions = await NoteReactions.find({
