fecb12bae1291b03a7a77eb0a71f81df812e5b7b
tamaina <tamaina@hotmail.co.jp>
2020-06-04 19:13:24 +0900

    missing ;

--- src/client/components/post-form.vue
+++ src/client/components/post-form.vue
@@ -67,7 +67,7 @@ import extractMentions from '../../misc/extract-mentions';
 import getAcct from '../../misc/acct/render';
 import { formatTimeString } from '../../misc/format-time-string';
 import { selectDriveFile } from '../scripts/select-drive-file';
-import { noteVisibilities } from '../../types'
+import { noteVisibilities } from '../../types';
 
 export default Vue.extend({
 	components: {
