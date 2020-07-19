1c200c9b941b9ec873bb765011546cac7c209390
MeiMei <30769358+mei23@users.noreply.github.com>
2020-07-20 00:57:10 +0900

    Fix #6564 (#6565)

--- src/client/components/poll.vue
+++ src/client/components/poll.vue
@@ -52,9 +52,9 @@ export default Vue.extend({
 		},
 		timer(): string {
 			return this.$t(
-				this.remaining > 86400 ? '_poll.remainingDays' :
-				this.remaining > 3600 ? '_poll.remainingHours' :
-				this.remaining > 60 ? '_poll.remainingMinutes' : '_poll.remainingSeconds', {
+				this.remaining >= 86400 ? '_poll.remainingDays' :
+				this.remaining >= 3600 ? '_poll.remainingHours' :
+				this.remaining >= 60 ? '_poll.remainingMinutes' : '_poll.remainingSeconds', {
 					s: Math.floor(this.remaining % 60),
 					m: Math.floor(this.remaining / 60) % 60,
 					h: Math.floor(this.remaining / 3600) % 24,
