7e2b6b6369a5eecad2374b84527dca1a712053c9
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2020-07-24 23:50:48 +0000

    Fix bug in #6585

--- src/client/components/avatar.vue
+++ src/client/components/avatar.vue
@@ -55,6 +55,7 @@ export default Vue.extend({
 						.map(x => '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%*+,-.:;=?@[]^_{|}~'.indexOf(x))
 						.reduce((a, c) => a * 83 + c, 0)
 						.toString(16)
+						.padStart(6, '0')
 				: undefined;
 		},
 		onClick(e) {
