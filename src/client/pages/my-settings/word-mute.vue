bb45026d7be43d4952ab38ae9a4f0db587f3eef4
syuilo <syuilotan@yahoo.co.jp>
2020-07-27 09:35:53 +0900

    wip

--- src/client/pages/my-settings/word-mute.vue
+++ src/client/pages/my-settings/word-mute.vue
@@ -1,7 +1,9 @@
 <template>
 <section class="_card">
 	<div class="_title"><fa :icon="faCommentSlash"/> {{ $t('wordMute') }}</div>
-	<div class="_content">
+	<mk-tab v-model="tab" :items="[{ label: $t('soft'), value: 'soft' }, { label: $t('hard'), value: 'hard' }]"/>
+	<div class="_content" v-if="tab === 'soft'">
+		<mk-info>{{ $t('softMuteDescription') }}</mk-info>
 		<mk-textarea v-model="mutedWords">
 			<span>{{ $t('muteWords') }}</span>
 			<template #desc>{{ $t('muteWordsDescription') }}</template>
@@ -18,15 +20,20 @@ import Vue from 'vue';
 import { faCommentSlash, faSave } from '@fortawesome/free-solid-svg-icons';
 import MkButton from '../../components/ui/button.vue';
 import MkTextarea from '../../components/ui/textarea.vue';
+import MkTab from '../../components/tab.vue';
+import MkInfo from '../../components/ui/info.vue';
 
 export default Vue.extend({
 	components: {
 		MkButton,
 		MkTextarea,
+		MkTab,
+		MkInfo,
 	},
 	
 	data() {
 		return {
+			tab: 'soft',
 			mutedWords: '',
 			changed: false,
 			faCommentSlash, faSave,
