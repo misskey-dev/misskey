691b04fe6ba70d19afaaab57d45a4ca6863948ff
okpierre <1679025+okpierre@users.noreply.github.com>
2020-07-29 01:48:36 -0400

    Update index.timeline.vue
    added aria-label

--- src/client/pages/user/index.timeline.vue
+++ src/client/pages/user/index.timeline.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="kjeftjfm" v-size="[{ max: 500 }]">
 	<div class="with">
-		<button class="_button" @click="with_ = null" :class="{ active: with_ === null }">{{ $t('notes') }}</button>
-		<button class="_button" @click="with_ = 'replies'" :class="{ active: with_ === 'replies' }">{{ $t('notesAndReplies') }}</button>
-		<button class="_button" @click="with_ = 'files'" :class="{ active: with_ === 'files' }">{{ $t('withFiles') }}</button>
+		<button class="_button" @click="with_ = null" :class="{ active: with_ === null }" aria-label="$t('notes')">{{ $t('notes') }}</button>
+		<button class="_button" @click="with_ = 'replies'" :class="{ active: with_ === 'replies' }" aria-label="$t('notesAndReplies')">{{ $t('notesAndReplies') }}</button>
+		<button class="_button" @click="with_ = 'files'" :class="{ active: with_ === 'files' } aria-label="$t('withFiles')">{{ $t('withFiles') }}</button>
 	</div>
 	<x-notes ref="timeline" :pagination="pagination" @before="$emit('before')" @after="e => $emit('after', e)"/>
 </div>
