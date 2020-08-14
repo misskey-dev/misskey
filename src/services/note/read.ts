10f9131d358b40d1f6d2102ed2cbbc7e5fe97462
syuilo <syuilotan@yahoo.co.jp>
2020-08-15 04:16:47 +0900

    wip

--- src/services/note/read.ts
+++ src/services/note/read.ts
@@ -18,55 +18,63 @@ export default (
 		noteId: noteId
 	});
 
-	// v11 TODO: https://github.com/typeorm/typeorm/issues/2415
-	//if (res.affected === 0) {
-	//	return;
+	async function careEvent() {
+		const [count1, count2] = await Promise.all([
+			NoteUnreads.count({
+				userId: userId,
+				isSpecified: false
+			}),
+			NoteUnreads.count({
+				userId: userId,
+				isSpecified: true
+			})
+		]);
+
+		if (count1 === 0) {
+			// 全て既読になったイベントを発行
+			publishMainStream(userId, 'readAllUnreadMentions');
+		}
+
+		if (count2 === 0) {
+			// 全て既読になったイベントを発行
+			publishMainStream(userId, 'readAllUnreadSpecifiedNotes');
+		}
+	}
+
+	// TODO: https://github.com/typeorm/typeorm/issues/2415
+	//if (res.affected > 0) {
+	//	careEvent();
 	//}
 
-	const [count1, count2] = await Promise.all([
-		NoteUnreads.count({
-			userId: userId,
-			isSpecified: false
-		}),
-		NoteUnreads.count({
-			userId: userId,
-			isSpecified: true
-		})
-	]);
+	careEvent();
 
-	if (count1 === 0) {
-		// 全て既読になったイベントを発行
-		publishMainStream(userId, 'readAllUnreadMentions');
+	async function careAntenna() {
+		const antennas = await Antennas.find({ userId });
+
+		await Promise.all(antennas.map(async antenna => {
+			await AntennaNotes.update({
+				antennaId: antenna.id,
+				noteId: noteId
+			}, {
+				read: true
+			});
+
+			const count = await AntennaNotes.count({
+				antennaId: antenna.id,
+				read: false
+			});
+
+			if (count === 0) {
+				publishMainStream(userId, 'readAntenna', antenna);
+			}
+		}));
+
+		Users.getHasUnreadAntenna(userId).then(unread => {
+			if (!unread) {
+				publishMainStream(userId, 'readAllAntennas');
+			}
+		});
 	}
 
-	if (count2 === 0) {
-		// 全て既読になったイベントを発行
-		publishMainStream(userId, 'readAllUnreadSpecifiedNotes');
-	}
-
-	const antennas = await Antennas.find({ userId });
-
-	await Promise.all(antennas.map(async antenna => {
-		await AntennaNotes.update({
-			antennaId: antenna.id,
-			noteId: noteId
-		}, {
-			read: true
-		});
-
-		const count = await AntennaNotes.count({
-			antennaId: antenna.id,
-			read: false
-		});
-
-		if (count === 0) {
-			publishMainStream(userId, 'readAntenna', antenna);
-		}
-	}));
-
-	Users.getHasUnreadAntenna(userId).then(unread => {
-		if (!unread) {
-			publishMainStream(userId, 'readAllAntennas');
-		}
-	});
+	careAntenna();
 });
