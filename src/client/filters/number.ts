940f6952201a8db3f23b91d30fb8ad46b6a87922
syuilo <syuilotan@yahoo.co.jp>
2020-09-22 12:22:16 +0900

    lint

--- src/client/filters/number.ts
+++ src/client/filters/number.ts
@@ -1,1 +1,1 @@
-export default n => n == null ? 'N/A' : n.toLocaleString()
+export default n => n == null ? 'N/A' : n.toLocaleString();
--- src/client/filters/user.ts
+++ src/client/filters/user.ts
@@ -4,12 +4,12 @@ import { url } from '@/config';
 
 export const acct = user => {
 	return getAcct(user);
-}
+};
 
 export const userName = user => {
 	return getUserName(user);
-}
+};
 
 export const userPage = (user, path?, absolute = false) => {
 	return `${absolute ? url : ''}/@${acct(user)}${(path ? `/${path}` : '')}`;
-}
+};
