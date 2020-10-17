32f82c2a91acdb62573ff890669b6373b79411fc
mei23 <m@m544.net>
2020-10-18 00:29:58 +0900

    Fix lint

--- src/client/@types/vuex-shim.d.ts
+++ src/client/@types/vuex-shim.d.ts
@@ -2,10 +2,12 @@ import { ComponentCustomProperties } from 'vue';
 import { Store } from 'vuex';
 
 declare module '@vue/runtime-core' {
+
+	// tslint:disable-next-line:no-empty-interface
 	interface State {
 	}
 
 	interface ComponentCustomProperties {
-		$store: Store<State>
+		$store: Store<State>;
 	}
 }
