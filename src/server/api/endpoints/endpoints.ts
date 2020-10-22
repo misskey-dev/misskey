73b5a49c86fae7213bca5909c7129d4d607796ed
YuzuRyo61 <yuzuryo61@yuzulia.work>
2020-10-22 18:32:38 +0900

    Update api document

--- src/server/api/endpoints/endpoints.ts
+++ src/server/api/endpoints/endpoints.ts
@@ -2,12 +2,32 @@ import define from '../define';
 import endpoints from '../endpoints';
 
 export const meta = {
+	desc: {
+		'ja-JP': '使用できるAPI一覧を返します。',
+		'en-US': 'Returns a list of available APIs.'
+	},
+
 	requireCredential: false as const,
 
 	tags: ['meta'],
 
 	params: {
 	},
+
+	res: {
+		type: 'array' as const,
+		optional: false as const, nullable: false as const,
+		items: {
+			type: 'string' as const,
+			optional: false as const, nullable: false as const
+		},
+		example: [
+			'admin/abuse-user-reports',
+			'admin/accounts/create',
+			'admin/announcements/create',
+			'...'
+		]
+	}
 };
 
 export default define(meta, async () => {
