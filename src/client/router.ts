8495e37566094ed7128bf7df24dabdb51d3bebcf
syuilo <syuilotan@yahoo.co.jp>
2020-11-09 22:31:50 +0900

    Fix router

--- src/client/router.ts
+++ src/client/router.ts
@@ -73,6 +73,7 @@ export const router = createRouter({
 		{ path: '/games/reversi', component: page('reversi/index') },
 		{ path: '/games/reversi/:gameId', component: page('reversi/game'), props: route => ({ gameId: route.params.gameId }) },
 		{ path: '/mfm-cheat-sheet', component: page('mfm-cheat-sheet') },
+		{ path: '/api-console', component: page('api-console') },
 		{ path: '/test', component: page('test') },
 		{ path: '/auth/:token', component: page('auth') },
 		{ path: '/miauth/:session', component: page('miauth') },
