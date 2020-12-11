69c3c4e3dc71c722f9d85c4d2d6a112b6ce85296
syuilo <Syuilotan@yahoo.co.jp>
2020-12-11 21:16:20 +0900

    Resolve #6806 (#6935)
    * :v:
    
    * :v:
    
    * Update privacy.vue

--- src/remote/activitypub/type.ts
+++ src/remote/activitypub/type.ts
@@ -135,6 +135,7 @@ export interface IPerson extends IObject {
 	name?: string;
 	preferredUsername?: string;
 	manuallyApprovesFollowers?: boolean;
+	discoverable?: boolean;
 	inbox?: string;
 	sharedInbox?: string;	// 後方互換性のため
 	publicKey: {
