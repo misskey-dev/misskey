e6495ea6e2feeb163efd25c9fec3e1bbc0e8f15f
syuilo <syuilotan@yahoo.co.jp>
2020-12-11 21:52:57 +0900

    Update vue.d.ts

--- src/client/@types/vue.d.ts
+++ src/client/@types/vue.d.ts
@@ -1,5 +1,5 @@
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+	import type { DefineComponent } from 'vue'
+	const component: DefineComponent<{}, {}, any>
+	export default component
 }
