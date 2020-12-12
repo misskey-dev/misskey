49b3ee36bdc3b46c985902a10c081de19ec8dc98
Xeltica <7106976+Xeltica@users.noreply.github.com>
2020-12-12 23:24:50 +0900

    Update vetur.config.js (#6975)

--- vetur.config.js
+++ vetur.config.js
@@ -20,11 +20,11 @@ module.exports = {
       // Where is `package.json` in the project?
       // We use it to determine the version of vue.
       // It is relative to root property.
-      package: './package.json',
+      package: '../../package.json',
       // **optional**
       // Where is TypeScript config file in the project?
       // It is relative to root property.
-      tsconfig: './src/client/tsconfig.json',
+      tsconfig: './tsconfig.json',
       // **optional** default: `'./.vscode/vetur/snippets'`
       // Where is vetur custom snippets folders?
       snippetFolder: './.vscode/vetur/snippets',
