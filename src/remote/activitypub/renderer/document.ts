973c65a980d960af85a19be80b892c607b9d032c
Austin Lanari <austin@jumanji.io>
2020-12-23 19:57:53 -0800

    send name as file comment or file name on document

--- src/remote/activitypub/renderer/document.ts
+++ src/remote/activitypub/renderer/document.ts
@@ -4,5 +4,6 @@ import { DriveFiles } from '../../../models';
 export default (file: DriveFile) => ({
 	type: 'Document',
 	mediaType: file.type,
+	name: file.comment || file.name,
 	url: DriveFiles.getPublicUrl(file)
 });
