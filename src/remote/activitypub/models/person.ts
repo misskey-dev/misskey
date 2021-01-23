5cf4ebeec35ceb755d6a6572dbbfef48080e72fb
InspectorCaracal <51038201+InspectorCaracal@users.noreply.github.com>
2021-01-22 22:08:52 -0700

    create/update person consistency

--- src/remote/activitypub/models/person.ts
+++ src/remote/activitypub/models/person.ts
@@ -331,7 +331,7 @@ export async function updatePerson(uri: string, resolver?: Resolver | null, hint
 		lastFetchedAt: new Date(),
 		inbox: person.inbox,
 		sharedInbox: person.sharedInbox || (person.endpoints ? person.endpoints.sharedInbox : undefined),
-		followersUri: person.followers,
+		followersUri: person.followers ? getApId(person.followers) : undefined,
 		featured: person.featured,
 		emojis: emojiNames,
 		name: person.name,
