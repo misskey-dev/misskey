4522c752ee95aa943d72365cb6ebd80cc1df35c8
tamaina <tamaina@hotmail.co.jp>
2021-02-21 14:45:20 +0900

    fix

--- src/client/scripts/unison-reload.ts
+++ src/client/scripts/unison-reload.ts
@@ -4,12 +4,12 @@ import { BroadcastChannel } from 'broadcast-channel';
 export const reloadChannel = new BroadcastChannel<boolean>('reload');
 
 // BroadcastChannelを用いて、クライアントが一斉にreloadするようにします。
-export function unisonReload(redirectToRoot?: boolean) {
+export function unisonReload(redirectToRoot: boolean = false) {
 	reloadChannel.postMessage(!!redirectToRoot);
 	reload();
 }
 
-export function reload(redirectToRoot?: boolean) {
+export function reload(redirectToRoot: boolean = false) {
 	if (redirectToRoot) {
 		location.href = '/';
 	} else {
