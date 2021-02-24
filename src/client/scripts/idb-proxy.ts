a7180b32621c132264dd147a36e97fcb1af15dfc
tamaina <tamaina@hotmail.co.jp>
2021-02-24 19:00:01 +0900

    ok

--- src/client/scripts/idb-proxy.ts
+++ src/client/scripts/idb-proxy.ts
@@ -1,23 +1,23 @@
 // FirefoxのプライベートモードなどではindexedDBが使用不可能なので、使う
 import {
-    get as iget,
-    set as iset,
-    del as idel
+	get as iget,
+	set as iset,
+	del as idel
 } from 'idb-keyval';
 
-const fallbackName = (key: string) => `idbfallback::${key}`; 
+const fallbackName = (key: string) => `idbfallback::${key}`;
 
 export async function get(key: string) {
-    if (window.indexedDB) return iget(key);
-    return JSON.parse(localStorage.getItem(fallbackName(key)) || 'null');
+	if (window.indexedDB) return iget(key);
+	return JSON.parse(localStorage.getItem(fallbackName(key)));
 }
 
 export async function set(key: string, val: any) {
-    if (window.indexedDB) return iset(key, val);
-    return localStorage.setItem(fallbackName(key), JSON.stringify(val));
+	if (window.indexedDB) return iset(key, val);
+	return localStorage.setItem(fallbackName(key), JSON.stringify(val));
 }
 
 export async function del(key: string) {
-    if (window.indexedDB) return idel(key);
-    return localStorage.removeItem(fallbackName(key));
+	if (window.indexedDB) return idel(key);
+	return localStorage.removeItem(fallbackName(key));
 }
