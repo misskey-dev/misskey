c88d7a10878ab022a1d4b840a9ee5ecdbe79dcf1
tamaina <tamaina@hotmail.co.jp>
2021-02-24 19:29:35 +0900

    SWはプロキシ不要

--- src/client/sw/notification-read.ts
+++ src/client/sw/notification-read.ts
@@ -1,6 +1,6 @@
 declare var self: ServiceWorkerGlobalScope;
 
-import { get } from '@/scripts/idb-proxy';
+import { get } from 'idb-keyval';
 import { pushNotificationData } from '../../types';
 import { api } from './operations';
 
