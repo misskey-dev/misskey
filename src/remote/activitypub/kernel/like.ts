e967283cd1b1516b1e6d2eda53af8694e70af4b2
mei23 <m@m544.net>
2021-04-04 22:52:08 +0900

    Fix #7431

--- src/remote/activitypub/kernel/like.ts
+++ src/remote/activitypub/kernel/like.ts
@@ -11,6 +11,13 @@ export default async (actor: IRemoteUser, activity: ILike) => {
 
 	await extractEmojis(activity.tag || [], actor.host).catch(() => null);
 
-	await create(actor, note, activity._misskey_reaction || activity.content || activity.name);
+	await create(actor, note, activity._misskey_reaction || activity.content || activity.name).catch(e => {
+		if (e.id === '51c42bb4-931a-456b-bff7-e5a8a70dd298') {
+			return 'skip: already reacted';
+		} else {
+			throw e;
+		}
+	});
+
 	return `ok`;
 };
