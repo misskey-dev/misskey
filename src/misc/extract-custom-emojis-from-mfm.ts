ac3bac111c9ae20f5a2fa1ee64ebc57c42adc0ae
marihachi <marihachi0620@gmail.com>
2021-04-10 16:02:22 +0900

    refactor extractCustomEmojisFromMfm()

--- src/misc/extract-custom-emojis-from-mfm.ts
+++ src/misc/extract-custom-emojis-from-mfm.ts
@@ -2,17 +2,13 @@ import * as mfm from 'mfm-js';
 import { unique } from '@/prelude/array';
 
 export function extractCustomEmojisFromMfm(nodes: mfm.MfmNode[]): string[] {
-	const emojiNodes = [] as mfm.MfmEmojiCode[];
+	const emojis = [] as string[];
 
-	function scan(nodes: mfm.MfmNode[]) {
-		for (const node of nodes) {
-			if (node.type === 'emojiCode') emojiNodes.push(node);
-			else if (node.children) scan(node.children);
+	mfm.inspect(nodes, (node) => {
+		if (node.type === 'emojiCode' && node.props.name.length <= 100) {
+			emojis.push(node.props.name);
 		}
-	}
+	});
 
-	scan(nodes);
-
-	const emojis = emojiNodes.filter(x => x.props.name.length <= 100).map(x => x.props.name!);
 	return unique(emojis);
 }
