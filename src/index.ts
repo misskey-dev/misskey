c7bfdea17bd557935a71758136633c51c5720d3a
marihachi <marihachi0620@gmail.com>
2021-05-16 11:12:29 +0900

    misc (#10)

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { APIClient } from './api';
+import { Endpoints } from './endpoints';
+import Stream from './streaming';
+import { Acct } from './acct';
+
+export {
+	APIClient,
+	Endpoints,
+	Stream,
+	Acct
+};
