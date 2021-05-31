5e6e1084cfebeab9fc9a17f1f9c75bee9de10ddc
syuilo <Syuilotan@yahoo.co.jp>
2021-05-31 23:03:08 +0900

    Update test

--- test-d/api.ts
+++ test-d/api.ts
@@ -2,21 +2,28 @@ import { expectType } from 'tsd';
 import * as Misskey from '../src';
 
 describe('API', () => {
-	describe('request', () => {
-		test('success', async () => {
-			const res = await Misskey.api.request('https://misskey.test', 'meta', { detail: true }, 'TOKEN');
-			expectType<Misskey.entities.InstanceMetadata>(res);
+	test('success', async () => {
+		const cli = new Misskey.api.APIClient({
+			origin: 'https://misskey.test',
+			credential: 'TOKEN'
 		});
+		const res = await cli.request('meta', { detail: true });
+		expectType<Misskey.entities.InstanceMetadata>(res);
 	});
 
-	describe('APIClient', () => {
-		test('success', async () => {
-			const cli = new Misskey.api.APIClient({
-				origin: 'https://misskey.test'
-			});
-			cli.i = { token: 'TOKEN' };
-			const res = await cli.request('meta', { detail: true });
-			expectType<Misskey.entities.InstanceMetadata>(res);
+	test('conditional respose type', async () => {
+		const cli = new Misskey.api.APIClient({
+			origin: 'https://misskey.test',
+			credential: 'TOKEN'
 		});
+
+		const res = await cli.request('meta', { detail: true });
+		expectType<Misskey.entities.DetailedInstanceMetadata>(res);
+
+		const res2 = await cli.request('meta', { detail: false });
+		expectType<Misskey.entities.LiteInstanceMetadata>(res2);
+
+		const res3 = await cli.request('meta', { });
+		expectType<Misskey.entities.LiteInstanceMetadata>(res3);
 	});
 });
