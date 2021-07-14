f67cfc2504552b76c3b66b35eb30c3e1a58011fb
mei23 <m@m544.net>
2021-07-14 10:59:31 +0900

    Revert TODO

--- src/server/api/streaming.ts
+++ src/server/api/streaming.ts
@@ -17,6 +17,9 @@ module.exports = (server: http.Server) => {
 	ws.on('request', async (request) => {
 		const q = request.resourceURL.query as ParsedUrlQuery;
 
+		// TODO: トークンが間違ってるなどしてauthenticateに失敗したら
+		// コネクション切断するなりエラーメッセージ返すなりする
+		// (現状はエラーがキャッチされておらずサーバーのログに流れて邪魔なので)
 		const [user, app] = await authenticate(q.i as string);
 
 		if (user?.isSuspended) {
