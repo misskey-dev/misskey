f1c9e596e8454742f15c89077755505f1bd6c368
syuilo <Syuilotan@yahoo.co.jp>
2021-07-19 15:11:28 +0900

    アイコンを四角で表示するオプション

--- src/client/store.ts
+++ src/client/store.ts
@@ -194,6 +194,10 @@ export const defaultStore = markRaw(new Storage('base', {
 		where: 'device',
 		default: false
 	},
+	squareAvatars: {
+		where: 'device',
+		default: false
+	},
 }));
 
 // TODO: 他のタブと永続化されたstateを同期
