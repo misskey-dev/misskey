c96b2767b92394da3471ce489c387d0599ae7e31
syuilo <Syuilotan@yahoo.co.jp>
2021-08-15 21:44:00 +0900

    fix typo

--- src/services/chart/core.ts
+++ src/services/chart/core.ts
@@ -203,7 +203,7 @@ export default abstract class Chart<T extends Record<string, any>> {
 			indices: [{
 				columns: ['date', 'group'],
 				unique: true,
-			}, { // groupにnullが含まれると↑のuniqeuは機能しないので↓の部分インデックスでカバー
+			}, { // groupにnullが含まれると↑のuniqueは機能しないので↓の部分インデックスでカバー
 				columns: ['date'],
 				unique: true,
 				where: '"group" IS NULL'
