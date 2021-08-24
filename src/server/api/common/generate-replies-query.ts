f976f25ca1a09096298930897d4fb2ed384ef436
puffaboo <grinding@graduate.org>
2021-08-24 18:43:50 +0100

    added back type parameters for User

--- src/server/api/common/generate-replies-query.ts
+++ src/server/api/common/generate-replies-query.ts
@@ -1,10 +1,8 @@
 import { User } from '@/models/entities/user';
 import { Brackets, SelectQueryBuilder } from 'typeorm';
 
-export function generateRepliesQuery(q: SelectQueryBuilder<any>, me: any) {
-	const userId = me['id'] ?? null;
-	const showReplies: boolean = me['showTimelineReplies'] ?? false;
-	if (userId == null) {
+export function generateRepliesQuery(q: SelectQueryBuilder<any>, me?: Pick<User, 'id' | 'showTimelineReplies'> | null) {
+	if (me == null) {
 		q.andWhere(new Brackets(qb => { qb
 			.where(`note.replyId IS NULL`) // 返信ではない
 			.orWhere(new Brackets(qb => { qb // 返信だけど投稿者自身への返信
@@ -12,13 +10,13 @@ export function generateRepliesQuery(q: SelectQueryBuilder<any>, me: any) {
 				.andWhere('note.replyUserId = note.userId');
 			}));
 		}));
-	} else if (!showReplies) {
+	} else if (!me.showTimelineReplies) {
 		q.andWhere(new Brackets(qb => { qb
 			.where(`note.replyId IS NULL`) // 返信ではない
-			.orWhere('note.replyUserId = :meId', { meId: userId }) // 返信だけど自分のノートへの返信
+			.orWhere('note.replyUserId = :meId', { meId: me.id }) // 返信だけど自分のノートへの返信
 			.orWhere(new Brackets(qb => { qb // 返信だけど自分の行った返信
 				.where(`note.replyId IS NOT NULL`)
-				.andWhere('note.userId = :meId', { meId: userId });
+				.andWhere('note.userId = :meId', { meId: me.id });
 			}))
 			.orWhere(new Brackets(qb => { qb // 返信だけど投稿者自身への返信
 				.where(`note.replyId IS NOT NULL`)
