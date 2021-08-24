b2b150247ec77130a0f0d9d7215a771b2bded214
syuilo <Syuilotan@yahoo.co.jp>
2021-08-24 20:59:32 +0900

    lint

--- src/client/scripts/live2d/face-point.ts
+++ src/client/scripts/live2d/face-point.ts
@@ -1,9 +1,9 @@
 export class FacePoint {
-  public angleX: number = 0
-  public angleY: number = 0
-  public angleZ: number = 0
-  public angleEyeX: number = 0
-  public angleEyeY: number = 0
-  public mouseDistance: number = 0
-  public autoBlink: boolean = false
+	public angleX = 0;
+	public angleY = 0;
+	public angleZ = 0;
+	public angleEyeX = 0;
+	public angleEyeY = 0;
+	public mouseDistance = 0;
+	public autoBlink = false;
 }
