b473146b328b9837220a1b251f6a6fae464bb1d9
syuilo <Syuilotan@yahoo.co.jp>
2021-09-04 10:57:18 +0900

    remove live2d stuff

--- src/client/scripts/live2d/math-util.ts
+++
@@ -1,9 +0,0 @@
-export function getDistance(x1: number, y1: number, x2: number, y2: number) {
-	return Math.sqrt(
-		Math.pow(x2 - x1, 2) +
-		Math.pow(y2 - y1, 2)
-	);
-}
-export function getAngle(x1: number, y1: number, x2: number, y2: number) {
-	return Math.atan2(y2 - y1, x2 - x1); // * 180 / Math.PI
-}
