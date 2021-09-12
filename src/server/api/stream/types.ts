9f4ad3c8730bd8350321f63a2a5b494ac7ee7af2
tamaina <tamaina@hotmail.co.jp>
2021-09-12 23:38:12 +0900

    fix lint

--- src/server/api/stream/types.ts
+++ src/server/api/stream/types.ts
@@ -19,7 +19,7 @@ import { Packed } from '@/misc/schema';
 
 // 辞書(interface or type)から{ type, body }ユニオンを定義
 // https://stackoverflow.com/questions/49311989/can-i-infer-the-type-of-a-value-using-extends-keyof-type
-type EventUnions<T extends object> = { [K in keyof T]: { type: K; body: T[K]; } }
+type EventUnions<T extends object> = { [K in keyof T]: { type: K; body: T[K]; } };
 type EventUnionFromDictionary<
 	T extends object,
 	U = EventUnions<T>
