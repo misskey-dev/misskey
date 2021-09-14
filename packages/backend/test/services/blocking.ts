d766fe25a2ddabb53485266bc9aaa9a54e69865b
FloatingGhost <hannah@coffee-and-dreams.uk>
2021-09-14 19:33:35 +0100

    Add unit tests for block & unblock federation

---
+++ packages/backend/test/services/blocking.ts
@@ -0,0 +1,60 @@
+process.env.NODE_ENV = 'test';
+
+import * as assert from 'assert';
+import * as childProcess from 'child_process';
+import { async, signup, request, post, startServer, shutdownServer, initTestDb } from '../utils';
+import * as sinon from 'sinon';
+import config from '@/config/index';
+
+describe('Creating a block activity', () => {
+	let p: childProcess.ChildProcess;
+
+	// alice blocks bob
+	let alice: any;
+	let bob: any;
+	let carol: any;
+
+	before(async () => {
+		await initTestDb();
+		p = await startServer();
+		alice = await signup({ username: 'alice' });
+		bob = await signup({ username: 'bob' });
+		carol = await signup({ username: 'carol' });
+		bob.host = 'http://remote';
+		carol.host = 'http://remote';
+	});
+
+	beforeEach(() => {
+		sinon.restore();
+	});
+
+	after(async () => {
+		await shutdownServer(p);
+	});
+
+	it('Should federate blocks normally', async(async () => {
+		const createBlock = (await import('../../src/services/blocking/create')).default;
+		const deleteBlock = (await import('../../src/services/blocking/delete')).default;
+
+		const queues = await import('../../src/queue/index');
+		const spy = sinon.spy(queues, 'deliver');
+		await createBlock(alice, bob);
+		assert(spy.calledOnce);
+		await deleteBlock(alice, bob);
+		assert(spy.calledTwice);
+	}));
+
+	it('Should not federate blocks if activityPub.federateBlocks is false', async () => {
+		const createBlock = (await import('../../src/services/blocking/create'))  .default;
+		const deleteBlock = (await import('../../src/services/blocking/delete')).default;
+
+		const queues = await import('../../src/queue/index');
+		config.activityPub = {
+			federateBlocks: false
+		};
+		const spy = sinon.spy(queues, 'deliver');
+		await createBlock(alice, carol);
+		await deleteBlock(alice, carol);
+		assert(spy.notCalled);
+	});
+});
