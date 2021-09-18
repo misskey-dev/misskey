80e8c620c6cd8ebf65769fb51ba30250488a4cfb
syuilo <Syuilotan@yahoo.co.jp>
2021-09-18 23:16:22 +0900

    Update basic.js

--- cypress/integration/basic.js
+++ cypress/integration/basic.js
@@ -1,8 +1,16 @@
 describe('Basic', () => {
 	beforeEach(() => {
 		cy.request('POST', '/api/reset-db').as('reset');
-		cy.get('@reset').its('status').should('equal', 204)
+		cy.get('@reset').its('status').should('equal', 204);
+		cy.clearLocalStorage();
+		cy.clearCookies();
 		cy.reload(true);
+	});
+
+	afterEach(() => {
+		// テスト終了直前にページ遷移するようなテストケース(例えばアカウント作成)だと、たぶんCypressのバグでブラウザの内容が次のテストケースに引き継がれてしまう(例えばアカウントが作成し終わった段階からテストが始まる)。
+		// waitを入れることでそれを防止できる
+		cy.wait(1000);
 	});
 
   it('successfully loads', () => {
@@ -12,14 +20,20 @@ describe('Basic', () => {
 	it('setup instance', () => {
     cy.visit('/');
 
+		cy.intercept('POST', '/api/admin/accounts/create').as('signup');
+	
 		cy.get('[data-cy-admin-username] input').type('admin');
 		cy.get('[data-cy-admin-password] input').type('admin1234');
 		cy.get('[data-cy-admin-ok]').click();
+
+		// なぜか動かない
+		//cy.wait('@signup').should('have.property', 'response.statusCode');
+		cy.wait('@signup');
   });
 
 	it('signup', () => {
 		// インスタンス初期セットアップ
-		cy.request('POST', '/api/signup', {
+		cy.request('POST', '/api/admin/accounts/create', {
 			username: 'admin',
 			password: 'pass',
 		}).as('setup');
@@ -27,54 +41,87 @@ describe('Basic', () => {
 		cy.get('@setup').then(() => {
 			cy.visit('/');
 
+			cy.intercept('POST', '/api/signup').as('signup');
+
 			cy.get('[data-cy-signup]').click();
 			cy.get('[data-cy-signup-username] input').type('alice');
 			cy.get('[data-cy-signup-password] input').type('alice1234');
 			cy.get('[data-cy-signup-password-retype] input').type('alice1234');
 			cy.get('[data-cy-signup-submit]').click();
+
+			cy.wait('@signup');
 		});
   });
 
 	it('signin', () => {
 		// インスタンス初期セットアップ
-		cy.request('POST', '/api/signup', {
+		cy.request('POST', '/api/admin/accounts/create', {
 			username: 'admin',
 			password: 'pass',
+		}).as('setup');
+
+		cy.get('@setup').then(() => {
+			// ユーザー作成
+			cy.request('POST', '/api/signup', {
+				username: 'alice',
+				password: 'alice1234',
+			}).as('signup');
 		});
 
-		// ユーザー作成
-		cy.request('POST', '/api/signup', {
-			username: 'alice',
-			password: 'alice1234',
+		cy.get('@signup').then(() => {
+			cy.visit('/');
+
+			cy.intercept('POST', '/api/signin').as('signin');
+
+			cy.get('[data-cy-signin]').click();
+			cy.get('[data-cy-signin-username] input').type('alice');
+			// Enterキーでサインインできるかの確認も兼ねる
+			cy.get('[data-cy-signin-password] input').type('alice1234{enter}');
+
+			cy.wait('@signin');
 		});
-
-    cy.visit('/');
-
-		cy.get('[data-cy-signin]').click();
-		cy.get('[data-cy-signin-username] input').type('alice');
-		// Enterキーでサインインできるかの確認も兼ねる
-		cy.get('[data-cy-signin-password] input').type('alice1234{enter}');
   });
 
 	it('note', () => {
     cy.visit('/');
 
-		//#region TODO: この辺はUI操作ではなくAPI操作でログインする
-		cy.get('[data-cy-signin]').click();
-		cy.get('[data-cy-signin-username] input').type('alice');
-		// Enterキーでサインインできるかの確認も兼ねる
-		cy.get('[data-cy-signin-password] input').type('alice1234{enter}');
-		//#endregion
+		// インスタンス初期セットアップ
+		cy.request('POST', '/api/admin/accounts/create', {
+			username: 'admin',
+			password: 'pass',
+		}).as('setup');
 
-		cy.get('[data-cy-open-post-form]').click();
-		cy.get('[data-cy-post-form-text]').type('Hello, Misskey!');
-		cy.get('[data-cy-open-post-form-submit]').click();
+		cy.get('@setup').then(() => {
+			// ユーザー作成
+			cy.request('POST', '/api/signup', {
+				username: 'alice',
+				password: 'alice1234',
+			}).as('signup');
+		});
 
-		// TODO: 投稿した文字列が画面内にあるか(=タイムラインに流れてきたか)のテスト
+		cy.get('@signup').then(() => {
+			cy.visit('/');
+
+			cy.intercept('POST', '/api/signin').as('signin');
+
+			cy.get('[data-cy-signin]').click();
+			cy.get('[data-cy-signin-username] input').type('alice');
+			cy.get('[data-cy-signin-password] input').type('alice1234{enter}');
+
+			cy.wait('@signin').as('signinEnd');
+		});
+
+		cy.get('@signinEnd').then(() => {
+			cy.get('[data-cy-open-post-form]').click();
+			cy.get('[data-cy-post-form-text]').type('Hello, Misskey!');
+			cy.get('[data-cy-open-post-form-submit]').click();
+
+			cy.contains('Hello, Misskey!');
+		});
   });
 
 	it('suspend', function() {
-		cy.request('POST', '/api/signup', {
+		cy.request('POST', '/api/admin/accounts/create', {
 			username: 'admin',
 			password: 'pass',
 		}).its('body').as('admin');
