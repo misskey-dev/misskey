ce32cd576be1496eba4dc2eec804e1059d99457d
Johann150 <johann.galle@protonmail.com>
2021-09-23 15:32:16 +0200

    fix inboxQueue import (#7829)

--- src/server/api/endpoints/admin/queue/inbox-delayed.ts
+++ src/server/api/endpoints/admin/queue/inbox-delayed.ts
@@ -1,6 +1,6 @@
 import { URL } from 'url';
 import define from '../../../define';
-import { inboxQueue } from '@/queue/index';
+import { inboxQueue } from '@/queue/queues';
 
 export const meta = {
 	tags: ['admin'],
