772da0ee52fd7fe90e215d36efd870db3bab8cdb
tamaina <tamaina@hotmail.co.jp>
2021-10-19 23:16:34 +0900

    readableRead

--- src/misc/detect-url-mime.ts
+++ src/misc/detect-url-mime.ts
@@ -1,7 +1,8 @@
 import { getUrl } from './download-url';
 import { detectType } from './get-file-info';
+import { readableRead } from './stream/read';
 
 export async function detectUrlMime(url: string) {
-	const { mime } = await detectType(getUrl(url));
+	const { mime } = await detectType(readableRead(getUrl(url)));
 	return mime;
 }
