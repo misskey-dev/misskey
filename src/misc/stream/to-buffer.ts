f6f2a20ea1894c10474488cafe1b6c427a1de9b2
tamaina <tamaina@hotmail.co.jp>
2021-10-20 18:42:20 +0900

    wip

---
+++ src/misc/stream/to-buffer.ts
@@ -0,0 +1,23 @@
+import { Readable } from 'stream';
+
+export async function toBuffer(readable: Readable) {
+	const chunks = await toBufferArray(readable);
+	return Buffer.concat(chunks);
+}
+
+export function toBufferArray(readable: Readable) {
+	return new Promise<Uint8Array[]>((resolve, reject) => {
+		const timeout = setTimeout(() => {
+			reject('STREAM TIMEOUT');
+		}, 5000);
+
+		const chunks: Uint8Array[] = [];
+		readable
+			.on('data', chunk => chunks.push(chunk))
+			.on('end', () => {
+				clearTimeout(timeout);
+				resolve(chunks);	
+			})
+			.on('error', reject);
+	});
+}
