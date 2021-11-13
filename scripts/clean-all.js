65740da979773eb6ae88b9e0fae8ff6c8c65c2e9
syuilo <Syuilotan@yahoo.co.jp>
2021-11-13 13:01:22 +0900

    npm scriptの互換性を修正

--- scripts/clean-all.js
+++ scripts/clean-all.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 
 (async () => {
 	fs.rmSync(__dirname + '/../packages/backend/built', { recursive: true, force: true });
+	fs.rmSync(__dirname + '/../packages/backend/node_modules', { recursive: true, force: true });
+
 	fs.rmSync(__dirname + '/../packages/client/built', { recursive: true, force: true });
+	fs.rmSync(__dirname + '/../packages/client/node_modules', { recursive: true, force: true });
+
 	fs.rmSync(__dirname + '/../built', { recursive: true, force: true });
+	fs.rmSync(__dirname + '/../node_modules', { recursive: true, force: true });
 })();
