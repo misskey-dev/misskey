2bf63b158c06e6426370183e8589fe76bcbdc6bb
syuilo <Syuilotan@yahoo.co.jp>
2021-12-11 22:31:57 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,28 @@ module.exports = {
 	rules: {
 		'indent': ['error', 'tab', {
 			'SwitchCase': 1,
+			'MemberExpression': 'off',
 			'flatTernaryExpressions': true,
+			'ArrayExpression': 'first',
+			'ObjectExpression': 'first',
 		}],
 		'eol-last': ['error', 'always'],
 		'semi': ['error', 'always'],
 		'quotes': ['error', 'single'],
+		'comma-dangle': ['error', 'always-multiline'],
 		'keyword-spacing': ['error', {
 			'before': true,
 			'after': true,
 		}],
+		'key-spacing': ['error', {
+			'beforeColon': false,
+			'afterColon': true,
+		}],
+		'space-infix-ops': ['error'],
+		'space-before-blocks': ['error', 'always'],
+		'object-curly-spacing': ['error', 'always'],
+		'nonblock-statement-body-position': ['error', 'beside'],
+		'no-multiple-empty-lines': ['error', { 'max': 1 }],
 		'no-multi-spaces': ['error'],
 		'no-var': ['error'],
 		'prefer-arrow-callback': ['error'],
@@ -28,5 +41,10 @@ module.exports = {
 		'no-constant-condition': ['warn'],
 		'no-empty-pattern': ['warn'],
 		'@typescript-eslint/no-inferrable-types': ['warn'],
+		'@typescript-eslint/no-non-null-assertion': ['warn'],
+		'@typescript-eslint/explicit-function-return-type': ['warn'],
+		'@typescript-eslint/no-misused-promises': ['error', {
+			'checksVoidReturn': false,
+		}],
 	},
 };
