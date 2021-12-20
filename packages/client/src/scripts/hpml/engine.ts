27df835c5d99ad4e0bf1542da4e9ab0de65f2367
marihachi <marihachi0620@gmail.com>
2021-12-21 02:35:42 +0900

    improve attribute collection

--- packages/client/src/scripts/hpml/engine.ts
+++ packages/client/src/scripts/hpml/engine.ts
@@ -92,34 +92,39 @@ export class Hpml {
 	@autobind
 	private collectVars() {
 		if (this.ast == null) return;
-		// extracts all of exported variables
 		const infos: Record<string, VariableInfo> = {};
 		for (const node of this.ast) {
-			if (node.type == 'def' && node.attr.find(attr => (attr.name == 'export')) != null) {
-				// check input attribute
-				let inputAttr: VariableInfo['inputAttr'];
-				const inputAttrNode = node.attr.find(attr => (attr.name == 'input'));
-				if (inputAttrNode != null) {
-					if (inputAttrNode.value == null || inputAttrNode.value.type != 'obj') {
-						throw new HpmlError('The input attribute expects a value of type obj.');
+			if (node.type === 'def') {
+				const exportAttrNode = node.attr.find(attr => (attr.name === 'export'));
+				const inputAttrNode = node.attr.find(attr => (attr.name === 'input'));
+				if (exportAttrNode != null) {
+					let inputAttr: VariableInfo['inputAttr'];
+					if (inputAttrNode != null) {
+						if (inputAttrNode.value == null || inputAttrNode.value.type != 'obj') {
+							throw new HpmlError('The input attribute expects a value of type obj.');
+						}
+						const blockTypeNode = inputAttrNode.value.value.get('type');
+						if (blockTypeNode == null || blockTypeNode.type != 'str') {
+							throw new HpmlError('The type field of the input attribute expects a value of type str.');
+						}
+						const blockType = blockTypeNode.value;
+						if (inputBlockTable[blockType] == null) {
+							throw new HpmlError('The type field of the input attribute is unknown value.');
+						}
+						inputAttr = {
+							blockType
+						};
 					}
-					const blockTypeNode = inputAttrNode.value.value.get('type');
-					if (blockTypeNode == null || blockTypeNode.type != 'str') {
-						throw new HpmlError('The type field of the input attribute expects a value of type str.');
-					}
-					const blockType = blockTypeNode.value;
-					if (inputBlockTable[blockType] == null) {
-						throw new HpmlError('The type field of the input attribute is unknown value.');
-					}
-					inputAttr = {
-						blockType
+					infos[node.name] = {
+						defined: false,
+						attrs: node.attr,
+						inputAttr: inputAttr
 					};
+				} else {
+					if (inputAttrNode != null) {
+						throw new HpmlError("The 'export' attribute is required to add the 'input' attribute to the declaration.");
+					}
 				}
-				infos[node.name] = {
-					defined: false,
-					attrs: node.attr,
-					inputAttr: inputAttr
-				};
 			}
 		}
 		this.variableInfos = infos;
