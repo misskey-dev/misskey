99db3543c103c5108f0cf990bb6cb340acdfbf69
tamaina <tamaina@hotmail.co.jp>
2021-12-20 23:54:50 +0900

    wip

--- packages/client/src/store.ts
+++ packages/client/src/store.ts
@@ -138,6 +138,10 @@ export const defaultStore = markRaw(new Storage('base', {
 		where: 'device',
 		default: false
 	},
+	disableDrawer: {
+		where: 'device',
+		default: false
+	},
 	useBlurEffectForModal: {
 		where: 'device',
 		default: true
