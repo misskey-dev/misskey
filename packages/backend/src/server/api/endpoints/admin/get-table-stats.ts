1574b8582607b77c1fce2b3c07c14fdbf666dff0
tamaina <tamaina@hotmail.co.jp>
2022-01-15 19:31:41 +0900

    wip

--- packages/backend/src/server/api/endpoints/admin/get-table-stats.ts
+++ packages/backend/src/server/api/endpoints/admin/get-table-stats.ts
@@ -2,7 +2,7 @@ import define from '../../define';
 import { getConnection } from 'typeorm';
 
 export const meta = {
-	requireCredential: true as const,
+	requireCredential: true,
 	requireModerator: true,
 
 	tags: ['admin'],
@@ -11,8 +11,8 @@ export const meta = {
 	},
 
 	res: {
-		type: 'object' as const,
-		optional: false as const, nullable: false as const,
+		type: 'object',
+		optional: false, nullable: false,
 		example: {
 			migrations: {
 				count: 66,
@@ -20,7 +20,7 @@ export const meta = {
 			},
 		},
 	},
-};
+} as const;
 
 // eslint-disable-next-line import/no-default-export
 export default define(meta, async () => {
--- packages/backend/src/server/api/endpoints/admin/show-user.ts
+++ packages/backend/src/server/api/endpoints/admin/show-user.ts
@@ -2,11 +2,12 @@ import $ from 'cafy';
 import { ID } from '@/misc/cafy-id';
 import define from '../../define';
 import { Users } from '@/models/index';
+import { SchemaType } from '@/misc/schema';
 
 export const meta = {
 	tags: ['admin'],
 
-	requireCredential: true as const,
+	requireCredential: true,
 	requireModerator: true,
 
 	params: {
@@ -16,148 +17,25 @@ export const meta = {
 	},
 
 	res: {
-		type: 'object' as const,
-		nullable: false as const, optional: false as const,
-		properties: {
-			id: {
-				type: 'string' as const,
-				nullable: false as const, optional: false as const,
-				format: 'id',
+		type: 'object',
+		nullable: false, optional: false,
+		anyOf: [
+			{
+				type: 'object',
+				ref: 'UserLite',
 			},
-			createdAt: {
-				type: 'string' as const,
-				nullable: false as const, optional: false as const,
-				format: 'date-time',
-			},
-			updatedAt: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-				format: 'date-time',
-			},
-			lastFetchedAt: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-			},
-			username: {
-				type: 'string' as const,
-				nullable: false as const, optional: false as const,
-			},
-			name: {
-				type: 'string' as const,
-				nullable: false as const, optional: false as const,
-			},
-			folowersCount: {
-				type: 'number' as const,
-				nullable: false as const, optional: false as const,
-			},
-			followingCount: {
-				type: 'number' as const,
-				nullable: false as const, optional: false as const,
-			},
-			notesCount: {
-				type: 'number' as const,
-				nullable: false as const, optional: false as const,
-			},
-			avatarId: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-			},
-			bannerId: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-			},
-			tags: {
-				type: 'array' as const,
-				nullable: false as const, optional: false as const,
-				items: {
-					type: 'string' as const,
-					nullable: false as const, optional: false as const,
-				},
-			},
-			avatarUrl: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-				format: 'url',
-			},
-			bannerUrl: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-				format: 'url',
-			},
-			avatarBlurhash: {
-				type: 'any' as const,
-				nullable: true as const, optional: false as const,
-				default: null,
-			},
-			bannerBlurhash: {
-				type: 'any' as const,
-				nullable: true as const, optional: false as const,
-				default: null,
-			},
-			isSuspended: {
-				type: 'boolean' as const,
-				nullable: false as const, optional: false as const,
-			},
-			isSilenced: {
-				type: 'boolean' as const,
-				nullable: false as const, optional: false as const,
-			},
-			isLocked: {
-				type: 'boolean' as const,
-				nullable: false as const, optional: false as const,
-			},
-			isBot: {
-				type: 'boolean' as const,
-				nullable: false as const, optional: false as const,
-			},
-			isCat: {
-				type: 'boolean' as const,
-				nullable: false as const, optional: false as const,
-			},
-			isAdmin: {
-				type: 'boolean' as const,
-				nullable: false as const, optional: false as const,
-			},
-			isModerator: {
-				type: 'boolean' as const,
-				nullable: false as const, optional: false as const,
-			},
-			emojis: {
-				type: 'array' as const,
-				nullable: false as const, optional: false as const,
-				items: {
-					type: 'string' as const,
-					nullable: false as const, optional: false as const,
-				},
-			},
-			host: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-			},
-			inbox: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-			},
-			sharedInbox: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-			},
-			featured: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-			},
-			uri: {
-				type: 'string' as const,
-				nullable: true as const, optional: false as const,
-			},
-			token: {
-				type: 'string' as const,
-				nullable: false as const, optional: false as const,
-				default: '<MASKED>',
-			},
-		},
+			{
+				type: 'object',
+				properties: {
+					token: {
+						type: 'string',
+						nullable: true, optional: false,
+					},
+				}
+			}
+		],
 	},
-};
+} as const;
 
 // eslint-disable-next-line import/no-default-export
 export default define(meta, async (ps, me) => {
@@ -171,8 +49,11 @@ export default define(meta, async (ps, me) => {
 		throw new Error('cannot show info of admin');
 	}
 
+	type A = SchemaType<typeof meta.res>
+
 	return {
 		...user,
+		foo: 'bar',
 		token: user.token != null ? '<MASKED>' : user.token,
 	};
 });
