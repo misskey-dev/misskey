7be09a4af9f57981298cb20c32a69755241dc227
tamaina <tamaina@hotmail.co.jp>
2022-01-18 23:06:16 +0900

    refactor: Composition APIへ移行 (#8138)
    * components/drive-file-thumbnail.vue
    
    * components/drive-select-dialog.vue
    
    * components/drive-window.vue
    
    * wip
    
    * wip drive.file.vue, drive.vue
    
    * fix prop
    
    * wip(
    
    * components/drive.folder.vue
    
    * maybe ok
    
    * :v:
    
    * fix variable
    
    * FIX FOLDER VARIABLE
    
    * components/emoji-picker-dialog.vue
    
    * Hate `$emit`
    
    * hate global property
    
    * components/emoji-picker-window.vue
    
    * components/emoji-picker.section.vue
    
    * fix
    
    * fixx
    
    * wip components/emoji-picker.vue
    
    * fix
    
    * defineExpose
    
    * ユニコード絵文字の型をもっといい感じに
    
    * components/featured-photos.vue
    
    * components/follow-button.vue
    
    * forgot-password.vue
    
    * forgot-password.vue
    
    * :art:
    
    * fix

--- packages/client/src/scripts/emojilist.ts
+++ packages/client/src/scripts/emojilist.ts
@@ -1,7 +1,11 @@
-// initial converted from https://github.com/muan/emojilib/commit/242fe68be86ed6536843b83f7e32f376468b38fb
-export const emojilist = require('../emojilist.json') as {
+export const unicodeEmojiCategories = ['face', 'people', 'animals_and_nature', 'food_and_drink', 'activity', 'travel_and_places', 'objects', 'symbols', 'flags'] as const;
+
+export type UnicodeEmojiDef = {
 	name: string;
 	keywords: string[];
 	char: string;
-	category: 'people' | 'animals_and_nature' | 'food_and_drink' | 'activity' | 'travel_and_places' | 'objects' | 'symbols' | 'flags';
-}[];
+	category: typeof unicodeEmojiCategories[number];
+}
+
+// initial converted from https://github.com/muan/emojilib/commit/242fe68be86ed6536843b83f7e32f376468b38fb
+export const emojilist = require('../emojilist.json') as UnicodeEmojiDef[];
