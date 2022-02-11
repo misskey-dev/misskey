9f9b8d1cae861cb2b8064ebb20796813c1d86867
syuilo <Syuilotan@yahoo.co.jp>
2022-02-11 21:17:07 +0900

    Update 1644010796173-convert-hard-mutes.js

--- packages/backend/migration/1644010796173-convert-hard-mutes.js
+++ packages/backend/migration/1644010796173-convert-hard-mutes.js
@@ -5,7 +5,7 @@ module.exports = class convertHardMutes1644010796173 {
     name = 'convertHardMutes1644010796173'
 
     async up(queryRunner) {
-        let entries = await queryRunner.query(`SELECT "userId", "mutedWords" FROM "user_profile"`);
+        let entries = await queryRunner.query(`SELECT "userId", "mutedWords" FROM "user_profile" WHERE "userHost" IS NULL`);
         for(let i = 0; i < entries.length; i++) {
             let words = entries[i].mutedWords
                 .map(line => {
