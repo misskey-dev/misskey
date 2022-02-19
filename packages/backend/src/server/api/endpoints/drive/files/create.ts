0cab102f3a09d9bee2ee7e6754c25ebd9e0d0b29
tamaina <tamaina@hotmail.co.jp>
2022-02-20 01:12:30 +0900

    wip

--- packages/backend/src/server/api/endpoints/drive/files/create.ts
+++ packages/backend/src/server/api/endpoints/drive/files/create.ts
@@ -35,7 +35,7 @@ export const meta = {
 	},
 } as const;
 
-const paramDef = {
+export const paramDef = {
 	type: 'object',
 	properties: {
 		folderId: { type: 'string', format: 'misskey:id', nullable: true, default: null },
