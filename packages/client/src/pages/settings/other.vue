5fbdec15e0516783fb3dfbdb831bca6ba6f3f68c
Johann150 <johann.galle@protonmail.com>
2022-02-23 22:21:34 +0100

    remove reportError setting

--- packages/client/src/pages/settings/other.vue
+++ packages/client/src/pages/settings/other.vue
@@ -4,8 +4,6 @@
 		{{ $ts.showFeaturedNotesInTimeline }}
 	</FormSwitch>
 
-	<FormSwitch v-model="reportError" class="_formBlock">{{ $ts.sendErrorReports }}<template #caption>{{ $ts.sendErrorReportsDescription }}</template></FormSwitch>
-
 	<FormLink to="/settings/account-info" class="_formBlock">{{ $ts.accountInfo }}</FormLink>
 
 	<FormLink to="/settings/delete-account" class="_formBlock"><template #icon><i class="fas fa-exclamation-triangle"></i></template>{{ $ts.closeAccount }}</FormLink>
@@ -15,7 +13,6 @@
 <script lang="ts">
 import { defineAsyncComponent, defineComponent } from 'vue';
 import FormSwitch from '@/components/form/switch.vue';
-import FormSection from '@/components/form/section.vue';
 import FormLink from '@/components/form/link.vue';
 import * as os from '@/os';
 import { debug } from '@/config';
@@ -25,7 +22,6 @@ import * as symbols from '@/symbols';
 
 export default defineComponent({
 	components: {
-		FormSection,
 		FormSwitch,
 		FormLink,
 	},
@@ -41,10 +37,6 @@ export default defineComponent({
 			},
 			debug,
 		}
-	},
-
-	computed: {
-		reportError: defaultStore.makeGetterSetter('reportError'),
 	},
 
 	methods: {
