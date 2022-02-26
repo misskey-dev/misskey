c680a4e1535817f94c60d7fd70747e7f7f551150
syuilo <Syuilotan@yahoo.co.jp>
2022-02-26 14:54:10 +0900

    Update activitypub.ts

--- packages/backend/test/activitypub.ts
+++ packages/backend/test/activitypub.ts
@@ -33,8 +33,8 @@ describe('ActivityPub', () => {
 		};
 
 		it('Minimum Actor', async () => {
-			const { MockResolver } = await import('./misc/mock-resolver');
-			const { createPerson } = await import('../src/remote/activitypub/models/person');
+			const { MockResolver } = await import('./misc/mock-resolver.js');
+			const { createPerson } = await import('../src/remote/activitypub/models/person.js');
 
 			const resolver = new MockResolver();
 			resolver._register(actor.id, actor);
@@ -47,8 +47,8 @@ describe('ActivityPub', () => {
 		});
 
 		it('Minimum Note', async () => {
-			const { MockResolver } = await import('./misc/mock-resolver');
-			const { createNote } = await import('../src/remote/activitypub/models/note');
+			const { MockResolver } = await import('./misc/mock-resolver.js');
+			const { createNote } = await import('../src/remote/activitypub/models/note.js');
 
 			const resolver = new MockResolver();
 			resolver._register(actor.id, actor);
@@ -80,8 +80,8 @@ describe('ActivityPub', () => {
 		};
 
 		it('Actor', async () => {
-			const { MockResolver } = await import('./misc/mock-resolver');
-			const { createPerson } = await import('../src/remote/activitypub/models/person');
+			const { MockResolver } = await import('./misc/mock-resolver.js');
+			const { createPerson } = await import('../src/remote/activitypub/models/person.js');
 
 			const resolver = new MockResolver();
 			resolver._register(actor.id, actor);
