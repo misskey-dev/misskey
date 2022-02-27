39d9c613acdddb1deb7a4f1b0ad8d60e06bb5240
syuilo <Syuilotan@yahoo.co.jp>
2022-02-27 14:10:31 +0900

    fix

--- packages/backend/src/misc/emoji-regex.ts
+++ packages/backend/src/misc/emoji-regex.ts
@@ -1,4 +1,4 @@
-import * as twemoji from 'twemoji-parser/dist/lib/regex.js';
+import twemoji from 'twemoji-parser/dist/lib/regex.js';
 const twemojiRegex = twemoji.default;
 
 export const emojiRegex = new RegExp(`(${twemojiRegex.source})`);
--- packages/backend/src/server/api/endpoints/users/search-by-username-and-host.ts
+++ packages/backend/src/server/api/endpoints/users/search-by-username-and-host.ts
@@ -31,6 +31,8 @@ export const paramDef = {
 	required: [],
 } as const;
 
+// TODO: avatar,bannerをJOINしたいけどエラーになる
+
 // eslint-disable-next-line import/no-default-export
 export default define(meta, paramDef, async (ps, me) => {
 	const activeThreshold = new Date(Date.now() - (1000 * 60 * 60 * 24 * 30)); // 30日
