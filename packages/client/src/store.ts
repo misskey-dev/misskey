77d5c1bd690856ca58b2116fd53df0351c8a5b88
Johann150 <johann.galle@protonmail.com>
2022-03-04 09:17:34 +0100

    remove from client store

--- packages/client/src/store.ts
+++ packages/client/src/store.ts
@@ -214,10 +214,6 @@ export const defaultStore = markRaw(new Storage('base', {
 		where: 'device',
 		default: 'sideFull' as 'sideFull' | 'sideIcon' | 'top'
 	},
-	reportError: {
-		where: 'device',
-		default: false
-	},
 	squareAvatars: {
 		where: 'device',
 		default: false
