b1f3e412bb8f43711afc62244f5af7b3eedb88c0
ThatOneCalculator <kainoa@t1c.dev>
2022-04-03 17:08:17 -0700

    Fix

--- packages/backend/src/db/searchClient.ts
+++ packages/backend/src/db/searchClient.ts
@@ -1,11 +1,11 @@
-
-import sonic from './sonic';
-import es from './elasticsearch';
+import sonic from "./sonic";
+import es from "./elasticsearch";
 
 // This file is just to make it easier to add new drivers in the future, simply import searchClient and whatever driver is available is used
 
 export const clients = [sonic, es];
 
-const client = clients.find(client => client && client.available) || null;
+const searchClient =
+	clients.find((client) => client && client.available) || null;
 
-export default client;
+export default searchClient;
