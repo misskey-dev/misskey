96e57595a6e4fd0a7d6b6cce5e094d55628d1fd5
tamaina <tamaina@hotmail.co.jp>
2022-04-27 06:07:12 +0000

    clean up

--- packages/client/src/init.ts
+++ packages/client/src/init.ts
@@ -169,12 +169,12 @@ fetchInstanceMetaPromise.then(() => {
 });
 
 const app = createApp(
-	window.location.search === '?zen' ? defineAsyncComponent(() =>import('@/ui/zen.vue')) :
-	!$i                               ? defineAsyncComponent(() =>import('@/ui/visitor.vue')) :
-	ui === 'deck'                     ? defineAsyncComponent(() =>import('@/ui/deck.vue')) :
-	ui === 'desktop'                  ? defineAsyncComponent(() =>import('@/ui/desktop.vue')) :
-	ui === 'classic'                  ? defineAsyncComponent(() =>import('@/ui/classic.vue')) :
-	defineAsyncComponent(() =>import('@/ui/universal.vue'))
+	window.location.search === '?zen' ? defineAsyncComponent(() => import('@/ui/zen.vue')) :
+	!$i                               ? defineAsyncComponent(() => import('@/ui/visitor.vue')) :
+	ui === 'deck'                     ? defineAsyncComponent(() => import('@/ui/deck.vue')) :
+	ui === 'desktop'                  ? defineAsyncComponent(() => import('@/ui/desktop.vue')) :
+	ui === 'classic'                  ? defineAsyncComponent(() => import('@/ui/classic.vue')) :
+	defineAsyncComponent(() => import('@/ui/universal.vue'))
 );
 
 if (_DEV_) {
