e712550ba0109c7d1875f30b31873705b886ab52
Nya Candy <dev@candinya.com>
2022-04-28 20:00:01 +0800

    feat: change signin IP -> country

--- packages/backend/src/server/api/common/signin.ts
+++ packages/backend/src/server/api/common/signin.ts
@@ -5,8 +5,14 @@ import { ILocalUser } from '@/models/entities/user.js';
 import { Signins } from '@/models/index.js';
 import { genId } from '@/misc/gen-id.js';
 import { publishMainStream } from '@/services/stream.js';
+import { Reader } from '@maxmind/geoip2-node';
+import { readFileSync } from 'fs';
 
-export default function(ctx: Koa.Context, user: ILocalUser, redirect = false) {
+// Open maxmind database
+const dbBuffer = readFileSync('../../../../assets/geoip.mmdb');
+const r = Reader.openBuffer(dbBuffer);
+
+export function signin(ctx: Koa.Context, user: ILocalUser, redirect = false): void {
 	if (redirect) {
 		//#region Cookie
 		ctx.cookies.set('igi', user.token!, {
@@ -27,14 +33,16 @@ export default function(ctx: Koa.Context, user: ILocalUser, redirect = false) {
 		ctx.status = 200;
 	}
 
-	(async () => {
+	(async (): Promise<void> => {
 		// Append signin history
 		const record = await Signins.insert({
 			id: genId(),
 			createdAt: new Date(),
 			userId: user.id,
-			ip: ctx.ip,
-			headers: ctx.headers,
+			// ip: ctx.ip, // Hide for security reason
+			ip: r.country(ctx.ip).country?.isoCode || 'Unknown',
+			// headers: ctx.headers,
+			headers: () => ('Hide for security reasons'),
 			success: true,
 		}).then(x => Signins.findOneByOrFail(x.identifiers[0]));
 
@@ -42,3 +50,6 @@ export default function(ctx: Koa.Context, user: ILocalUser, redirect = false) {
 		publishMainStream(user.id, 'signin', await Signins.pack(record));
 	})();
 }
+
+// For backward compatibility
+export default signin;
