misskey-dev/misskey

commit 4b872856c2c79ea4e604af481cd2c78487993d88

fix: keep file order (#8659)

packages/backend/src/server/api/endpoints/notes/create.ts
@@ -172,10 +172,14 @@ export default define(meta, paramDef, async (ps, user) => {
 	let files: DriveFile[] = [];
 	const fileIds = ps.fileIds != null ? ps.fileIds : ps.mediaIds != null ? ps.mediaIds : null;
 	if (fileIds != null) {
-		files = await DriveFiles.findBy({
-			userId: user.id,
-			id: In(fileIds),
-		});
+		files = await DriveFiles.createQueryBuilder('file')
+			.where('file.userId = :userId AND file.id IN (:...fileIds)', {
+				userId: user.id,
+				fileIds,
+			})
+			.orderBy('array_position(ARRAY[:...fileIds], "id")')
+			.setParameters({ fileIds })
+			.getMany();
 	}
 
 	let renote: Note | null = null;

commit 037ca92275bd8917eb7bf6f62f7613adc2bbaf36

fix: postgres type error
Fix a bug introduced in #8659. Solution was already tested there.

packages/backend/src/server/api/endpoints/notes/create.ts
@@ -177,7 +177,7 @@ export default define(meta, paramDef, async (ps, user) => {
 				userId: user.id,
 				fileIds,
 			})
-			.orderBy('array_position(ARRAY[:...fileIds], "id")')
+			.orderBy('array_position(ARRAY[:...fileIds], "id"::text)')
 			.setParameters({ fileIds })
 			.getMany();
 	}