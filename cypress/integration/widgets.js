ce980e2d1b05cea443e6827f59677e31db052741
Andreas Nedbal <git@pixelde.su>
2022-05-26 04:47:04 +0200

    fix(client): use mk instead of msky as class prefix

--- cypress/integration/widgets.js
+++ cypress/integration/widgets.js
@@ -38,26 +38,26 @@ describe('After user signed in', () => {
 	});
 
   it('widget edit toggle is visible', () => {
-		cy.get('.msky-widget-edit').should('be.visible');
+		cy.get('.mk-widget-edit').should('be.visible');
   });
 
 	it('widget select should be visible in edit mode', () => {
-		cy.get('.msky-widget-edit').click();
-		cy.get('.msky-widget-select').should('be.visible');
+		cy.get('.mk-widget-edit').click();
+		cy.get('.mk-widget-select').should('be.visible');
   });
 
 	it('first widget should be removed', () => {
-		cy.get('.msky-widget-edit').click();
+		cy.get('.mk-widget-edit').click();
 		cy.get('.customize-container:first-child .remove._button').click();
 		cy.get('.customize-container').should('have.length', 2);
 	});
 
 	function buildWidgetTest(widgetName) {
 		it(`${widgetName} widget should get added`, () => {
-			cy.get('.msky-widget-edit').click();
-			cy.get('.msky-widget-select select').select(widgetName, { force: true });
+			cy.get('.mk-widget-edit').click();
+			cy.get('.mk-widget-select select').select(widgetName, { force: true });
 			cy.get('.bg._modalBg.transparent').click({ multiple: true, force: true });
-			cy.get('.msky-widget-add').click({ force: true });
+			cy.get('.mk-widget-add').click({ force: true });
 			cy.get(`.mkw-${widgetName}`).should('be.visible');
 		});
 	}
