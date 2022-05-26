4c926ed0c43a43594f61ea2d105daab4acf3c799
Andreas Nedbal <git@pixelde.su>
2022-05-26 03:58:03 +0200

    fix(tests): force widget button press

--- cypress/integration/widgets.js
+++ cypress/integration/widgets.js
@@ -3,7 +3,7 @@ function buildWidgetTest(widgetName) {
 		cy.get('.msky-widget-edit').click();
 		cy.get('.msky-widget-select select').select(widgetName, { force: true });
 		cy.get('.bg._modalBg.transparent').click({ multiple: true, force: true });
-		cy.get('.msky-widget-add').click();
+		cy.get('.msky-widget-add').click({ force: true });
 		cy.get(`.mkw-${widgetName}`, { timeout: 6000 }).should('be.visible');
   });
 }
