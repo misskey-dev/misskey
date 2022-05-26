ab79d9b908b66311bd8b82f56e88bebdd99947c6
Andreas Nedbal <git@pixelde.su>
2022-05-26 03:32:21 +0200

    fix(client): add more widget identifier classes

--- packages/client/src/widgets/post-form.vue
+++ packages/client/src/widgets/post-form.vue
@@ -1,5 +1,5 @@
 <template>
-<XPostForm class="_panel" :fixed="true" :autofocus="false"/>
+<XPostForm class="_panel mkw-postForm" :fixed="true" :autofocus="false"/>
 </template>
 
 <script lang="ts" setup>
