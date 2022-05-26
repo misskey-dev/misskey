9f5d4df443dc53935cf7ef75a406091a43a2792f
Andreas Nedbal <git@pixelde.su>
2022-05-26 04:07:51 +0200

    fix(tests): remove timeout from final widget check

--- cypress/integration/widgets.js
+++ cypress/integration/widgets.js
@@ -4,7 +4,7 @@ function buildWidgetTest(widgetName) {
 		cy.get('.msky-widget-select select').select(widgetName, { force: true });
 		cy.get('.bg._modalBg.transparent').click({ multiple: true, force: true });
 		cy.get('.msky-widget-add').click({ force: true });
-		cy.get(`.mkw-${widgetName}`, { timeout: 6000 }).should('be.visible');
+		cy.get(`.mkw-${widgetName}`).should('be.visible');
   });
 }
 
