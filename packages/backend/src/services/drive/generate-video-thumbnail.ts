d54cf8262ac01a3deb6b8dd7689ec144d4d09ea8
mei23 <m@m544.net>
2022-06-13 21:54:10 +0900

    fix cleanup

--- packages/backend/src/services/drive/generate-video-thumbnail.ts
+++ packages/backend/src/services/drive/generate-video-thumbnail.ts
@@ -6,6 +6,8 @@ import FFmpeg from 'fluent-ffmpeg';
 export async function GenerateVideoThumbnail(source: string): Promise<IImage> {
 	const [dir, cleanup] = await createTempDir();
 
+	const outFile = `${dir}/out.png`;
+
 	try {
 		await new Promise((res, rej) => {
 			FFmpeg({
@@ -22,8 +24,9 @@ export async function GenerateVideoThumbnail(source: string): Promise<IImage> {
 		});
 
 		// JPEGに変換 (Webpでもいいが、MastodonはWebpをサポートせず表示できなくなる)
-		return await convertToJpeg(`${dir}/out.png`, 498, 280);
+		return await convertToJpeg(outFile, 498, 280);
 	} finally {
+		await fs.promises.unlink(outFile);
 		cleanup();
 	}
 }
