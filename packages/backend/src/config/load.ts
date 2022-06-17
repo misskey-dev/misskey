39afc5fb364861d8f2bc352ebf39eb73a28a9b20
Nya Candy <dev@candinya.com>
2022-06-17 12:21:11 +0800

    feat: modify UA

--- packages/backend/src/config/load.ts
+++ packages/backend/src/config/load.ts
@@ -45,7 +45,7 @@ export default function load() {
 	mixin.apiUrl = `${mixin.scheme}://${mixin.host}/api`;
 	mixin.authUrl = `${mixin.scheme}://${mixin.host}/auth`;
 	mixin.driveUrl = `${mixin.scheme}://${mixin.host}/files`;
-	mixin.userAgent = `Misskey/${meta.version} (${config.url})`;
+	mixin.userAgent = `Misskey/${meta.version}~nya (${config.url})`;
 	mixin.clientEntry = clientManifest['src/init.ts'];
 
 	if (!config.redis.prefix) config.redis.prefix = mixin.host;
