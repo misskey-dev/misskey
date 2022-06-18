966e9ed37ae9f56b583da973f5da76544b53a7db
syuilo <Syuilotan@yahoo.co.jp>
2022-06-18 18:25:57 +0900

    Update notifications.vue

--- packages/client/src/pages/notifications.vue
+++ packages/client/src/pages/notifications.vue
@@ -8,11 +8,12 @@
 
 <script lang="ts" setup>
 import { computed } from 'vue';
+import { notificationTypes } from 'misskey-js';
 import XNotifications from '@/components/notifications.vue';
 import * as os from '@/os';
 import * as symbols from '@/symbols';
-import { notificationTypes } from 'misskey-js';
 import { i18n } from '@/i18n';
+import { definePageMetadata } from '@/scripts/page-metadata';
 
 let tab = $ref('all');
 let includeTypes = $ref<string[] | null>(null);
@@ -23,46 +24,44 @@ function setFilter(ev) {
 		active: includeTypes && includeTypes.includes(t),
 		action: () => {
 			includeTypes = [t];
-		}
+		},
 	}));
 	const items = includeTypes != null ? [{
 		icon: 'fas fa-times',
 		text: i18n.ts.clear,
 		action: () => {
 			includeTypes = null;
-		}
+		},
 	}, null, ...typeItems] : typeItems;
 	os.popupMenu(items, ev.currentTarget ?? ev.target);
 }
 
-defineExpose({
-	[symbols.PAGE_INFO]: computed(() => ({
-		title: i18n.ts.notifications,
-		icon: 'fas fa-bell',
-		bg: 'var(--bg)',
-		actions: [{
-			text: i18n.ts.filter,
-			icon: 'fas fa-filter',
-			highlighted: includeTypes != null,
-			handler: setFilter,
-		}, {
-			text: i18n.ts.markAllAsRead,
-			icon: 'fas fa-check',
-			handler: () => {
-				os.apiWithDialog('notifications/mark-all-as-read');
-			},
-		}],
-		tabs: [{
-			active: tab === 'all',
-			title: i18n.ts.all,
-			onClick: () => { tab = 'all'; },
-		}, {
-			active: tab === 'unread',
-			title: i18n.ts.unread,
-			onClick: () => { tab = 'unread'; },
-		},]
-	})),
-});
+definePageMetadata(computed(() => ({
+	title: i18n.ts.notifications,
+	icon: 'fas fa-bell',
+	bg: 'var(--bg)',
+	actions: [{
+		text: i18n.ts.filter,
+		icon: 'fas fa-filter',
+		highlighted: includeTypes != null,
+		handler: setFilter,
+	}, {
+		text: i18n.ts.markAllAsRead,
+		icon: 'fas fa-check',
+		handler: () => {
+			os.apiWithDialog('notifications/mark-all-as-read');
+		},
+	}],
+	tabs: [{
+		active: tab === 'all',
+		title: i18n.ts.all,
+		onClick: () => { tab = 'all'; },
+	}, {
+		active: tab === 'unread',
+		title: i18n.ts.unread,
+		onClick: () => { tab = 'unread'; },
+	}],
+})));
 </script>
 
 <style lang="scss" scoped>
