55c22eec8bbbe255198b5d9b2957dfc1cf9caf9c
syuilo <Syuilotan@yahoo.co.jp>
2022-06-24 10:51:45 +0900

    chore(client): tweak ui

--- packages/client/src/components/mini-chart.vue
+++ packages/client/src/components/mini-chart.vue
@@ -2,26 +2,14 @@
 <svg :viewBox="`0 0 ${ viewBoxX } ${ viewBoxY }`" style="overflow:visible">
 	<defs>
 		<linearGradient :id="gradientId" x1="0" x2="0" y1="1" y2="0">
-			<stop offset="0%" stop-color="hsl(200, 80%, 70%)"></stop>
-			<stop offset="100%" stop-color="hsl(90, 80%, 70%)"></stop>
+			<stop offset="0%" :stop-color="color"></stop>
+			<stop offset="100%" :stop-color="colorAlpha"></stop>
 		</linearGradient>
 		<mask :id="maskId" x="0" y="0" :width="viewBoxX" :height="viewBoxY">
 			<polygon
 				:points="polygonPoints"
 				fill="#fff"
 				fill-opacity="0.5"
-			/>
-			<polyline
-				:points="polylinePoints"
-				fill="none"
-				stroke="#fff"
-				stroke-width="2"
-			/>
-			<circle
-				:cx="headX"
-				:cy="headY"
-				r="3"
-				fill="#fff"
 			/>
 		</mask>
 	</defs>
@@ -30,12 +18,25 @@
 		:width="viewBoxX + 20" :height="viewBoxY + 20"
 		:style="`stroke: none; fill: url(#${ gradientId }); mask: url(#${ maskId })`"
 	/>
+	<polyline
+		:points="polylinePoints"
+		fill="none"
+		:stroke="color"
+		stroke-width="2"
+	/>
+	<circle
+		:cx="headX"
+		:cy="headY"
+		r="3"
+		:fill="color"
+	/>
 </svg>
 </template>
 
 <script lang="ts" setup>
 import { onUnmounted, watch } from 'vue';
 import { v4 as uuid } from 'uuid';
+import tinycolor from 'tinycolor2';
 
 const props = defineProps<{
 	src: number[];
@@ -50,6 +51,9 @@ let polygonPoints = $ref('');
 let headX = $ref<number | null>(null);
 let headY = $ref<number | null>(null);
 let clock = $ref<number | null>(null);
+const accent = tinycolor(getComputedStyle(document.documentElement).getPropertyValue('--accent'));
+const color = accent.toRgbString();
+const colorAlpha = accent.clone().setAlpha(1).toRgbString();
 
 function draw(): void {
 	const stats = props.src.slice().reverse();
