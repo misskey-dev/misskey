f17e322f7d7204abe5ab30a866e51d3d38691049
mei23 <m@m544.net>
2022-06-25 23:20:48 +0900

    temp debug 3

--- packages/backend/src/server/api/api-handler.ts
+++ packages/backend/src/server/api/api-handler.ts
@@ -4,6 +4,7 @@ import { IEndpoint } from './endpoints.js';
 import authenticate, { AuthenticationError } from './authenticate.js';
 import call from './call.js';
 import { ApiError } from './error.js';
+import { inspect } from 'util';
 
 export default (endpoint: IEndpoint, ctx: Koa.Context) => new Promise<void>((res) => {
 	const body = ctx.request.body;
@@ -28,6 +29,7 @@ export default (endpoint: IEndpoint, ctx: Koa.Context) => new Promise<void>((res
 		}
 		res();
 	};
+	console.log('body', inspect(body));
 
 	// Authentication
 	authenticate(body['i']).then(([user, app]) => {
