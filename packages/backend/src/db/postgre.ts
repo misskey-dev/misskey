c0c797658c26baf1db25ffeee7efbc6b779819d6
mei23 <m@m544.net>
2022-06-25 21:58:20 +0900

    temp

--- packages/backend/src/db/postgre.ts
+++ packages/backend/src/db/postgre.ts
@@ -85,7 +85,7 @@ class MyCustomLogger implements Logger {
 	}
 
 	public logQuery(query: string, parameters?: any[]) {
-		sqlLogger.info(this.highlight(query).substring(0, 100));
+		//sqlLogger.info(this.highlight(query).substring(0, 100));
 	}
 
 	public logQueryError(error: string, query: string, parameters?: any[]) {
