6e7b56f330dbc1d8aecc7f1dfb00fddab96c9d09
syuilo <Syuilotan@yahoo.co.jp>
2022-07-02 12:40:06 +0900

    respect logging setting in drive/files/create

--- packages/backend/src/server/api/endpoints/drive/files/create.ts
+++ packages/backend/src/server/api/endpoints/drive/files/create.ts
@@ -2,6 +2,7 @@ import ms from 'ms';
 import { addFile } from '@/services/drive/add-file.js';
 import { DriveFiles } from '@/models/index.js';
 import { DB_MAX_IMAGE_COMMENT_LENGTH } from '@/misc/hard-limits.js';
+import { fetchMeta } from '@/misc/fetch-meta.js';
 import define from '../../../define.js';
 import { apiLogger } from '../../../logger.js';
 import { ApiError } from '../../../error.js';
@@ -66,9 +67,21 @@ export default define(meta, paramDef, async (ps, user, _, file, cleanup, ip, hea
 		name = null;
 	}
 
+	const meta = await fetchMeta();
+
 	try {
 		// Create file
-		const driveFile = await addFile({ user, path: file.path, name, comment: ps.comment, folderId: ps.folderId, force: ps.force, sensitive: ps.isSensitive, requestIp: ip, requestHeaders: headers });
+		const driveFile = await addFile({
+			user,
+			path: file.path,
+			name,
+			comment: ps.comment,
+			folderId: ps.folderId,
+			force: ps.force,
+			sensitive: ps.isSensitive,
+			requestIp: meta.enableIpLogging ? ip : null,
+			requestHeaders: meta.enableIpLogging ? headers : null,
+		});
 		return await DriveFiles.pack(driveFile, { self: true });
 	} catch (e) {
 		if (e instanceof Error || typeof e === 'string') {
