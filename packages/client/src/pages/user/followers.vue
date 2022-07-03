b2af1948a4917127ac96b39f03da0b39e8a5063e
syuilo <Syuilotan@yahoo.co.jp>
2022-07-03 14:44:18 +0900

    fix(client): フォロワー一覧がフォローににゃっているんだにゃあ

--- packages/client/src/pages/user/followers.vue
+++ packages/client/src/pages/user/followers.vue
@@ -4,7 +4,7 @@
 	<MkSpacer :content-max="1000">
 		<transition name="fade" mode="out-in">
 			<div v-if="user">
-				<XFollowList :user="user" type="following"/>
+				<XFollowList :user="user" type="followers"/>
 			</div>
 			<MkError v-else-if="error" @retry="fetch()"/>
 			<MkLoading v-else/>
