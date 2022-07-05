4592ceb30e04279f70e0463550160f098c1254b9
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-07-05 00:42:11 -0700

    Replace fontawesome with tabler svg

--- packages/backend/src/server/web/boot.js
+++ packages/backend/src/server/web/boot.js
@@ -110,9 +110,11 @@
 				"beforeend",
 				`<link rel="stylesheet" href="../boot.css" />`);
 			document.documentElement.innerHTML = `
-			<div class="icon-warning">
-				<i class="fas fa-exclamation-triangle"></i>
-			</div>
+			<svg class="icon-warning" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-triangle" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+   			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   			<path d="M12 9v2m0 4v.01"></path>
+   			<path d="M5 19h14a2 2 0 0 0 1.84 -2.75l-7.1 -12.25a2 2 0 0 0 -3.5 0l-7.1 12.25a2 2 0 0 0 1.75 2.75"></path>
+			</svg>
 			<h1>An error has occurred!</h1>
 			<button class="button-big" onclick="location.reload(true);">
 				<span class="button-label-big">Refresh</span>
