1a5ee65b2126ffc41fea023d79cdea2128635e0f
ThatOneCalculator <kainoa@t1c.dev>
2022-07-05 13:02:44 -0700

    Render welcome back message as MFM

--- packages/client/src/components/toast.vue
+++ packages/client/src/components/toast.vue
@@ -2,9 +2,7 @@
 <div class="mk-toast">
 	<transition :name="$store.state.animation ? 'toast' : ''" appear @after-leave="emit('closed')">
 		<div v-if="showing" class="body _acrylic" :style="{ zIndex }">
-			<div class="message">
-				{{ message }}
-			</div>
+				<Mfm class="message" :text="message" :is-note="false"></Mfm>
 		</div>
 	</transition>
 </div>
