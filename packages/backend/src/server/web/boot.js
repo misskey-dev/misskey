db7b5d777dfb8c030151a225839ff43c72bf2bf0
ThatOneCalculator <kainoa@t1c.dev>
2022-07-05 00:55:30 -0700

    Rename to `error.css`

--- packages/backend/src/server/web/boot.js
+++ packages/backend/src/server/web/boot.js
@@ -108,7 +108,7 @@
 		if (!errorsElement) {
 			document.getElementsByTagName("head")[0].insertAdjacentHTML(
 				"beforeend",
-				`<link rel="stylesheet" href="../boot.css" />`);
+				`<link rel="stylesheet" href="../error.css" />`);
 			document.documentElement.innerHTML = `
 			<svg class="icon-warning" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-triangle" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
    			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
