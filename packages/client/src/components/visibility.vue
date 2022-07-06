624f9d976fc388b03a26ed12cd8a9754d68ffaa9
Johann150 <johann.galle@protonmail.com>
2022-07-06 11:26:04 +0200

    add tooltip for specified recipients

--- packages/client/src/components/visibility.vue
+++ packages/client/src/components/visibility.vue
@@ -9,13 +9,36 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import XDetails from '@/components/users-tooltip.vue';
+import * as os from '@/os';
+import { useTooltip } from '@/scripts/use-tooltip.ts';
 
 const props = defineProps<{
 	note: {
 		visibility: string;
 		localOnly?: boolean;
+		visibleUserIds?: string[];
 	},
 }>();
+
+// Using $ref here breaks the component.
+const specified = ref<HTMLElement>();
+
+if (props.note.visibility === 'specified') {
+	useTooltip(specified, async (showing) => {
+		const users = await os.api('users/show', {
+			userIds: props.note.visibleUserIds,
+			limit: 10,
+		});
+
+		os.popup(XDetails, {
+			showing,
+			users,
+			count: props.note.visibleUserIds.length,
+			targetElement: specified.value,
+		}, {}, 'closed');
+	});
+}
 </script>
 
 <style lang="scss" scoped>
