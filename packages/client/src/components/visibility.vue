5c35b4dbc8400ef7f1593e80babe78a034f339d6
Johann150 <johann.galle@protonmail.com>
2022-07-06 09:14:22 +0200

    enhance: reusable visibility component

---
+++ packages/client/src/components/visibility.vue
@@ -0,0 +1,25 @@
+<template>
+<span v-if="note.visibility !== 'public'" class="visibility">
+	<i v-if="note.visibility === 'home'" class="fas fa-home"></i>
+	<i v-else-if="note.visibility === 'followers'" class="fas fa-unlock"></i>
+	<i v-else-if="note.visibility === 'specified'" ref="specified" class="fas fa-envelope"></i>
+</span>
+<span v-if="note.localOnly" class="localOnly"><i class="fas fa-biohazard"></i></span>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const props = defineProps<{
+	note: {
+		visibility: string;
+		localOnly?: boolean;
+	},
+}>();
+</script>
+
+<style lang="scss" scoped>
+.visibility, .localOnly {
+	margin-left: .3em;
+}
+</style>
