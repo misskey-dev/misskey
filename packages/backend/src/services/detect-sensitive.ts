85769fb7bd98430ee3bd6c40c1a7cd6196d4f09e
mei23 <m@m544.net>
2022-07-12 06:04:35 +0900

    Lazy loading tensorflow

--- packages/backend/src/services/detect-sensitive.ts
+++ packages/backend/src/services/detect-sensitive.ts
@@ -2,7 +2,6 @@ import * as fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 import * as nsfw from 'nsfwjs';
-import * as tf from '@tensorflow/tfjs-node';
 
 const _filename = fileURLToPath(import.meta.url);
 const _dirname = dirname(_filename);
@@ -11,10 +10,12 @@ let model: nsfw.NSFWJS;
 
 export async function detectSensitive(path: string): Promise<nsfw.predictionType[] | null> {
 	try {
+		const tf = await import('@tensorflow/tfjs-node');
+
 		if (model == null) model = await nsfw.load(`file://${_dirname}/../../nsfw-model/`, { size: 299 });
 
 		const buffer = await fs.promises.readFile(path);
-		const image = await tf.node.decodeImage(buffer, 3) as tf.Tensor3D;
+		const image = await tf.node.decodeImage(buffer, 3) as any;
 		try {
 			const predictions = await model.classify(image);
 			return predictions;
