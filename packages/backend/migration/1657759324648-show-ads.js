d16afd3d13f1e8e7c7d20c781e03bdf584200a8c
ThatOneCalculator <kainoa@t1c.dev>
2022-07-13 23:54:22 -0700

    spacing

--- packages/backend/migration/1657759324648-show-ads.js
+++ packages/backend/migration/1657759324648-show-ads.js
@@ -1,11 +1,11 @@
 export class showAds1657759324648 {
 	name = 'showAds1657759324648'
 
-    async up(queryRunner) {
-			await queryRunner.query(`ALTER TABLE "user_profile" ADD "showAds" boolean NOT NULL DEFAULT false`);
-    }
+	async up(queryRunner) {
+		await queryRunner.query(`ALTER TABLE "user_profile" ADD "showAds" boolean NOT NULL DEFAULT false`);
+	}
 
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user_profile" DROP "showAds"`);
-    }
+	async down(queryRunner) {
+			await queryRunner.query(`ALTER TABLE "user_profile" DROP "showAds"`);
+	}
 }
