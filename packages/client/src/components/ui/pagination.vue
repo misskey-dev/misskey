392805d1b10fc4bbe784e41f5c1f3b335410e88d
tamaina <tamaina@hotmail.co.jp>
2022-07-15 06:51:46 +0000

    use Number.EPSILON
    Co-authored-by: acid-chicken <root@acid-chicken.com>

--- packages/client/src/components/ui/pagination.vue
+++ packages/client/src/components/ui/pagination.vue
@@ -112,7 +112,7 @@ watch([() => props.pagination.reversed, $$(scrollableElement)], () => {
 	}, {
 		root: scrollableElement,
 		rootMargin: props.pagination.reversed ? '-100% 0px 100% 0px' : '100% 0px -100% 0px',
-		threshold: 0.01,
+		threshold: Number.EPSILON,
 	});
 }, { immediate: true });
 
