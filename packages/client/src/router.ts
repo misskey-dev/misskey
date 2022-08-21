d84d866c0daf43ccf4f92bf07cc25407da75ad10
futchitwo <74236683+futchitwo@users.noreply.github.com>
2022-08-21 21:21:20 +0900

    Fix: apps page routing

--- packages/client/src/router.ts
+++ packages/client/src/router.ts
@@ -134,6 +134,10 @@ export const routes = [{
 		name: 'api',
 		component: page(() => import('./pages/settings/api.vue')),
 	}, {
+		path: '/apps',
+		name: 'apps',
+		component: page(() => import('./pages/settings/apps.vue')),
+	}, {
 		path: '/webhook/edit/:webhookId',
 		name: 'webhook',
 		component: page(() => import('./pages/settings/webhook.edit.vue')),
