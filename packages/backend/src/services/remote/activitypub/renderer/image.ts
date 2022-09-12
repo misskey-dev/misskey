924849458fe7cf74cad2eb9c662863a20b3c8295
syuilo <Syuilotan@yahoo.co.jp>
2022-09-13 01:25:58 +0900

    wip

--- packages/backend/src/services/remote/activitypub/renderer/image.ts
+++
@@ -1,9 +0,0 @@
-import { DriveFile } from '@/models/entities/drive-file.js';
-import { DriveFiles } from '@/models/index.js';
-
-export default (file: DriveFile) => ({
-	type: 'Image',
-	url: DriveFiles.getPublicUrl(file),
-	sensitive: file.isSensitive,
-	name: file.comment,
-});
