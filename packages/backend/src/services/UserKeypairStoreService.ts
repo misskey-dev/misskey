c64966b576fa27817fe2a5e1fb08d606121f407d
syuilo <Syuilotan@yahoo.co.jp>
2022-09-13 00:39:35 +0900

    wip

---
+++ packages/backend/src/services/UserKeypairStoreService.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import type { User } from '@/models/entities/user.js';
+import type { UserKeypairs } from '@/models/index.js';
+import { Cache } from '@/misc/cache.js';
+import type { UserKeypair } from '@/models/entities/user-keypair.js';
+
+@Injectable()
+export class UserKeypairStoreService {
+	#cache: Cache<UserKeypair>;
+
+	constructor(
+		@Inject('userKeypairsRepository')
+		private userKeypairsRepository: typeof UserKeypairs,
+	) {
+		this.#cache = new Cache<UserKeypair>(Infinity);
+	}
+
+	public async getUserKeypair(userId: User['id']): Promise<UserKeypair> {
+		return await this.#cache.fetch(userId, () => this.userKeypairsRepository.findOneByOrFail({ userId: userId }));
+	}
+}
