f78a33e9f9512492c946c1f9cc5459823798f688
syuilo <Syuilotan@yahoo.co.jp>
2022-09-16 04:22:39 +0900

    wip

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
-import { EndpointsModule } from '@/server/api/endpoints.module.js';
 import { QueueModule } from '@/queue/queue.module.js';
 import { CoreModule } from './services/CoreModule.js';
 import { DI } from './di-symbols.js';
 import { loadConfig } from './config.js';
 import { db } from './db/postgre';
 import { RepositoryModule } from './RepositoryModule.js';
+import { ServerModule } from './server/ServerModule.js';
 
 @Module({
 	imports: [
 		RepositoryModule,
 		CoreModule,
-		EndpointsModule,
 		QueueModule,
+		ServerModule,
 	],
 	providers: [{
 		provide: DI.config,
---
+++ packages/backend/src/server/ServerModule.ts
@@ -0,0 +1,53 @@
+import { Module } from '@nestjs/common';
+import { EndpointsModule } from '@/server/api/EndpointsModule.js';
+import { CoreModule } from '@/services/CoreModule.js';
+import { ApiCallService } from './api/ApiCallService.js';
+import { FileServerService } from './FileServerService.js';
+import { MediaProxyServerService } from './MediaProxyServerService.js';
+import { NodeinfoServerService } from './NodeinfoServerService.js';
+import { ServerService } from './ServerService.js';
+import { WellKnownServerService } from './WellKnownServerService.js';
+import { GetterService } from './api/common/GetterService.js';
+import { DiscordServerService } from './api/integration/DiscordServerService.js';
+import { GithubServerService } from './api/integration/GithubServerService.js';
+import { TwitterServerService } from './api/integration/TwitterServerService.js';
+import { ChannelsService } from './api/stream/ChannelsService.js';
+import { ActivityPubServerService } from './ActivityPubServerService.js';
+import { ApiLoggerService } from './api/ApiLoggerService.js';
+import { ApiServerService } from './api/ApiServerService.js';
+import { AuthenticateService } from './api/AuthenticateService.js';
+import { RateLimiterService } from './api/RateLimiterService.js';
+import { SigninApiService } from './api/SigninApiService.js';
+import { SigninService } from './api/SigninService.js';
+import { SignupApiService } from './api/SignupApiService.js';
+import { StreamingApiServerService } from './api/StreamingApiServerService.js';
+
+@Module({
+	imports: [
+		EndpointsModule,
+		CoreModule,
+	],
+	providers: [
+		ActivityPubServerService,
+		FileServerService,
+		MediaProxyServerService,
+		NodeinfoServerService,
+		ServerService,
+		WellKnownServerService,
+		GetterService,
+		DiscordServerService,
+		GithubServerService,
+		TwitterServerService,
+		ChannelsService,
+		ApiCallService,
+		ApiLoggerService,
+		ApiServerService,
+		AuthenticateService,
+		RateLimiterService,
+		SigninApiService,
+		SigninService,
+		SignupApiService,
+		StreamingApiServerService,
+	],
+})
+export class ServerModule {}
