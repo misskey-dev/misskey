567b4ac6153875ef732be5303940064b010d3163
syuilo <Syuilotan@yahoo.co.jp>
2022-09-16 23:49:55 +0900

    Update ServerModule.ts

--- packages/backend/src/server/ServerModule.ts
+++ packages/backend/src/server/ServerModule.ts
@@ -24,6 +24,21 @@ import { StreamingApiServerService } from './api/StreamingApiServerService.js';
 import { ClientServerService } from './web/ClientServerService.js';
 import { FeedService } from './web/FeedService.js';
 import { UrlPreviewService } from './web/UrlPreviewService.js';
+import { MainChannelService } from './api/stream/channels/main.js';
+import { AdminChannelService } from './api/stream/channels/admin.js';
+import { AntennaChannelService } from './api/stream/channels/antenna.js';
+import { ChannelChannelService } from './api/stream/channels/channel.js';
+import { DriveChannelService } from './api/stream/channels/drive.js';
+import { GlobalTimelineChannelService } from './api/stream/channels/global-timeline.js';
+import { HashtagChannelService } from './api/stream/channels/hashtag.js';
+import { HomeTimelineChannelService } from './api/stream/channels/home-timeline.js';
+import { HybridTimelineChannelService } from './api/stream/channels/hybrid-timeline.js';
+import { LocalTimelineChannelService } from './api/stream/channels/local-timeline.js';
+import { MessagingIndexChannelService } from './api/stream/channels/messaging-index.js';
+import { MessagingChannelService } from './api/stream/channels/messaging.js';
+import { QueueStatsChannelService } from './api/stream/channels/queue-stats.js';
+import { ServerStatsChannelService } from './api/stream/channels/server-stats.js';
+import { UserListChannelService } from './api/stream/channels/user-list.js';
 
 @Module({
 	imports: [
@@ -54,6 +69,21 @@ import { UrlPreviewService } from './web/UrlPreviewService.js';
 		SigninService,
 		SignupApiService,
 		StreamingApiServerService,
+		MainChannelService,
+		AdminChannelService,
+		AntennaChannelService,
+		ChannelChannelService,
+		DriveChannelService,
+		GlobalTimelineChannelService,
+		HashtagChannelService,
+		HomeTimelineChannelService,
+		HybridTimelineChannelService,
+		LocalTimelineChannelService,
+		MessagingIndexChannelService,
+		MessagingChannelService,
+		QueueStatsChannelService,
+		ServerStatsChannelService,
+		UserListChannelService,
 	],
 	exports: [
 		ServerService,
