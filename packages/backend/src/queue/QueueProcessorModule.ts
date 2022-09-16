25b5bcdb42d1493087c830200ee6ef349bc9154d
syuilo <Syuilotan@yahoo.co.jp>
2022-09-17 00:52:49 +0900

    Update QueueProcessorModule.ts

--- packages/backend/src/queue/QueueProcessorModule.ts
+++ packages/backend/src/queue/QueueProcessorModule.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from '@/services/CoreModule.js';
 import { QueueLoggerService } from './QueueLoggerService.js';
 import { QueueProcessorService } from './QueueProcessorService.js';
+import { DbQueueProcessorsService } from './DbQueueProcessorsService.js';
+import { ObjectStorageQueueProcessorsService } from './ObjectStorageQueueProcessorsService.js';
+import { DeliverProcessorService } from './processors/DeliverProcessorService.js';
+import { EndedPollNotificationProcessorService } from './processors/EndedPollNotificationProcessorService.js';
+import { InboxProcessorService } from './processors/InboxProcessorService.js';
+import { WebhookDeliverProcessorService } from './processors/WebhookDeliverProcessorService.js';
+import { SystemQueueProcessorsService } from './SystemQueueProcessorsService.js';
 
 @Module({
 	imports: [
@@ -9,6 +16,13 @@ import { QueueProcessorService } from './QueueProcessorService.js';
 	],
 	providers: [
 		QueueLoggerService,
+		SystemQueueProcessorsService,
+		ObjectStorageQueueProcessorsService,
+		DbQueueProcessorsService,
+		WebhookDeliverProcessorService,
+		EndedPollNotificationProcessorService,
+		DeliverProcessorService,
+		InboxProcessorService,
 		QueueProcessorService,
 	],
 	exports: [
