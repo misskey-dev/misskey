fd398ec038f6c945d7eec442d620d64b7e7b7087
syuilo <Syuilotan@yahoo.co.jp>
2022-09-17 23:01:01 +0900

    Update AppModule.ts

--- packages/backend/src/AppModule.ts
+++ packages/backend/src/AppModule.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { QueueModule } from '@/services/queue/QueueModule.js';
-import { CoreModule } from './services/CoreModule.js';
-import { ServerModule } from './server/ServerModule.js';
-import { GlobalModule } from './GlobalModule.js';
-import { QueueProcessorModule } from './queue/QueueProcessorModule.js';
+import { CoreModule } from '@/services/CoreModule.js';
+import { ServerModule } from '@/server/ServerModule.js';
+import { GlobalModule } from '@/GlobalModule.js';
+import { QueueProcessorModule } from '@/queue/QueueProcessorModule.js';
 
 @Module({
 	imports: [
 		GlobalModule,
 		CoreModule,
-		QueueModule,
 		ServerModule,
 		QueueProcessorModule,
 	],
