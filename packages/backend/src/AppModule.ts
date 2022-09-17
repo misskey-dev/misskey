b69bd0ee7cea9000612a5f364ec3e2cdf7c1e555
syuilo <Syuilotan@yahoo.co.jp>
2022-09-18 06:10:10 +0900

    refactor

--- packages/backend/src/AppModule.ts
+++ packages/backend/src/AppModule.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { CoreModule } from '@/core/CoreModule.js';
 import { ServerModule } from '@/server/ServerModule.js';
 import { GlobalModule } from '@/GlobalModule.js';
 import { QueueProcessorModule } from '@/queue/QueueProcessorModule.js';
@@ -7,7 +6,6 @@ import { QueueProcessorModule } from '@/queue/QueueProcessorModule.js';
 @Module({
 	imports: [
 		GlobalModule,
-		CoreModule,
 		ServerModule,
 		QueueProcessorModule,
 	],
