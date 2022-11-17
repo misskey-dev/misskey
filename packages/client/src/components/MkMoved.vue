984ce14a3a551ca203cc54d7fb167d927f528cf2
tamaina <tamaina@hotmail.co.jp>
2022-11-18 02:14:56 +0900

    fix languages and refactor MkMoved

--- packages/client/src/components/MkMoved.vue
+++ packages/client/src/components/MkMoved.vue
@@ -1,10 +1,9 @@
 <template>
-<div class="mkmoved _block"><i class="fas fa-info-circle" style="margin-right: 8px;"></i>{{ i18n.ts.accountMoved }}<a class="link" :href="href">{{ acct }}</a></div>
+<div class="_block" :class="$style.mkmoved"><i class="fas fa-info-circle" style="margin-right: 8px;"></i>{{ i18n.ts.accountMoved }}<MkA :class="$style.link" class="_link" :to="href">{{ acct }}</MkA></div>
 </template>
 
 <script lang="ts" setup>
 import { i18n } from '@/i18n';
-import { computed } from 'vue';
 
 const props = defineProps<{
 	acct: string;
@@ -13,16 +12,15 @@ const props = defineProps<{
 const href = $computed(() => `/${props.acct}`);
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" module>
 .mkmoved {
 	font-size: 0.8em;
 	padding: 16px;
 	background: var(--infoBg);
 	color: var(--infoFg);
+}
 
-	> .link {
-		margin-left: 4px;
-		color: var(--accent);
-	}
+.link {
+	margin-left: 4px;
 }
 </style>
