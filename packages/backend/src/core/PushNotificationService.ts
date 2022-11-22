b84b828609a68920b33fc1a82db0149ff2bd248a
tamaina <tamaina@hotmail.co.jp>
2022-11-23 03:09:21 +0900

    change PushNotificationService

--- packages/backend/src/core/PushNotificationService.ts
+++ packages/backend/src/core/PushNotificationService.ts
@@ -67,6 +67,14 @@ export class PushNotificationService {
 		});
 	
 		for (const subscription of subscriptions) {
+			// Continue if sendReadMessage is false
+			if ([
+				'readNotifications',
+				'readAllNotifications',
+				'readAllMessagingMessages',
+				'readAllMessagingMessagesOfARoom',
+			].includes(type) && !subscription.sendReadMessage) continue;
+
 			const pushSubscription = {
 				endpoint: subscription.endpoint,
 				keys: {
