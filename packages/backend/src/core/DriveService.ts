b73ee90bf5741b16302addcd9b15ee4d827f79c1
tamaina <tamaina@hotmail.co.jp>
2022-12-05 17:06:14 +0000

    avifはMastodonでは絶望的 see https://github.com/misskey-dev/misskey/issues/9283

--- packages/backend/src/core/DriveService.ts
+++ packages/backend/src/core/DriveService.ts
@@ -288,7 +288,7 @@ export class DriveService {
 			}
 
 			satisfyWebpublic = !!(
-				type !== 'image/svg+xml' && type !== 'image/webp' &&
+				type !== 'image/svg+xml' && type !== 'image/webp' && type !== 'image/avif' &&
 			!(metadata.exif ?? metadata.iptc ?? metadata.xmp ?? metadata.tifftagPhotoshop) &&
 			metadata.width && metadata.width <= 2048 &&
 			metadata.height && metadata.height <= 2048
