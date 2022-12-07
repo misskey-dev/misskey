8b4ba370f733b17da3837b15499449c49cec8a80
syuilo <Syuilotan@yahoo.co.jp>
2022-12-08 07:46:11 +0900

    Update ServerService.ts

--- packages/backend/src/server/ServerService.ts
+++ packages/backend/src/server/ServerService.ts
@@ -150,6 +150,6 @@ export class ServerService {
 			}
 		});
 
-		fastify.listen({ port: this.config.port });
+		fastify.listen({ port: this.config.port, host: this.config.host ?? 'localhost' });
 	}
 }
