d27dc2cb3f299f034c2623bcae406683a3b14cdc
tamaina <tamaina@hotmail.co.jp>
2022-12-08 10:34:33 +0000

    バ〜カアホ

--- packages/backend/src/core/UtilityService.ts
+++ packages/backend/src/core/UtilityService.ts
@@ -27,7 +27,7 @@ export class UtilityService {
 	@bindThis
 	public isBlockedHost(blockedHosts: string[], host: string): boolean {
 		if (host == null) return false;
-		return blockedHosts.some(x => x.endsWith(host));
+		return blockedHosts.some(x => host.endsWith(x));
 	}
 
 	@bindThis
