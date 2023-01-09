16b68fa2a8fb2ffb20ce8437b2478ba3969c80b6
あずき⪥™ <44765629+melt-adzuki@users.noreply.github.com>
2023-01-09 23:50:42 +0900

    enhance bottom spacing

--- packages/frontend/src/ui/universal.vue
+++ packages/frontend/src/ui/universal.vue
@@ -370,12 +370,6 @@ const wallpaper = miLocalStorage.getItem('wallpaper') != null;
 }
 
 .spacer {
-	$widgets-hide-threshold: 1090px;
-
-	height: calc(env(safe-area-inset-bottom, 0px) + 96px);
-
-	@media (min-width: ($widgets-hide-threshold + 1px)) {
-		display: none;
-	}
+	height: calc(var(--minBottomSpacing));
 }
 </style>
