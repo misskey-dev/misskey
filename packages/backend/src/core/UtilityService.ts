3df1513c7937fda238d884371752f6c30a8fe003
tamaina <tamaina@hotmail.co.jp>
2023-01-12 07:09:24 +0000

    ILIKE, ARRAY

--- packages/backend/src/core/UtilityService.ts
+++ packages/backend/src/core/UtilityService.ts
@@ -27,7 +27,7 @@ export class UtilityService {
 	@bindThis
 	public isBlockedHost(blockedHosts: string[], host: string | null): boolean {
 		if (host == null) return false;
-		return blockedHosts.some(x => `.${host}`.endsWith(`.${x}`));
+		return blockedHosts.some(x => `.${host.toLowerCase()}`.endsWith(`.${x}`));
 	}
 
 	@bindThis
