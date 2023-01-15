a9acd72eb7438c87206086942f43b60edd0be1ab
tamaina <tamaina@hotmail.co.jp>
2023-01-15 14:28:02 +0000

    fix(client): Fix #9600 chat paging issue Previous chat history not shown

--- packages/frontend/src/components/MkPagination.vue
+++ packages/frontend/src/components/MkPagination.vue
@@ -194,8 +194,6 @@ const fetchMore = async (): Promise<void> => {
 		limit: SECOND_FETCH_LIMIT + 1,
 		...(props.pagination.offsetMode ? {
 			offset: offset.value,
-		} : props.pagination.reversed ? {
-			sinceId: items.value[0].id,
 		} : {
 			untilId: items.value[items.value.length - 1].id,
 		}),
@@ -262,8 +260,6 @@ const fetchMoreAhead = async (): Promise<void> => {
 		limit: SECOND_FETCH_LIMIT + 1,
 		...(props.pagination.offsetMode ? {
 			offset: offset.value,
-		} : props.pagination.reversed ? {
-			untilId: items.value[0].id,
 		} : {
 			sinceId: items.value[items.value.length - 1].id,
 		}),
