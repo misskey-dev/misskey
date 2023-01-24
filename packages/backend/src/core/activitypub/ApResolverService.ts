9fd1b35d958523205444fecb7e9184072a1720e4
tamaina <tamaina@hotmail.co.jp>
2023-01-24 01:34:14 +0000

    fix TypeError: Cannot read properties of undefined (reading 'getLogger')

--- packages/backend/src/core/activitypub/ApResolverService.ts
+++ packages/backend/src/core/activitypub/ApResolverService.ts
@@ -39,7 +39,7 @@ export class Resolver {
 		private recursionLimit = 100,
 	) {
 		this.history = new Set();
-		this.logger = this.loggerService.getLogger('ap-resolve'); // なぜか TypeError: Cannot read properties of undefined (reading 'getLogger') と言われる
+		this.logger = this.loggerService?.getLogger('ap-resolve'); // なぜか TypeError: Cannot read properties of undefined (reading 'getLogger') と言われる
 		this.undiciFetcher = this.httpRequestService.createFetcher({
 			maxRedirections: 0,
 		}, {}, this.logger);
