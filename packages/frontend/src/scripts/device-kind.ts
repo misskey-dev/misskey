f3a3f247dfa23eb9c22f23ff6a54f9bb4a098d2f
tamaina <tamaina@hotmail.co.jp>
2023-02-01 11:54:06 +0000

    use top-level await, await in device-kind.ts

--- packages/frontend/src/scripts/device-kind.ts
+++ packages/frontend/src/scripts/device-kind.ts
@@ -1,5 +1,7 @@
 import { defaultStore } from '@/store';
 
+await defaultStore.ready;
+
 const ua = navigator.userAgent.toLowerCase();
 const isTablet = /ipad/.test(ua) || (/mobile|iphone|android/.test(ua) && window.innerWidth > 700);
 const isSmartphone = !isTablet && /mobile|iphone|android/.test(ua);
