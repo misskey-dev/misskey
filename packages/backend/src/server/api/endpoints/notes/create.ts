7c3143b8e5cc4a5875ed9ec4013c55c4e989efe2
tamaina <tamaina@hotmail.co.jp>
2023-02-01 20:04:01 +0900

    enhance(backend): enhance SchemaType handling of anyOf (#9762)
    * enhance(backend): enhance anyOf handling
    
    * clean up

--- packages/backend/src/server/api/endpoints/notes/create.ts
+++ packages/backend/src/server/api/endpoints/notes/create.ts
@@ -90,48 +90,13 @@ export const paramDef = {
 		visibleUserIds: { type: 'array', uniqueItems: true, items: {
 			type: 'string', format: 'misskey:id',
 		} },
-		text: { type: 'string', maxLength: MAX_NOTE_TEXT_LENGTH, nullable: true },
 		cw: { type: 'string', nullable: true, maxLength: 100 },
 		localOnly: { type: 'boolean', default: false },
 		noExtractMentions: { type: 'boolean', default: false },
 		noExtractHashtags: { type: 'boolean', default: false },
 		noExtractEmojis: { type: 'boolean', default: false },
-		fileIds: {
-			type: 'array',
-			uniqueItems: true,
-			minItems: 1,
-			maxItems: 16,
-			items: { type: 'string', format: 'misskey:id' },
-		},
-		mediaIds: {
-			deprecated: true,
-			description: 'Use `fileIds` instead. If both are specified, this property is discarded.',
-			type: 'array',
-			uniqueItems: true,
-			minItems: 1,
-			maxItems: 16,
-			items: { type: 'string', format: 'misskey:id' },
-		},
 		replyId: { type: 'string', format: 'misskey:id', nullable: true },
-		renoteId: { type: 'string', format: 'misskey:id', nullable: true },
 		channelId: { type: 'string', format: 'misskey:id', nullable: true },
-		poll: {
-			type: 'object',
-			nullable: true,
-			properties: {
-				choices: {
-					type: 'array',
-					uniqueItems: true,
-					minItems: 2,
-					maxItems: 10,
-					items: { type: 'string', minLength: 1, maxLength: 50 },
-				},
-				multiple: { type: 'boolean', default: false },
-				expiresAt: { type: 'integer', nullable: true },
-				expiredAfter: { type: 'integer', nullable: true, minimum: 1 },
-			},
-			required: ['choices'],
-		},
 	},
 	anyOf: [
 		{
@@ -143,21 +108,60 @@ export const paramDef = {
 		},
 		{
 			// (re)note with files, text and poll are optional
+			properties: {
+				fileIds: {
+					type: 'array',
+					uniqueItems: true,
+					minItems: 1,
+					maxItems: 16,
+					items: { type: 'string', format: 'misskey:id' },
+				},
+			},
 			required: ['fileIds'],
 		},
 		{
 			// (re)note with files, text and poll are optional
+			properties: {
+				mediaIds: {
+					deprecated: true,
+					description: 'Use `fileIds` instead. If both are specified, this property is discarded.',
+					type: 'array',
+					uniqueItems: true,
+					minItems: 1,
+					maxItems: 16,
+					items: { type: 'string', format: 'misskey:id' },
+				},
+			},
 			required: ['mediaIds'],
 		},
 		{
 			// (re)note with poll, text and files are optional
 			properties: {
-				poll: { type: 'object', nullable: false },
+				poll: {
+					type: 'object',
+					nullable: true,
+					properties: {
+						choices: {
+							type: 'array',
+							uniqueItems: true,
+							minItems: 2,
+							maxItems: 10,
+							items: { type: 'string', minLength: 1, maxLength: 50 },
+						},
+						multiple: { type: 'boolean' },
+						expiresAt: { type: 'integer', nullable: true },
+						expiredAfter: { type: 'integer', nullable: true, minimum: 1 },
+					},
+					required: ['choices'],
+				},
 			},
 			required: ['poll'],
 		},
 		{
 			// pure renote
+			properties: {
+				renoteId: { type: 'string', format: 'misskey:id', nullable: true },
+			},
 			required: ['renoteId'],
 		},
 	],
