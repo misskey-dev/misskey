c1a5662d104530bbaf079f00514c73180944b9bd
Aigamo <ycanardeau@gmail.com>
2023-02-09 02:25:24 +0000

    Show error messages

--- packages/backend/src/boot/main.ts
+++ packages/backend/src/boot/main.ts
@@ -1,3 +1,41 @@
+import {
+	AggregateError,
+	Envs,
+	WebAppOptions,
+	addSingletonCtor,
+	createWebAppBuilder,
+	inject,
+} from 'yohira';
+
+class A {
+	constructor(@inject(Symbol.for('A')) private readonly a: A) {}
+}
+
 export async function main(): Promise<void> {
-	console.log('Hello, World!');
+	try {
+		const options = new WebAppOptions();
+		options.envName = Envs.Development;
+		const builder = createWebAppBuilder(options);
+
+		addSingletonCtor(builder.services, Symbol.for('A'), A);
+
+		const app = builder.build();
+
+		await app.run();
+	} catch (error) {
+		if (error instanceof AggregateError) {
+			const messages: string[] = [];
+			messages.push(error.message);
+			for (const innerError of error.errors) {
+				if (innerError instanceof Error) {
+					messages.push(innerError.message);
+				} else {
+					messages.push(innerError);
+				}
+			}
+			console.error(messages.join('\n'));
+		} else {
+			throw error;
+		}
+	}
 }
