2023e2ee04ee3cf3967418988a24e1af563510ef
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-02-12 07:55:24 +0100

    Revert "Add `coveragePathIgnorePatterns`"
    This reverts commit fcf8c6806b339b7c21f53137f9939d00020904aa.

--- packages/backend/jest.config.cjs
+++ packages/backend/jest.config.cjs
@@ -26,9 +26,9 @@ module.exports = {
 	coverageDirectory: "coverage",
 
 	// An array of regexp pattern strings used to skip coverage collection
-	coveragePathIgnorePatterns: [
-	  "/node_modules/",
-	],
+	// coveragePathIgnorePatterns: [
+	//   "\\\\node_modules\\\\"
+	// ],
 
 	// Indicates which provider should be used to instrument code for coverage
 	coverageProvider: "v8",
