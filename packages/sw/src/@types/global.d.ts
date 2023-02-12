a504a9e4e429461f5157b71d34b0b5a958e30b07
tamaina <tamaina@hotmail.co.jp>
2023-02-12 15:20:49 +0000

    @typesは越境しない

---
+++ packages/sw/src/@types/global.d.ts
@@ -0,0 +1,7 @@
+type FIXME = any;
+
+declare const _LANGS_: string[][];
+declare const _VERSION_: string;
+declare const _ENV_: string;
+declare const _DEV_: boolean;
+declare const _PERF_PREFIX_: string;
