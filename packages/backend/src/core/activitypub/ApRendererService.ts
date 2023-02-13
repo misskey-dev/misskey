e8d4f3eac35f39f664379b7c8b0a3c9db2f3a7ac
syuilo <Syuilotan@yahoo.co.jp>
2023-02-13 16:19:29 +0900

    refactor: fix types

--- packages/backend/src/core/activitypub/ApRendererService.ts
+++ packages/backend/src/core/activitypub/ApRendererService.ts
@@ -181,7 +181,7 @@ export class ApRendererService {
 	// to anonymise reporters, the reporting actor must be a system user
 	// object has to be a uri or array of uris
 	@bindThis
-	public renderFlag(user: LocalUser, object: IObject, content: string): IFlag {
+	public renderFlag(user: LocalUser, object: IObject | string | string[], content: string): IFlag {
 		return {
 			type: 'Flag',
 			actor: `${this.config.url}/users/${user.id}`,
