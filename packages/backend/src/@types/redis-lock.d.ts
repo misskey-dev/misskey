2fe102b59c7776ff426a12cce1dc7ffdc3a5b6b9
dojineko <dojineko.spiral@gmail.com>
2023-02-18 04:19:52 +0900

    Update packages/backend/src/@types/redis-lock.d.ts
    Co-authored-by: Acid Chicken (硫酸鶏) <root@acid-chicken.com>

--- packages/backend/src/@types/redis-lock.d.ts
+++ packages/backend/src/@types/redis-lock.d.ts
@@ -2,7 +2,7 @@ declare module 'redis-lock' {
 	import type Redis from 'ioredis';
 
 	type lock = (lockName: string, timeout?: number, taskToPerform?: function) => void;
-	function redisLock (client: Redis.Redis, retryDelay: number): lock;
+	function redisLock(client: Redis.Redis, retryDelay: number): Lock;
 
 	export = redisLock;
 }
