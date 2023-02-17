ff9a717eb63610eec1a6574d49684d6edf443fef
dojineko <dojineko.spiral@gmail.com>
2023-02-17 18:26:56 +0900

    add tiny definition for redis-lock

---
+++ packages/backend/src/@types/redis-lock.d.ts
@@ -0,0 +1,8 @@
+declare module 'redis-lock' {
+	import type Redis from 'ioredis';
+
+	type lock = (lockName: string, timeout?: number, taskToPerform?: function) => void;
+	function redisLock (client: Redis.Redis, retryDelay: number): lock;
+
+	export = redisLock;
+}
