ea119b34f4340c1f906ea9e80b36b807d963d68a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-02-18 18:54:57 +0100

    test(backend): restore AP unit tests

--- packages/backend/test/misc/mock-resolver.ts
+++ packages/backend/test/misc/mock-resolver.ts
@@ -1,5 +1,16 @@
-import Resolver from '../../src/activitypub/resolver.js';
-import { IObject } from '../../src/activitypub/type.js';
+import type { Config } from '@/config.js';
+import type { ApDbResolverService } from '@/core/activitypub/ApDbResolverService.js';
+import type { ApRendererService } from '@/core/activitypub/ApRendererService.js';
+import type { ApRequestService } from '@/core/activitypub/ApRequestService.js';
+import { Resolver } from '@/core/activitypub/ApResolverService.js';
+import type { IObject } from '@/core/activitypub/type.js';
+import type { HttpRequestService } from '@/core/HttpRequestService.js';
+import type { InstanceActorService } from '@/core/InstanceActorService.js';
+import type { LoggerService } from '@/core/LoggerService.js';
+import type { MetaService } from '@/core/MetaService.js';
+import type { UtilityService } from '@/core/UtilityService.js';
+import { bindThis } from '@/decorators.js';
+import type { NoteReactionsRepository, NotesRepository, PollsRepository, UsersRepository } from '@/models/index.js';
 
 type MockResponse = {
 	type: string;
@@ -8,6 +19,25 @@ type MockResponse = {
 
 export class MockResolver extends Resolver {
 	private _rs = new Map<string, MockResponse>();
+
+	constructor(loggerService: LoggerService) {
+		super(
+			{} as Config,
+			{} as UsersRepository,
+			{} as NotesRepository,
+			{} as PollsRepository,
+			{} as NoteReactionsRepository,
+			{} as UtilityService,
+			{} as InstanceActorService,
+			{} as MetaService,
+			{} as ApRequestService,
+			{} as HttpRequestService,
+			{} as ApRendererService,
+			{} as ApDbResolverService,
+			loggerService,
+		);
+	}
+
 	public async _register(uri: string, content: string | Record<string, any>, type = 'application/activity+json') {
 		this._rs.set(uri, {
 			type,
