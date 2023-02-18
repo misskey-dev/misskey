8da740a6c050beb8a4087bb1f4139f62bc961021
noonworks <noonworks@gmail.com>
2023-02-18 11:35:10 +0000

    コンフリクトするファイルを削除

--- packages/backend/src/@types/redis-lock.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'redis-lock' {
-	import Redis from 'ioredis';
-	declare function redisLock(client: Redis.Redis, retryDelay: number): (lockName: string, timeout?: number) => Promise<Redis.Pipeline | void>;
-	export = redisLock;
-}
