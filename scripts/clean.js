ba1a5ee6826f3405324d7cb24e0499b24c76d105
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2023-02-19 00:24:15 +0900

    update clean

--- scripts/clean.js
+++ scripts/clean.js
@@ -4,5 +4,6 @@ const fs = require('fs');
 	fs.rmSync(__dirname + '/../packages/backend/built', { recursive: true, force: true });
 	fs.rmSync(__dirname + '/../packages/frontend/built', { recursive: true, force: true });
 	fs.rmSync(__dirname + '/../packages/sw/built', { recursive: true, force: true });
+	fs.rmSync(__dirname + '/built', { recursive: true, force: true });
 	fs.rmSync(__dirname + '/../built', { recursive: true, force: true });
 })();
