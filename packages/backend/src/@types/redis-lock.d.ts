c37180d0b8c729beb565b14b209c340677121dce
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-02-19 23:59:30 +0900

    chore: redis-lockの型定義を追加

---
+++ packages/backend/src/@types/redis-lock.d.ts
@@ -0,0 +1,7 @@
+declare module 'redis-lock' {
+	import { Redis } from 'ioredis';
+  function init(client: Redis, retryDelay: number = 50): typeof lock;
+	async function lock(lockName: string, timeout: number = 5000): Promise<() => void>;
+
+	export = init;
+}
