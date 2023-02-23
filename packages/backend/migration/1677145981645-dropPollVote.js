2042b33b5d86b9d37beef680dfa9efca250a9c8e
tamaina <tamaina@hotmail.co.jp>
2023-02-23 09:57:17 +0000

    create migration

---
+++ packages/backend/migration/1677145981645-dropPollVote.js
@@ -0,0 +1,17 @@
+export class ad1676438468213 {
+    name = 'dropPollVote1677145981645'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TYPE "public"."notification_type_enum" RENAME TO "notification_type_enum_old"`);
+        await queryRunner.query(`CREATE TYPE "public"."notification_type_enum" AS ENUM('follow', 'mention', 'reply', 'renote', 'quote', 'reaction', 'pollEnded', 'receiveFollowRequest', 'followRequestAccepted', 'achievementEarned', 'app')`);
+        await queryRunner.query(`ALTER TABLE "notification" ALTER COLUMN "type" TYPE "public"."notification_type_enum" USING "type"::"text"::"public"."notification_type_enum"`);
+        await queryRunner.query(`DROP TYPE "public"."notification_type_enum_old"`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`CREATE TYPE "public"."notification_type_enum_old" AS ENUM('follow', 'mention', 'reply', 'renote', 'quote', 'reaction', 'pollVote', 'pollEnded', 'receiveFollowRequest', 'followRequestAccepted', 'achievementEarned', 'app')`);
+        await queryRunner.query(`ALTER TABLE "notification" ALTER COLUMN "type" TYPE "public"."notification_type_enum_old" USING "type"::"text"::"public"."notification_type_enum_old"`);
+        await queryRunner.query(`DROP TYPE "public"."notification_type_enum"`);
+        await queryRunner.query(`ALTER TYPE "public"."notification_type_enum_old" RENAME TO "notification_type_enum"`);
+    }
+}
