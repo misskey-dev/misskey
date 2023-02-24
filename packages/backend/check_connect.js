6b52403d05e1a590f5d814213f3d19656ae79871
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-02-24 14:09:17 +0900

    起動時にRedisの疎通確認を行う (#9832)
    * 起動時にRedisの疎通確認を行う
    
    * check:connectをstart内に移動
    
    ---------
    
    Co-authored-by: tamaina <tamaina@hotmail.co.jp>

---
+++ packages/backend/check_connect.js
@@ -0,0 +1,10 @@
+import {loadConfig} from './built/config.js';
+import {createRedisConnection} from "./built/redis.js";
+
+const config = loadConfig();
+const redis = createRedisConnection(config);
+
+redis.on('connect', () => redis.disconnect());
+redis.on('error', (e) => {
+    throw e;
+});
