ea04778649f24719c40ff40f16438175f62da534
syuilo <Syuilotan@yahoo.co.jp>
2023-02-25 18:26:35 +0900

    feat: in-channel featured note
    Resolve #9938

--- packages/backend/src/server/api/endpoints/notes/featured.ts
+++ packages/backend/src/server/api/endpoints/notes/featured.ts
@@ -28,6 +28,7 @@ export const paramDef = {
 	properties: {
 		limit: { type: 'integer', minimum: 1, maximum: 100, default: 10 },
 		offset: { type: 'integer', default: 0 },
+		channelId: { type: 'string', nullable: true, format: 'misskey:id' },
 	},
 	required: [],
 } as const;
@@ -62,6 +63,8 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				.leftJoinAndSelect('renote.user', 'renoteUser')
 				.leftJoinAndSelect('renoteUser.avatar', 'renoteUserAvatar')
 				.leftJoinAndSelect('renoteUser.banner', 'renoteUserBanner');
+
+			if (ps.channelId) query.andWhere('note.channelId = :channelId', { channelId: ps.channelId });
 
 			if (me) this.queryService.generateMutedUserQuery(query, me);
 			if (me) this.queryService.generateBlockedUserQuery(query, me);
