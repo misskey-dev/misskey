fe4769a91be0621f67ce7880e519b76f7390093c
Koki Sakano <kosakanoki@gmail.com>
2023-03-01 19:46:54 +0900

    fix/vueの定義を最小限にする

--- packages/frontend/src/types/vue-shim.d.ts
+++ packages/frontend/src/types/vue-shim.d.ts
@@ -1,8 +1,2 @@
-// ref: https://github.com/vuejs/vue-cli/issues/1198
 declare module '*.vue' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent;
-  // vueはデフォルトインポートで読み込むのでルールを無効化する
-  // eslint-disable-next-line import/no-default-export
-  export default component;
 }
