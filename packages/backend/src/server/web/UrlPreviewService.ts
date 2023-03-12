dff0bee7484fd65927cb9b85780ada2922986768
tamaina <tamaina@hotmail.co.jp>
2023-03-12 17:31:52 +0900

    fix: Do not use avif (#10301)
    * fix: Do not use avif
    
    * fix
    
    * fix

--- packages/backend/src/server/web/UrlPreviewService.ts
+++ packages/backend/src/server/web/UrlPreviewService.ts
@@ -33,7 +33,7 @@ export class UrlPreviewService {
 	private wrap(url?: string | null): string | null {
 		return url != null
 			? url.match(/^https?:\/\//)
-				? `${this.config.mediaProxy}/preview.avif?${query({
+				? `${this.config.mediaProxy}/preview.webp?${query({
 					url,
 					preview: '1',
 				})}`
