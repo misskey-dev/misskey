045995978266104e4ab2a9b028ca2fed5d52d291
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-03-12 13:48:43 +0100

    refactor test

--- packages/frontend/test/url-preview.test.ts
+++ packages/frontend/test/url-preview.test.ts
@@ -1,13 +1,32 @@
 import { describe, test, assert, afterEach } from 'vitest';
 import { render, cleanup, type RenderResult } from '@testing-library/vue';
 import './init';
+import type { summaly } from 'summaly';
 import { directives } from '@/directives';
 import MkUrlPreview from '@/components/MkUrlPreview.vue';
 
+type SummalyResult = Awaited<ReturnType<typeof summaly>>;
+
 describe('MkMediaImage', () => {
-	const renderPreview = async (url: string): Promise<RenderResult> => {
+	const renderPreviewBy = async (summary: Partial<SummalyResult>): Promise<RenderResult> => {
+		if (!summary.player) {
+			summary.player = {
+				url: null,
+				width: null,
+				height: null,
+				allow: [],
+			};
+		}
+
+		fetchMock.mockOnceIf(/^\/url?/, () => {
+			return {
+				status: 200,
+				body: JSON.stringify(summary),
+			};
+		});
+
 		const result = render(MkUrlPreview, {
-			props: { url },
+			props: { url: summary.url },
 			global: { directives },
 		});
 
@@ -22,66 +41,53 @@ describe('MkMediaImage', () => {
 		return result;
 	};
 
+	const renderAndOpenPreview = async (summary: Partial<SummalyResult>): Promise<HTMLIFrameElement | null> => {
+		const mkUrlPreview = await renderPreviewBy(summary);
+		const buttons = mkUrlPreview.getAllByRole('button');
+		buttons[0].click();
+		// Wait for the click event to be fired
+		await Promise.resolve();
+
+		return mkUrlPreview.container.querySelector('iframe');
+	};
+
 	afterEach(() => {
 		fetchMock.resetMocks();
 		cleanup();
 	});
 
 	test('Should render the description', async () => {
-		fetchMock.mockOnceIf(/^\/url?/, () => {
-			return {
-				status: 200,
-				body: JSON.stringify({
-					url: 'https://example.local',
-					description: 'Mocked description',
-					player: {},
-				}),
-			};
+		const mkUrlPreview = await renderPreviewBy({
+			url: 'https://example.local',
+			description: 'Mocked description',
 		});
-
-		const mkUrlPreview = await renderPreview('https://example.local');
 		mkUrlPreview.getByText('Mocked description');
 	});
 
 	test('Having a player should render a button', async () => {
-		fetchMock.mockOnceIf(/^\/url?/, () => {
-			return {
-				status: 200,
-				body: JSON.stringify({
-					url: 'https://example.local',
-					player: {
-						url: 'https://example.local/player',
-					},
-				}),
-			};
+		const mkUrlPreview = await renderPreviewBy({
+			url: 'https://example.local',
+			player: {
+				url: 'https://example.local/player',
+				width: null,
+				height: null,
+				allow: [],
+			},
 		});
-
-		const mkUrlPreview = await renderPreview('https://example.local');
 		const buttons = mkUrlPreview.getAllByRole('button');
 		assert.strictEqual(buttons.length, 2, 'two buttons');
 	});
 
 	test('Having a player should setup the iframe', async () => {
-		fetchMock.mockOnceIf(/^\/url?/, () => {
-			return {
-				status: 200,
-				body: JSON.stringify({
-					url: 'https://example.local',
-					player: {
-						url: 'https://example.local/player',
-						allow: [],
-					},
-				}),
-			};
+		const iframe = await renderAndOpenPreview({
+			url: 'https://example.local',
+			player: {
+				url: 'https://example.local/player',
+				width: null,
+				height: null,
+				allow: [],
+			},
 		});
-
-		const mkUrlPreview = await renderPreview('https://example.local');
-		const buttons = mkUrlPreview.getAllByRole('button');
-		buttons[0].click();
-		// Wait for the click event to be fired
-		await Promise.resolve();
-
-		const iframe = mkUrlPreview.container.querySelector('iframe');
 		assert.exists(iframe, 'iframe should exist');
 		assert.strictEqual(iframe?.src, 'https://example.local/player?autoplay=1&auto_play=1');
 		assert.strictEqual(
@@ -91,80 +97,43 @@ describe('MkMediaImage', () => {
 	});
 
 	test('Having a player with `allow` field should set permissions', async () => {
-		fetchMock.mockOnceIf(/^\/url?/, () => {
-			return {
-				status: 200,
-				body: JSON.stringify({
-					url: 'https://example.local',
-					player: {
-						url: 'https://example.local/player',
-						allow: ['fullscreen', 'web-share'],
-					},
-				}),
-			};
+		const iframe = await renderAndOpenPreview({
+			url: 'https://example.local',
+			player: {
+				url: 'https://example.local/player',
+				width: null,
+				height: null,
+				allow: ['fullscreen', 'web-share'],
+			},
 		});
-
-		const mkUrlPreview = await renderPreview('https://example.local');
-		const buttons = mkUrlPreview.getAllByRole('button');
-		buttons[0].click();
-		// Wait for the click event to be fired
-		await Promise.resolve();
-
-		const iframe = mkUrlPreview.container.querySelector('iframe');
 		assert.exists(iframe, 'iframe should exist');
 		assert.strictEqual(iframe?.allow, 'fullscreen;web-share');
 	});
 
 	test('Having a player width should keep the fixed aspect ratio', async () => {
-		fetchMock.mockOnceIf(/^\/url?/, () => {
-			return {
-				status: 200,
-				body: JSON.stringify({
-					url: 'https://example.local',
-					player: {
-						url: 'https://example.local/player',
-						width: 400,
-						height: 200,
-						allow: [],
-					},
-				}),
-			};
+		const iframe = await renderAndOpenPreview({
+			url: 'https://example.local',
+			player: {
+				url: 'https://example.local/player',
+				width: 400,
+				height: 200,
+				allow: [],
+			},
 		});
-
-		const mkUrlPreview = await renderPreview('https://example.local');
-		const buttons = mkUrlPreview.getAllByRole('button');
-		buttons[0].click();
-		// Wait for the click event to be fired
-		await Promise.resolve();
-
-		const iframe = mkUrlPreview.container.querySelector('iframe');
 		assert.exists(iframe, 'iframe should exist');
 		assert.strictEqual(iframe?.parentElement?.style.paddingTop, '50%');
 	});
 
 	test('Having a player width should keep the fixed height', async () => {
-		fetchMock.mockOnceIf(/^\/url?/, () => {
-			return {
-				status: 200,
-				body: JSON.stringify({
-					url: 'https://example.local',
-					player: {
-						url: 'https://example.local/player',
-						width: null,
-						height: 200,
-						allow: [],
-					},
-				}),
-			};
+		const iframe = await renderAndOpenPreview({
+			url: 'https://example.local',
+			player: {
+				url: 'https://example.local/player',
+				width: null,
+				height: 200,
+				allow: [],
+			},
 		});
-
-		const mkUrlPreview = await renderPreview('https://example.local');
-		const buttons = mkUrlPreview.getAllByRole('button');
-		buttons[0].click();
-		// Wait for the click event to be fired
-		await Promise.resolve();
-
-		const iframe = mkUrlPreview.container.querySelector('iframe');
 		assert.exists(iframe, 'iframe should exist');
 		assert.strictEqual(iframe?.parentElement?.style.paddingTop, '200px');
 	});
