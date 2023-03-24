cabed6098d75ac2d0a8b7845f958b15d88f8750c
syuilo <Syuilotan@yahoo.co.jp>
2023-03-24 16:58:57 +0900

    perf(frontend): cache user lists

--- packages/frontend/src/cache.ts
+++ packages/frontend/src/cache.ts
@@ -3,3 +3,4 @@ import { Cache } from '@/scripts/cache';
 
 export const clipsCache = new Cache<misskey.entities.Clip[]>(Infinity);
 export const rolesCache = new Cache(Infinity);
+export const userListsCache = new Cache<misskey.entities.UserList[]>(Infinity);
