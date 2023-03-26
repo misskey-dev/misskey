5560e9e0ded1d027467b8a7153dc1cd7bb1ef5d9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-03-26 11:49:25 +0900

    #252

--- packages/backend/src/core/DriveService.ts
+++ packages/backend/src/core/DriveService.ts
@@ -278,10 +278,8 @@ export class DriveService {
 			}
 		}
 
-		if (!['image/jpeg', 'image/png', 'image/avif', 'image/svg+xml'].includes(type)) {
-			this.registerLogger.debug('web image and thumbnail not created (not an required file)');
-		//if (!isMimeImage(type, 'sharp-convertible-image-with-bmp')) {
-		//	this.registerLogger.debug('web image and thumbnail not created (cannot convert by sharp)');
+		if (!isMimeImage(type, 'sharp-convertible-image-with-bmp')) {
+			this.registerLogger.debug('web image and thumbnail not created (cannot convert by sharp)');
 			return {
 				webpublic: null,
 				thumbnail: null,
