5d940625817ec77f7beebbb8a760f6bf58a90c26
syuilo <Syuilotan@yahoo.co.jp>
2023-03-31 09:20:52 +0900

    lint fix

--- packages/misskey-js/src/api.ts
+++ packages/misskey-js/src/api.ts
@@ -49,7 +49,7 @@ export class APIClient {
 		this.credential = opts.credential;
 		// ネイティブ関数をそのまま変数に代入して使おうとするとChromiumではIllegal invocationエラーが発生するため、
 		// 環境で実装されているfetchを使う場合は無名関数でラップして使用する
-		this.fetch = opts.fetch || ((...args) => fetch(...args));
+		this.fetch = opts.fetch ?? ((...args) => fetch(...args));
 	}
 
 	public request<E extends keyof Endpoints, P extends Endpoints[E]['req']>(
