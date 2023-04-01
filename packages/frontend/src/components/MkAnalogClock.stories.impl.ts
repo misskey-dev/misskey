8b30a0d641c84ca1abbd09f890e05a92f47dea58
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2023-04-01 21:57:31 +0900

    build(#10336): missing dependencies

--- packages/frontend/src/components/MkAnalogClock.stories.impl.ts
+++ packages/frontend/src/components/MkAnalogClock.stories.impl.ts
@@ -1,15 +1,28 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { StoryObj } from '@storybook/vue3';
 import MkAnalogClock from './MkAnalogClock.vue';
 export const Default = {
-	render(args, { argTypes }) {
+	render(args) {
 		return {
 			components: {
 				MkAnalogClock,
 			},
-			props: Object.keys(argTypes),
-			template: '<MkAnalogClock v-bind="$props" />',
+			setup() {
+				return {
+					args,
+				};
+			},
+			computed: {
+				props() {
+					return {
+						...this.args,
+					};
+				},
+			},
+			template: '<MkAnalogClock v-bind="props" />',
 		};
 	},
 	parameters: {
 		layout: 'fullscreen',
 	},
-};
+} satisfies StoryObj<typeof MkAnalogClock>;
