fc76829eb402c87147eec3a9dde33d5ce85704e2
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2023-04-01 22:22:07 +0900

    build(#10336): use fast-glob

--- packages/frontend/.storybook/generate.tsx
+++ packages/frontend/.storybook/generate.tsx
@@ -1,10 +1,9 @@
 import { existsSync, readFileSync } from 'node:fs';
 import { writeFile } from 'node:fs/promises';
 import { basename, dirname } from 'node:path/posix';
-import { promisify } from 'node:util';
 import { GENERATOR, type State, generate } from 'astring';
 import type * as estree from 'estree';
-import glob from 'glob';
+import glob from 'fast-glob';
 import { format } from 'prettier';
 
 interface SatisfiesExpression extends estree.BaseExpression {
@@ -617,7 +616,7 @@ function toStories(component: string): string {
 }
 
 // promisify(glob)('src/{components,pages,ui,widgets}/**/*.vue').then(
-promisify(glob)('src/components/global/**/*.vue').then(
+glob('src/components/global/**/*.vue').then(
 	(components) =>
 		Promise.all(
 			components.map((component) => {
