5d23c32d37c150ace8fe5a8c521587269b9bab09
okayurisotto <aytkzm@gmail.com>
2023-04-08 14:21:17 +0900

    refactor(sw): remove dead code

--- packages/sw/src/scripts/twemoji-base.ts
+++ packages/sw/src/scripts/twemoji-base.ts
@@ -1,5 +1,3 @@
-export const twemojiSvgBase = '/twemoji';
-
 export function char2fileName(char: string): string {
 	let codes = Array.from(char).map(x => x.codePointAt(0)?.toString(16));
 	if (!codes.includes('200d')) codes = codes.filter(x => x !== 'fe0f');
