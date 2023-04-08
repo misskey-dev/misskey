b10f2d87e5b918c5fc90fb17a2a3fc3b83b9c406
okayurisotto <aytkzm@gmail.com>
2023-04-08 14:20:11 +0900

    refactor(sw): remove dead code

--- packages/sw/src/filters/user.ts
+++ packages/sw/src/filters/user.ts
@@ -4,7 +4,3 @@ import * as Acct from 'misskey-js/built/acct';
 export const acct = (user: misskey.Acct) => {
 	return Acct.toString(user);
 };
-
-export const userName = (user: misskey.entities.User) => {
-	return user.name || user.username;
-};
