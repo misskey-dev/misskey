1383b7217b4d8801865c318dc5230230c7f604eb
Namekuji <nmkj@mx.kazuno.co>
2023-04-09 02:03:38 -0400

    make relationship jobs concurrent

--- packages/backend/src/queue/RelationshipQueueProcessorsService.ts
+++ packages/backend/src/queue/RelationshipQueueProcessorsService.ts
@@ -1,20 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { bindThis } from '@/decorators.js';
 import { RelationshipProcessorService } from './processors/RelationshipProcessorService.js';
 import type Bull from 'bull';
+import { DI } from '@/di-symbols.js';
+import type { Config } from '@/config.js';
 
 @Injectable()
 export class RelationshipQueueProcessorsService {
 	constructor(
+		@Inject(DI.config)
+		private config: Config,
+
 		private relationshipProcessorService: RelationshipProcessorService,
 	) {
 	}
 
 	@bindThis
 	public start(q: Bull.Queue): void {
-		q.process('follow', (job) => this.relationshipProcessorService.processFollow(job));
-		q.process('unfollow', (job) => this.relationshipProcessorService.processUnfollow(job));
-		q.process('block', (job) => this.relationshipProcessorService.processBlock(job));
-		q.process('unblock', (job) => this.relationshipProcessorService.processUnblock(job));
+		const maxJobs = (this.config.deliverJobConcurrency ?? 128) / 4; // conservative?
+		q.process('follow', maxJobs, (job) => this.relationshipProcessorService.processFollow(job));
+		q.process('unfollow', maxJobs, (job) => this.relationshipProcessorService.processUnfollow(job));
+		q.process('block', maxJobs, (job) => this.relationshipProcessorService.processBlock(job));
+		q.process('unblock', maxJobs, (job) => this.relationshipProcessorService.processUnblock(job));
 	}
 }
