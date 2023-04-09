67ec4107d05a843a28593221640ee9cf5f445364
okayurisotto <aytkzm@gmail.com>
2023-04-09 12:41:42 +0900

    update(sw): esbuild 0.17.15

--- packages/sw/build.js
+++ packages/sw/build.js
@@ -28,17 +28,20 @@ const buildOptions = {
 	outdir: `${__dirname}/../../built/_sw_dist_`,
 	treeShaking: true,
 	tsconfig: `${__dirname}/tsconfig.json`,
-	watch: watch
-		? {
-				onRebuild(error, result) {
-					if (error) console.error('SW: watch build failed:', error);
-					else console.log('SW: watch build succeeded:', result);
-				},
-		  }
-		: false,
 };
 
-esbuild.build(buildOptions).then(result => {
-	if (watch) console.log('watching...');
-	else console.log('done,', JSON.stringify(result));
+esbuild.context(buildOptions).then(context => {
+	if (watch) {
+		context
+			.watch()
+			.then(() => {
+				console.log('watching...');
+			})
+			.catch(() => {
+				console.error('SW: watch build failed');
+			});
+	} else {
+		context.dispose();
+		console.log('done');
+	}
 });
