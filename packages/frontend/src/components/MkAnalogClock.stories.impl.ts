0af76f7dfa2e535c28caa83d8e482e069e6e462f
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2023-04-09 14:22:42 +0900

    fix: typo

--- packages/frontend/src/components/MkAnalogClock.stories.impl.ts
+++ packages/frontend/src/components/MkAnalogClock.stories.impl.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { StoryObj } from '@storybook/vue3';
-import isChromatic from 'chromatic';
+import isChromatic from 'chromatic/isChromatic';
 import MkAnalogClock from './MkAnalogClock.vue';
 export const Default = {
 	render(args) {
