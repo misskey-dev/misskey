15be36ba55a411c5aac69037f693e1d922451f15
tamaina <tamaina@hotmail.co.jp>
2023-04-12 13:37:25 +0000

    fix

--- packages/frontend/src/components/MkMediaList.vue
+++ packages/frontend/src/components/MkMediaList.vue
@@ -190,10 +190,10 @@ const previewable = (file: misskey.entities.DriveFile): boolean => {
 
 	&.n1 {
 		grid-template-rows: 1fr;
-		min-height: 64px;
 		max-height: 128px;
 
 		&.n1expand {
+			min-height: 64px;
 			max-height: max(min(calc(var(--containerHeight, 100svh) * 0.5), 50svh, 334px), 64px);
 		}
 
