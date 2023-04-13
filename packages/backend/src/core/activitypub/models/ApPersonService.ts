91a59da055bfb2b538b1d010023681128fdcb231
Namekuji <nmkj@mx.kazuno.co>
2023-04-13 13:45:04 -0400

    fix exception format

--- packages/backend/src/core/activitypub/models/ApPersonService.ts
+++ packages/backend/src/core/activitypub/models/ApPersonService.ts
@@ -463,7 +463,7 @@ export class ApPersonService implements OnModuleInit {
 		const url = getOneApHrefNullable(person.url);
 
 		if (url && !url.startsWith('https://')) {
-			throw new Error('unexpected shcema of person url: ' + url);
+			throw new Error(`unexpected shcema of person url: ${url}`);
 		}
 
 		const updates = {
