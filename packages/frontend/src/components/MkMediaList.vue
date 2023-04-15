741717dd4903ed89b6536d8ea1ca061aacfa7dcb
tamaina <tamaina@hotmail.co.jp>
2023-04-15 03:34:11 +0000

    fallback

--- packages/frontend/src/components/MkMediaList.vue
+++ packages/frontend/src/components/MkMediaList.vue
@@ -51,6 +51,7 @@ function calcAspectRatio() {
 
 	if (props.mediaList.length !== 1 || !(img.properties.width && img.properties.height)) {
 		gallery.value.style.aspectRatio = '';
+		gallery.value.style.minHeight = '64px';
 		return;
 	}
 
