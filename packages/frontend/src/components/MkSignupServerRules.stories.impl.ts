99fb538de362c4fa277ff97823b7c32bc9ebe2b7
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2023-04-17 18:10:00 +0900

    test: add interaction tests

--- packages/frontend/src/components/MkSignupServerRules.stories.impl.ts
+++ packages/frontend/src/components/MkSignupServerRules.stories.impl.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { expect } from '@storybook/jest';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
 import { StoryObj } from '@storybook/vue3';
 import { onBeforeUnmount } from 'vue';
 import MkSignupServerRules from './MkSignupServerRules.vue';
+import { i18n } from '@/i18n';
 import { instance } from '@/instance';
-export const Default = {
+export const Empty = {
 	render(args) {
 		return {
 			components: {
@@ -24,20 +27,38 @@ export const Default = {
 			template: '<MkSignupServerRules v-bind="props" />',
 		};
 	},
+	async play({ canvasElement }) {
+		const canvas = within(canvasElement);
+		const groups = await canvas.findAllByRole('group');
+		const buttons = await canvas.findAllByRole('button');
+		for (const group of groups) {
+			if (group.ariaExpanded === 'true') {
+				continue;
+			}
+			const button = await within(group).findByRole('button');
+			userEvent.click(button);
+			await waitFor(() => expect(group).toHaveAttribute('aria-expanded', 'true'));
+		}
+		const labels = await canvas.findAllByText(i18n.ts.agree);
+		for (const label of labels) {
+			expect(buttons.at(-1)).toBeDisabled();
+			await waitFor(() => userEvent.click(label));
+		}
+		expect(buttons.at(-1)).toBeEnabled();
+	},
 	args: {
-		serverRules: [
-			'サーバールール 1',
-			'サーバールール 2',
-			'サーバールール 3',
-		],
+		serverRules: [],
+		tosUrl: null,
 	},
 	decorators: [
 		(_, context) => ({
 			setup() {
 				instance.serverRules = context.args.serverRules;
+				instance.tosUrl = context.args.tosUrl;
 				onBeforeUnmount(() => {
 					// FIXME: 呼び出されない
 					instance.serverRules = [];
+					instance.tosUrl = null;
 				});
 			},
 			template: '<story/>',
@@ -45,5 +66,29 @@ export const Default = {
 	],
 	parameters: {
 		layout: 'centered',
+	},
+} satisfies StoryObj<typeof MkSignupServerRules>;
+export const ServerRulesOnly = {
+	...Empty,
+	args: {
+		...Empty.args,
+		serverRules: [
+			'ルール',
+		],
+	},
+} satisfies StoryObj<typeof MkSignupServerRules>;
+export const TOSOnly = {
+	...Empty,
+	args: {
+		...Empty.args,
+		tosUrl: 'https://example.com/tos',
+	},
+} satisfies StoryObj<typeof MkSignupServerRules>;
+export const ServerRulesAndTOS = {
+	...Empty,
+	args: {
+		...Empty.args,
+		serverRules: ServerRulesOnly.args.serverRules,
+		tosUrl: TOSOnly.args.tosUrl,
 	},
 } satisfies StoryObj<typeof MkSignupServerRules>;
