e68e261b6bc7eddb5bf4023e845f0d1d593db90d
nenohi <kimutipartylove@gmail.com>
2023-04-20 17:00:16 +0900

    wip

--- packages/backend/src/server/api/endpoints/roles/notes.ts
+++ packages/backend/src/server/api/endpoints/roles/notes.ts
@@ -65,13 +65,15 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 		super(meta, paramDef, async (ps, me) => {
 			const role = await this.rolesRepository.findOneBy({
 				id: ps.roleId,
-				isExplorable: true,
+				isPublic: true,
 			});
 
 			if (role == null) {
 				throw new ApiError(meta.errors.noSuchRole);
 			}
-
+			if (!role.isExplorable) { 
+				return [];
+			}
 			const limit = ps.limit + (ps.untilId ? 1 : 0) + (ps.sinceId ? 1 : 0); // untilIdに指定したものも含まれるため+1
 			const noteIdsRes = await this.redisClient.xrevrange(
 				`roleTimeline:${role.id}`,
