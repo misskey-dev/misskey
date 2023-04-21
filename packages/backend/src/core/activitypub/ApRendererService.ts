4df0407a28f2324bc2b22d468f9cda76bee66f6f
ssmucny <smucny.sam@gmail.com>
2023-04-21 19:13:37 -0400

    Add event to AP publishing

--- packages/backend/src/core/activitypub/ApRendererService.ts
+++ packages/backend/src/core/activitypub/ApRendererService.ts
@@ -19,7 +19,7 @@ import { MfmService } from '@/core/MfmService.js';
 import { UserEntityService } from '@/core/entities/UserEntityService.js';
 import { DriveFileEntityService } from '@/core/entities/DriveFileEntityService.js';
 import type { UserKeypair } from '@/models/entities/UserKeypair.js';
-import type { UsersRepository, UserProfilesRepository, NotesRepository, DriveFilesRepository, EmojisRepository, PollsRepository } from '@/models/index.js';
+import type { UsersRepository, UserProfilesRepository, NotesRepository, DriveFilesRepository, EmojisRepository, PollsRepository, EventsRepository } from '@/models/index.js';
 import { bindThis } from '@/decorators.js';
 import { CustomEmojiService } from '@/core/CustomEmojiService.js';
 import { isNotNull } from '@/misc/is-not-null.js';
@@ -51,6 +51,9 @@ export class ApRendererService {
 
 		@Inject(DI.pollsRepository)
 		private pollsRepository: PollsRepository,
+
+		@Inject(DI.eventsRepository)
+		private eventsRepository: EventsRepository,
 
 		private customEmojiService: CustomEmojiService,
 		private userEntityService: UserEntityService,
@@ -424,6 +427,17 @@ export class ApRendererService {
 			})),
 		} as const : {};
 
+		let asEvent = {};
+		if (note.hasEvent) {
+			const event = await this.eventsRepository.findOneBy({ noteId: note.id });
+			asEvent = event ? {
+				type: 'Event',
+				name: event.title,
+				startTime: event.start,
+				endTime: event.end,
+			} as const : {};
+		}
+
 		return {
 			id: `${this.config.url}/notes/${note.id}`,
 			type: 'Note',
@@ -444,6 +458,7 @@ export class ApRendererService {
 			attachment: files.map(x => this.renderDocument(x)),
 			sensitive: note.cw != null || files.some(file => file.isSensitive),
 			tag,
+			...asEvent,
 			...asPoll,
 		};
 	}
