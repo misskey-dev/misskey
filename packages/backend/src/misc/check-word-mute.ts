07ed6179955a2cdef6505d929c9525d7760f558a
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2023-04-26 15:51:12 +0900

    perf: use slacc on check-word-mute when all of specified words are single word

--- packages/backend/src/misc/check-word-mute.ts
+++ packages/backend/src/misc/check-word-mute.ts
@@ -1,3 +1,4 @@
+import { Slacc } from '@misskey-dev/slacc';
 import RE2 from 're2';
 import type { Note } from '@/models/entities/Note.js';
 import type { User } from '@/models/entities/User.js';
@@ -12,6 +13,8 @@ type UserLike = {
 	id: User['id'];
 };
 
+const slaccCache = new Map<string, Slacc>();
+
 export async function checkWordMute(note: NoteLike, me: UserLike | null | undefined, mutedWords: Array<string | string[]>): Promise<boolean> {
 	// 自分自身
 	if (me && (note.userId === me.id)) return false;
@@ -20,6 +23,20 @@ export async function checkWordMute(note: NoteLike, me: UserLike | null | undefi
 		const text = ((note.cw ?? '') + '\n' + (note.text ?? '')).trim();
 
 		if (text === '') return false;
+
+		if (mutedWords.every((filter) => Array.isArray(filter) && filter.length === 1)) {
+			const filter = mutedWords.map(filter => filter[0]);
+			const key = filter.join('\n');
+			const slacc = slaccCache.get(key) ?? Slacc.withPatterns(filter);
+			slaccCache.delete(key);
+			for (const obsoleteKeys of slaccCache.keys()) {
+				if (slaccCache.size > 1000) {
+					slaccCache.delete(obsoleteKeys);
+				}
+			}
+			slaccCache.set(key, slacc);
+			return slacc.isMatch(text);
+		}
 
 		const matched = mutedWords.some(filter => {
 			if (Array.isArray(filter)) {
