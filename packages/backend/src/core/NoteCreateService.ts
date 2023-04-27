a74ab5714cfbf0195009efac2d57cf144a5a30f7
nenohi <kimutipartylove@gmail.com>
2023-04-27 21:04:12 +0900

    なんか動いたかも

--- packages/backend/src/core/NoteCreateService.ts
+++ packages/backend/src/core/NoteCreateService.ts
@@ -679,20 +679,18 @@ export class NoteCreateService implements OnApplicationShutdown {
 		if (sensitiveWord.length > 0) {
 			if (text === '') return false;
 			const matched = sensitiveWord.some(filter => {
-				const words = filter.split(' ');
-				if (Array.isArray(words)) {
+				// represents RegExp
+				const regexp = filter.match(/^\/(.+)\/(.*)$/);
+				// This should never happen due to input sanitisation.
+				if (!regexp) {
+					const words = filter.split(' ');
 					return words.every(keyword => text.includes(keyword));
-				} else {
-					// represents RegExp
-					const regexp = filter.match(/^\/(.+)\/(.*)$/);
+				}
+				try {
+					return new RE2(regexp[1], regexp[2]).test(text);
+				} catch (err) {
 					// This should never happen due to input sanitisation.
-					if (!regexp) return false;
-					try {
-						return new RE2(regexp[1], regexp[2]).test(text);
-					} catch (err) {
-						// This should never happen due to input sanitisation.
-						return false;
-					}
+					return false;
 				}
 			});
 			if (matched) return true;
