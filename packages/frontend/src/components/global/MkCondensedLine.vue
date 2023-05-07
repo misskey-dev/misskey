5440f80cc5db3d3e6167da877eb708e9746fa58c
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2023-05-07 19:08:43 +0900

    chore: min-scale for MkAcct

--- packages/frontend/src/components/global/MkCondensedLine.vue
+++ packages/frontend/src/components/global/MkCondensedLine.vue
@@ -7,20 +7,29 @@
 </template>
 
 <script lang="ts">
+interface Props {
+	readonly minScale?: number;
+}
+
 const contentSymbol = Symbol();
 const observer = new ResizeObserver((entries) => {
 	for (const entry of entries) {
 		const content = (entry.target[contentSymbol] ? entry.target : entry.target.firstElementChild) as HTMLSpanElement;
+		const props: Required<Props> = content[contentSymbol];
 		const container = content.parentElement as HTMLSpanElement;
 		const contentWidth = content.getBoundingClientRect().width;
 		const containerWidth = container.getBoundingClientRect().width;
-		container.style.transform = `scaleX(${Math.min(1, containerWidth / contentWidth)})`;
+		container.style.transform = `scaleX(${Math.max(props.minScale, Math.min(1, containerWidth / contentWidth))})`;
 	}
 });
 </script>
 
 <script setup lang="ts">
 import { ref, watch } from 'vue';
+
+const props = withDefaults(defineProps<Props>(), {
+	minScale: 0,
+});
 
 const content = ref<HTMLSpanElement>();
 
@@ -33,7 +42,7 @@ watch(content, (value, oldValue) => {
 		}
 	}
 	if (value) {
-		value[contentSymbol] = contentSymbol;
+		value[contentSymbol] = props;
 		observer.observe(value);
 		if (value.parentElement) {
 			observer.observe(value.parentElement);
