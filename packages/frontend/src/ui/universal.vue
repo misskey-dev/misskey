b0c79cba736aa96dde9da7a7dfd13cce2bfdbcaa
syuilo <Syuilotan@yahoo.co.jp>
2023-05-08 18:52:08 +0900

    fix of f8ed6b1a5

--- packages/frontend/src/ui/universal.vue
+++ packages/frontend/src/ui/universal.vue
@@ -5,7 +5,7 @@
 	<MkStickyContainer :class="$style.contents">
 		<template #header><XStatusBars :class="$style.statusbars"/></template>
 		<main style="min-width: 0;" :style="{ background: pageMetadata?.value?.bg }" @contextmenu.stop="onContextmenu">
-			<div :class="$style.content" style="container-type: size;">
+			<div :class="$style.content" style="container-type: inline-size;">
 				<RouterView/>
 			</div>
 			<div :class="$style.spacer"></div>
