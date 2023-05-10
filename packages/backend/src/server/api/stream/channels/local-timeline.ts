81b1001464cb030ba3ea4a40772b4d2fcad826ba
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-05-11 08:09:30 +0900

    #262

--- packages/backend/src/server/api/stream/channels/local-timeline.ts
+++ packages/backend/src/server/api/stream/channels/local-timeline.ts
@@ -3,17 +3,21 @@ import { normalizeForSearch } from '@/misc/normalize-for-search.js';
 import { isUserRelated } from '@/misc/is-user-related.js';
 import type { Packed } from '@/misc/json-schema.js';
 import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
+import { MetaService } from '@/core/MetaService.js';
+import { RoleService } from '@/core/RoleService.js';
 import { bindThis } from '@/decorators.js';
 import Channel from '../channel.js';
 
 class LocalTimelineChannel extends Channel {
 	public readonly chName = 'localTimeline';
-	public static shouldShare = false;
+	public static shouldShare = true;
 	public static requireCredential = false;
 	private tags: string[][] = [['delmulin']];
 
 	constructor(
 		private noteEntityService: NoteEntityService,
+		private metaService: MetaService,
+		private roleService: RoleService,
 
 		id: string,
 		connection: Channel['connection'],
@@ -24,7 +28,10 @@ class LocalTimelineChannel extends Channel {
 
 	@bindThis
 	public async init(params: any) {
-		// Subscribe stream
+		const policies = await this.roleService.getUserPolicies(this.user ? this.user.id : null);
+		if (!policies.ltlAvailable) return;
+
+		// Subscribe events
 		this.subscriber.on('notesStream', this.onNote);
 	}
 
@@ -34,6 +41,15 @@ class LocalTimelineChannel extends Channel {
 		const matched = this.tags.some(tags => tags.every(tag => noteTags.includes(normalizeForSearch(tag))));
 		if (!matched) return;
 
+		if (note.channelId != null && !this.followingChannels.has(note.channelId)) return;
+
+		// リプライなら再pack
+		if (note.replyId != null) {
+			note.reply = await this.noteEntityService.pack(note.replyId, this.user, {
+				detail: true,
+			});
+		}
+
 		// Renoteなら再pack
 		if (note.renoteId != null) {
 			note.renote = await this.noteEntityService.pack(note.renoteId, this.user, {
@@ -41,12 +57,26 @@ class LocalTimelineChannel extends Channel {
 			});
 		}
 
+		// 関係ない返信は除外
+		if (note.reply && this.user && !this.user.showTimelineReplies) {
+			const reply = note.reply;
+			// 「チャンネル接続主への返信」でもなければ、「チャンネル接続主が行った返信」でもなければ、「投稿者の投稿者自身への返信」でもない場合
+			if (reply.userId !== this.user.id && note.userId !== this.user.id && reply.userId !== note.userId) return;
+		}
+
 		// 流れてきたNoteがミュートしているユーザーが関わるものだったら無視する
 		if (isUserRelated(note, this.userIdsWhoMeMuting)) return;
 		// 流れてきたNoteがブロックされているユーザーが関わるものだったら無視する
 		if (isUserRelated(note, this.userIdsWhoBlockingMe)) return;
 
-		if (note.renote && note.renote && !note.text && isUserRelated(note, this.userIdsWhoMeMutingRenotes)) return;
+		if (note.renote && !note.text && isUserRelated(note, this.userIdsWhoMeMutingRenotes)) return;
+
+		// 流れてきたNoteがミュートすべきNoteだったら無視する
+		// TODO: 将来的には、単にMutedNoteテーブルにレコードがあるかどうかで判定したい(以下の理由により難しそうではある)
+		// 現状では、ワードミュートにおけるMutedNoteレコードの追加処理はストリーミングに流す処理と並列で行われるため、
+		// レコードが追加されるNoteでも追加されるより先にここのストリーミングの処理に到達することが起こる。
+		// そのためレコードが存在するかのチェックでは不十分なので、改めてcheckWordMuteを呼んでいる
+		if (this.userProfile && await checkWordMute(note, this.user, this.userProfile.mutedWords)) return;
 
 		this.connection.cacheNote(note);
 
@@ -66,6 +96,8 @@ export class LocalTimelineChannelService {
 	public readonly requireCredential = LocalTimelineChannel.requireCredential;
 
 	constructor(
+		private metaService: MetaService,
+		private roleService: RoleService,
 		private noteEntityService: NoteEntityService,
 	) {
 	}
@@ -74,6 +106,8 @@ export class LocalTimelineChannelService {
 	public create(id: string, connection: Channel['connection']): LocalTimelineChannel {
 		return new LocalTimelineChannel(
 			this.noteEntityService,
+			this.metaService,
+			this.roleService,
 			id,
 			connection,
 		);
