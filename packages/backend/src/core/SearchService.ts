26d85c8bed71ba7b41dadeda26c674fa24c6b298
syuilo <Syuilotan@yahoo.co.jp>
2023-05-11 16:33:39 +0900

    enhance(backend): ノートのハッシュタグもMeilisearchに突っ込むように
    今後ハッシュタグ検索とか実装するときのため

--- packages/backend/src/core/SearchService.ts
+++ packages/backend/src/core/SearchService.ts
@@ -82,6 +82,7 @@ export class SearchService {
 					'userId',
 					'userHost',
 					'channelId',
+					'tags',
 				],
 				typoTolerance: {
 					enabled: false,
@@ -107,6 +108,7 @@ export class SearchService {
 				channelId: note.channelId,
 				cw: note.cw,
 				text: note.text,
+				tags: note.tags,
 			}], {
 				primaryKey: 'id',
 			});
