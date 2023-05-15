23f106a0c1d16e821a712d39be1c90d17cd6c901
syuilo <Syuilotan@yahoo.co.jp>
2023-05-15 19:08:46 +0900

    refactor(frontend): boot分割したり副作用減らしたりとか
    #10838

--- packages/frontend/src/pages/settings/preferences-backups.vue
+++ packages/frontend/src/pages/settings/preferences-backups.vue
@@ -40,7 +40,7 @@ import MkInfo from '@/components/MkInfo.vue';
 import * as os from '@/os';
 import { ColdDeviceStorage, defaultStore } from '@/store';
 import { unisonReload } from '@/scripts/unison-reload';
-import { stream } from '@/stream';
+import { useStream } from '@/stream';
 import { $i } from '@/account';
 import { i18n } from '@/i18n';
 import { version, host } from '@/config';
@@ -125,7 +125,7 @@ type Profile = {
 	};
 };
 
-const connection = $i && stream.useChannel('main');
+const connection = $i && useStream().useChannel('main');
 
 let profiles = $ref<Record<string, Profile> | null>(null);
 
