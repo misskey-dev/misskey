37142d7495d33de2b3b366c3671f2b99132361bd
tamaina <tamaina@hotmail.co.jp>
2023-05-15 17:30:52 +0900

    wip

--- packages/misskey-js/src/endpoints.ts
+++ packages/misskey-js/src/endpoints.ts
@@ -229,4 +229,37 @@ export const endpoints = {
 			},
 		}],
 	},
+	"admin/ad/update": {
+		tags: ['admin'],
+
+		requireCredential: true,
+		requireModerator: true,
+
+		errors: {
+			noSuchAd: {
+				message: 'No such ad.',
+				code: 'NO_SUCH_AD',
+				id: 'b7aa1727-1354-47bc-a182-3a9c3973d300',
+			},
+		},
+
+		defines: [{
+			req: {
+				type: 'object',
+				properties: {
+					id: { type: 'string', format: 'misskey:id' },
+					memo: { type: 'string' },
+					url: { type: 'string', minLength: 1 },
+					imageUrl: { type: 'string', minLength: 1 },
+					place: { type: 'string' },
+					priority: { type: 'string' },
+					ratio: { type: 'integer' },
+					expiresAt: { type: 'integer' },
+					startsAt: { type: 'integer' },
+				},
+				required: ['id', 'memo', 'url', 'imageUrl', 'place', 'priority', 'ratio', 'expiresAt', 'startsAt'],
+			},
+			res: undefined,
+		}],
+	},
 } as const satisfies { [x: string]: IEndpointMeta; };
