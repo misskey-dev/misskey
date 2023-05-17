59d0a8c470d433b9773e360aef0b9bba009f2330
mappi-pr <mappi@mizuiromoon.com>
2023-05-17 18:57:27 +0900

    fix:Firefoxでサインアップする時、メールアドレスがユーザー名として認識される

--- packages/frontend/src/components/MkSignupDialog.form.vue
+++ packages/frontend/src/components/MkSignupDialog.form.vue
@@ -9,7 +9,7 @@
 				<template #label>{{ i18n.ts.invitationCode }}</template>
 				<template #prefix><i class="ti ti-key"></i></template>
 			</MkInput>
-			<MkInput v-model="username" type="text" pattern="^[a-zA-Z0-9_]{1,20}$" :spellcheck="false" required data-cy-signup-username @update:model-value="onChangeUsername">
+			<MkInput v-model="username" type="text" pattern="^[a-zA-Z0-9_]{1,20}$" :spellcheck="false" autocomplete="username" required data-cy-signup-username @update:model-value="onChangeUsername">
 				<template #label>{{ i18n.ts.username }} <div v-tooltip:dialog="i18n.ts.usernameInfo" class="_button _help"><i class="ti ti-help-circle"></i></div></template>
 				<template #prefix>@</template>
 				<template #suffix>@{{ host }}</template>
