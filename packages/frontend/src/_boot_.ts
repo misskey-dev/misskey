8050f89d7efb30bda222dca861b7c7c77080925d
tamaina <tamaina@hotmail.co.jp>
2023-05-26 05:06:52 +0000

    Revert "fix(client): /shareをsubBootPathsから外す"
    This reverts commit 406e5d297ba3c2d45734de538c2cad078182190d.

--- packages/frontend/src/_boot_.ts
+++ packages/frontend/src/_boot_.ts
@@ -5,7 +5,7 @@ import '@/style.scss';
 import { mainBoot } from './boot/main-boot';
 import { subBoot } from './boot/sub-boot';
 
-const subBootPaths = ['/auth', '/miauth', '/signup-complete'];
+const subBootPaths = ['/share', '/auth', '/miauth', '/signup-complete'];
 
 if (subBootPaths.some(i => location.pathname === i || location.pathname.startsWith(i + '/'))) {
 	subBoot();
