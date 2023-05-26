eee1e741747d4a9fff59b613d304341ccfb6fd10
tamaina <tamaina@hotmail.co.jp>
2023-05-26 05:18:01 +0000

    shareページに"Misskeyへ"ボタンを設置 Resolve #10898

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -1060,6 +1060,7 @@ export interface Locale {
     "rolesThatCanBeUsedThisEmojiAsReactionPublicRoleWarn": string;
     "cancelReactionConfirm": string;
     "changeReactionConfirm": string;
+    "goToMisskey": string;
     "_initialAccountSetting": {
         "accountCreated": string;
         "letsStartAccountSetup": string;
