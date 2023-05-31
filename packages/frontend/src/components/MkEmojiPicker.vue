6dd219b6c7d13d6852a5e4173fb8cd7430bd41ff
mappi <mappi@mizuiromoon.com>
2023-05-31 14:03:54 +0900

    fix: Firefoxにおける絵文字ピッカーのTabキーフォーカス問題の修正 (#10926)
    * fix 10744
    
    * fix 10744
    
    * Update CHANGELOG.md
    
    * add comment

--- packages/frontend/src/components/MkEmojiPicker.vue
+++ packages/frontend/src/components/MkEmojiPicker.vue
@@ -1,7 +1,8 @@
 <template>
 <div class="omfetrab" :class="['s' + size, 'w' + width, 'h' + height, { asDrawer, asWindow }]" :style="{ maxHeight: maxHeight ? maxHeight + 'px' : undefined }">
 	<input ref="searchEl" :value="q" class="search" data-prevent-emoji-insert :class="{ filled: q != null && q != '' }" :placeholder="i18n.ts.search" type="search" @input="input()" @paste.stop="paste" @keydown.stop.prevent.enter="onEnter">
-	<div ref="emojisEl" class="emojis">
+	<!-- FirefoxのTabフォーカスが想定外の挙動となるためtabindex="-1"を追加 https://github.com/misskey-dev/misskey/issues/10744 -->
+	<div ref="emojisEl" class="emojis" tabindex="-1">
 		<section class="result">
 			<div v-if="searchResultCustom.length > 0" class="body">
 				<button
