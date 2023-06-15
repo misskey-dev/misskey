fc54e56ca70a2d7e9104aa1077a1979db98cd402
yuriha-chan <yuriha.tsuchiura@gmail.com>
2023-06-15 23:09:19 +0900

    fix

--- packages/frontend/src/workers/test-webgl2.ts
+++ packages/frontend/src/workers/test-webgl2.ts
@@ -1,11 +1,11 @@
-if (!OffscreenCanvas) {
-    postMessage({ result: false });
-    return;
-}
-const canvas = new OffscreenCanvas(1, 1);
-const gl = canvas.getContext('webgl2');
-if (gl) {
-    postMessage({ result: true });
+if (window.OffscreenCanvas) {
+    const canvas = new OffscreenCanvas(1, 1);
+    const gl = canvas.getContext('webgl2');
+    if (gl) {
+        postMessage({ result: true });
+    } else {
+        postMessage({ result: false });
+    }
 } else {
     postMessage({ result: false });
 }
