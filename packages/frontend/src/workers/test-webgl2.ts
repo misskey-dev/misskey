91af2d7b068999829ba692eab9803d8417f6d918
riku6460 <17585784+riku6460@users.noreply.github.com>
2023-06-16 14:33:13 +0900

    Fix offscreencanvas undefined (#88)
    * Suppress ReferenceError on some environments (i.e. older iOS)
    
    * fix
    
    * fix
    
    * lint
    
    ---------
    
    Co-authored-by: yuriha-chan <yuriha.tsuchiura@gmail.com>

--- packages/frontend/src/workers/test-webgl2.ts
+++ packages/frontend/src/workers/test-webgl2.ts
@@ -1,7 +1,13 @@
-const canvas = new OffscreenCanvas(1, 1);
-const gl = canvas.getContext('webgl2');
-if (gl) {
-    postMessage({ result: true });
-} else {
+try {
+    // throw ReferenceError in Safari <= 16.3
+    const canvas = new OffscreenCanvas(1, 1);
+    const gl = canvas.getContext('webgl2');
+    if (gl) {
+        postMessage({ result: true });
+    } else {
+        postMessage({ result: false });
+    }
+} catch (err) {
+    // assert(e instanceof ReferenceError)
     postMessage({ result: false });
 }
