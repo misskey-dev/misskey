3b19cc7eb1c8e9d5ccb948685b8fbe5e9630faa2
Chocolate Pie <106949016+chocolate-pie@users.noreply.github.com>
2023-06-21 09:39:58 +0900

    enhance: Storybookのストーリーを作成

---
+++ packages/frontend/src/components/MkAbuseReportResolver.stories.impl.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import MkAbuseReportResolver from './MkAbuseReportResolver.vue';
+import type { StoryObj } from '@storybook/vue3';
+export const Default = {
+	render(args) {
+		return {
+			components: {
+				MkAbuseReportResolver,
+			},
+			setup() {
+				return {
+					args,
+				};
+			},
+			computed: {
+				props() {
+					return {
+						...this.args,
+					};
+				},
+			},
+			template: '<MkAbuseReportResolver v-bind="props" />',
+		};
+	},
+	args: {
+		editable: true,
+		data: {
+			name: 'Sample',
+			targetUserPattern: '^.*@.+$',
+			reporterPattern: null,
+			reportContentPattern: null,
+			expiresAt: 'indefinitely',
+			forward: false,
+		},
+	},
+	parameters: {
+		layout: 'centered',
+	},
+} satisfies StoryObj<typeof MkAbuseReportResolver>;
