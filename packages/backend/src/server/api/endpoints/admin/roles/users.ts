eca41ca472f902df5211afdc6c62205f4b49bcf3
nenohi <kimutipartylove@gmail.com>
2023-06-30 06:35:20 +0900

    Revert "admin/role/userでuseridが指定できるように (#90)"
    This reverts commit c492d450e9242c082704414d0dd9fee16acb838a.

--- packages/backend/src/server/api/endpoints/admin/roles/users.ts
+++ packages/backend/src/server/api/endpoints/admin/roles/users.ts
@@ -28,7 +28,6 @@ export const paramDef = {
 		roleId: { type: 'string', format: 'misskey:id' },
 		sinceId: { type: 'string', format: 'misskey:id' },
 		untilId: { type: 'string', format: 'misskey:id' },
-		userId: { type: 'string', format: 'misskey:id' },
 		limit: { type: 'integer', minimum: 1, maximum: 100, default: 10 },
 	},
 	required: ['roleId'],
@@ -63,10 +62,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 					.orWhere('assign.expiresAt > :now', { now: new Date() });
 				}))
 				.innerJoinAndSelect('assign.user', 'user');
-
-			if (ps.userId != null) {
-				query.andWhere('assign.userId = :userId', { userId: ps.userId });
-			}
 
 			const assigns = await query
 				.take(ps.limit)
