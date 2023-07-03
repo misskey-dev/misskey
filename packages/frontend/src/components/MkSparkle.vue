892277ecde8b7375050703a8c3d115c3b5406f07
mappi-pr <mappi@mizuiromoon.com>
2023-07-03 11:13:44 +0900

    Fix 11021

--- packages/frontend/src/components/MkSparkle.vue
+++ packages/frontend/src/components/MkSparkle.vue
@@ -32,7 +32,8 @@
 		</path>
 	</svg>
 	-->
-	<svg v-for="particle in particles" :key="particle.id" :width="width" :height="height" :viewBox="`0 0 ${width} ${height}`" xmlns="http://www.w3.org/2000/svg" style="position: absolute; top: -32px; left: -32px; z-index: -10;">
+	<!-- MFMで上位レイヤーに表示されるため、リンクをクリックできるようにstyleにpointer-events: none;を付与。 -->
+	<svg v-for="particle in particles" :key="particle.id" :width="width" :height="height" :viewBox="`0 0 ${width} ${height}`" xmlns="http://www.w3.org/2000/svg" style="position: absolute; top: -32px; left: -32px; pointer-events: none;">
 		<path
 			style="transform-origin: center; transform-box: fill-box;"
 			:transform="`translate(${particle.x} ${particle.y})`"
