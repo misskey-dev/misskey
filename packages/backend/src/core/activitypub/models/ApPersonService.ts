5bf82ff26597033344bfa187ce0bb1cbae45bf73
okayurisotto <okayurisotto@proton.me>
2023-07-04 05:19:23 +0900

    cleanup(`ApPersonService.ts`)

--- packages/backend/src/core/activitypub/models/ApPersonService.ts
+++ packages/backend/src/core/activitypub/models/ApPersonService.ts
@@ -433,7 +433,7 @@ export class ApPersonService implements OnModuleInit {
 
 		const fields = this.analyzeAttachments(person.attachment ?? []);
 
-		const tags = extractApHashtags(person.tag).map(tag => normalizeForSearch(tag)).splice(0, 32);
+		const tags = extractApHashtags(person.tag).map(normalizeForSearch).splice(0, 32);
 
 		const bday = person['vcard:bday']?.match(/^\d{4}-\d{2}-\d{2}/);
 
