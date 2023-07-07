5077df297335ff815a894bf99b27e0c221c2114d
tamaina <tamaina@hotmail.co.jp>
2023-07-07 05:02:28 +0000

    100 -> 15

--- packages/backend/src/core/activitypub/models/ApPersonService.ts
+++ packages/backend/src/core/activitypub/models/ApPersonService.ts
@@ -642,8 +642,8 @@ export class ApPersonService implements OnModuleInit {
 			await resolver.resolveOrderedCollectionPage(collection.first) :
 			collection;
 
-		// Perform activity but only the first 100 ones
-		await this.apInboxService.performActivity(user, firstPage, 100);
+		// Perform activity but only the first 15 ones
+		await this.apInboxService.performActivity(user, firstPage, 15);
 	}
 
 	@bindThis
