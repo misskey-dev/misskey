0588b0ea15575e19d4c9910084e5731651d512f4
SASAGAWA Kiyoshi <sasagawa@kent-and-co.com>
2023-07-07 16:18:14 +0900

    fix: add suuport Japanese message of errors.

--- packages/backend/src/misc/is-duplicate-key-value-error.ts
+++ packages/backend/src/misc/is-duplicate-key-value-error.ts
@@ -1,3 +1,7 @@
 export function isDuplicateKeyValueError(e: unknown | Error): boolean {
-	return (e as any).message && (e as Error).message.startsWith('duplicate key value');
+	return (
+		(e as any).message &&
+		((e as Error).message.startsWith("duplicate key value") ||
+			(e as Error).message.startsWith("重複したキー値"))
+	);
 }
