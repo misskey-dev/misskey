55d4fef710fae3409d6b941fa3e157411d655473
meronmks <meronmks.8914@gmail.com>
2023-07-07 20:18:02 +0900

    無効+Redisも空なアンテナの再有効化手段を antennas/update だけに

--- packages/backend/src/server/api/endpoints/antennas/show.ts
+++ packages/backend/src/server/api/endpoints/antennas/show.ts
@@ -55,11 +55,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				throw new ApiError(meta.errors.noSuchAntenna);
 			}
 
-			this.antennasRepository.update(antenna.id, {
-				isActive: true,
-				lastUsedAt: new Date(),
-			});
-
 			return await this.antennaEntityService.pack(antenna);
 		});
 	}
