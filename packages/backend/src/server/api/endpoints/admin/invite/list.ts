bfb02ba278e520e6eae28908cd4f14b81497b497
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-08 17:25:18 +0900

    change(backend): admin/invite/listはオフセットでページネーションするように

--- packages/backend/src/server/api/endpoints/admin/invite/list.ts
+++ packages/backend/src/server/api/endpoints/admin/invite/list.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable } from '@nestjs/common';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import type { RegistrationTicketsRepository } from '@/models/index.js';
 import { InviteCodeEntityService } from '@/core/entities/InviteCodeEntityService.js';
-import { QueryService } from '@/core/QueryService.js';
 import { DI } from '@/di-symbols.js';
 
 export const meta = {
@@ -25,8 +24,7 @@ export const paramDef = {
 	type: 'object',
 	properties: {
 		limit: { type: 'integer', minimum: 1, maximum: 100, default: 30 },
-		sinceId: { type: 'string', format: 'misskey:id' },
-		untilId: { type: 'string', format: 'misskey:id' },
+		offset: { type: 'integer', default: 0 },
 		type: { type: 'string', enum: ['unused', 'used', 'expired', 'all'], default: 'all' },
 	},
 	required: [],
@@ -40,33 +38,22 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 		private registrationTicketsRepository: RegistrationTicketsRepository,
 
 		private inviteCodeEntityService: InviteCodeEntityService,
-		private queryService: QueryService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			const query = this.queryService.makePaginationQuery(this.registrationTicketsRepository.createQueryBuilder('ticket'), ps.sinceId, ps.untilId)
+			const query = this.registrationTicketsRepository.createQueryBuilder('ticket')
 				.leftJoinAndSelect('ticket.createdBy', 'createdBy')
 				.leftJoinAndSelect('ticket.usedBy', 'usedBy');
 
 			switch (ps.type) {
-				case 'unused': {
-					query.andWhere('ticket.usedBy IS NULL');
-					break;
-				}
-
-				case 'used': {
-					query.andWhere('ticket.usedBy IS NOT NULL');
-					break;
-				}
-
-				case 'expired': {
-					query.andWhere('ticket.expiresAt < :now', { now: new Date() });
-					break;
-				}
+				case 'unused': query.andWhere('ticket.usedBy IS NULL'); break;
+				case 'used': query.andWhere('ticket.usedBy IS NOT NULL'); break;
+				case 'expired': query.andWhere('ticket.expiresAt < :now', { now: new Date() }); break;
 			}
 
-			const tickets = await query
-				.take(ps.limit)
-				.getMany();
+			query.take(ps.limit);
+			query.skip(ps.offset);
+
+			const tickets = await query.getMany();
 
 			return await this.inviteCodeEntityService.packMany(tickets, me);
 		});
