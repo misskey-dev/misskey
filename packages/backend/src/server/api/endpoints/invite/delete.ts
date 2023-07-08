861d358269767802318e0399f18d6cf0a70c9deb
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-08 16:17:38 +0900

    change(backend): モデレーターであれば作成者以外でも招待コードを削除できるように

--- packages/backend/src/server/api/endpoints/invite/delete.ts
+++ packages/backend/src/server/api/endpoints/invite/delete.ts
@@ -1,10 +1,7 @@
-import { MoreThan } from 'typeorm';
-import rndstr from 'rndstr';
 import { Inject, Injectable } from '@nestjs/common';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import type { RegistrationTicketsRepository } from '@/models/index.js';
-import { IdService } from '@/core/IdService.js';
-import { MetaService } from '@/core/MetaService.js';
+import { RoleService } from '@/core/RoleService.js';
 import { DI } from '@/di-symbols.js';
 import { ApiError } from '../../error.js';
 
@@ -20,11 +17,18 @@ export const meta = {
 			code: 'NO_SUCH_INVITE_CODE',
 			id: 'cd4f9ae4-7854-4e3e-8df9-c296f051e634',
 		},
+
 		cantDelete: {
 			message: 'You can\'t delete this invite code.',
 			code: 'CAN_NOT_DELETE_INVITE_CODE',
 			id: 'ff17af39-000c-4d4e-abdf-848fa30fc1ce',
-		}
+		},
+
+		accessDenied: {
+			message: 'Access denied.',
+			code: 'ACCESS_DENIED',
+			id: '5eb8d909-2540-4970-90b8-dd6f86088121',
+		},
 	},
 } as const;
 
@@ -42,20 +46,27 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 	constructor(
 		@Inject(DI.registrationTicketsRepository)
 		private registrationTicketsRepository: RegistrationTicketsRepository,
+
+		private roleService: RoleService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			const ticket = await this.registrationTicketsRepository.findOneBy({
-				id: ps.inviteId,
-				createdBy: {
-					id: me.id,
+			const ticket = await this.registrationTicketsRepository.findOne({
+				where: {
+					id: ps.inviteId,
 				},
+				relations: ['createdBy'],
 			});
+			const isModerator = await this.roleService.isModerator(me);
 
 			if (ticket == null) {
 				throw new ApiError(meta.errors.noSuchCode);
 			}
 
-			if (ticket.usedAt) {
+			if (ticket.createdBy?.id !== me.id && !isModerator) {
+				throw new ApiError(meta.errors.accessDenied);
+			}
+
+			if (ticket.usedAt && !isModerator) {
 				throw new ApiError(meta.errors.cantDelete);
 			}
 
