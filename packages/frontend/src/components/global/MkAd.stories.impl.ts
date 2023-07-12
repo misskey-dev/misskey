2184323ae782108180cf165d6bc31d8d98a0cd3b
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-07-12 23:57:26 +0200

    use waitFor

--- packages/frontend/src/components/global/MkAd.stories.impl.ts
+++ packages/frontend/src/components/global/MkAd.stories.impl.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { expect } from '@storybook/jest';
-import { userEvent, within } from '@storybook/testing-library';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
 import { StoryObj } from '@storybook/vue3';
 import MkAd from './MkAd.vue';
 import { i18n } from '@/i18n';
-import { tick } from '@/scripts/test-utils.js';
 const common = {
 	render(args) {
 		return {
@@ -37,8 +36,7 @@ const common = {
 		const i = buttons[0];
 		await expect(i).toBeInTheDocument();
 		await userEvent.click(i);
-		await tick();
-		await expect(a).not.toBeInTheDocument();
+		await waitFor(() => expect(a).not.toBeInTheDocument());
 		await expect(i).not.toBeInTheDocument();
 		buttons = canvas.getAllByRole<HTMLButtonElement>('button');
 		await expect(buttons).toHaveLength(args.__hasReduce ? 2 : 1);
@@ -51,11 +49,10 @@ const common = {
 		await expect(back).toBeInTheDocument();
 		await expect(back).toHaveTextContent(i18n.ts._ad.back);
 		await userEvent.click(back);
-		await tick();
+		await waitFor(() => expect(back).not.toBeInTheDocument());
 		if (reduce) {
 			await expect(reduce).not.toBeInTheDocument();
 		}
-		await expect(back).not.toBeInTheDocument();
 		const aAgain = canvas.getByRole<HTMLAnchorElement>('link');
 		await expect(aAgain).toBeInTheDocument();
 		const imgAgain = within(aAgain).getByRole('img');
