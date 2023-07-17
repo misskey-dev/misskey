0f64372abb9471e66cd32a0f5f76ae09a73b43e1
tamaina <tamaina@hotmail.co.jp>
2023-07-17 10:40:55 +0000

    prepend()でキューが5つ以下の時はexecuteQueueを呼んでしまう

--- packages/frontend/src/components/MkPagination.vue
+++ packages/frontend/src/components/MkPagination.vue
@@ -515,12 +515,18 @@ const prepend = (item: MisskeyEntity): void => {
 
 	if (
 		!isPausingUpdate.value && // タブがバックグラウンドの時/スクロール調整中はキューに追加する
-		queueSize.value === 0 && // キューに残っている場合はキューに追加する
 		active.value && // keepAliveで隠されている間はキューに追加する
 		!backed // 先頭に表示されていない時はキューに追加する
 	) {
 		if (items.value.has(item.id)) return; // 既にタイムラインにある場合は何もしない
-		unshiftItems([item]);
+		if (queueSize.value === 0) return unshiftItems([item]);
+		if (queueSize.value <= 5) {
+			// キューが5つ以下の時はexecuteQueueを呼んでしまう
+			prependQueue(item);
+			executeQueue();
+			return;
+		}
+		prependQueue(item);
 	} else {
 		prependQueue(item);
 	}
