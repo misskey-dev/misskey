4bef4953b89e448eaa5634a5c0d788d2356d2af0
tamaina <tamaina@hotmail.co.jp>
2023-07-17 08:13:46 +0000

    15%

--- packages/frontend/src/components/MkPagination.vue
+++ packages/frontend/src/components/MkPagination.vue
@@ -184,7 +184,7 @@ watch([() => props.pagination.reversed, $$(scrollableElement)], () => {
 		weakBacked = entries[0].isIntersecting;
 	}, {
 		root: scrollableElement,
-		rootMargin: props.pagination.reversed ? '-110% 0px 110% 0px' : '110% 0px -110% 0px',
+		rootMargin: props.pagination.reversed ? '-115% 0px 115% 0px' : '115% 0px -115% 0px',
 		threshold: [0],
 	});
 	console.log('new scrollObserver', scrollObserver);
