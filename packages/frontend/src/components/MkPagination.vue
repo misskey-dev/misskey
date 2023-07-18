b0c6675ef331c6f5719546acb5ebae532e5196f4
tamaina <tamaina@hotmail.co.jp>
2023-07-18 06:55:08 +0000

    :art:

--- packages/frontend/src/components/MkPagination.vue
+++ packages/frontend/src/components/MkPagination.vue
@@ -41,7 +41,7 @@
 import { computed, ComputedRef, isRef, nextTick, onActivated, onBeforeUnmount, onDeactivated, onMounted, ref, watch } from 'vue';
 import * as misskey from 'misskey-js';
 import * as os from '@/os';
-import { isBottomVisible, isTopVisible, getScrollContainer, scrollToBottom, scrollToTop, scrollBy } from '@/scripts/scroll';
+import { isBottomVisible, isTopVisible, getScrollContainer, scrollToBottom, scrollToTop } from '@/scripts/scroll';
 import { useDocumentVisibility } from '@/scripts/use-document-visibility';
 import MkButton from '@/components/MkButton.vue';
 import { defaultStore } from '@/store';
@@ -428,10 +428,6 @@ function visibilityChange() {
 		timerForSetPause.value = window.setTimeout(() => {
 			isPausingUpdate.value = true;
 			timerForSetPause.value = null;
-			if (!backed) {
-				scrollBy(scrollableElement, { top: 32, behavior: 'instant' });
-				backed = true;
-			}
 		},
 		BACKGROUND_PAUSE_WAIT_SEC * 1000);
 	} else { // 'visible'
