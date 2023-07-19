aded685443443154520ae8f4da95408f0f3bde94
Yangjin Cho <7033942cyj@dongguk.edu>
2023-07-19 12:31:38 +0900

    fix(frontend): Fix cat ears are awkward on reply modal (#11309)
    * fix(frontend): #11279 adjusted margin & padding
    
    * Revert "fix(frontend): #11279 adjusted margin & padding"
    
    This reverts commit e82a2d482a916aade7bb6472b45fb1665f7456dd.
    
    * fix(frontend): adjusted margin & padding

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -907,6 +907,7 @@ defineExpose({
 	display: flex;
 	flex-wrap: nowrap;
 	gap: 4px;
+	margin-bottom: -10px;
 }
 
 .headerLeft {
@@ -1024,7 +1025,7 @@ defineExpose({
 }
 
 .targetNote {
-	padding: 0 20px 16px 20px;
+	padding: 10px 20px 16px 20px;
 }
 
 .withQuote {
