76e93bc6baf3ba4e6a13bcf88fddbf1da1fa77bc
Soli0222 <personal@str08.net>
2023-07-22 12:46:57 +0000

    first commit

---
+++ packages/backend/migration/1690029913007-isSheep.js
@@ -0,0 +1,15 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class IsSheep1690029913007 {
+    name = 'IsSheep1690029913007'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "user" ADD "isSheep" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`COMMENT ON COLUMN "user"."isSheep" IS 'Whether the User is a sheep.'`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`COMMENT ON COLUMN "user"."isSheep" IS 'Whether the User is a sheep.'`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isSheep"`);
+    }
+}
