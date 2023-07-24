c2633e807ffe0c3ccf0862be83b2c9d0ef862d24
anatawa12 <anatawa12@icloud.com>
2023-07-24 15:14:13 +0900

    feat(frontend): create admin only webhook

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -2144,6 +2144,8 @@ export interface Locale {
             "renote": string;
             "reaction": string;
             "mention": string;
+            "usersLabel": string;
+            "usersCaption": string;
         };
     };
 }
