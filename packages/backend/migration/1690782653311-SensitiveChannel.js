c5cbbbdc68c0e76a6e3a78d9119d8891bcf3969e
anatawa12 <anatawa12@icloud.com>
2023-07-31 16:51:41 +0900

    chore: license header for each file

--- packages/backend/migration/1690782653311-SensitiveChannel.js
+++ packages/backend/migration/1690782653311-SensitiveChannel.js
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and other misskey contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 export class SensitiveChannel1690782653311 {
 	name = 'SensitiveChannel1690782653311'
 
