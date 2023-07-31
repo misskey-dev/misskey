a52f63ec6a94cb61cdd3ae4130f10439ef8188d6
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-07-31 05:22:57 +0200

    fix(frontend, misskey-js): make block switch work (#11429)
    * fix(frontend, misskey-js): make block switch work
    
    * Update CHANGELOG.md

--- packages/misskey-js/src/entities.ts
+++ packages/misskey-js/src/entities.ts
@@ -346,6 +346,11 @@ export type DetailedInstanceMetadata = LiteInstanceMetadata & {
 
 export type InstanceMetadata = LiteInstanceMetadata | DetailedInstanceMetadata;
 
+export type AdminInstanceMetadata = DetailedInstanceMetadata & {
+	// TODO: There are more fields.
+	blockedHosts: string[];
+};
+
 export type ServerInfo = {
 	machine: string;
 	cpu: {
@@ -482,7 +487,7 @@ export type Blocking = {
 
 export type Instance = {
 	id: ID;
-	caughtAt: DateString;
+	firstRetrievedAt: DateString;
 	host: string;
 	usersCount: number;
 	notesCount: number;
@@ -496,6 +501,7 @@ export type Instance = {
 	lastCommunicatedAt: DateString;
 	isNotResponding: boolean;
 	isSuspended: boolean;
+	isBlocked: boolean;
 	softwareName: string | null;
 	softwareVersion: string | null;
 	openRegistrations: boolean | null;
