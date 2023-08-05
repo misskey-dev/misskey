05f37c7330dadd7e043d78851ba54d1684861304
anatawa12 <anatawa12@icloud.com>
2023-08-05 20:48:37 +0900

    feat: report abuse for renote

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -656,6 +656,7 @@ export interface Locale {
     "sample": string;
     "abuseReports": string;
     "reportAbuse": string;
+    "reportAbuseRenote": string;
     "reportAbuseOf": string;
     "fillAbuseReportDescription": string;
     "abuseReported": string;
