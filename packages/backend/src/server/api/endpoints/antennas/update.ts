ebb90aa9393a2c18b9fe25151c2404adb92b3b4c
White-Green <43771790+White-Green@users.noreply.github.com>
2023-08-06 00:08:25 +0900

    fix: antennas validation

--- packages/backend/src/server/api/endpoints/antennas/update.ts
+++ packages/backend/src/server/api/endpoints/antennas/update.ts
@@ -83,6 +83,9 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 		private globalEventService: GlobalEventService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
+			if (ps.keywords.flat().every(x => x === '') && ps.excludeKeywords.flat().every(x => x === '')) {
+				throw new Error('either keywords or excludeKeywords is required.');
+			}
 			// Fetch the antenna
 			const antenna = await this.antennasRepository.findOneBy({
 				id: ps.antennaId,
