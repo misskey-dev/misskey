de956d8deeaa2a035321d48f7a8dd31c982ee745
tamaina <tamaina@hotmail.co.jp>
2023-08-09 03:39:03 +0000

    fix

--- packages/frontend/src/scripts/entity-manager.ts
+++ packages/frontend/src/scripts/entity-manager.ts
@@ -195,7 +195,7 @@ export class NoteManager {
         } else {
             if (this.isDebuggerEnabled) console.log('NoteManager: get note (cached)', id, this.notesComputed.get(id), this.notesSource.get(id)?.value);
         }
-        return computed(() => this.notesComputed.get(id)!.value);
+        return computed(() => this.notesComputed.get(id)?.value ?? null);
     }
 
     /**
