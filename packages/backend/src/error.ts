f82dfdfbd26286c87beb33b5a1e41f636480945b
tamaina <tamaina@hotmail.co.jp>
2023-08-11 10:54:15 +0000

    add SPDX

--- packages/backend/src/error.ts
+++ packages/backend/src/error.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: MomentQYC and other misskey contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 export function ErrorHandling(message: string): Error {
 	const error = new Error(message);
 	if (process.env.NODE_ENV === 'production') {
