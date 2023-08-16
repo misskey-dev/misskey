346fb977b1406911ea2658aa665e3df9ea1af222
anatawa12 <anatawa12@icloud.com>
2023-08-16 19:07:08 +0900

    feat(frontend): detect lossless webp and recompress as lossless

--- packages/frontend/src/scripts/upload/compress-config.ts
+++ packages/frontend/src/scripts/upload/compress-config.ts
@@ -13,7 +13,7 @@ const compressTypeMapFallback = {
 	'lossless': { quality: 1, mimeType: 'image/png' },
 } as const;
 
-const prefferedCompressKindMap = {
+const inputCompressKindMap = {
 	'image/jpeg': 'lossy',
 	'image/png': 'lossless',
 	'image/webp': 'lossy',
@@ -23,12 +23,39 @@ const prefferedCompressKindMap = {
 const reiszeSizeConfig = { maxWidth: 2048, maxHeight: 2048 } as const;
 const noRseizeSizeConfig = { maxWidth: Number.MAX_SAFE_INTEGER, maxHeight: Number.MAX_SAFE_INTEGER } as const;
 
-export async function getCompressionConfig(file: File): Promise<BrowserImageResizerConfig | undefined> {
-	// TODO: 可能ならwebpの可逆/非可逆の判定を行いたい
-	let compressKind: 'lossy' | 'lossless' | undefined = prefferedCompressKindMap[file.type];
-	if (!compressKind) return;
-	if (await isAnimated(file)) return;
+async function isLosslessWebp(file: Blob): Promise<boolean> {
+	// file header
+	//   'RIFF': u32 @ 0x00
+	//   file size: u32 @ 0x04
+	//   'WEBP': u32 @ 0x08
+	// for simple lossless
+	//   'VP8L': u32 @ 0x0C
+	// so read 16 bytes and check those three magic numbers
+	const buffer = new Uint8Array(await file.slice(0, 16).arrayBuffer());
 
+	const header = 'RIFF\x00\x00\x00\x00WEBPVP8L';
+	for (let i = 0; i < header.length; i++) {
+		const code = header.charCodeAt(i);
+		if (code === 0) continue;
+		if (buffer[i] !== code) return false;
+	}
+	return true;
+}
+
+async function inputImageKind(file: File): Promise<'lossy' | 'lossless' | undefined> {
+	let compressKind: 'lossy' | 'lossless' | undefined = inputCompressKindMap[file.type];
+	if (!compressKind) return; // unknown image format
+	if (await isAnimated(file)) return; // animated image format
+	// WEBPs can be lossless
+	if (await isLosslessWebp(file)) compressKind = 'lossless';
+	return compressKind;
+}
+
+export async function getCompressionConfig(file: File): Promise<BrowserImageResizerConfig | undefined> {
+	const inputCompressKind = await inputImageKind(file);
+	if (!inputCompressKind) return undefined;
+
+	let compressKind: 'lossy' | 'lossless';
 	let resize: boolean;
 
 	switch (defaultStore.state.imageCompressionMode) {
@@ -36,9 +63,11 @@ export async function getCompressionConfig(file: File): Promise<BrowserImageResi
 		case null:
 		default:
 			resize = true;
+			compressKind = inputCompressKind;
 			break;
 		case 'noResizeCompress':
 			resize = false;
+			compressKind = inputCompressKind;
 			break;
 		case 'resizeCompressLossy':
 			resize = true;
@@ -51,13 +80,22 @@ export async function getCompressionConfig(file: File): Promise<BrowserImageResi
 	}
 
 	const webpSupported = isWebpSupported();
-	if (!webpSupported && !resize && file.type === 'image/webp') {
-		// input is webp and resize is disabled but webp compression is not enabled: it's liley to have lower compression
-		return undefined;
-	}
 
 	const imgFormatConfig = (webpSupported ? compressTypeMap : compressTypeMapFallback)[compressKind];
 	const sizeConfig = resize ? reiszeSizeConfig : noRseizeSizeConfig;
+
+	if (!resize) {
+		// we don't resize images so we may omit recompression
+		if (imgFormatConfig.mimeType === file.type && inputCompressKind === compressKind) {
+			// we don't have to recompress already compressed to preferred image format.
+			return undefined;
+		}
+
+		if (!webpSupported && file.type === 'image/webp' && compressKind === 'lossless') {
+			// lossless webp -> png recompression likely to increase image size so don't recompress
+			return undefined;
+		}
+	}
 
 	return {
 		debug: true,
