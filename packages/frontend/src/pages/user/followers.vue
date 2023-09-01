1162e51f6b8a92df8cf8f153faca07496b9b25ce
syuilo <Syuilotan@yahoo.co.jp>
2023-09-01 17:30:27 +0900

    Update followers.vue

--- packages/frontend/src/pages/user/followers.vue
+++ packages/frontend/src/pages/user/followers.vue
@@ -21,7 +21,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 <script lang="ts" setup>
 import { computed, watch } from 'vue';
 import * as Misskey from 'misskey-js';
-import * as Misskey from 'misskey-js';
 import XFollowList from './follow-list.vue';
 import * as os from '@/os';
 import { definePageMetadata } from '@/scripts/page-metadata';
