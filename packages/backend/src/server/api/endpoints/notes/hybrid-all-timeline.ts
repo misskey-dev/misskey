06f411ab668c5b5aa038a66cdad1d37066642118
mattyatea <mattyacocacora0@gmail.com>
2023-09-03 21:32:31 +0900

    fix: いろいろ

--- packages/backend/src/server/api/endpoints/notes/hybrid-all-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/hybrid-all-timeline.ts
@@ -77,7 +77,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			const query = this.queryService.makePaginationQuery(this.notesRepository.createQueryBuilder('note'),
 				ps.sinceId, ps.untilId, ps.sinceDate, ps.untilDate)
 				.andWhere('note.id > :minId', { minId: this.idService.genId(new Date(Date.now() - (1000 * 60 * 60 * 24 * 10))) }) // 10日前まで
-				.andWhere('(note.visibility = \'public\' OR note.visibility = \'home\') AND (note.userHost IS NULL)')
+				.andWhere('(note.visibility = \'home\') AND (note.userHost IS NULL)')
 				.innerJoinAndSelect('note.user', 'user')
 				.leftJoinAndSelect('note.reply', 'reply')
 				.leftJoinAndSelect('note.renote', 'renote')
