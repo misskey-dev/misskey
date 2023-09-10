874971670069976cb2dd6a7bd41ecc3f456379b8
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-09-10 15:14:31 +0900

    enhance: センシティブチャンネルはユーザーのノート一覧から除外 (#11797)
    * enhance: センシティブチャンネルはユーザーのノート一覧から除外
    
    * READMEに明記
    
    * Update notes.ts
    
    ---------
    
    Co-authored-by: syuilo <Syuilotan@yahoo.co.jp>

--- packages/backend/src/server/api/endpoints/users/notes.ts
+++ packages/backend/src/server/api/endpoints/users/notes.ts
@@ -80,8 +80,11 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				.innerJoinAndSelect('note.user', 'user')
 				.leftJoinAndSelect('note.reply', 'reply')
 				.leftJoinAndSelect('note.renote', 'renote')
+				.leftJoinAndSelect('note.channel', 'channel')
 				.leftJoinAndSelect('reply.user', 'replyUser')
 				.leftJoinAndSelect('renote.user', 'renoteUser');
+
+			query.andWhere('channel.isSensitive = false');
 
 			this.queryService.generateVisibilityQuery(query, me);
 			if (me) {
