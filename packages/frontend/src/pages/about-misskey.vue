0111f99b326ef7fb549ed3190d4bb33a53d31c3d
mattyatea <mattyacocacora0@gmail.com>
2023-09-17 18:01:03 +0900

    feat: 当フォークのソースコードを表示させた

--- packages/frontend/src/pages/about-misskey.vue
+++ packages/frontend/src/pages/about-misskey.vue
@@ -34,6 +34,11 @@ SPDX-License-Identifier: AGPL-3.0-only
 							{{ i18n.ts._aboutMisskey.source }}
 							<template #suffix>GitHub</template>
 						</FormLink>
+						<FormLink to="https://github.com/mattyatea/misskey" external>
+							<template #icon><i class="ti ti-code"></i></template>
+							{{ i18n.ts._aboutMisskey.forksource }}
+							<template #suffix>GitHub</template>
+						</FormLink>
 						<FormLink to="https://crowdin.com/project/misskey" external>
 							<template #icon><i class="ti ti-language-hiragana"></i></template>
 							{{ i18n.ts._aboutMisskey.translation }}
