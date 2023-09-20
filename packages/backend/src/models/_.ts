f55e676b26fe687bcfd76a7714c3c1e1c7900875
taichanne30 <dev@taichan.site>
2023-09-21 07:15:00 +0900

    add NoteNotification

--- packages/backend/src/models/_.ts
+++ packages/backend/src/models/_.ts
@@ -36,6 +36,7 @@ import { MiNoteFavorite } from '@/models/NoteFavorite.js';
 import { MiNoteReaction } from '@/models/NoteReaction.js';
 import { MiNoteThreadMuting } from '@/models/NoteThreadMuting.js';
 import { MiNoteUnread } from '@/models/NoteUnread.js';
+import { MiNoteNotification } from '@/models/NoteNotification.js';
 import { MiPage } from '@/models/Page.js';
 import { MiPageLike } from '@/models/PageLike.js';
 import { MiPasswordResetRequest } from '@/models/PasswordResetRequest.js';
@@ -104,6 +105,7 @@ export {
 	MiNoteReaction,
 	MiNoteThreadMuting,
 	MiNoteUnread,
+	MiNoteNotification,
 	MiPage,
 	MiPageLike,
 	MiPasswordResetRequest,
