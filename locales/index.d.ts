2bdeb81866fdfef77e711696af8fa9c3887d1ceb
Camilla Ett <camilla.ett@gmail.com>
2023-09-28 17:28:07 +0900

    fix(frontend): 各localesからsfx/chat sfx/chatbgを削除

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -1810,8 +1810,6 @@ export interface Locale {
         "note": string;
         "noteMy": string;
         "notification": string;
-        "chat": string;
-        "chatBg": string;
         "antenna": string;
         "channel": string;
     };
