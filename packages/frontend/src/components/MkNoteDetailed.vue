24bab4ca5d52ccbaa4e39c92703dfcd21f4d7294
yu <ybw2016v@126.com>
2023-10-01 11:57:36 +0800

    自动展开回复

--- packages/frontend/src/components/MkNoteDetailed.vue
+++ packages/frontend/src/components/MkNoteDetailed.vue
@@ -12,9 +12,9 @@ SPDX-License-Identifier: AGPL-3.0-only
 	:class="$style.root"
 >
 	<div v-if="appearNote.reply && appearNote.reply.replyId">
-		<div v-if="!conversationLoaded" style="padding: 16px">
+		<!-- <div v-if="!conversationLoaded" style="padding: 16px">
 			<MkButton style="margin: 0 auto;" primary rounded @click="loadConversation">{{ i18n.ts.loadConversation }}</MkButton>
-		</div>
+		</div> -->
 		<MkNoteSub v-for="note in conversation" :key="note.id" :class="$style.replyToMore" :note="note"/>
 	</div>
 	<MkNoteSub v-if="appearNote.reply" :note="appearNote.reply" :class="$style.replyTo"/>
@@ -137,9 +137,9 @@ SPDX-License-Identifier: AGPL-3.0-only
 	</div>
 	<div>
 		<div v-if="tab === 'replies'" :class="$style.tab_replies">
-			<div v-if="!repliesLoaded" style="padding: 16px">
+			<!-- <div v-if="!repliesLoaded" style="padding: 16px">
 				<MkButton style="margin: 0 auto;" primary rounded @click="loadReplies">{{ i18n.ts.loadReplies }}</MkButton>
-			</div>
+			</div> -->
 			<MkNoteSub v-for="note in replies" :key="note.id" :note="note" :class="$style.reply" :detail="true"/>
 		</div>
 		<div v-else-if="tab === 'renotes'" :class="$style.tab_renotes">
@@ -495,9 +495,9 @@ function blur() {
 	el.value.blur();
 }
 
-const repliesLoaded = ref(false);
+// const repliesLoaded = ref(false);
 function loadReplies() {
-	repliesLoaded.value = true;
+	// repliesLoaded.value = true;
 	os.api('notes/children', {
 		noteId: appearNote.id,
 		limit: 30,
@@ -506,15 +506,20 @@ function loadReplies() {
 	});
 }
 
-const conversationLoaded = ref(false);
+// const conversationLoaded = ref(false);
 function loadConversation() {
-	conversationLoaded.value = true;
+	// conversationLoaded.value = true;
 	os.api('notes/conversation', {
 		noteId: appearNote.replyId,
 	}).then(res => {
 		conversation.value = res.reverse();
 	});
 }
+
+onMounted(()=>{
+	loadReplies();
+	loadConversation();
+});
 </script>
 
 <style lang="scss" module>
