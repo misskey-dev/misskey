bb775d2e97342932c315e4f60c14df3eac7f00d2
atsu1125 <atsuchan@atsuchan.page>
2023-10-19 18:57:53 +0900

    fix(frontend): Recieve Unrenote lint fixing

--- packages/frontend/src/scripts/use-note-capture.ts
+++ packages/frontend/src/scripts/use-note-capture.ts
@@ -84,7 +84,7 @@ export function useNoteCapture(props: {
 		if (connection) {
 			// TODO: このノートがストリーミング経由で流れてきた場合のみ sr する
 			connection.send(document.body.contains(props.rootEl.value) ? 'sr' : 's', { id: note.value.id });
-			if (pureNote.value.id != note.value.id) connection.send('s', { id: pureNote.value.id });
+			if (pureNote.value.id !== note.value.id) connection.send('s', { id: pureNote.value.id });
 			if (withHandler) connection.on('noteUpdated', onStreamNoteUpdated);
 		}
 	}
