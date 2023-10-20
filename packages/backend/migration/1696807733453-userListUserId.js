26ccb2c17484d28b274b2b395ddadb90fb2283af
riku6460 <17585784+riku6460@users.noreply.github.com>
2023-10-21 04:47:06 +0900

    5億年かかるマイグレを修正 (MisskeyIO#190)

--- packages/backend/migration/1696807733453-userListUserId.js
+++ packages/backend/migration/1696807733453-userListUserId.js
@@ -8,11 +8,7 @@ export class UserListUserId1696807733453 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user_list_membership" ADD "userListUserId" character varying(32) NOT NULL DEFAULT ''`);
-				const memberships = await queryRunner.query(`SELECT "id", "userListId" FROM "user_list_membership"`);
-				for(let i = 0; i < memberships.length; i++) {
-					const userList = await queryRunner.query(`SELECT "userId" FROM "user_list" WHERE "id" = $1`, [memberships[i].userListId]);
-					await queryRunner.query(`UPDATE "user_list_membership" SET "userListUserId" = $1 WHERE "id" = $2`, [userList[0].userId, memberships[i].id]);
-				}
+        await queryRunner.query(`UPDATE "user_list_membership" SET "userListUserId" = "user_list"."userId" FROM "user_list" WHERE "user_list_membership"."userListId" = "user_list"."id"`);
     }
 
     async down(queryRunner) {
