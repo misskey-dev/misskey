5b950396d36d4b870b5d7c1ff5b47fc90ef0a79b
syuilo <Syuilotan@yahoo.co.jp>
2023-10-21 17:55:40 +0900

    .js

--- packages/frontend/test/home.test.ts
+++ packages/frontend/test/home.test.ts
@@ -7,8 +7,8 @@ import { describe, test, assert, afterEach } from 'vitest';
 import { render, cleanup, type RenderResult } from '@testing-library/vue';
 import './init';
 import type * as Misskey from 'misskey-js';
-import { directives } from '@/directives';
-import { components } from '@/components/index';
+import { directives } from '@/directives/index.js';
+import { components } from '@/components/index.js';
 import XHome from '@/pages/user/home.vue';
 
 describe('XHome', () => {
