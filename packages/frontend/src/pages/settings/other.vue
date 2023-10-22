1aeae6217e1906f94109491882fa723284f0ccb0
syuilo <Syuilotan@yahoo.co.jp>
2023-10-22 16:12:01 +0900

    chore(frontend): tweak settings ui

--- packages/frontend/src/pages/settings/other.vue
+++ packages/frontend/src/pages/settings/other.vue
@@ -73,6 +73,13 @@ SPDX-License-Identifier: AGPL-3.0-only
 	<FormSection>
 		<FormLink to="/registry"><template #icon><i class="ti ti-adjustments"></i></template>{{ i18n.ts.registry }}</FormLink>
 	</FormSection>
+
+	<FormSection>
+		<div class="_gaps_s">
+			<MkButton danger @click="updateRepliesAll(true)"><i class="ti ti-messages"></i> {{ i18n.ts.showRepliesToOthersInTimelineAll }}</MkButton>
+			<MkButton danger @click="updateRepliesAll(false)"><i class="ti ti-messages-off"></i> {{ i18n.ts.hideRepliesToOthersInTimelineAll }}</MkButton>
+		</div>
+	</FormSection>
 </div>
 </template>
 
@@ -136,6 +143,15 @@ async function reloadAsk() {
 	if (canceled) return;
 
 	unisonReload();
+}
+
+async function updateRepliesAll(withReplies: boolean) {
+	const { canceled } = os.confirm({
+		type: 'warning',
+		text: withReplies ? i18n.ts.confirmShowRepliesAll : i18n.ts.confirmHideRepliesAll,
+	});
+	if (canceled) return;
+	await os.api('following/update-all', { withReplies });
 }
 
 watch([
