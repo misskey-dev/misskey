c76b8301827dcd12567e22111befb4c1023460c5
mattyatea <mattyacocacora0@gmail.com>
2023-10-27 06:22:00 +0900

    Feat: アバターデコレーションの重複判定をするようにした
    Signed-off-by: mattyatea <mattyacocacora0@gmail.com>

--- packages/backend/src/server/api/endpoints/admin/avatar-decorations/create.ts
+++ packages/backend/src/server/api/endpoints/admin/avatar-decorations/create.ts
@@ -6,12 +6,20 @@
 import { Injectable } from '@nestjs/common';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import { AvatarDecorationService } from '@/core/AvatarDecorationService.js';
+import { ApiError } from '@/server/api/error.js';
 
 export const meta = {
 	tags: ['admin'],
 
 	requireCredential: true,
 	requireModerator: true,
+	errors: {
+		duplicateName: {
+			message: 'Duplicate name.',
+			code: 'DUPLICATE_NAME',
+			id: 'f7a3462c-4e6e-4069-8421-b9bd4f4c3975',
+		},
+	},
 } as const;
 
 export const paramDef = {
@@ -33,6 +41,8 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		private avatarDecorationService: AvatarDecorationService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
+			const isDuplicate = await this.avatarDecorationService.checkDuplicate(ps.name);
+			if (isDuplicate) throw new ApiError(meta.errors.duplicateName);
 			await this.avatarDecorationService.create({
 				name: ps.name,
 				description: ps.description,
