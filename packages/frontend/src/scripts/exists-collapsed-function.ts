01a3c5add94f5090018f2345f9424a0b2dd15e62
Fairy-Phy <phy.public@gmail.com>
2023-10-31 22:07:17 +0900

    change: for of -> some

--- packages/frontend/src/scripts/exists-collapsed-function.ts
+++ packages/frontend/src/scripts/exists-collapsed-function.ts
@@ -3,9 +3,9 @@ import type { MfmNode } from 'mfm-js';
 const COLLAPSED_FUNCTIONS = ['x2', 'x3', 'x4', 'scale'];
 
 export function existsCollapsedFunction(nodes: MfmNode[]): boolean {
-	for (const node of nodes) {
+	return nodes.some(node => {
 		if (node.type === 'fn' && COLLAPSED_FUNCTIONS.some(f => node.props.name === f)) return true;
-		if (node.children && existsCollapsedFunction(node.children)) return true;
-	}
-	return false;
+		if (node.children) return existsCollapsedFunction(node.children);
+		return false;
+	});
 }
