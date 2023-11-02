ed699b4aedc8bec8df13e2a6777298992b0f0f95
syuilo <Syuilotan@yahoo.co.jp>
2023-11-02 18:12:01 +0900

    Revert "enhance(frontend): 「内容を隠す」でリアクションも隠れるように"
    This reverts commit e85b8217c0eda4b0cb2ebf5642cabd2af7212140.

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -84,7 +84,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 				</div>
 				<MkA v-if="appearNote.channel && !inChannel" :class="$style.channel" :to="`/channels/${appearNote.channel.id}`"><i class="ti ti-device-tv"></i> {{ appearNote.channel.name }}</MkA>
 			</div>
-			<MkReactionsViewer v-show="appearNote.cw == null || showContent" :note="appearNote" :maxNumber="16">
+			<MkReactionsViewer :note="appearNote" :maxNumber="16">
 				<template #more>
 					<div :class="$style.reactionOmitted">{{ i18n.ts.more }}</div>
 				</template>
