3a213e483f4c6be7a44b56ddc5df01c85811a3b9
anatawa12 <anatawa12@icloud.com>
2023-11-23 18:56:20 +0900

    Hard mute (#12376)
    * feat(backend,misskey-js): hard mute storage in backend
    
    * fix(backend,misskey-js): mute word record type
    
    * chore(frontend): generalize XWordMute
    
    * feat(frontend): configure hard mute
    
    * feat(frontend): hard mute notes on the timelines
    
    * lint(backend,frontend): fix lint failure
    
    * chore(misskey-js): update api.md
    
    * fix(backend): test failure
    
    * chore(frontend): check word mute for reply
    
    * chore: limit hard mute count

--- packages/misskey-js/src/api.types.ts
+++ packages/misskey-js/src/api.types.ts
@@ -430,7 +430,8 @@ export type Endpoints = {
 		injectFeaturedNote?: boolean;
 		receiveAnnouncementEmail?: boolean;
 		alwaysMarkNsfw?: boolean;
-		mutedWords?: string[][];
+		mutedWords?: (string[] | string)[];
+		hardMutedWords?: (string[] | string)[];
 		notificationRecieveConfig?: any;
 		emailNotificationTypes?: string[];
 		alsoKnownAs?: string[];
