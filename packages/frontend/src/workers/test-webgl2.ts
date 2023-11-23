84a7f12e7f017e5c76c0403d9ce8222779d7b34f
まっちゃとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2023-11-24 03:26:28 +0900

    enhance(frontend): ブラウザの互換性向上 (MisskeyIO#257)

--- packages/frontend/src/workers/test-webgl2.ts
+++ packages/frontend/src/workers/test-webgl2.ts
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-const canvas = globalThis.OffscreenCanvas && new OffscreenCanvas(1, 1);
-// 環境によってはOffscreenCanvasが存在しないため
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+const canvas = typeof OffscreenCanvas !== 'undefined'
+	? new OffscreenCanvas(1, 1)
+	: undefined;
 const gl = canvas?.getContext('webgl2');
 if (gl) {
 	postMessage({ result: true });
