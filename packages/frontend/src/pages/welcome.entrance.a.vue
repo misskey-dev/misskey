27dcc4e3f92b559bb353c93c2f91539637045197
FruitRiin <nassii74@gmail.com>
2023-11-25 22:39:04 +0900

    色々修正

--- packages/frontend/src/pages/welcome.entrance.a.vue
+++ packages/frontend/src/pages/welcome.entrance.a.vue
@@ -9,7 +9,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 	<XTimeline class="tl"/>
 	<div class="shape1"></div>
 	<div class="shape2"></div>
-	<img src="/client-assets/misskey.svg" class="misskey"/>
+	<img :src="misskeysvg" class="misskey"/>
 	<div class="emojis">
 		<MkEmoji :normal="true" :noStyle="true" emoji="👍"/>
 		<MkEmoji :normal="true" :noStyle="true" emoji="❤"/>
@@ -38,6 +38,7 @@ import * as Misskey from 'misskey-js';
 import XTimeline from './welcome.timeline.vue';
 import MarqueeText from '@/components/MkMarquee.vue';
 import MkFeaturedPhotos from '@/components/MkFeaturedPhotos.vue';
+import misskeysvg from '/client-assets/misskey.svg';
 import MkInfo from '@/components/MkInfo.vue';
 import { instanceName } from '@/config.js';
 import * as os from '@/os.js';
@@ -52,7 +53,7 @@ let meta = $ref<Misskey.entities.Instance>();
 let instances = $ref<any[]>();
 
 function getInstanceIcon(instance): string {
-  return getProxiedImageUrl(instance.iconUrl, 'preview');
+	return getProxiedImageUrl(instance.iconUrl, 'preview');
 }
 
 os.api('meta', { detail: true }).then(_meta => {
--- packages/frontend/vite.config.local-dev.ts
+++ packages/frontend/vite.config.local-dev.ts
@@ -44,6 +44,36 @@ function toBase62(n: number): string {
 
 export function getConfig(): UserConfig {
 	return {
+		root: 'src',
+		publicDir: '../assets',
+		base: './',
+		server: {
+			host: '127.0.0.1',
+			port: 5173,
+			proxy: {
+				'/api': {
+					changeOrigin: true,
+					target: 'http://127.0.0.1:3000/',
+				},
+				'/assets': 'http://127.0.0.1:3000/',
+				'/files': 'http://127.0.0.1:3000/',
+				'/twemoji': 'http://127.0.0.1:3000/',
+				'/fluent-emoji': 'http://127.0.0.1:3000/',
+				'/sw.js': 'http://127.0.0.1:3000/',
+				'/streaming': {
+					target: 'ws://127.0.0.1:3000/',
+					ws: true,
+				},
+				'/favicon.ico': 'http://127.0.0.1:3000/',
+				'/identicon': {
+					target: 'http://127.0.0.1:3000/',
+					rewrite(path) {
+						return path.replace('@127.0.0.1:5173', '');
+					},
+
+				},
+			},
+		},
 
 		plugins: [
 			pluginVue({
