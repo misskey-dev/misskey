10a5394414cd812e0ab2f5d9575637bb3bbf245e
HidemaruOwO <hidemal@outlook.jp>
2023-11-28 18:59:19 +0900

    ✨ サーバーメトリクスを自分好みにカスタム (packages/frontend/src/widgets/server-metric/index.vue)

--- packages/frontend/src/widgets/server-metric/index.vue
+++ packages/frontend/src/widgets/server-metric/index.vue
@@ -7,15 +7,12 @@ SPDX-License-Identifier: AGPL-3.0-only
 	<MkContainer :showHeader="widgetProps.showHeader" :naked="widgetProps.transparent">
 		<template #icon><i class="ti ti-server"></i></template>
 		<template #header>{{ i18n.ts._widgets.serverMetric }}</template>
-		<template #func="{ buttonStyleClass }"><button class="_button" :class="buttonStyleClass" @click="toggleView()"><i
-					class="ti ti-selector"></i></button></template>
 
 		<div v-if="meta" data-cy-mkw-serverMetric class="mkw-serverMetric">
-			<XCpuMemory v-if="widgetProps.view === 0" :connection="connection" :meta="meta" />
-			<XNet v-else-if="widgetProps.view === 1" :connection="connection" :meta="meta" />
-			<XCpu v-else-if="widgetProps.view === 2" :connection="connection" :meta="meta" />
-			<XMemory v-else-if="widgetProps.view === 3" :connection="connection" :meta="meta" />
-			<XDisk v-else-if="widgetProps.view === 4" :connection="connection" :meta="meta" />
+			<XCpu :connection="connection" :meta="meta" />
+			<XMemory :connection="connection" :meta="meta" />
+			<XDisk :connection="connection" :meta="meta" />
+			<XNet :connection="connection" :meta="meta" />
 		</div>
 	</MkContainer>
 </template>
