25304f828c40c0d64a391d6af5b9febc95bdef30
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2023-11-30 14:35:56 +0900

    enhance: 設定したタグをトレンドに表示させないようにする項目を管理画面で設定できるように (#12512)
    * enhance: hiddenTagsを管理画面で設定できるように
    
    * Update locales/ja-JP.yml
    
    Co-authored-by: syuilo <Syuilotan@yahoo.co.jp>
    
    ---------
    
    Co-authored-by: syuilo <Syuilotan@yahoo.co.jp>

--- packages/frontend/src/pages/admin/moderation.vue
+++ packages/frontend/src/pages/admin/moderation.vue
@@ -39,6 +39,11 @@ SPDX-License-Identifier: AGPL-3.0-only
 						<template #label>{{ i18n.ts.sensitiveWords }}</template>
 						<template #caption>{{ i18n.ts.sensitiveWordsDescription }}<br>{{ i18n.ts.sensitiveWordsDescription2 }}</template>
 					</MkTextarea>
+
+					<MkTextarea v-model="hiddenTags">
+						<template #label>{{ i18n.ts.hiddenTags }}</template>
+						<template #caption>{{ i18n.ts.hiddenTagsDescription }}</template>
+					</MkTextarea>
 				</div>
 			</FormSuspense>
 		</MkSpacer>
@@ -72,6 +77,7 @@ import FormLink from '@/components/form/link.vue';
 let enableRegistration: boolean = $ref(false);
 let emailRequiredForSignup: boolean = $ref(false);
 let sensitiveWords: string = $ref('');
+let hiddenTags: string = $ref('');
 let preservedUsernames: string = $ref('');
 let tosUrl: string | null = $ref(null);
 let privacyPolicyUrl: string | null = $ref(null);
@@ -81,6 +87,7 @@ async function init() {
 	enableRegistration = !meta.disableRegistration;
 	emailRequiredForSignup = meta.emailRequiredForSignup;
 	sensitiveWords = meta.sensitiveWords.join('\n');
+	hiddenTags = meta.hiddenTags.join('\n');
 	preservedUsernames = meta.preservedUsernames.join('\n');
 	tosUrl = meta.tosUrl;
 	privacyPolicyUrl = meta.privacyPolicyUrl;
@@ -93,6 +100,7 @@ function save() {
 		tosUrl,
 		privacyPolicyUrl,
 		sensitiveWords: sensitiveWords.split('\n'),
+		hiddenTags: hiddenTags.split('\n'),
 		preservedUsernames: preservedUsernames.split('\n'),
 	}).then(() => {
 		fetchInstance();
