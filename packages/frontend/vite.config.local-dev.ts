608f566383e0aa8925349b73c87fcfb9d6fb11c2
samunohito <46447427+samunohito@users.noreply.github.com>
2023-12-07 21:48:14 +0900

    fix vite.config.local-dev.ts

--- packages/frontend/vite.config.local-dev.ts
+++ packages/frontend/vite.config.local-dev.ts
@@ -1,5 +1,8 @@
+import dns from 'dns';
 import { defineConfig } from 'vite';
 import { getConfig } from './vite.config.js';
+
+dns.setDefaultResultOrder('ipv4first');
 
 const defaultConfig = getConfig();
 
@@ -10,31 +13,31 @@ const devConfig = {
 	publicDir: '../assets',
 	base: './',
 	server: {
-		host: '127.0.0.1',
+		host: 'localhost',
 		port: 5173,
 		proxy: {
 			'/api': {
 				changeOrigin: true,
-				target: 'http://127.0.0.1:3000/',
+				target: 'http://localhost:3000/',
 			},
-			'/assets': 'http://127.0.0.1:3000/',
-			'/files': 'http://127.0.0.1:3000/',
-			'/twemoji': 'http://127.0.0.1:3000/',
-			'/fluent-emoji': 'http://127.0.0.1:3000/',
-			'/sw.js': 'http://127.0.0.1:3000/',
+			'/assets': 'http://localhost:3000/',
+			'/files': 'http://localhost:3000/',
+			'/twemoji': 'http://localhost:3000/',
+			'/fluent-emoji': 'http://localhost:3000/',
+			'/sw.js': 'http://localhost:3000/',
 			'/streaming': {
-				target: 'ws://127.0.0.1:3000/',
+				target: 'ws://localhost:3000/',
 				ws: true,
 			},
-			'/favicon.ico': 'http://127.0.0.1:3000/',
+			'/favicon.ico': 'http://localhost:3000/',
 			'/identicon': {
-				target: 'http://127.0.0.1:3000/',
+				target: 'http://localhost:3000/',
 				rewrite(path) {
-					return path.replace('@127.0.0.1:5173', '');
+					return path.replace('@localhost:5173', '');
 				},
 			},
-			'/url': 'http://127.0.0.1:3000',
-			'/proxy': 'http://127.0.0.1:3000',
+			'/url': 'http://localhost:3000',
+			'/proxy': 'http://localhost:3000',
 		},
 	},
 	build: {
