4b00c12d74e7483c5edc3d5e6b5f0e699979c9e4
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2023-12-09 05:08:36 +0000

    Revert "Revert "chore: 開発モードでフロントエンドとバックエンドを独立して起動するようにする（再） (#12593)""
    This reverts commit 5c15a7fa201db99cd8930affe02a2a915ea4924f.

--- packages/frontend/src/pages/welcome.entrance.a.vue
+++ packages/frontend/src/pages/welcome.entrance.a.vue
@@ -9,7 +9,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 	<XTimeline class="tl"/>
 	<div class="shape1"></div>
 	<div class="shape2"></div>
-	<img src="/client-assets/misskey.svg" class="misskey"/>
+	<img :src="misskeysvg" class="misskey"/>
 	<div class="emojis">
 		<MkEmoji :normal="true" :noStyle="true" emoji="👍"/>
 		<MkEmoji :normal="true" :noStyle="true" emoji="❤"/>
@@ -38,6 +38,7 @@ import * as Misskey from 'misskey-js';
 import XTimeline from './welcome.timeline.vue';
 import MarqueeText from '@/components/MkMarquee.vue';
 import MkFeaturedPhotos from '@/components/MkFeaturedPhotos.vue';
+import misskeysvg from '/client-assets/misskey.svg';
 import MkInfo from '@/components/MkInfo.vue';
 import { instanceName } from '@/config.js';
 import * as os from '@/os.js';
@@ -55,7 +56,6 @@ function getInstanceIcon(instance: Misskey.entities.FederationInstance): string 
 	if (!instance.iconUrl) {
 		return '';
 	}
-
 	return getProxiedImageUrl(instance.iconUrl, 'preview');
 }
 
