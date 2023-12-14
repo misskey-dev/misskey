bf1e66e0cb36e59b38d5472d759c1e79c50bc32f
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-12-14 15:14:21 +0900

    fix: devモードでキャッシュクリアするとページが表示されなくなる問題を修正

--- packages/frontend/src/scripts/clear-cache.ts
+++ packages/frontend/src/scripts/clear-cache.ts
@@ -6,6 +6,7 @@ import { fetchCustomEmojis } from '@/custom-emojis.js';
 export async function clearCache() {
 	os.waiting();
 	miLocalStorage.removeItem('locale');
+	miLocalStorage.removeItem('localeVersion');
 	miLocalStorage.removeItem('theme');
 	miLocalStorage.removeItem('emojis');
 	miLocalStorage.removeItem('lastEmojisFetchedAt');
