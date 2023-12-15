3f6597894a7cfb252b650483548cea1cb8bf87b8
sorairo <sorairo@shiosyakeyakini.info>
2023-12-15 23:41:47 +0000

    Fix(backend): JSONSchemaに不足しているパラメータを追加

--- packages/backend/src/models/json-schema/user.ts
+++ packages/backend/src/models/json-schema/user.ts
@@ -74,6 +74,14 @@ export const packedUserLiteSchema = {
 						format: 'url',
 						nullable: false, optional: false,
 					},
+					offsetX: {
+						type: 'number',
+						nullable: true, optional: true,
+					},
+					offsetY: {
+						type: 'number',
+						nullable: true, optional: true,
+					},
 				},
 			},
 		},
