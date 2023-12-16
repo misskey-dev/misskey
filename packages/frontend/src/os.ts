704ebcde767ad5d061e3ae2142f72ef0f90110af
FruitRiin <nassii74@gmail.com>
2023-12-16 13:24:00 +0900

    開いてるページで投稿先がチャンネルになってほしい

--- packages/frontend/src/os.ts
+++ packages/frontend/src/os.ts
@@ -25,6 +25,7 @@ import MkContextMenu from '@/components/MkContextMenu.vue';
 import { MenuItem } from '@/types/menu.js';
 import copyToClipboard from '@/scripts/copy-to-clipboard.js';
 import { showMovedDialog } from '@/scripts/show-moved-dialog.js';
+import {mainRouter} from "@/router.js";
 
 export const openingWindowsCount = ref(0);
 
@@ -601,6 +602,18 @@ export function post(props: Record<string, any> = {}): Promise<void> {
 		//       Vueが渡されたコンポーネントに内部的に__propsというプロパティを生やす影響で、
 		//       複数のpost formを開いたときに場合によってはエラーになる
 		//       もちろん複数のpost formを開けること自体Misskeyサイドのバグなのだが
+
+
+		const route = mainRouter.getCurrentPath().split("/")
+		if(route[1] === "channels")
+		props = {
+			...props,
+			// 本当はチャンネル名や色もほしいけどどっからとってこよう
+			channel: {
+				id: route[2]
+			}
+		}
+
 		let dispose;
 		popup(MkPostFormDialog, props, {
 			closed: () => {
