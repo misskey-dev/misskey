8cd1b9964428876d6d5b36b555d0db876d7862ca
1Step621 <onestep621@gmail.com>
2023-12-19 18:24:43 +0900

    fix cw button label

--- packages/frontend/src/components/MkCwButton.vue
+++ packages/frontend/src/components/MkCwButton.vue
@@ -42,7 +42,7 @@ const emit = defineEmits<{
 const label = computed(() => {
 	return concat([
 		props.text ? [i18n.t('_cw.chars', { count: props.text.length })] : [],
-		props.renote != null ? [i18n.ts.quote] : [],
+		props.renote ? [i18n.ts.quote] : [],
 		props.files.length !== 0 ? [i18n.t('_cw.files', { count: props.files.length })] : [],
 		props.poll != null ? [i18n.ts.poll] : [],
 	] as string[][]).join(' / ');
