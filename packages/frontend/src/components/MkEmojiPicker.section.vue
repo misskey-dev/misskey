1375a6fbdbd2b15f8b3b3f0ab40e7b18352984bc
mattyatea <mattyacocacora0@gmail.com>
2023-12-23 09:06:51 +0900

    fix: クソフォルダ改変

--- packages/frontend/src/components/MkEmojiPicker.section.vue
+++ packages/frontend/src/components/MkEmojiPicker.section.vue
@@ -4,59 +4,58 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<!-- このコンポーネントの要素のclassは親から利用されるのでむやみに弄らないこと -->
-<!-- フォルダの中にはカスタム絵文字だけ（Unicode絵文字もこっち） -->
-<section v-if="!hasChildSection" v-panel style="border-radius: 6px; border-bottom: 0.5px solid var(--divider);">
-	<header class="_acrylic" @click="shown = !shown">
-		<i class="toggle ti-fw" :class="shown ? 'ti ti-chevron-down' : 'ti ti-chevron-up'"></i> <slot></slot> (<i class="ti ti-icons"></i>:{{ emojis.length }})
-	</header>
-	<div v-if="shown" class="body">
-		<button
-			v-for="emoji in emojis"
-			:key="emoji"
-			:data-emoji="emoji"
-			class="_button item"
-			@pointerenter="computeButtonTitle"
-			@click="emit('chosen', emoji, $event)"
-		>
-			<MkCustomEmoji v-if="emoji[0] === ':'" class="emoji" :name="emoji" :normal="true"/>
-			<MkEmoji v-else class="emoji" :emoji="emoji" :normal="true"/>
-		</button>
-	</div>
-</section>
-<!-- フォルダの中にはカスタム絵文字やフォルダがある -->
-<section v-else v-panel style="border-radius: 6px; border-bottom: 0.5px solid var(--divider);">
-	<header class="_acrylic" @click="shown = !shown">
-		<i class="toggle ti-fw" :class="shown ? 'ti ti-chevron-down' : 'ti ti-chevron-up'"></i> <slot></slot> (<i class="ti ti-folder ti-fw"></i>:{{ customEmojiTree.length }} <i class="ti ti-icons ti-fw"></i>:{{ emojis.length }})
-	</header>
-	<div v-if="shown" style="padding-left: 9px;">
-		<MkEmojiPickerSection
-			v-for="child in customEmojiTree"
-			:key="`custom:${child.value}`"
-			:initialShown="initialShown"
-			:emojis="computed(() => customEmojis.filter(e => e.category === child.category).map(e => `:${e.name}:`))"
-			:hasChildSection="child.children.length !== 0"
-			:customEmojiTree="child.children"
-			@chosen="nestedChosen"
-		>
-			{{ child.value || i18n.ts.other }}
-		</MkEmojiPickerSection>
-	</div>
-	<div v-if="shown" class="body">
-		<button
-			v-for="emoji in emojis"
-			:key="emoji"
-			:data-emoji="emoji"
-			class="_button item"
-			@pointerenter="computeButtonTitle"
-			@click="emit('chosen', emoji, $event)"
-		>
-            jhkjh
-			<MkCustomEmoji v-if="emoji[0] === ':'" class="emoji" :name="emoji" :normal="true"/>
-			<MkEmoji v-else class="emoji" :emoji="emoji" :normal="true"/>
-		</button>
-	</div>
-</section>
+    <!-- このコンポーネントの要素のclassは親から利用されるのでむやみに弄らないこと -->
+    <!-- フォルダの中にはカスタム絵文字だけ（Unicode絵文字もこっち） -->
+    <section v-if="!hasChildSection" style="border-radius: 6px; padding-top: 9px;">
+        <header class="_acrylic" @click="shown = !shown"  style="border-radius: 6px;">
+            <i class="toggle ti-fw" :class="shown ? 'ti ti-chevron-down' : 'ti ti-chevron-up'"></i> <slot></slot> ({{ emojis.length }})
+        </header>
+        <div v-if="shown" class="body">
+            <button
+                    v-for="emoji in emojis"
+                    :key="emoji"
+                    :data-emoji="emoji"
+                    class="_button item"
+                    @pointerenter="computeButtonTitle"
+                    @click="emit('chosen', emoji, $event)"
+            >
+                <MkCustomEmoji v-if="emoji[0] === ':'" class="emoji" :name="emoji" :normal="true"/>
+                <MkEmoji v-else class="emoji" :emoji="emoji" :normal="true"/>
+            </button>
+        </div>
+    </section>
+    <!-- フォルダの中にはカスタム絵文字やフォルダがある -->
+    <section v-else style="border-radius: 6px; padding-top: 9px;">
+        <header class="_acrylic" @click="shown = !shown">
+            <i class="toggle ti-fw" :class="shown ? 'ti ti-chevron-down' : 'ti ti-chevron-up'"></i> <slot></slot> (フォルダー)
+        </header>
+        <div v-if="shown" style="padding-left: 9px; ">
+            <MkEmojiPickerSection
+                    v-for="child in customEmojiTree"
+                    :key="`custom:${child.value}`"
+                    :initialShown="initialShown"
+                    :emojis="computed(() => customEmojis.filter(e => e.category === child.category || e.category === child.category+'/'+child.category).map(e => `:${e.name}:`))"
+                    :hasChildSection="child.children.length !== 0"
+                    :customEmojiTree="child.children"
+                    @chosen="nestedChosen"
+            >
+                {{ child.value || i18n.ts.other }}
+            </MkEmojiPickerSection>
+        </div>
+        <div v-if="shown" class="body">
+            <button
+                    v-for="emoji in emojis"
+                    :key="emoji"
+                    :data-emoji="emoji"
+                    class="_button item"
+                    @pointerenter="computeButtonTitle"
+                    @click="emit('chosen', emoji, $event)"
+            >
+                <MkCustomEmoji v-if="emoji[0] === ':'" class="emoji" :name="emoji" :normal="true"/>
+                <MkEmoji v-else class="emoji" :emoji="emoji" :normal="true"/>
+            </button>
+        </div>
+    </section>
 </template>
 
 <script lang="ts" setup>
@@ -68,32 +67,18 @@ import MkEmojiPickerSection from '@/components/MkEmojiPicker.section.vue';
 
 const props = defineProps<{
     category?: string[];
-	emojis: string[] | Ref<string[]>;
-	initialShown?: boolean;
-	hasChildSection?: boolean;
-	customEmojiTree?: CustomEmojiFolderTree[];
+    emojis: string[] | Ref<string[]>;
+    initialShown?: boolean;
+    hasChildSection?: boolean;
+    customEmojiTree?: CustomEmojiFolderTree[];
 }>();
-
+console.log(props.customEmojiTree);
 const emit = defineEmits<{
     (ev: 'chosen', v: string, event: MouseEvent): void;
 }>();
 
-const toggleShown = (index) => {
-    shown_fold.value[index] = !shown_fold.value[index];
-
-};
-
-const toggleShown_fol = () => {
-    for (let i = 0; i < shown_fold.value.length; i++) {
-        shown_fold.value[i] = false;
-    }
-    shown_fol.value = !shown_fol.value;
-};
-
 const emojis = computed(() => Array.isArray(props.emojis) ? props.emojis : props.emojis.value);
-const shown_fold = ref(Array(props.category === undefined ? 0 : props.category.length).fill(false));
 const shown = ref(!!props.initialShown);
-const shown_fol = ref(!!props.initialShown);
 
 /** @see MkEmojiPicker.vue */
 function computeButtonTitle(ev: MouseEvent): void {
@@ -103,6 +88,6 @@ function computeButtonTitle(ev: MouseEvent): void {
 }
 
 function nestedChosen(emoji: any, ev?: MouseEvent) {
-	emit('chosen', emoji, ev);
+    emit('chosen', emoji, ev);
 }
 </script>
