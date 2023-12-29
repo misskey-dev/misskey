c28685c7faf5f4f3b5d6880dc96f4db82334e5ed
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-12-29 18:21:48 +0900

    update migration sql

--- packages/backend/migration/1697580470000-approvalSignup.js
+++ packages/backend/migration/1697580470000-approvalSignup.js
@@ -9,6 +9,8 @@ export class ApprovalSignup1697580470000 {
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "meta" ADD "approvalRequiredForSignup" boolean DEFAULT false NOT NULL`);
         await queryRunner.query(`ALTER TABLE "user" ADD "approved" boolean DEFAULT false NOT NULL`);
+        //▼ 既存のユーザーについては全員Approveにする
+        await queryRunner.query(`UPDATE "user" SET "approved" = true`);
         await queryRunner.query(`ALTER TABLE "user" ADD "signupReason" character varying(1000) NULL`);
         await queryRunner.query(`ALTER TABLE "user_pending" ADD "reason" character varying(1000) NULL`);
     }
