f59198d9c6ac9908654e698296c54126005cedef
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-12-30 00:16:17 +0900

    attempting to fix test

--- packages/backend/test/e2e/users.ts
+++ packages/backend/test/e2e/users.ts
@@ -179,6 +179,7 @@ describe('ユーザー', () => {
 			policies: user.policies,
 			...(security ? {
 				email: user.email,
+				signupReason: user.signupReason,
 				emailVerified: user.emailVerified,
 				securityKeysList: user.securityKeysList,
 			} : {}),
@@ -430,6 +431,7 @@ describe('ユーザー', () => {
 		assert.deepStrictEqual(response.loggedInDays, 0);
 		assert.deepStrictEqual(response.policies, DEFAULT_POLICIES);
 		assert.notStrictEqual(response.email, undefined);
+		assert.notStrictEqual(response.signupReason, undefined);
 		assert.strictEqual(response.emailVerified, false);
 		assert.deepStrictEqual(response.securityKeysList, []);
 	});
