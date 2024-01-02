09b1bac3ad13dfdf2eaa15d03efb8b8000fbdbb5
mattyatea <mattyatea@pm.me>
2024-01-03 01:33:20 +0900

    bug fix

--- packages/backend/migration/1704206095136-avatardecoration.js
+++ packages/backend/migration/1704206095136-avatardecoration.js
@@ -2,7 +2,7 @@ export class Avatardecoration1704206095136 {
     name = 'Avatardecoration1704206095136'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "avatar_decoration" ADD "category" character varying(256) NOT NULL DEFAULT '{}'`);
+        await queryRunner.query(`ALTER TABLE "avatar_decoration" ADD "category" character varying(256) NOT NULL DEFAULT ''`);
     }
 
     async down(queryRunner) {
