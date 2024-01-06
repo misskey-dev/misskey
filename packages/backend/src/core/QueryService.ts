ce8d9871b144b5a2462ea9e20ec90590a9fd1764
mattyatea <mattyatea@pm.me>
2024-01-06 14:49:59 +0900

    fix

--- packages/backend/src/core/QueryService.ts
+++ packages/backend/src/core/QueryService.ts
@@ -212,8 +212,8 @@ export class QueryService {
 				// または 自分自身
 					.orWhere('note.userId = :meId')
 				// または 自分宛て
-					.orWhere(':meId IN (note.visibleUserIds)')
-					.orWhere(':meId IN (note.mentions)')
+					.orWhere(':meId IN (SELECT unnest(note.visibleUserIds))')
+					.orWhere(':meId IN (SELECT unnest(note.mentions))')
 					.orWhere(new Brackets(qb => {
 						qb
 						// または フォロワー宛ての投稿であり、
