617ec1126169af7c2c20cafd102436704c36da2f
kakkokari-gtyih <daisho7308+f@gmail.com>
2024-01-15 16:15:49 +0900

    Refactor hms

--- packages/frontend/src/filters/hms.ts
+++ packages/frontend/src/filters/hms.ts
@@ -3,8 +3,26 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export function hms (ms: number, enableMs = false) {
-	const res: string[] = [];
+import { i18n } from '@/i18n.js';
+
+export function hms(ms: number, options: {
+	textFormat?: 'colon' | 'locale';
+	enableSeconds?: boolean;
+	enableMs?: boolean;
+}) {
+	const _options = {
+		textFormat: 'colon',
+		enableSeconds: true,
+		enableMs: false,
+		...options,
+	};
+
+	const res: {
+		h?: string;
+		m?: string;
+		s?: string;
+		ms?: string;
+	} = {};
 
 	// ミリ秒を秒に変換
 	let seconds = Math.floor(ms / 1000);
@@ -14,20 +32,32 @@ export function hms (ms: number, enableMs = false) {
 
 	// 時間を計算
 	const hours = Math.floor(seconds / 3600);
-	if (hours > 0) res.push(format(hours));
+	res.h = format(hours);
 	seconds %= 3600;
 
 	// 分を計算
 	const minutes = Math.floor(seconds / 60);
-	res.push(format(minutes));
+	res.m = format(minutes);
 	seconds %= 60;
 
 	// 残った秒数を取得
 	seconds = seconds % 60;
-	res.push(format(seconds));
+	res.s = format(seconds);
+
+	// ミリ秒を取得
+	res.ms = format(Math.floor(mili / 10));
 
 	// 結果を返す
-	return res.join(':') + (enableMs ? '.' + format(Math.floor(mili / 10)) : '');
+	if (_options.textFormat === 'locale') {
+		res.h += i18n.ts._time.hour;
+		res.m += i18n.ts._time.minute;
+		res.s += i18n.ts._time.second;
+	}
+	return [
+		res.h.startsWith('00') ? undefined : res.h,
+		res.m,
+		(_options.enableSeconds ? res.s : undefined),
+	].filter(v => v !== undefined).join(_options.textFormat === 'colon' ? ':' : ' ') + (_options.enableMs ? _options.textFormat === 'colon' ? `.${res.ms}` : ` ${res.ms}` : '');
 }
 
 function format(n: number) {
