ec09bf23d27941a70417279c1b42b759d4a1b082
kakkokari-gtyih <daisho7308+f@gmail.com>
2024-01-15 15:36:34 +0900

    Refactor seekbar styles

--- packages/frontend/src/components/MkMediaRange.vue
+++ packages/frontend/src/components/MkMediaRange.vue
@@ -37,114 +37,114 @@ const modelValue = computed({
 <style lang="scss" module>
 .controlsSeekbar {
 	position: relative;
+}
 
-	> .seek {
-		position: relative;
-		-webkit-appearance: none;
-		appearance: none;
-		background: transparent;
-		border: 0;
-		border-radius: 26px;
-		color: var(--accent);
-		display: block;
-		height: 19px;
-		margin: 0;
-		min-width: 0;
-		padding: 0;
-		transition: box-shadow .3s ease;
-		width: 100%;
+.seek {
+	position: relative;
+	-webkit-appearance: none;
+	appearance: none;
+	background: transparent;
+	border: 0;
+	border-radius: 26px;
+	color: var(--accent);
+	display: block;
+	height: 19px;
+	margin: 0;
+	min-width: 0;
+	padding: 0;
+	transition: box-shadow .3s ease;
+	width: 100%;
 
-		&::-webkit-slider-runnable-track {
-			background-color: var(--sliderBg);
-			background-image: linear-gradient(to right,currentColor var(--value,0),transparent var(--value,0));
-			border: 0;
-			border-radius: 99rem;
-			height: 5px;
-			transition: box-shadow .3s ease;
-			user-select: none;
-		}
-
-		&::-moz-range-track {
-			background: transparent;
-			border: 0;
-			border-radius: 99rem;
-			height: 5px;
-			transition: box-shadow .3s ease;
-			user-select: none;
-			background-color: var(--sliderBg);
-		}
-
-		&::-webkit-slider-thumb {
-			-webkit-appearance: none;
-			appearance: none;
-			background: #fff;
-			border: 0;
-			border-radius: 100%;
-			box-shadow: 0 1px 1px rgba(35, 40, 47, .15),0 0 0 1px rgba(35, 40, 47, .2);
-			height: 13px;
-			margin-top: -4px;
-			position: relative;
-			transition: all .2s ease;
-			width: 13px;
-
-			&:active {
-				box-shadow: 0 1px 1px rgba(35, 40, 47, .15), 0 0 0 1px rgba(35, 40, 47, .15), 0 0 0 3px rgba(255, 255, 255, .5);
-			}
-		}
-
-		&::-moz-range-thumb {
-			background: #fff;
-			border: 0;
-			border-radius: 100%;
-			box-shadow: 0 1px 1px rgba(35, 40, 47, .15),0 0 0 1px rgba(35, 40, 47, .2);
-			height: 13px;
-			position: relative;
-			transition: all .2s ease;
-			width: 13px;
-
-			&:active {
-				box-shadow: 0 1px 1px rgba(35, 40, 47, .15), 0 0 0 1px rgba(35, 40, 47, .15), 0 0 0 3px rgba(255, 255, 255, .5);
-			}
-		}
-
-		&::-moz-range-progress {
-			background: currentColor;
-			border-radius: 99rem;
-			height: 5px;
-		}
-	}
-
-	> .buffer {
-		appearance: none;
-		background: transparent;
-		color: var(--sliderBg);
+	&::-webkit-slider-runnable-track {
+		background-color: var(--sliderBg);
+		background-image: linear-gradient(to right,currentColor var(--value,0),transparent var(--value,0));
 		border: 0;
 		border-radius: 99rem;
 		height: 5px;
-		left: 0;
-		margin-top: -2.5px;
-		padding: 0;
-		position: absolute;
-		top: 50%;
-		width: 100%;
+		transition: box-shadow .3s ease;
+		user-select: none;
+	}
 
-		&::-webkit-progress-bar {
-			background: transparent;
-		}
+	&::-moz-range-track {
+		background: transparent;
+		border: 0;
+		border-radius: 99rem;
+		height: 5px;
+		transition: box-shadow .3s ease;
+		user-select: none;
+		background-color: var(--sliderBg);
+	}
 
-		&::-webkit-progress-value {
-			background: currentColor;
-			border-radius: 100px;
-			min-width: 5px;
-			transition: width .2s ease;
-		}
+	&::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+		background: #fff;
+		border: 0;
+		border-radius: 100%;
+		box-shadow: 0 1px 1px rgba(35, 40, 47, .15),0 0 0 1px rgba(35, 40, 47, .2);
+		height: 13px;
+		margin-top: -4px;
+		position: relative;
+		transition: all .2s ease;
+		width: 13px;
 
-		&::-moz-progress-bar {
-			background: currentColor;
-			border-radius: 100px;
-			min-width: 5px;
-			transition: width .2s ease;
+		&:active {
+			box-shadow: 0 1px 1px rgba(35, 40, 47, .15), 0 0 0 1px rgba(35, 40, 47, .15), 0 0 0 3px rgba(255, 255, 255, .5);
 		}
+	}
+
+	&::-moz-range-thumb {
+		background: #fff;
+		border: 0;
+		border-radius: 100%;
+		box-shadow: 0 1px 1px rgba(35, 40, 47, .15),0 0 0 1px rgba(35, 40, 47, .2);
+		height: 13px;
+		position: relative;
+		transition: all .2s ease;
+		width: 13px;
+
+		&:active {
+			box-shadow: 0 1px 1px rgba(35, 40, 47, .15), 0 0 0 1px rgba(35, 40, 47, .15), 0 0 0 3px rgba(255, 255, 255, .5);
+		}
+	}
+
+	&::-moz-range-progress {
+		background: currentColor;
+		border-radius: 99rem;
+		height: 5px;
+	}
+}
+
+.buffer {
+	appearance: none;
+	background: transparent;
+	color: var(--sliderBg);
+	border: 0;
+	border-radius: 99rem;
+	height: 5px;
+	left: 0;
+	margin-top: -2.5px;
+	padding: 0;
+	position: absolute;
+	top: 50%;
+	width: 100%;
+
+	&::-webkit-progress-bar {
+		background: transparent;
+	}
+
+	&::-webkit-progress-value {
+		background: currentColor;
+		border-radius: 100px;
+		min-width: 5px;
+		transition: width .2s ease;
+	}
+
+	&::-moz-progress-bar {
+		background: currentColor;
+		border-radius: 100px;
+		min-width: 5px;
+		transition: width .2s ease;
 	}
 }
 </style>
