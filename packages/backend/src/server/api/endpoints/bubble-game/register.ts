945d6a2b094565b50bbe46739f7f9084ad703f59
syuilo <Syuilotan@yahoo.co.jp>
2024-01-17 20:11:32 +0900

    enhance(drop-and-fusion): ゲームバランスの調整など

--- packages/backend/src/server/api/endpoints/bubble-game/register.ts
+++ packages/backend/src/server/api/endpoints/bubble-game/register.ts
@@ -63,8 +63,8 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				throw new ApiError(meta.errors.invalidSeed);
 			}
 
-			// シードが古すぎる(1時間以上前)のも弾く
-			if (seedDate.getTime() < now.getTime() - 1000 * 60 * 60) {
+			// シードが古すぎる(5時間以上前)のも弾く
+			if (seedDate.getTime() < now.getTime() - 1000 * 60 * 60 * 5) {
 				throw new ApiError(meta.errors.invalidSeed);
 			}
 
