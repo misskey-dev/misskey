f31d910fe19825942ec797b7d414f36e06aa2d19
kakkokari-gtyih <daisho7308+f@gmail.com>
2024-01-29 13:19:33 +0900

    fix types

--- packages/misskey-js/src/autogen/entities.ts
+++ packages/misskey-js/src/autogen/entities.ts
@@ -1,6 +1,6 @@
 /*
- * version: 2024.2.0-beta.4
- * generatedAt: 2024-01-24T01:14:40.897Z
+ * version: 2024.2.0-beta.5
+ * generatedAt: 2024-01-29T04:18:53.707Z
  */
 
 import { operations } from './types.js';
--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -2,8 +2,8 @@
 /* eslint @typescript-eslint/no-explicit-any: 0 */
 
 /*
- * version: 2024.2.0-beta.4
- * generatedAt: 2024-01-24T01:14:40.815Z
+ * version: 2024.2.0-beta.5
+ * generatedAt: 2024-01-29T04:18:53.556Z
  */
 
 /**
