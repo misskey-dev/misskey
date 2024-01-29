3570faaab137bc0e0d17cdbd777099f8a1fc1359
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-01-29 16:29:38 +0900

    update: misskey-js-autogen

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -2,8 +2,8 @@
 /* eslint @typescript-eslint/no-explicit-any: 0 */
 
 /*
- * version: 2024.2.0-beta.6
- * generatedAt: 2024-01-24T07:32:10.370Z
+ * version: 2024.2.0-beta.7
+ * generatedAt: 2024-01-29T07:28:30.181Z
  */
 
 /**
