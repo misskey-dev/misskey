62fed7fccb465509d6f5884d71fee0f97457a634
1Step621 <onestep621@gmail.com>
2024-02-02 03:04:00 +0900

    lint fixes

--- packages/frontend/src/scripts/check-reaction-permissions.ts
+++ packages/frontend/src/scripts/check-reaction-permissions.ts
@@ -7,13 +7,13 @@ export function checkReactionPermissions(me: Misskey.entities.MeDetailed, note: 
   deniedReason: 'localOnly' | 'isSensitive' | 'roleIdsThatCanBeUsedThisEmojiAsReaction';
 } {
   if (emoji.localOnly && note.user.host !== me.host) {
-    return { allowed: false, deniedReason: 'localOnly' }
-  };
+    return { allowed: false, deniedReason: 'localOnly' };
+  }
   if (emoji.isSensitive && (note.reactionAcceptance === 'nonSensitiveOnly' || note.reactionAcceptance === 'nonSensitiveOnlyForLocalLikeOnlyForRemote')) {
-    return { allowed: false, deniedReason: 'isSensitive' }
+    return { allowed: false, deniedReason: 'isSensitive' };
   }
   if (!(emoji.roleIdsThatCanBeUsedThisEmojiAsReaction.length === 0 || me.roles.some(role => emoji.roleIdsThatCanBeUsedThisEmojiAsReaction.includes(role.id)))) {
-    return { allowed: false, deniedReason: 'roleIdsThatCanBeUsedThisEmojiAsReaction' }
+    return { allowed: false, deniedReason: 'roleIdsThatCanBeUsedThisEmojiAsReaction' };
   }
   return { allowed: true };
 }
