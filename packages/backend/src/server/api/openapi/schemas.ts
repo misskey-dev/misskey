06bebd772df1fb0bd495296917912fdecb63d71f
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-02-02 00:13:14 +0900

    remove: 不要なimportを削除

--- packages/backend/src/server/api/openapi/schemas.ts
+++ packages/backend/src/server/api/openapi/schemas.ts
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { de } from 'date-fns/locale';
 import type { Schema } from '@/misc/json-schema.js';
 import { refs } from '@/misc/json-schema.js';
 
