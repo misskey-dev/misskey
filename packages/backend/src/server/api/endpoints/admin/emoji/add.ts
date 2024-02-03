c88c8af8d994cdaf79a54472c4bac2791ede8045
samunohito <46447427+samunohito@users.noreply.github.com>
2024-02-03 18:51:01 +0900

    add mimetype check

--- packages/backend/src/server/api/endpoints/admin/emoji/add.ts
+++ packages/backend/src/server/api/endpoints/admin/emoji/add.ts
@@ -9,6 +9,7 @@ import type { DriveFilesRepository } from '@/models/_.js';
 import { DI } from '@/di-symbols.js';
 import { CustomEmojiService } from '@/core/CustomEmojiService.js';
 import { EmojiEntityService } from '@/core/entities/EmojiEntityService.js';
+import { FILE_TYPE_IMAGE } from '@/const.js';
 import { ApiError } from '../../../error.js';
 
 export const meta = {
@@ -23,6 +24,11 @@ export const meta = {
 			message: 'No such file.',
 			code: 'NO_SUCH_FILE',
 			id: 'fc46b5a4-6b92-4c33-ac66-b806659bb5cf',
+		},
+		notSupportFileType: {
+			message: 'Not support file type.',
+			code: 'NOT_SUPPORT_FILE_TYPE',
+			id: 'f7599d96-8750-af68-1633-9575d625c1a7',
 		},
 		duplicateName: {
 			message: 'Duplicate name.',
@@ -78,6 +84,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			if (driveFile == null) throw new ApiError(meta.errors.noSuchFile);
 			const isDuplicate = await this.customEmojiService.checkDuplicate(ps.name);
 			if (isDuplicate) throw new ApiError(meta.errors.duplicateName);
+			if (!FILE_TYPE_IMAGE.includes(driveFile.type)) throw new ApiError(meta.errors.notSupportFileType);
 
 			const emoji = await this.customEmojiService.add({
 				driveFile,
