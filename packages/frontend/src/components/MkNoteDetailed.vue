74245df3829622a3cc0c880ea710b5c1c4f5c584
1Step621 <86859447+1STEP621@users.noreply.github.com>
2024-02-06 16:45:21 +0900

    Enhance(frontend): フロント側でもリアクション権限のチェックをするように (#13134)
    * フロント側でもリアクション権限のチェックをするように
    
    * update CHANGELOG.md
    
    * lint fixes
    
    * remove unrelated diffs
    
    * deny -> reject
    denyは「(信用しないことを理由に)拒否する」という意味らしい
    
    * allow -> accept
    
    * EmojiSimpleにlocalOnlyを含めるように
    
    * リアクション権限のない絵文字は打てないように(ダイアログを出すのではなく)
    
    * regenerate type definitions
    
    * lint fix
    
    * remove unused locales
    
    * remove unnecessary async

--- packages/frontend/src/components/MkNoteDetailed.vue
+++ packages/frontend/src/components/MkNoteDetailed.vue
@@ -385,7 +385,7 @@ function react(viaKeyboard = false): void {
 		}
 	} else {
 		blur();
-		reactionPicker.show(reactButton.value ?? null, reaction => {
+		reactionPicker.show(reactButton.value ?? null, note.value, reaction => {
 			sound.playMisskeySfx('reaction');
 
 			misskeyApi('notes/reactions/create', {
--- packages/misskey-js/src/autogen/endpoint.ts
+++ packages/misskey-js/src/autogen/endpoint.ts
@@ -1,6 +1,6 @@
 /*
  * version: 2024.2.0-beta.8
- * generatedAt: 2024-02-04T11:51:13.595Z
+ * generatedAt: 2024-02-04T16:51:09.467Z
  */
 
 import type {
