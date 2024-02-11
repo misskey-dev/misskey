95c4bb47e112f900dde3c1b334195075ab54409e
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2024-02-12 01:24:14 +0900

    chore: use ignore-walk

--- scripts/tarball.mjs
+++ scripts/tarball.mjs
@@ -3,37 +3,25 @@ import { mkdir } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import glob from 'fast-glob';
+import walk from 'ignore-walk';
 import Pack from 'tar/lib/pack.js';
 import meta from '../package.json' assert { type: "json" };
 
-const patterns = [
-	'{assets,fluent-emojis,locales,misskey-assets}/**/*',
-	'packages/.config/example.yml',
-	'packages/{backend,frontend,misskey-bubble-game,misskey-js,misskey-js/generator,misskey-reversi,sw}/{src/**/*,.swcrc,*}',
-	'packages/backend/{assets,migration,nsfw-model}/**/*',
-	'packages/frontend/{.storybook,@types,assets,lib,public}/**/*',
-	'!packages/frontend/.storybook/{locale.ts,themes.ts,*.js}',
-	'!packages/misskey-js/CONTRIBUTING.md',
-	'packages/meta.json',
-	'scripts/{changelog-checker/{src/**/*,*},*}',
-	'.node-version',
-	'.npmrc',
-	'CHANGELOD.md',
-	'COPYING',
-	'LICENSE',
-	'README.md',
-	'SECURITY.md',
-	'package.json',
-	'pnpm-lock.yaml',
-	'pnpm-workspace.yaml',
+const cwd = fileURLToPath(new URL('..', import.meta.url));
+const ignore = [
+	'**/.git/**/*',
+	'**/*ignore',
+	'**/.gitmodules',
+	// Exclude files you don't want to include in the tarball here
 ];
 
 export default async function build() {
-	const cwd = fileURLToPath(new URL('..', import.meta.url));
 	const mkdirPromise = mkdir(resolve(cwd, 'built', 'tarball'), { recursive: true });
 	const pack = new Pack({ cwd, gzip: true });
+	const patterns = await walk({ path: cwd, ignoreFiles: ['.gitignore'] });
 
-	for await (const entry of glob.stream(patterns)) {
+	for await (const entry of glob.stream(patterns, { cwd, ignore, dot: true })) {
+		console.log(entry);
 		pack.add(entry);
 	}
 
