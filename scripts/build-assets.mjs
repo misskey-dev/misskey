f6cedec9dbb614ed29bb7d77b037765028dc8352
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2024-02-13 16:28:34 +0900

    build: fix typo

--- scripts/build-assets.mjs
+++ scripts/build-assets.mjs
@@ -16,7 +16,7 @@ import meta from '../package.json' assert { type: "json" };
 import buildTarball from './tarball.mjs';
 
 const configDir = fileURLToPath(new URL('../.config', import.meta.url));
-const path = process.env.MISSKEY_CONFIG_YML
+const configPath = process.env.MISSKEY_CONFIG_YML
 	? path.resolve(configDir, process.env.MISSKEY_CONFIG_YML)
 	: process.env.NODE_ENV === 'test'
 		? path.resolve(configDir, 'test.yml')
@@ -25,7 +25,7 @@ const path = process.env.MISSKEY_CONFIG_YML
 let locales = buildLocales();
 
 async function loadConfig() {
-	return fs.readFile(path, 'utf-8').then(data => yaml.load(data)).catch(() => null);
+	return fs.readFile(configPath, 'utf-8').then(data => yaml.load(data)).catch(() => null);
 }
 
 async function copyFrontendFonts() {
