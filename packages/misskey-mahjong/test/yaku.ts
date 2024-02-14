bab479dbe5db3a9c9783a492f3f9b769c3c934f7
imtan <lacrimalelacrimal@gmail.com>
2024-02-14 14:46:06 +0900

    add ippatsu test

--- packages/misskey-mahjong/test/yaku.ts
+++ packages/misskey-mahjong/test/yaku.ts
@@ -78,6 +78,25 @@ describe('Yaku', () => {
 		});
 	});
 
+	describe('ippatsu', () => {
+		it('valid', () => {
+			assert.deepStrictEqual(calcYakus({
+				house: 'e',
+				handTiles: ['m1', 'm2', 'm3', 'p6', 'p6', 'p6', 's6', 's7', 's8', 'n', 'n', 'n', 'm3', 'm3'],
+				huros: [],
+				riichi: true,
+				ippatsu: true,
+			}), ['riichi', 'ippatsu']);
+		});
+		it('invalid', () => {
+			assert.deepStrictEqual(calcYakus({
+				house: 'e',
+				handTiles: ['m1', 'm2', 'm3', 'p6', 'p6', 'p6', 's6', 's7', 's8', 'n', 'n', 'n', 'm3', 'm3'],
+				huros: [],
+				riichi: true,
+			}).includes('ippatsu'), false);
+		});
+	});
 
 	describe('kokushi', () => {
 		it('valid', () => {
