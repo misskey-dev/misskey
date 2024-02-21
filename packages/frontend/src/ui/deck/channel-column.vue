fb0eb5a31fb44cddc5d517fbd5b65b670bc60e4f
syuilo <4439005+syuilo@users.noreply.github.com>
2024-02-21 18:35:05 +0900

    :art:

--- packages/frontend/src/ui/deck/channel-column.vue
+++ packages/frontend/src/ui/deck/channel-column.vue
@@ -11,7 +11,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 	<template v-if="column.channelId">
 		<div style="padding: 8px; text-align: center;">
-			<MkButton primary gradate rounded inline @click="post"><i class="ti ti-pencil"></i></MkButton>
+			<MkButton primary gradate rounded inline small @click="post"><i class="ti ti-pencil"></i></MkButton>
 		</div>
 		<MkTimeline ref="timeline" src="channel" :channel="column.channelId"/>
 	</template>
