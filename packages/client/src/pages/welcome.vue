ea59a473e0824ea05aceadbdfb5e54369caac7d0
nacika-ins <nacika.inscatolare@gmail.com>
2024-02-26 19:51:48 +0900

    fix: ログイン画面を変更

--- packages/client/src/pages/welcome.vue
+++ packages/client/src/pages/welcome.vue
@@ -8,7 +8,7 @@
 <script lang="ts" setup>
 import { computed } from 'vue';
 import XSetup from './welcome.setup.vue';
-import XEntrance from './welcome.entrance.a.vue';
+import XEntrance from './welcome.entrance.c.vue';
 import { instanceName } from '@/config';
 import * as os from '@/os';
 import { definePageMetadata } from '@/scripts/page-metadata';
