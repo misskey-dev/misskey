d1bf432e14faa7bb420b6338038195963f6b73d2
syuilo <4439005+syuilo@users.noreply.github.com>
2024-03-01 17:28:46 +0900

    add missing license headers

--- packages/backend/src/misc/FileWriterStream.ts
+++ packages/backend/src/misc/FileWriterStream.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import * as fs from 'node:fs/promises';
 import type { PathLike } from 'node:fs';
 
--- packages/backend/src/misc/JsonArrayStream.ts
+++ packages/backend/src/misc/JsonArrayStream.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { TransformStream } from 'node:stream/web';
 
 /**
