6158ef138ec8088c0e3c615b2605c0fa1a6f402c
syuilo <4439005+syuilo@users.noreply.github.com>
2024-03-01 17:27:03 +0900

    format

--- packages/frontend/test/autocomplete.test.ts
+++ packages/frontend/test/autocomplete.test.ts
@@ -7,28 +7,28 @@ import { assert, describe, test } from 'vitest';
 import { searchEmoji } from '@/scripts/search-emoji.js';
 
 describe('emoji autocomplete', () => {
-  test('名前の完全一致は名前の前方一致より優先される', async () => {
-    const result = searchEmoji('foooo', [{ emoji: ':foooo:', name: 'foooo' }, { emoji: ':foooobaaar:', name: 'foooobaaar' }]);
-    assert.equal(result[0].emoji, ':foooo:');
-  });
+	test('名前の完全一致は名前の前方一致より優先される', async () => {
+		const result = searchEmoji('foooo', [{ emoji: ':foooo:', name: 'foooo' }, { emoji: ':foooobaaar:', name: 'foooobaaar' }]);
+		assert.equal(result[0].emoji, ':foooo:');
+	});
 
-  test('名前の前方一致は名前の部分一致より優先される', async () => {
-    const result = searchEmoji('baaa', [{ emoji: ':baaar:', name: 'baaar' }, { emoji: ':foooobaaar:', name: 'foooobaaar' }]);
-    assert.equal(result[0].emoji, ':baaar:');
-  });
+	test('名前の前方一致は名前の部分一致より優先される', async () => {
+		const result = searchEmoji('baaa', [{ emoji: ':baaar:', name: 'baaar' }, { emoji: ':foooobaaar:', name: 'foooobaaar' }]);
+		assert.equal(result[0].emoji, ':baaar:');
+	});
 
-  test('名前の完全一致はタグの完全一致より優先される', async () => {
-    const result = searchEmoji('foooo', [{ emoji: ':foooo:', name: 'foooo' }, { emoji: ':baaar:', name: 'foooo', aliasOf: 'baaar' }]);
-    assert.equal(result[0].emoji, ':foooo:');
-  });
+	test('名前の完全一致はタグの完全一致より優先される', async () => {
+		const result = searchEmoji('foooo', [{ emoji: ':foooo:', name: 'foooo' }, { emoji: ':baaar:', name: 'foooo', aliasOf: 'baaar' }]);
+		assert.equal(result[0].emoji, ':foooo:');
+	});
 
-  test('名前の前方一致はタグの前方一致より優先される', async () => {
-    const result = searchEmoji('foo', [{ emoji: ':foooo:', name: 'foooo' }, { emoji: ':baaar:', name: 'foooo', aliasOf: 'baaar' }]);
-    assert.equal(result[0].emoji, ':foooo:');
-  });
+	test('名前の前方一致はタグの前方一致より優先される', async () => {
+		const result = searchEmoji('foo', [{ emoji: ':foooo:', name: 'foooo' }, { emoji: ':baaar:', name: 'foooo', aliasOf: 'baaar' }]);
+		assert.equal(result[0].emoji, ':foooo:');
+	});
 
-  test('名前の部分一致はタグの部分一致より優先される', async () => {
-    const result = searchEmoji('oooo', [{ emoji: ':foooo:', name: 'foooo' }, { emoji: ':baaar:', name: 'foooo', aliasOf: 'baaar' }]);
-    assert.equal(result[0].emoji, ':foooo:');
-  });
+	test('名前の部分一致はタグの部分一致より優先される', async () => {
+		const result = searchEmoji('oooo', [{ emoji: ':foooo:', name: 'foooo' }, { emoji: ':baaar:', name: 'foooo', aliasOf: 'baaar' }]);
+		assert.equal(result[0].emoji, ':foooo:');
+	});
 });
