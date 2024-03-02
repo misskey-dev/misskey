d355fc75bfc3defd833a7fc379e093b7d614cda0
nacika-ins <nacika.inscatolare@gmail.com>
2024-03-02 09:04:59 +0900

    fix: ソースコード元変更

--- packages/frontend/src/pages/about-misskey.vue
+++ packages/frontend/src/pages/about-misskey.vue
@@ -51,7 +51,7 @@
           </div>
           <FormSection>
             <div class="_formLinks">
-              <FormLink to="https://github.com/misskey-dev/misskey" external>
+              <FormLink to="https://github.com/nacika-ins/misskey" external>
                 <template #icon><i class="ti ti-code"></i></template>
                 {{ i18n.ts._aboutMisskey.source }}
                 <template #suffix>GitHub</template>
