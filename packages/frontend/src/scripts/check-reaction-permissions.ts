4de2b3cfb9b5de102a123a9fea365adc4440241d
samunohito <46447427+samunohito@users.noreply.github.com>
2024-03-02 11:00:35 +0900

    fix

--- packages/frontend/src/scripts/check-reaction-permissions.ts
+++ packages/frontend/src/scripts/check-reaction-permissions.ts
@@ -2,7 +2,7 @@ import * as Misskey from 'misskey-js';
 import { UnicodeEmojiDef } from './emojilist.js';
 
 export function checkReactionPermissions(me: Misskey.entities.MeDetailed, note: Misskey.entities.Note, emoji: Misskey.entities.EmojiSimple | UnicodeEmojiDef | string): boolean {
-	if (typeof emoji === 'string') return false;
+	if (typeof emoji === 'string') return true; // UnicodeEmojiDefにも無い絵文字であれば文字列で来る。Unicode絵文字であることには変わりないので常にリアクション可能とする;
 	if ('char' in emoji) return true; // UnicodeEmojiDefなら常にリアクション可能
 
 	const roleIdsThatCanBeUsedThisEmojiAsReaction = emoji.roleIdsThatCanBeUsedThisEmojiAsReaction ?? [];
