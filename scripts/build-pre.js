74a7ff19cb6b5ccd1fc2b51e4406989a2cb19f4a
nacika-ins <nacika.inscatolare@gmail.com>
2024-03-03 09:15:08 +0900

    fix: build-pre で作成するmeta.jsonの内容を変更

--- scripts/build-pre.js
+++ scripts/build-pre.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const meta = require('../built/_vite_/meta.json');
 
 fs.mkdirSync(__dirname + '/../built', { recursive: true });
-fs.writeFileSync(__dirname + '/../built/meta.json', JSON.stringify({ version: meta.buildHash }), 'utf-8');
+fs.writeFileSync(__dirname + '/../built/meta.json', JSON.stringify({ version: meta.version, buildHash: meta.buildHash }), 'utf-8');
