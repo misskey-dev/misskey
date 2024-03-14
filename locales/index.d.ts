71d0538647684d67de1b15467f890bd65ffc770d
tamaina <tamaina@hotmail.co.jp>
2024-03-14 18:18:32 +0900

    fix(frontend): update locales/index.d.ts

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -6805,6 +6805,10 @@ export interface Locale extends ILocale {
          * ソースを表示
          */
         "viewSource": string;
+        /**
+         * ログを表示
+         */
+        "viewLog": string;
     };
     "_preferencesBackups": {
         /**
