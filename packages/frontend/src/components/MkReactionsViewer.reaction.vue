eed71639d0310b002b8f68ce0ace1e5a59e8df6e
sweshelo <sweshelo@gmail.com>
2024-03-15 23:46:53 +0900

    fix: カスタム絵文字はreactionが不正な文字列になる場合があるのを考慮

--- packages/frontend/src/components/MkReactionsViewer.reaction.vue
+++ packages/frontend/src/components/MkReactionsViewer.reaction.vue
@@ -78,7 +78,7 @@ async function toggleReaction() {
 		}
 		misskeyApi('notes/reactions/delete', {
 			noteId: props.note.id,
-      reaction: props.reaction,
+      reaction: (isCustomEmoji.value) ? `:${emoji.value.name}:` : props.reaction,
 		})
 	} else {
 		sound.playMisskeySfx('reaction');
