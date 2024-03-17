27c897d19f0ca29c233e79e067c560e9e00db191
まっちゃとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2024-03-17 11:21:48 +0900

    fix(frontend): ノートの詳細ページのユーザーもしくはチャンネルを遡るとタイムラインの並び順番が逆になる問題を修正 (MisskeyIO#529)

--- packages/frontend/src/pages/note.vue
+++ packages/frontend/src/pages/note.vue
@@ -85,7 +85,6 @@ const prevUserPagination: Paging = {
 };
 
 const nextUserPagination: Paging = {
-	reversed: true,
 	endpoint: 'users/notes',
 	limit: 10,
 	params: computed(() => note.value ? ({
@@ -104,7 +103,6 @@ const prevChannelPagination: Paging = {
 };
 
 const nextChannelPagination: Paging = {
-	reversed: true,
 	endpoint: 'channels/timeline',
 	limit: 10,
 	params: computed(() => note.value ? ({
