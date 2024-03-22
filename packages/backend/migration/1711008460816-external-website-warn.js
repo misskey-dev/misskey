4590046e25638a7f23e97a239ce8b934419dfa0f
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-03-22 17:12:31 +0900

    spdx

--- packages/backend/migration/1711008460816-external-website-warn.js
+++ packages/backend/migration/1711008460816-external-website-warn.js
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 export class ExternalWebsiteWarn1711008460816 {
     name = 'ExternalWebsiteWarn1711008460816'
 
