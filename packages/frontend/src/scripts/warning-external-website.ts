523fad6f012d16b690fe506045bd21189f1100b1
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-03-24 17:15:01 +0900

    Revert "use url"
    This reverts commit abf6a74a93c424b6bf2b7a48adf3ae70c6167792.

--- packages/frontend/src/scripts/warning-external-website.ts
+++ packages/frontend/src/scripts/warning-external-website.ts
@@ -9,10 +9,11 @@ import { defaultStore } from '@/store.js';
 import * as os from '@/os.js';
 import MkUrlWarningDialog from '@/components/MkUrlWarningDialog.vue';
 
+const extractDomain = /^(https?:\/\/|\/\/)?([^@/\s]+@)?(www\.)?([^:/\s]+)/i;
 const isRegExp = /^\/(.+)\/(.*)$/;
 
 export async function warningExternalWebsite(ev: MouseEvent, url: string) {
-	const domain = new URL(url).hostname;
+	const domain = extractDomain.exec(url)?.[4];
 	const self = !domain || url.startsWith(local);
 	const isWellKnownWebsite = self || instance.wellKnownWebsites.some(expression => {
 		const r = isRegExp.exec(expression);
