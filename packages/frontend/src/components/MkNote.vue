d4ca973e3408a7d28788efe57bcd882c0ce9eedc
1Step621 <86859447+1STEP621@users.noreply.github.com>
2024-03-30 20:30:22 +0900

    Enhance(frontend): もっと！ボタンでリアクション一覧が開けるように (#12935)
    * もっと！ボタンでリアクション一覧が開けるように
    
    * update CHANGELOG.md && デバッグ用に最大リアクション表示数を1にしてたのを一応戻した
    
    * fix
    
    * デザイン調整
    
    * maxNumberもどす
    
    * fix CHANGELOG
    
    * fix
    
    * move changelog
    
    * :art:
    
    ---------
    
    Co-authored-by: かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
    Co-authored-by: syuilo <4439005+syuilo@users.noreply.github.com>

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -97,7 +97,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 			</div>
 			<MkReactionsViewer v-if="appearNote.reactionAcceptance !== 'likeOnly'" :note="appearNote" :maxNumber="16" @mockUpdateMyReaction="emitUpdReaction">
 				<template #more>
-					<div :class="$style.reactionOmitted">{{ i18n.ts.more }}</div>
+					<MkA :to="`/notes/${appearNote.id}/reactions`" :class="[$style.reactionOmitted]">{{ i18n.ts.more }}</MkA>
 				</template>
 			</MkReactionsViewer>
 			<footer :class="$style.footer">
@@ -1020,9 +1020,8 @@ function emitUpdReaction(emoji: string, delta: number) {
 
 .reactionOmitted {
 	display: inline-block;
-	height: 32px;
-	margin: 2px;
-	padding: 0 6px;
+	margin-left: 8px;
 	opacity: .8;
+	font-size: 95%;
 }
 </style>
