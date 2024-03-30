d3d69ae5abfae57953b43082b7dcee658330f23d
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-03-30 15:58:26 +0900

    spdx

--- packages/backend/src/server/api/endpoints/users/get-following-birthday-users.ts
+++ packages/backend/src/server/api/endpoints/users/get-following-birthday-users.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { Inject, Injectable } from '@nestjs/common';
 import { Brackets } from 'typeorm';
 import { DI } from '@/di-symbols.js';
