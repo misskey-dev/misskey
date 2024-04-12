b8c5553a4205b2235c82369d23acb5bfc5bbcabd
anatawa12 <anatawa12@icloud.com>
2024-04-12 21:47:16 +0900

    fix: some replies are removed from global timeline

--- packages/backend/src/server/api/stream/channels/global-timeline.ts
+++ packages/backend/src/server/api/stream/channels/global-timeline.ts
@@ -52,13 +52,6 @@ class GlobalTimelineChannel extends Channel {
 		if (note.visibility !== 'public') return;
 		if (note.channelId != null) return;
 
-		// 関係ない返信は除外
-		if (note.reply && !this.following[note.userId]?.withReplies) {
-			const reply = note.reply;
-			// 「チャンネル接続主への返信」でもなければ、「チャンネル接続主が行った返信」でもなければ、「投稿者の投稿者自身への返信」でもない場合
-			if (reply.userId !== this.user!.id && note.userId !== this.user!.id && reply.userId !== note.userId) return;
-		}
-
 		if (note.renote && note.text == null && (note.fileIds == null || note.fileIds.length === 0) && !this.withRenotes) return;
 
 		// Ignore notes from instances the user has muted
