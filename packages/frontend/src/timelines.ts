f6267d6f43fb6a5a2bae9704ef06c1fefec96767
anatawa12 <anatawa12@icloud.com>
2024-04-17 14:23:49 +0900

    fix: missing license header

--- packages/frontend/src/timelines.ts
+++ packages/frontend/src/timelines.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { $i } from '@/account.js';
 import { instance } from '@/instance.js';
 
