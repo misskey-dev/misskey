080dab40395c4438a28570cc973b24dc94b0fcb1
taiy <53635909+taiyme@users.noreply.github.com>
2024-04-23 16:08:16 +0900

    fix(frontend): 1:1以外のエラー画像のアスペクト比が崩れる問題 (#195)
    (cherry picked from commit 8bc23016a017508b821a857253d54266d4bd99cc)

--- packages/frontend/src/pages/list.vue
+++ packages/frontend/src/pages/list.vue
@@ -131,7 +131,6 @@ definePageMetadata(() => ({
 .root {
 	padding: 32px;
 	text-align: center;
-  align-items: center;
 }
 
 .text {
@@ -140,7 +139,6 @@ definePageMetadata(() => ({
 
 .img {
 	vertical-align: bottom;
-  width: 128px;
 	height: 128px;
 	margin-bottom: 16px;
 	border-radius: 16px;
