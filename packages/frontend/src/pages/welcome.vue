fee91794fdfb0b791f3c6e19c40eb414ad8552fa
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-05-01 13:37:21 +0900

    refactor(frontend): 非ログイン画面でのmeta取得を減らす

--- packages/frontend/src/pages/welcome.vue
+++ packages/frontend/src/pages/welcome.vue
@@ -4,26 +4,19 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<div v-if="meta">
-	<XSetup v-if="meta.requireSetup"/>
+<div v-if="instance">
+	<XSetup v-if="instance.requireSetup"/>
 	<XEntrance v-else/>
 </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
-import * as Misskey from 'misskey-js';
+import { computed } from 'vue';
 import XSetup from './welcome.setup.vue';
 import XEntrance from './welcome.entrance.a.vue';
 import { instanceName } from '@/config.js';
-import { misskeyApi } from '@/scripts/misskey-api.js';
 import { definePageMetadata } from '@/scripts/page-metadata.js';
-
-const meta = ref<Misskey.entities.MetaResponse | null>(null);
-
-misskeyApi('meta', { detail: true }).then(res => {
-	meta.value = res;
-});
+import { instance } from '@/instance.js';
 
 const headerActions = computed(() => []);
 
