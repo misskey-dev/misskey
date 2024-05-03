7e5bace5c11833eca18d9742cb883efab40b1c00
osamu <46447427+sam-osamu@users.noreply.github.com>
2024-05-03 18:07:29 +0900

    混入除去

--- packages/backend/src/postgres.ts
+++ packages/backend/src/postgres.ts
@@ -99,7 +99,7 @@ class MyCustomLogger implements Logger {
 
 	@bindThis
 	public logQuery(query: string, parameters?: any[]) {
-		sqlLogger.info(this.highlight(query));
+		sqlLogger.info(this.highlight(query).substring(0, 100));
 	}
 
 	@bindThis
