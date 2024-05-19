28a0511fbf9bb0e7cb288275c7f66fe26b9b55dd
anatawa12 <anatawa12@icloud.com>
2024-05-19 21:01:17 +0900

    fix: reply to my follower notes are not shown on the home timeline

--- packages/backend/src/server/api/endpoints/notes/timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/timeline.ts
@@ -114,7 +114,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				excludePureRenotes: !ps.withRenotes,
 				noteFilter: note => {
 					if (note.reply && note.reply.visibility === 'followers') {
-						if (!Object.hasOwn(followings, note.reply.userId)) return false;
+						if (!Object.hasOwn(followings, note.reply.userId) && note.reply.userId !== me.id) return false;
 					}
 
 					return true;
