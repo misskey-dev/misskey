e91a10640256632524235b037f49c68eaecd8b78
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2024-05-19 19:59:45 +0900

    style: header

--- packages/backend/src/server/HealthServerService.ts
+++ packages/backend/src/server/HealthServerService.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { Inject, Injectable } from '@nestjs/common';
 import * as Redis from 'ioredis';
 import { DataSource } from 'typeorm';
