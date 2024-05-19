252172160d6fedc756ff20aec1060dd5ed90130d
anatawa12 <anatawa12@icloud.com>
2024-05-19 23:59:29 +0900

    feat(backend): add `channelId` to `MiPoll` as a Denormalized field

---
+++ packages/backend/migration/1716129964060-ChannelIdDenormalizedForMiPoll.js
@@ -0,0 +1,16 @@
+export class ChannelIdDenormalizedForMiPoll1716129964060 {
+    name = 'ChannelIdDenormalizedForMiPoll1716129964060'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "poll" ADD "channelId" character varying(32)`);
+        await queryRunner.query(`COMMENT ON COLUMN "poll"."channelId" IS '[Denormalized]'`);
+        await queryRunner.query(`CREATE INDEX "IDX_c1240fcc9675946ea5d6c2860e" ON "poll" ("channelId") `);
+        await queryRunner.query(`UPDATE "poll" SET "channelId" = "note"."channelId" FROM "note" WHERE "poll"."noteId" = "note"."id"`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`DROP INDEX "public"."IDX_c1240fcc9675946ea5d6c2860e"`);
+        await queryRunner.query(`COMMENT ON COLUMN "poll"."channelId" IS '[Denormalized]'`);
+        await queryRunner.query(`ALTER TABLE "poll" DROP COLUMN "channelId"`);
+    }
+}
