33c3531029a3b2ee565298dfcef4b83b3677d7aa
anatawa12 <anatawa12@icloud.com>
2024-05-20 18:08:10 +0900

    fix: missing license header

--- packages/backend/migration/1716129964060-ChannelIdDenormalizedForMiPoll.js
+++ packages/backend/migration/1716129964060-ChannelIdDenormalizedForMiPoll.js
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 export class ChannelIdDenormalizedForMiPoll1716129964060 {
     name = 'ChannelIdDenormalizedForMiPoll1716129964060'
 
