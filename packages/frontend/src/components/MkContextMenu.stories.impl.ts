889dc68a7c1802804c3093e9ef928ec97bd6e84c
zyoshoka <root@zyoshoka.com>
2024-05-21 21:48:24 +0900

    fix: add right click in `MkContextMenu` play

--- packages/frontend/src/components/MkContextMenu.stories.impl.ts
+++ packages/frontend/src/components/MkContextMenu.stories.impl.ts
@@ -6,6 +6,7 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable import/no-default-export */
 import { StoryObj } from '@storybook/vue3';
+import { userEvent, within } from '@storybook/test';
 import MkContextMenu from './MkContextMenu.vue';
 import * as os from '@/os.js';
 export const Empty = {
@@ -33,6 +34,11 @@ export const Empty = {
 	},
 	args: {
 		items: [],
+	},
+	async play({ canvasElement }) {
+		const canvas = within(canvasElement);
+		const target = canvas.getByText('Right Click Here');
+		await userEvent.pointer({ keys: '[MouseRight>]', target });
 	},
 	parameters: {
 		layout: 'centered',
