af8f3feb8cb00ee3a1aaa41d9d8d1d99617f2305
anatawa12 <anatawa12@icloud.com>
2024-05-22 14:32:10 +0900

    chore(frontend)!: do not show url preview if URL is in brackets

--- packages/frontend/src/scripts/extract-url-from-mfm.ts
+++ packages/frontend/src/scripts/extract-url-from-mfm.ts
@@ -11,8 +11,8 @@ import { unique } from '@/scripts/array.js';
 const removeHash = (x: string) => x.replace(/#[^#]*$/, '');
 
 export function extractUrlFromMfm(nodes: mfm.MfmNode[], respectSilentFlag = true): string[] {
-	const urlNodes = mfm.extract(nodes, (node) => {
-		return (node.type === 'url') || (node.type === 'link' && (!respectSilentFlag || !node.props.silent));
+	const urlNodes = mfm.extract(nodes, (node): node is (mfm.MfmNode & {props:{url:string}}) => {
+		return (node.type === 'url' && !node.props.brackets) || (node.type === 'link' && (!respectSilentFlag || !node.props.silent));
 	});
 	const urls: string[] = unique(urlNodes.map(x => x.props.url));
 
