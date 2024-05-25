92d202698c37b94881cf2a4e41c75a85283c2a86
anatawa12 <anatawa12@icloud.com>
2024-05-25 22:02:22 +0900

    chore: change repositoryUrl to vmimi relay fork

---
+++ packages/backend/migration/1716641282089-repositoryUrl-vmimi-relay-timeline.js
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export class RepositoryUrlVmimiRelayTimeline1716641282089 {
+    name = 'RepositoryUrlVmimiRelayTimeline1716641282089'
+
+    async up(queryRunner) {
+        await queryRunner.query(`UPDATE "meta" SET "repositoryUrl" = 'https://github.com/anatawa12/misskey/tree/vmimi-relay-timeline-releases?tab=readme-ov-file#vmimi-relay-timeline'`);
+    }
+
+    async down(queryRunner) {
+        // no valid down migration
+    }
+}
