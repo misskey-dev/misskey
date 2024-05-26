271e0d1b27c491216a63ea639d62f1c897481653
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-05-26 17:38:41 +0900

    spdx

--- packages/frontend/src/pages/announcement.vue
+++ packages/frontend/src/pages/announcement.vue
@@ -1,3 +1,8 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
 <template>
 <MkStickyContainer>
 	<template #header><MkPageHeader :actions="headerActions" :tabs="headerTabs"/></template>
