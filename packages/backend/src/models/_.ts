c8ac5a7e2b202703a644a8d8696689cfe359640a
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2024-05-29 18:07:04 +0900

    fix(backend): fix typo

--- packages/backend/src/models/_.ts
+++ packages/backend/src/models/_.ts
@@ -115,7 +115,7 @@ export const miRepository = {
 			return builder.select(selection, selectionAliasName);
 		};
 		for (const columnName of this.createTableColumnNames(queryBuilder)) {
-			selectOrAddSelect(`${queryBuilder.alias}.${columnName}`, `${queryBuilder.alias}_${columnName}`);
+			selectOrAddSelect(`${builder.alias}.${columnName}`, `${builder.alias}_${columnName}`);
 		}
 	},
 } satisfies MiRepository<ObjectLiteral>;
