93665155657198ea011a714057ba703010b4c479
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2024-06-01 16:39:34 +0900

    test(backend): polyfill position

--- packages/backend/test/jest.setup.ts
+++ packages/backend/test/jest.setup.ts
@@ -5,14 +5,14 @@
 
 import { initTestDb, sendEnvResetRequest } from './utils.js';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-Symbol.dispose ??= Symbol('Symbol.dispose');
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-Symbol.asyncDispose ??= Symbol('Symbol.asyncDispose');
-
 beforeAll(async () => {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-expect-error
+	Symbol.dispose ??= Symbol('Symbol.dispose');
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-expect-error
+	Symbol.asyncDispose ??= Symbol('Symbol.asyncDispose');
+
 	await Promise.all([
 		initTestDb(false),
 		sendEnvResetRequest(),
