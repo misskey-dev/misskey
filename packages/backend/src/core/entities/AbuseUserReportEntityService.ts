8ccce569053101079eeeba7eedc8f69d789abfeb
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-06-06 17:36:30 +0900

    Use mod's own credential to get moderation notes

--- packages/backend/src/core/entities/AbuseUserReportEntityService.ts
+++ packages/backend/src/core/entities/AbuseUserReportEntityService.ts
@@ -8,6 +8,7 @@ import { DI } from '@/di-symbols.js';
 import type { AbuseUserReportsRepository } from '@/models/_.js';
 import { awaitAll } from '@/misc/prelude/await-all.js';
 import type { MiAbuseUserReport } from '@/models/AbuseUserReport.js';
+import type { MiUser } from '@/models/User.js';
 import { bindThis } from '@/decorators.js';
 import { IdService } from '@/core/IdService.js';
 import { isNotNull } from '@/misc/is-not-null.js';
@@ -28,6 +29,7 @@ export class AbuseUserReportEntityService {
 	@bindThis
 	public async pack(
 		src: MiAbuseUserReport['id'] | MiAbuseUserReport,
+		me?: { id: MiUser['id'] } | null | undefined,
 		hint?: {
 			packedReporter?: Packed<'UserDetailedNotMe'>,
 			packedTargetUser?: Packed<'UserDetailedNotMe'>,
@@ -44,13 +46,13 @@ export class AbuseUserReportEntityService {
 			reporterId: report.reporterId,
 			targetUserId: report.targetUserId,
 			assigneeId: report.assigneeId,
-			reporter: hint?.packedReporter ?? this.userEntityService.pack(report.reporter ?? report.reporterId, null, {
+			reporter: hint?.packedReporter ?? this.userEntityService.pack(report.reporter ?? report.reporterId, me, {
 				schema: 'UserDetailedNotMe',
 			}),
-			targetUser: hint?.packedTargetUser ?? this.userEntityService.pack(report.targetUser ?? report.targetUserId, null, {
+			targetUser: hint?.packedTargetUser ?? this.userEntityService.pack(report.targetUser ?? report.targetUserId, me, {
 				schema: 'UserDetailedNotMe',
 			}),
-			assignee: report.assigneeId ? hint?.packedAssignee ?? this.userEntityService.pack(report.assignee ?? report.assigneeId, null, {
+			assignee: report.assigneeId ? hint?.packedAssignee ?? this.userEntityService.pack(report.assignee ?? report.assigneeId, me, {
 				schema: 'UserDetailedNotMe',
 			}) : null,
 			forwarded: report.forwarded,
@@ -60,13 +62,14 @@ export class AbuseUserReportEntityService {
 	@bindThis
 	public async packMany(
 		reports: MiAbuseUserReport[],
+		me?: { id: MiUser['id'] } | null | undefined,
 	) {
 		const _reporters = reports.map(({ reporter, reporterId }) => reporter ?? reporterId);
 		const _targetUsers = reports.map(({ targetUser, targetUserId }) => targetUser ?? targetUserId);
 		const _assignees = reports.map(({ assignee, assigneeId }) => assignee ?? assigneeId).filter(isNotNull);
 		const _userMap = await this.userEntityService.packMany(
 			[..._reporters, ..._targetUsers, ..._assignees],
-			null,
+			me,
 			{ schema: 'UserDetailedNotMe' },
 		).then(users => new Map(users.map(u => [u.id, u])));
 		return Promise.all(
@@ -74,7 +77,7 @@ export class AbuseUserReportEntityService {
 				const packedReporter = _userMap.get(report.reporterId);
 				const packedTargetUser = _userMap.get(report.targetUserId);
 				const packedAssignee = report.assigneeId != null ? _userMap.get(report.assigneeId) : undefined;
-				return this.pack(report, { packedReporter, packedTargetUser, packedAssignee });
+				return this.pack(report, me, { packedReporter, packedTargetUser, packedAssignee });
 			}),
 		);
 	}
