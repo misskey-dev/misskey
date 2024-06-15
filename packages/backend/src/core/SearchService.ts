15be1942c864fbba9297798247b750c8342c3a96
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-06-15 21:13:06 +0900

    #331

--- packages/backend/src/core/SearchService.ts
+++ packages/backend/src/core/SearchService.ts
@@ -229,7 +229,7 @@ export class SearchService {
 					const config = loadConfig();
 					const defaultTag: string | null = config.defaultTag?.tag;
 					if (defaultTag) {
-						query.andWhere('(user.host IS NULL) OR (:t = any(note.tags))', { t: normalizeForSearch(defaultTag) });
+						query.andWhere(':t = any(note.tags)', { t: normalizeForSearch(defaultTag) });
 					} else {
 						query.andWhere('user.host IS NULL');
 					}
