444c67076d5410b2063f7d21e571f747a057f56b
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2024-06-16 00:53:47 +0000

    wip: テストのエラーを修正

--- packages/backend/test/unit/NoteCreateService.ts
+++ packages/backend/test/unit/NoteCreateService.ts
@@ -25,6 +25,7 @@ describe('NoteCreateService', () => {
 	describe('is-renote', () => {
 		const base: MiNote = {
 			id: 'some-note-id',
+			updatedAt: null,
 			replyId: null,
 			reply: null,
 			renoteId: null,
