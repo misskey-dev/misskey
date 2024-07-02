b0a47c1c517a644a88f833a23d89d5693d66341e
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-07-02 18:49:06 +0900

    fix

--- packages/backend/test/e2e/endpoints.ts
+++ packages/backend/test/e2e/endpoints.ts
@@ -125,9 +125,10 @@ describe('Endpoints', () => {
 			assert.strictEqual(res.body.name, null);
 		});
 
-		test('名前の前後に空白（Unicodeのホワイトスペース）を入れてもトリムされる', async () => {
+		test('名前の前後に空白（ホワイトスペース）を入れてもトリムされる', async () => {
 			const res = await api('i/update', {
-				name: ' あ い う \u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000',
+				// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#white_space
+				name: ' あ い う \u0009\u000b\u000c\u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\ufeff',
 			}, alice);
 			assert.strictEqual(res.status, 200);
 			assert.strictEqual(res.body.name, 'あ い う');
