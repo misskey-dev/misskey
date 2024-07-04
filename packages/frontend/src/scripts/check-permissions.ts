bc5fb65475c34dfc3653d4116122f5e2395f2fdc
taichanne30 <dev@taichan.site>
2024-07-04 12:00:25 +0900

    Fix: Vitest fails due to instance.policies being undefined

--- packages/frontend/src/scripts/check-permissions.ts
+++ packages/frontend/src/scripts/check-permissions.ts
@@ -6,4 +6,10 @@
 import { instance } from '@/instance.js';
 import { $i } from '@/account.js';
 
-export const notesSearchAvailable = (($i == null && instance.policies.canSearchNotes) || ($i != null && $i.policies.canSearchNotes)) as boolean;
+export const notesSearchAvailable = (
+	// FIXME: instance.policies would be null in Vitest
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+	($i == null && instance.policies != null && instance.policies.canSearchNotes) ||
+	($i != null && $i.policies.canSearchNotes) ||
+	false
+) as boolean;
