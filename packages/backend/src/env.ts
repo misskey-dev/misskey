33fc3d12ff22ae21cb047ebd01f1b7f6aa803a1f
mattyatea <mattyacocacora0@gmail.com>
2024-07-18 01:43:37 +0900

    Fix: 環境変数周りの修正

--- packages/backend/src/env.ts
+++ packages/backend/src/env.ts
@@ -21,5 +21,6 @@ for (const key of Object.keys(envOption) as (keyof typeof envOption)[]) {
 if (process.env.NODE_ENV === 'test') envOption.disableClustering = true;
 if (process.env.NODE_ENV === 'test') envOption.quiet = true;
 if (process.env.NODE_ENV === 'test') envOption.noDaemons = true;
+if (process.env.MANAGED === 'true') envOption.managed = true;
 
 export { envOption };
