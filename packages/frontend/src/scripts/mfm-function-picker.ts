cbc315c0fb3dae73f3bc573c698ee1c383a57785
woxtu <woxtup@gmail.com>
2024-07-18 00:26:20 +0900

    Update packages/frontend/src/scripts/mfm-function-picker.ts
    Co-authored-by: かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>

--- packages/frontend/src/scripts/mfm-function-picker.ts
+++ packages/frontend/src/scripts/mfm-function-picker.ts
@@ -7,7 +7,7 @@ import { Ref, nextTick } from 'vue';
 import * as os from '@/os.js';
 import { i18n } from '@/i18n.js';
 import { MFM_TAGS } from '@/const.js';
-import { MenuItem } from '@/types/menu.js';
+import type { MenuItem } from '@/types/menu.js';
 
 /**
  * MFMの装飾のリストを表示する
