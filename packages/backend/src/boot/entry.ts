f2c412c18057a9300540794ccbe4dfbf6d259ed6
tamaina <tamaina@hotmail.co.jp>
2024-07-17 16:14:24 +0900

    defaultMaxListeners = 512

--- packages/backend/src/boot/entry.ts
+++ packages/backend/src/boot/entry.ts
@@ -22,7 +22,7 @@ import 'reflect-metadata';
 process.title = `Misskey (${cluster.isPrimary ? 'master' : 'worker'})`;
 
 Error.stackTraceLimit = Infinity;
-EventEmitter.defaultMaxListeners = 128;
+EventEmitter.defaultMaxListeners = 512;
 
 const logger = new Logger('core', 'cyan');
 const clusterLogger = logger.createSubLogger('cluster', 'orange');
