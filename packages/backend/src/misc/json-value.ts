b08b5e42e36d7dab9a26597113f2463e91a1d841
anatawa12 <anatawa12@icloud.com>
2024-07-18 18:30:52 +0900

    fix license header

--- packages/backend/src/misc/json-value.ts
+++ packages/backend/src/misc/json-value.ts
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 export type JsonValue = JsonArray | JsonObject | string | number | boolean | null;
 export type JsonObject = {[K in string]?: JsonValue};
