31b0cd2bc97d9a6f08fe391e72f40653003dda62
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-07-27 12:19:18 +0900

    fix

--- packages/backend/scripts/dev.mjs
+++ packages/backend/scripts/dev.mjs
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: syuilo and misskey-project
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import { execa, execaNode } from 'execa';
 
 /** @type {import('execa').ExecaChildProcess | undefined} */
 let backendProcess;
