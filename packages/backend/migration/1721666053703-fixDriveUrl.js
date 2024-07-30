8b006dd3322745f433f6a31f74add123e3c51bae
taichanne30 <dev@taichan.site>
2024-07-30 15:26:31 +0900

    Fix: インデックスの張り直しを消した

--- packages/backend/migration/1721666053703-fixDriveUrl.js
+++ packages/backend/migration/1721666053703-fixDriveUrl.js
@@ -9,19 +9,15 @@ export class FixDriveUrl1721666053703 {
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "drive_file" ALTER COLUMN "url" TYPE character varying(1024), ALTER COLUMN "url" SET NOT NULL`);
         await queryRunner.query(`COMMENT ON COLUMN "drive_file"."url" IS 'The URL of the DriveFile.'`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_e5848eac4940934e23dbc17581"`);
         await queryRunner.query(`ALTER TABLE "drive_file" ALTER COLUMN "uri" TYPE character varying(1024)`);
         await queryRunner.query(`COMMENT ON COLUMN "drive_file"."uri" IS 'The URI of the DriveFile. it will be null when the DriveFile is local.'`);
         await queryRunner.query(`ALTER TABLE "drive_file" ALTER COLUMN "src" TYPE character varying(1024)`);
-        await queryRunner.query(`CREATE INDEX "IDX_e5848eac4940934e23dbc17581" ON "drive_file" ("uri") `);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`DROP INDEX "public"."IDX_e5848eac4940934e23dbc17581"`);
         await queryRunner.query(`ALTER TABLE "drive_file" ALTER COLUMN "src" TYPE character varying(512)`);
         await queryRunner.query(`COMMENT ON COLUMN "drive_file"."uri" IS 'The URI of the DriveFile. it will be null when the DriveFile is local.'`);
         await queryRunner.query(`ALTER TABLE "drive_file" ALTER COLUMN "uri" TYPE character varying(512)`);
-        await queryRunner.query(`CREATE INDEX "IDX_e5848eac4940934e23dbc17581" ON "drive_file" ("uri") `);
         await queryRunner.query(`COMMENT ON COLUMN "drive_file"."url" IS 'The URL of the DriveFile.'`);
         await queryRunner.query(`ALTER TABLE "drive_file" ALTER COLUMN "url" TYPE character varying(512), ALTER COLUMN "url" SET NOT NULL`);
     }
