e783253ff96b32123fff5f8b00696f209d6aac23
syuilo <4439005+syuilo@users.noreply.github.com>
2024-08-23 08:37:14 +0900

    wip

---
+++ packages/embed/src/components/EmEmoji.vue
@@ -0,0 +1,28 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
+<template>
+<span :alt="props.emoji">{{ colorizedNativeEmoji }}</span>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { colorizeEmoji } from '@/scripts/emojilist.js';
+
+const props = defineProps<{
+	emoji: string;
+	menu?: boolean;
+	menuReaction?: boolean;
+}>();
+
+const colorizedNativeEmoji = computed(() => colorizeEmoji(props.emoji));
+</script>
+
+<style lang="scss" module>
+.root {
+	height: 1.25em;
+	vertical-align: -0.25em;
+}
+</style>
