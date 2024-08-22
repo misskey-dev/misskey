a5f40891c7ad3dd7bac64679e4cbc0a16413e06d
syuilo <4439005+syuilo@users.noreply.github.com>
2024-08-23 08:28:42 +0900

    Create EmA.vue

---
+++ packages/embed/src/components/EmA.vue
@@ -0,0 +1,29 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
+<template>
+<a ref="el" :href="to" @click.prevent="nav">
+	<slot></slot>
+</a>
+</template>
+
+<script lang="ts" setup>
+import { computed, inject, shallowRef } from 'vue';
+
+const props = withDefaults(defineProps<{
+	to: string;
+	activeClass?: null | string;
+}>(), {
+	activeClass: null,
+});
+
+const el = shallowRef<HTMLElement>();
+
+defineExpose({ $el: el });
+
+function nav(ev: MouseEvent) {
+	location.href = props.to;
+}
+</script>
