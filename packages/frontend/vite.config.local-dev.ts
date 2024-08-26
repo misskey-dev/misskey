c2b897354f90678fc6d03971b49dcc6216c6116d
syuilo <4439005+syuilo@users.noreply.github.com>
2024-08-26 17:36:35 +0900

    Update vite.config.local-dev.ts

--- packages/frontend/vite.config.local-dev.ts
+++ packages/frontend/vite.config.local-dev.ts
@@ -15,6 +15,7 @@ const { port } = yaml.load(await readFile('../../.config/default.yml', 'utf-8'))
 
 const httpUrl = `http://localhost:${port}/`;
 const websocketUrl = `ws://localhost:${port}/`;
+const embedUrl = `http://localhost:5174/`;
 
 // activitypubリクエストはProxyを通し、それ以外はViteの開発サーバーを返す
 function varyHandler(req: IncomingMessage) {
@@ -52,6 +53,7 @@ const devConfig: UserConfig = {
 			'/favicon.ico': httpUrl,
 			'/robots.txt': httpUrl,
 			'/embed.js': httpUrl,
+			'/embed': embedUrl,
 			'/identicon': {
 				target: httpUrl,
 				rewrite(path) {
