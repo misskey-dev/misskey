2288a5f67d22bfabd0d56c05f599b6847f748655
syuilo <4439005+syuilo@users.noreply.github.com>
2024-08-26 15:53:45 +0900

    wip

--- packages/frontend-embed/src/pages/clip.vue
+++ packages/frontend-embed/src/pages/clip.vue
@@ -41,7 +41,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 <script setup lang="ts">
 import { ref, computed, shallowRef, inject, onActivated } from 'vue';
 import * as Misskey from 'misskey-js';
-import type { Paging } from '@/components/MkPagination.vue';
+import type { Paging } from '@/components/EmPagination.vue';
 import type { ParsedEmbedParams } from '@/embed-page.js';
 import EmNotes from '@/components/EmNotes.vue';
 import XNotFound from '@/pages/not-found.vue';
@@ -50,7 +50,7 @@ import { misskeyApi } from '@/scripts/misskey-api.js';
 import { i18n } from '@/i18n.js';
 import { instance } from '@/instance.js';
 import { url, instanceName } from '@/config.js';
-import { scrollToTop } from '@/scripts/scroll.js';
+import { scrollToTop } from '@/to-be-shared/scroll.js';
 import { isLink } from '@/scripts/is-link.js';
 import { defaultEmbedParams } from '@/embed-page.js';
 
--- packages/frontend-embed/src/pages/not-found.vue
+++ packages/frontend-embed/src/pages/not-found.vue
@@ -6,7 +6,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 <template>
 <div>
 	<div class="_fullinfo">
-		<img :src="notFoundImageUrl" class="_ghost"/>
 		<div>{{ i18n.ts.notFoundDescription }}</div>
 	</div>
 </div>
@@ -15,5 +14,4 @@ SPDX-License-Identifier: AGPL-3.0-only
 <script lang="ts" setup>
 import { } from 'vue';
 import { i18n } from '@/i18n.js';
-import { notFoundImageUrl } from '@/instance.js';
 </script>
