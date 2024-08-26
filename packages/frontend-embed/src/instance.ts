91fc4ffa5f1e5de5ce0d788db897262a25e13f97
syuilo <4439005+syuilo@users.noreply.github.com>
2024-08-26 11:55:19 +0900

    Create instance.ts

---
+++ packages/frontend-embed/src/instance.ts
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { computed, reactive } from 'vue';
+import * as Misskey from 'misskey-js';
+import { misskeyApi } from '@/misskey-api.js';
+
+export const instance = {
+	iconUrl: 'TODO',
+};
