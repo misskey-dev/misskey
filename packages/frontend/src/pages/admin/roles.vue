07acd7f727ac81ee4bb6d64a1b851ed69d351b1f
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-09-09 14:41:12 +0900

    Revert "Update roles.vue"
    This reverts commit 1488b670660cb1803d17d8f5c78f2d79e59fa52d.

--- packages/frontend/src/pages/admin/roles.vue
+++ packages/frontend/src/pages/admin/roles.vue
@@ -239,7 +239,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { computed, inject, reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import XHeader from './_header_.vue';
 import MkInput from '@/components/MkInput.vue';
 import MkFolder from '@/components/MkFolder.vue';
@@ -251,14 +251,10 @@ import * as os from '@/os.js';
 import { misskeyApi } from '@/scripts/misskey-api.js';
 import { i18n } from '@/i18n.js';
 import { definePageMetadata } from '@/scripts/page-metadata.js';
-import { fetchServerMetadata } from '@/server-metadata.js';
+import { instance, fetchServerMetadata } from '@/server-metadata.js';
 import MkFoldableSection from '@/components/MkFoldableSection.vue';
 import { ROLE_POLICIES } from '@/const.js';
 import { useRouter } from '@/router/supplier.js';
-
-import { DI } from '@/di.js';
-
-const serverMetadata = inject(DI.serverMetadata)!;
 
 const router = useRouter();
 const baseRoleQ = ref('');
@@ -267,7 +263,7 @@ const roles = await misskeyApi('admin/roles/list');
 
 const policies = reactive<Record<typeof ROLE_POLICIES[number], any>>({});
 for (const ROLE_POLICY of ROLE_POLICIES) {
-	policies[ROLE_POLICY] = serverMetadata.policies[ROLE_POLICY];
+	policies[ROLE_POLICY] = instance.policies[ROLE_POLICY];
 }
 
 function matchQuery(keywords: string[]): boolean {
