5aa103a984f4b442cb6eca85c1c8844e927b447b
MattyaDaihuku <87429435+MattyaDaihuku@users.noreply.github.com>
2024-09-14 10:39:46 +0000

    fix(frontend): 変な実装を修正

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -822,7 +822,7 @@ function emitUpdReaction(emoji: string, delta: number) {
 .instanceicon {
 	display: block !important;
 	padding-top: 33px;
-	margin-right: -25px;
+	width: 0;
 	height: 25px;
 	z-index: 10;
 	position: sticky !important;
@@ -973,7 +973,7 @@ function emitUpdReaction(emoji: string, delta: number) {
 	.instanceicon {
 		padding-top: 29px;
 		height: 21px;
-		margin-right: -21px;
+		width: 0;
 	}
 }
 
@@ -1025,7 +1025,7 @@ function emitUpdReaction(emoji: string, delta: number) {
 	.instanceicon {
 		padding-top: 27px;
 		height: 19px;
-		margin-right: -19px;
+		width: 0;
 		top: calc(14px + var(--stickyTop, 0px));
 	}
 }
