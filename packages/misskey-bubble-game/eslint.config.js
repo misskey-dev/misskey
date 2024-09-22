8e8c0f124cab6b95c6c2eae0e16616a07840defb
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-09-22 23:40:43 +0900

    fix

--- packages/misskey-bubble-game/eslint.config.js
+++ packages/misskey-bubble-game/eslint.config.js
@@ -1,7 +1,7 @@
 import tsParser from '@typescript-eslint/parser';
 import sharedConfig from '../shared/eslint.config.js';
 
-// eslint-disable-next-line
+// eslint-disable-next-line import/no-default-export
 export default [
 	...sharedConfig,
 	{
