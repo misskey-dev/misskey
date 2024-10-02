2cf7a6d180e552bf4f61c0f4330087b88cabd905
lqvp <83242690+lqvp@users.noreply.github.com>
2024-10-03 01:01:57 +0900

    モデレーターが閲覧出来ないように(v2024.8.0-revert)

--- packages/backend/src/server/api/endpoints/users/followers.ts
+++ packages/backend/src/server/api/endpoints/users/followers.ts
@@ -11,7 +11,6 @@ import { QueryService } from '@/core/QueryService.js';
 import { FollowingEntityService } from '@/core/entities/FollowingEntityService.js';
 import { UtilityService } from '@/core/UtilityService.js';
 import { DI } from '@/di-symbols.js';
-import { RoleService } from '@/core/RoleService.js';
 import { ApiError } from '../../error.js';
 
 export const meta = {
@@ -82,7 +81,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		private utilityService: UtilityService,
 		private followingEntityService: FollowingEntityService,
 		private queryService: QueryService,
-		private roleService: RoleService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
 			const user = await this.usersRepository.findOneBy(ps.userId != null
@@ -95,24 +93,21 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 
 			const profile = await this.userProfilesRepository.findOneByOrFail({ userId: user.id });
 
-			if (profile.followersVisibility !== 'public' && !await this.roleService.isModerator(me)) {
-				if (profile.followersVisibility === 'private') {
-					if (me == null || (me.id !== user.id)) {
-						throw new ApiError(meta.errors.forbidden);
-					}
-				} else if (profile.followersVisibility === 'followers') {
-					if (me == null) {
-						throw new ApiError(meta.errors.forbidden);
-					} else if (me.id !== user.id) {
-						const isFollowing = await this.followingsRepository.exists({
-							where: {
-								followeeId: user.id,
-								followerId: me.id,
-							},
-						});
-						if (!isFollowing) {
-							throw new ApiError(meta.errors.forbidden);
-						}
+			if (profile.followersVisibility === 'private') {
+				if (me == null || (me.id !== user.id)) {
+					throw new ApiError(meta.errors.forbidden);
+				}
+			} else if (profile.followersVisibility === 'followers') {
+				if (me == null) {
+					throw new ApiError(meta.errors.forbidden);
+				} else if (me.id !== user.id) {
+					const isFollowing = await this.followingsRepository.exists({
+						where: {
+							followeeId: user.id,
+							followerId: me.id,
+						},
+					});
+					if (!isFollowing) {
 					}
 				}
 			}
