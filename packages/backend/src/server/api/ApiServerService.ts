b6307b5ce2b3451250e1dfcb53f03a0709b4b2c4
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-10-05 00:08:50 +0900

    fix

--- packages/backend/src/server/api/ApiServerService.ts
+++ packages/backend/src/server/api/ApiServerService.ts
@@ -38,7 +38,7 @@ export class ApiServerService {
 		private userEntityService: UserEntityService,
 		private apiCallService: ApiCallService,
 		private signupApiService: SignupApiService,
-		private signinFlowApiService: SigninApiService,
+		private signinApiService: SigninApiService,
 		private signinWithPasskeyApiService: SigninWithPasskeyApiService,
 	) {
 		//this.createServer = this.createServer.bind(this);
@@ -133,7 +133,7 @@ export class ApiServerService {
 				'turnstile-response'?: string;
 				'm-captcha-response'?: string;
 			};
-		}>('/signin-flow', (request, reply) => this.signinFlowApiService.signin(request, reply));
+		}>('/signin-flow', (request, reply) => this.signinApiService.signin(request, reply));
 
 		fastify.post<{
 			Body: {
