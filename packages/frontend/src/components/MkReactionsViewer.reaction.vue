nakkaa/kakurega.app

feat: 誰がリアクションをしたのかを非表示にできる機能を実装

packages/frontend/src/components/MkReactionsViewer.reaction.vue
@@ -168,12 +168,12 @@ onMounted(() => {
 
 if (!mock) {
 	useTooltip(buttonEl, async (showing) => {
-		const reactions = await misskeyApiGet('notes/reactions', {
+		const reactions = !defaultStore.state.hideReactionUsers ? await misskeyApiGet('notes/reactions', {
 			noteId: props.note.id,
 			type: props.reaction,
 			limit: 10,
 			_cacheKey_: props.count,
-		});
+		}) : [];
 
 		const users = reactions.map(x => x.user);
 

misskey-dev/misskey

packages/frontend/src/components/MkReactionsViewer.reaction.vue
@@ -150,12 +150,12 @@ if (!mock) {
 	useTooltip(buttonEl, async (showing) => {
 		const useGet = !reactionChecksMuting.value;
 		const apiCall = useGet ? misskeyApiGet : misskeyApi;
-		const reactions = await apiCall('notes/reactions', {
+		const reactions = !defaultStore.state.hideReactionUsers ? await apiCall('notes/reactions', {
 			noteId: props.note.id,
 			type: props.reaction,
 			limit: 10,
 			_cacheKey_: props.count,
-		});
+		}) : [];
 
 		const users = reactions.map(x => x.user);
 		const count = users.length;