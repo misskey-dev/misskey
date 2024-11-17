4a68df9c050b9d0f033cbb02f6010cf3fcfa6a55
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-11-18 00:02:11 +0900

    fix

--- packages/backend/migration/1728986848483-defaultFollowUsers.js
+++ packages/backend/migration/1728986848483-defaultFollowUsers.js
@@ -8,11 +8,11 @@ export class DefaultFollowUsers1728986848483 {
 
 	async up(queryRunner) {
 		await queryRunner.query(`ALTER TABLE "meta" ADD "defaultFollowedUsers" character varying(1024) array NOT NULL DEFAULT '{}'`);
-		await queryRunner.query(`ALTER TABLE "meta" ADD "permanentFollowedUsers" character varying(1024) array NOT NULL DEFAULT '{}'`);
+		await queryRunner.query(`ALTER TABLE "meta" ADD "forciblyFollowedUsers" character varying(1024) array NOT NULL DEFAULT '{}'`);
 	}
 
 	async down(queryRunner) {
-		await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "permanentFollowedUsers"`);
+		await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "forciblyFollowedUsers"`);
 		await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "defaultFollowedUsers"`);
 	}
 }
