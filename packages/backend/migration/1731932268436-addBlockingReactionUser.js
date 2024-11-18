292809a3248f15577a6f23422e505a581006363d
鴇峰 朔華 <sakuhana@tsuitachi.net>
2024-11-18 22:08:05 +0900

    fix: SPDXつけ忘れ

--- packages/backend/migration/1731932268436-addBlockingReactionUser.js
+++ packages/backend/migration/1731932268436-addBlockingReactionUser.js
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 export class AddBlockingReactionUser1731932268436 {
     name = 'AddBlockingReactionUser1731932268436'
 
