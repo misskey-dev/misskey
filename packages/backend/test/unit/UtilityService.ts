5094611ade671d04d8bf86fa93a3708b18a845b8
dakkar <dakkar@thenautilus.net>
2024-11-24 14:04:03 +0000

    spaces / lint

--- packages/backend/test/unit/UtilityService.ts
+++ packages/backend/test/unit/UtilityService.ts
@@ -17,23 +17,23 @@ describe('UtilityService', () => {
 
 	describe('punyHost', () => {
 		test('simple', () => {
-			assert.equal(utilityService.punyHost('http://www.foo.com'),'www.foo.com');
+			assert.equal(utilityService.punyHost('http://www.foo.com'), 'www.foo.com');
 		});
 		test('japanese', () => {
-			assert.equal(utilityService.punyHost('http://www.新聞.com'),'www.xn--efvv70d.com');
+			assert.equal(utilityService.punyHost('http://www.新聞.com'), 'www.xn--efvv70d.com');
 		});
 	});
 
 	describe('punyHostPSLDomain', () => {
 		test('simple', () => {
-			assert.equal(utilityService.punyHostPSLDomain('http://www.foo.com'),'foo.com');
+			assert.equal(utilityService.punyHostPSLDomain('http://www.foo.com'), 'foo.com');
 		});
 		test('japanese', () => {
-			assert.equal(utilityService.punyHostPSLDomain('http://www.新聞.com'),'xn--efvv70d.com');
+			assert.equal(utilityService.punyHostPSLDomain('http://www.新聞.com'), 'xn--efvv70d.com');
 		});
 		test('lower', () => {
-			assert.equal(utilityService.punyHostPSLDomain('http://foo.github.io'),'foo.github.io');
-			assert.equal(utilityService.punyHostPSLDomain('http://foo.bar.github.io'),'bar.github.io');
+			assert.equal(utilityService.punyHostPSLDomain('http://foo.github.io'), 'foo.github.io');
+			assert.equal(utilityService.punyHostPSLDomain('http://foo.bar.github.io'), 'bar.github.io');
 		});
 	});
 });
