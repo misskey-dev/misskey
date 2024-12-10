b5b94d5b307a4cd272264351ddb8de86fce7c666
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-12-10 10:29:44 +0900

    Revert "spdx"
    This reverts commit 6d8aa7703599b71f7cae77a91216bd20100e0fc0.

--- packages/backend/test/unit/UtilityService.ts
+++ packages/backend/test/unit/UtilityService.ts
@@ -1,8 +1,3 @@
-/*
- * SPDX-FileCopyrightText: syuilo and misskey-project
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
 import * as assert from 'assert';
 import { Test } from '@nestjs/testing';
 
