3faca03d9370f12f9e892553184ae7d69967b6df
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-12-17 00:46:01 +0900

    Update index.vue

--- packages/frontend/src/pages/user/index.vue
+++ packages/frontend/src/pages/user/index.vue
@@ -106,7 +106,7 @@ const headerTabs = computed(() => user.value ? [{
 	title: i18n.ts.notes,
 	icon: 'ti ti-pencil',
 }, {
-	key: 'file',
+	key: 'files',
 	title: i18n.ts.files,
 	icon: 'ti ti-photo',
 }, {
