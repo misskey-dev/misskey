01d995110a0e3710a25eed0811d06df7e6ab0137
tai-cha <dev@taichan.site>
2024-12-26 04:11:05 +0900

    Fix: チュートリアルで非LTL投稿が出る（出なくていい）

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -471,7 +471,7 @@ function setVisibility() {
 		isSilenced: $i.isSilenced,
 		localOnly: localOnly.value,
 		src: visibilityButton.value,
-		isPublicNonLtlRestrected: $i.policies.canPublicNonLtlNote !== true,
+		isPublicNonLtlRestrected: $i.policies.canPublicNonLtlNote !== true || props.mock,
 		...(props.reply ? { isReplyVisibilitySpecified: props.reply.visibility === 'specified' } : {}),
 	}, {
 		changeVisibility: v => {
