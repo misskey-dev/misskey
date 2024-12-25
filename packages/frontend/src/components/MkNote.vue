b3fcf9f4cfa5a11d6a65a83da9e8df02da83befe
tai-cha <dev@taichan.site>
2024-12-26 02:31:06 +0900

    Fix: 変な変更してた

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -279,7 +279,6 @@ const renoteCollapsed = ref(
 	defaultStore.state.collapseRenotes && isRenote && (
 		($i && ($i.id === note.value.userId || $i.id === appearNote.value.userId)) || // `||` must be `||`! See https://github.com/misskey-dev/misskey/issues/13131
 		(appearNote.value.myReaction != null)
-		(appearNote.value.myReaction != null)
 	),
 );
 
