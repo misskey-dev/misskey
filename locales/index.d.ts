24ce8b2e21b34172b839aeadddd23990818d62a4
mai <74494945+chan-mai@users.noreply.github.com>
2024-12-26 08:54:19 +0000

    update: 言語拡張

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -8709,7 +8709,7 @@ export interface Locale extends ILocale {
          */
         "publicDescription": string;
         /**
-         * パブリック（非LTL）
+         * セミパブリック
          */
         "public_non_ltl": string;
         /**
