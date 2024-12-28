38fa15476f9eba32f5d28eff4d6a80326b3cfc1e
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-12-28 18:55:16 +0900

    fix backend

--- packages/backend/src/core/entities/MetaEntityService.ts
+++ packages/backend/src/core/entities/MetaEntityService.ts
@@ -130,6 +130,7 @@ export class MetaEntityService {
 
 			mediaProxy: this.config.mediaProxy,
 			enableUrlPreview: instance.urlPreviewEnabled,
+			enableStreamNotesCdnCache: instance.enableStreamNotesCdnCache,
 			noteSearchableScope: (this.config.meilisearch == null || this.config.meilisearch.scope !== 'local') ? 'global' : 'local',
 			maxFileSize: this.config.maxFileSize,
 		};
