05967333c8386338f1eddd3f4a64727d35e1d71c
tai-cha <dev@taichan.site>
2025-01-05 12:18:17 +0900

    Fix: typo

--- packages/backend/src/core/entities/NoteEntityService.ts
+++ packages/backend/src/core/entities/NoteEntityService.ts
@@ -463,7 +463,7 @@ export class NoteEntityService implements OnModuleInit {
 		});
 
 		if (!opts.skipTreatVisibility) {
-			his.treatVisibility(packed);
+			this.treatVisibility(packed);
 		}
 
 		if (!opts.skipHide) {
