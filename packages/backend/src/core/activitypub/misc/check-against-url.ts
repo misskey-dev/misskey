6182e4a8e8052bee2e800a2f315d7f29a9c821a5
おさむのひと <46447427+samunohito@users.noreply.github.com>
2025-01-08 12:17:56 +0900

    https://github.com/misskey-dev/misskey/issues/15039#issuecomment-2576411861 の反映
    Co-authored-by: Kagami Sascha Rosylight <saschanaz@outlook.com>

--- packages/backend/src/core/activitypub/misc/check-against-url.ts
+++ packages/backend/src/core/activitypub/misc/check-against-url.ts
@@ -5,17 +5,15 @@
 import type { IObject } from '../type.js';
 
 export function assertActivityMatchesUrls(activity: IObject, urls: string[]) {
-	const idOk = activity.id !== undefined && urls.includes(activity.id);
+	const hosts = urls.map(it => new URL(it).host);
+
+	const idOk = activity.id !== undefined && hosts.includes(new URL(activity.id).host);
 
 	// technically `activity.url` could be an `ApObject = IObject |
 	// string | (IObject | string)[]`, but if it's a complicated thing
 	// and the `activity.id` doesn't match, I think we're fine
 	// rejecting the activity
-	let urlOk = false;
-	if (typeof(activity.url) === 'string') {
-		const actUrlHost = new URL(activity.url).host;
-		urlOk = urls.map(it => new URL(it).host).includes(actUrlHost);
-	}
+	const urlOk = typeof(activity.url) === 'string' && hosts.includes(new URL(activity.url).host);
 
 	if (!idOk && !urlOk) {
 		throw new Error(`bad Activity: neither id(${activity?.id}) nor url(${activity?.url}) match location(${urls})`);
