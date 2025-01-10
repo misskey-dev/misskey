79a5e47b2378b3177566e68f5cf7f68ea33e15b0
anatawa12 <anatawa12@icloud.com>
2025-01-10 15:25:44 +0900

    chore: remove punycode.d.ts

--- packages/backend/src/@types/punycode.d.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: syuilo and misskey-project
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
-// see https://github.com/misskey-dev/misskey/pull/15248
-declare module 'punycode/punycode.es6.js' {
-	export * from 'punycode/';
-}
