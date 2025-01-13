491b0ee749e6d7b9ee4a4f639def84955c6c8d45
おさむのひと <46447427+samunohito@users.noreply.github.com>
2025-01-13 14:23:47 +0900

    fix ci

--- packages/frontend/src/components/hook/useLoading.ts
+++ packages/frontend/src/components/hook/useLoading.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { computed, h, ref } from 'vue';
 import MkLoading from '@/components/global/MkLoading.vue';
 
