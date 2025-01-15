ab6fb66a29d144697356c0a71da7152a1cb06280
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-01-15 11:42:56 +0900

    indent

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -478,10 +478,10 @@ export const defaultStore = markRaw(new Storage('base', {
 		where: 'device',
 		default: false,
 	},
-  confirmOnReact: {
-    where: 'device',
-    default: false,
-  },
+	confirmOnReact: {
+		where: 'device',
+		default: false,
+	},
 
 	sound_masterVolume: {
 		where: 'device',
