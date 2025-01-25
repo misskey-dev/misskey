9265182729c797b08a00b74cfaffeef249c7b6a8
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-01-25 15:56:23 +0900

    feat(frontend): tabler-iconsの使用されていないアイコンを削除するように

--- packages/frontend/src/_boot_.ts
+++ packages/frontend/src/_boot_.ts
@@ -6,7 +6,12 @@
 // https://vitejs.dev/config/build-options.html#build-modulepreload
 import 'vite/modulepreload-polyfill';
 
-import '@tabler/icons-webfont/dist/tabler-icons.scss';
+if (import.meta.env.DEV) {
+	await import('@tabler/icons-webfont/dist/tabler-icons.scss');
+} else {
+	await import('icons-subsetter/built/tabler-icons-classes.css');
+	await import('icons-subsetter/built/tabler-icons-frontend.css');
+}
 
 import '@/style.scss';
 import { mainBoot } from '@/boot/main-boot.js';
