c838f8005b3b7f100b1ebc5d986bda2410762fc8
tai-cha <dev@taichan.site>
2025-01-25 16:43:25 +0900

    消えてたのFix

--- packages/frontend/src/components/MkPostFormAttaches.vue
+++ packages/frontend/src/components/MkPostFormAttaches.vue
@@ -35,6 +35,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 <script lang="ts" setup>
 import { defineAsyncComponent, inject } from 'vue';
 import * as Misskey from 'misskey-js';
+import type { MenuItem } from '@/types/menu';
 import MkDriveFileThumbnail from '@/components/MkDriveFileThumbnail.vue';
 import * as os from '@/os.js';
 import { misskeyApi } from '@/scripts/misskey-api.js';
