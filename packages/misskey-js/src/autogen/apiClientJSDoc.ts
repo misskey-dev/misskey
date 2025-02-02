lqvp/misskey-tempura

feat: リアクションの統計を見れるように

packages/misskey-js/src/autogen/apiClientJSDoc.ts
@@ -3999,6 +3999,17 @@ declare module '../api.js' {
     ): Promise<SwitchCaseResponseType<E, P>>;
 
     /**
+     * No description provided.
+     * 
+     * **Credential required**: *Yes* / **Permission**: *read:account*
+     */
+    request<E extends 'reaction-stat', P extends Endpoints[E]['req']>(
+      endpoint: E,
+      params: P,
+      credential?: string | null,
+    ): Promise<SwitchCaseResponseType<E, P>>;
+
+    /**
      * Check push notification registration exists.
      * 
      * **Internal Endpoint**: This endpoint is an API for the misskey mainframe and is not intended for use by third parties.

packages/misskey-js/src/autogen/entities.ts
@@ -529,6 +529,8 @@ export type RequestResetPasswordRequest = operations['request-reset-password']['
 export type ResetPasswordRequest = operations['reset-password']['requestBody']['content']['application/json'];
 export type ServerInfoResponse = operations['server-info']['responses']['200']['content']['application/json'];
 export type StatsResponse = operations['stats']['responses']['200']['content']['application/json'];
+export type ReactionStatRequest = operations['reaction-stat']['requestBody']['content']['application/json'];
+export type ReactionStatResponse = operations['reaction-stat']['responses']['200']['content']['application/json'];
 export type SwShowRegistrationRequest = operations['sw___show-registration']['requestBody']['content']['application/json'];
 export type SwShowRegistrationResponse = operations['sw___show-registration']['responses']['200']['content']['application/json'];
 export type SwUpdateRegistrationRequest = operations['sw___update-registration']['requestBody']['content']['application/json'];

packages/misskey-js/src/autogen/types.ts
@@ -3445,6 +3445,15 @@ export type paths = {
      */
     post: operations['stats'];
   };
+  '/reaction-stat': {
+    /**
+     * reaction-stat
+     * @description No description provided.
+     *
+     * **Credential required**: *Yes* / **Permission**: *read:account*
+     */
+    post: operations['reaction-stat'];
+  };
   '/sw/show-registration': {
     /**
      * sw/show-registration
@@ -26967,6 +26976,63 @@ export type operations = {
             driveUsageLocal: number;
             driveUsageRemote: number;
           };
+        };
+      };
+      /** @description Client error */
+      400: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+      /** @description Authentication error */
+      401: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+      /** @description Forbidden error */
+      403: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+      /** @description I'm Ai */
+      418: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+      /** @description Internal server error */
+      500: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+    };
+  };
+  /**
+   * reaction-stat
+   * @description No description provided.
+   *
+   * **Credential required**: *Yes* / **Permission**: *read:account*
+   */
+  'reaction-stat': {
+    requestBody: {
+      content: {
+        'application/json': {
+          /** @default false */
+          site?: boolean;
+        };
+      };
+    };
+    responses: {
+      /** @description OK (with results) */
+      200: {
+        content: {
+          'application/json': {
+              reaction: string;
+              count: number;
+            }[];
         };
       };
       /** @description Client error */

misskey-dev/misskey

packages/misskey-js/src/autogen/apiClientJSDoc.ts
@@ -3674,6 +3674,17 @@ declare module '../api.js' {
     /**
      * No description provided.
      * 
+     * **Credential required**: *Yes* / **Permission**: *read:account*
+     */
+    request<E extends 'reactions-stat', P extends Endpoints[E]['req']>(
+      endpoint: E,
+      params: P,
+      credential?: string | null,
+    ): Promise<SwitchCaseResponseType<E, P>>;
+
+    /**
+     * No description provided.
+     * 
      * **Credential required**: *Yes* / **Permission**: *write:mutes*
      */
     request<E extends 'renote-mute/create', P extends Endpoints[E]['req']>(

packages/misskey-js/src/autogen/entities.ts
@@ -492,6 +492,8 @@ export type PagesUpdateRequest = operations['pages___update']['requestBody']['co
 export type PingResponse = operations['ping']['responses']['200']['content']['application/json'];
 export type PinnedUsersResponse = operations['pinned-users']['responses']['200']['content']['application/json'];
 export type PromoReadRequest = operations['promo___read']['requestBody']['content']['application/json'];
+export type ReactionsStatRequest = operations['reactions-stat']['requestBody']['content']['application/json'];
+export type ReactionsStatResponse = operations['reactions-stat']['responses']['200']['content']['application/json'];
 export type RenoteMuteCreateRequest = operations['renote-mute___create']['requestBody']['content']['application/json'];
 export type RenoteMuteDeleteRequest = operations['renote-mute___delete']['requestBody']['content']['application/json'];
 export type RenoteMuteListRequest = operations['renote-mute___list']['requestBody']['content']['application/json'];

packages/misskey-js/src/autogen/types.ts
@@ -3176,6 +3176,15 @@ export type paths = {
      */
     post: operations['promo___read'];
   };
+  '/reactions-stat': {
+    /**
+     * reactions-stat
+     * @description No description provided.
+     *
+     * **Credential required**: *Yes* / **Permission**: *read:account*
+     */
+    post: operations['reactions-stat'];
+  };
   '/renote-mute/create': {
     /**
      * renote-mute/create
@@ -24815,6 +24824,63 @@ export type operations = {
       /** @description OK (without any results) */
       204: {
         content: never;
+      };
+      /** @description Client error */
+      400: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+      /** @description Authentication error */
+      401: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+      /** @description Forbidden error */
+      403: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+      /** @description I'm Ai */
+      418: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+      /** @description Internal server error */
+      500: {
+        content: {
+          'application/json': components['schemas']['Error'];
+        };
+      };
+    };
+  };
+  /**
+   * reactions-stat
+   * @description No description provided.
+   *
+   * **Credential required**: *Yes* / **Permission**: *read:account*
+   */
+  'reactions-stat': {
+    requestBody: {
+      content: {
+        'application/json': {
+          /** @default false */
+          site?: boolean;
+        };
+      };
+    };
+    responses: {
+      /** @description OK (with results) */
+      200: {
+        content: {
+          'application/json': {
+              reaction: string;
+              count: number;
+            }[];
+        };
       };
       /** @description Client error */
       400: {