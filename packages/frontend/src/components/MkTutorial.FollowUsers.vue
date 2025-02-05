877eb63ebddedd27e37903081d4e73f40e29bb01
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-02-05 21:18:25 +0900

    fix

--- packages/frontend/src/components/MkTutorial.FollowUsers.vue
+++ packages/frontend/src/components/MkTutorial.FollowUsers.vue
@@ -37,7 +37,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 import * as Misskey from 'misskey-js';
 import { i18n } from '@/i18n.js';
 import MkFolder from '@/components/MkFolder.vue';
-import XUser from '@/components/MkUserSetupDialog.User.vue';
+import XUser from '@/components/MkTutorial.FollowUsers.UserCard.vue';
 import MkPagination from '@/components/MkPagination.vue';
 import type { Paging } from '@/components/MkPagination.vue';
 import type { TutorialPageCommonExpose } from '@/components/MkTutorial.vue';
