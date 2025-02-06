533c8fc7cc912a89b4be2a1f6364e99a432085a0
おさむのひと <46447427+samunohito@users.noreply.github.com>
2025-02-06 21:44:40 +0900

    SPDX

--- packages/backend/test/unit/boot/worker.ts
+++ packages/backend/test/unit/boot/worker.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { beforeEach, describe, expect, test } from '@jest/globals';
 import { setCpuCount } from '@/boot/common.js';
 import { WorkerArguments } from '@/boot/const.js';
