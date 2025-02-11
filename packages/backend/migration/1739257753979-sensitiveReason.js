a97aad3b8696d692e44f55d7f62928def80db26e
おさむのひと <46447427+samunohito@users.noreply.github.com>
2025-02-11 17:40:20 +0900

    マイグレーションファイルの作成

---
+++ packages/backend/migration/1739257753979-sensitiveReason.js
@@ -0,0 +1,22 @@
+export class SensitiveReason1739257753979 {
+    name = 'SensitiveReason1739257753979'
+
+    async up(queryRunner) {
+        await queryRunner.query(`
+            CREATE TYPE "public"."drive_file_sensitivechangereason_enum" AS ENUM('user', 'moderator', 'auto')
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "drive_file"
+            ADD "sensitiveChangeReason" "public"."drive_file_sensitivechangereason_enum" NOT NULL DEFAULT 'user'
+        `);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`
+            ALTER TABLE "drive_file" DROP COLUMN "sensitiveChangeReason"
+        `);
+        await queryRunner.query(`
+            DROP TYPE "public"."drive_file_sensitivechangereason_enum"
+        `);
+    }
+}
