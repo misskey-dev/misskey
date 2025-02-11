3d93454642fe85b627a75664b143a8d8615fd7a3
おさむのひと <46447427+samunohito@users.noreply.github.com>
2025-02-11 18:55:23 +0900

    fix SPDX

--- packages/backend/migration/1739257753979-sensitiveReason.js
+++ packages/backend/migration/1739257753979-sensitiveReason.js
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 export class SensitiveReason1739257753979 {
     name = 'SensitiveReason1739257753979'
 
