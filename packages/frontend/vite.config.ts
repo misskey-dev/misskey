fae82249603e43c8158b4831cd445b5c4044bf53
KanariKanaru <93921745+kanarikanaru@users.noreply.github.com>
2025-02-24 01:40:30 +0900

    enhance: フロントエンドビルド時にバージョン情報をファイルに付与 (#290)

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -158,8 +158,9 @@ export function getConfig(): UserConfig {
 						vue: ['vue'],
 						photoswipe: ['photoswipe', 'photoswipe/lightbox', 'photoswipe/style.css'],
 					},
-					chunkFileNames: process.env.NODE_ENV === 'production' ? '[hash:8].js' : '[name]-[hash:8].js',
-					assetFileNames: process.env.NODE_ENV === 'production' ? '[hash:8][extname]' : '[name]-[hash:8][extname]',
+					entryFileNames: process.env.NODE_ENV === 'production' ? `${meta.version}.[hash:8].js` : '[name]-[hash:8].js',
+					chunkFileNames: process.env.NODE_ENV === 'production' ? `${meta.version}.[hash:8].js` : '[name]-[hash:8].js',
+					assetFileNames: process.env.NODE_ENV === 'production' ? `${meta.version}.[hash:8][extname]` : '[name]-[hash:8][extname]',
 					paths(id) {
 						for (const p of externalPackages) {
 							if (p.match.test(id)) {
