01a3eabc4e1deb00b29398777d453d5b3583583b
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-03-03 17:46:38 +0900

    enhance(frontend): アニメーション設定で画面上のエフェクトも考慮するように (#15576)
    * enhance(frontend): アニメーション設定で画面上のエフェクトも考慮するように
    
    * Update Changelog

--- packages/frontend/src/scripts/get-note-menu.ts
+++ packages/frontend/src/scripts/get-note-menu.ts
@@ -550,7 +550,7 @@ export function getRenoteMenu(props: {
 			icon: 'ti ti-repeat',
 			action: () => {
 				const el = props.renoteButton.value;
-				if (el) {
+				if (el && defaultStore.state.animation) {
 					const rect = el.getBoundingClientRect();
 					const x = rect.left + (el.offsetWidth / 2);
 					const y = rect.top + (el.offsetHeight / 2);
@@ -588,7 +588,7 @@ export function getRenoteMenu(props: {
 			icon: 'ti ti-repeat',
 			action: () => {
 				const el = props.renoteButton.value;
-				if (el) {
+				if (el && defaultStore.state.animation) {
 					const rect = el.getBoundingClientRect();
 					const x = rect.left + (el.offsetWidth / 2);
 					const y = rect.top + (el.offsetHeight / 2);
@@ -639,7 +639,7 @@ export function getRenoteMenu(props: {
 					text: channel.name,
 					action: () => {
 						const el = props.renoteButton.value;
-						if (el) {
+						if (el && defaultStore.state.animation) {
 							const rect = el.getBoundingClientRect();
 							const x = rect.left + (el.offsetWidth / 2);
 							const y = rect.top + (el.offsetHeight / 2);
