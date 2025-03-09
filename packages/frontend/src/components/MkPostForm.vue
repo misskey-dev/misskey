6ad0f37f340714d46d3dc563f2cd2da356a02836
rca <nem@nem.x0.to>
2025-03-10 05:13:29 +0900

    add condition to disable post button when CW text is empty

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -265,6 +265,7 @@ const canPost = computed((): boolean => {
 			quoteId.value != null
 		) &&
 		(textLength.value <= maxTextLength.value) &&
+		(0 < cwTextLength.value) &&
 		(cwTextLength.value <= maxCwTextLength) &&
 		(files.value.length <= 16) &&
 		(!poll.value || poll.value.choices.length >= 2);
