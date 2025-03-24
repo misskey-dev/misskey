53e3e993a85fde057fd18dce8af1427c8965e407
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-24 14:31:22 +0900

    🎨

--- packages/frontend/src/pages/chat/home.vue
+++ packages/frontend/src/pages/chat/home.vue
@@ -5,6 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 <template>
 <PageWithHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs">
+	<MkPolkadots v-if="tab === 'home'" accented/>
 	<MkSpacer :contentMax="700">
 		<MkHorizontalSwipe v-model:tab="tab" :tabs="headerTabs">
 			<XHome v-if="tab === 'home'"/>
@@ -23,6 +24,7 @@ import XOwnedRooms from './home.ownedRooms.vue';
 import { i18n } from '@/i18n.js';
 import { definePage } from '@/page.js';
 import MkHorizontalSwipe from '@/components/MkHorizontalSwipe.vue';
+import MkPolkadots from '@/components/MkPolkadots.vue';
 
 const tab = ref('home');
 
