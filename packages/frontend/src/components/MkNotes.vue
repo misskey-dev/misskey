8baf54e62904b997207e37cafb14195268f54578
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-31 11:01:32 +0900

    fix(frontend): fix ad rendering of timeline

--- packages/frontend/src/components/MkNotes.vue
+++ packages/frontend/src/components/MkNotes.vue
@@ -23,10 +23,13 @@ SPDX-License-Identifier: AGPL-3.0-only
 			tag="div"
 		>
 			<template v-for="(note, i) in notes" :key="note.id">
-				<MkNote :class="$style.note" :note="note" :withHardMute="true"/>
-				<div v-if="note._shouldInsertAd_" :class="$style.ad">
-					<MkAd :preferForms="['horizontal', 'horizontal-big']"/>
+				<div v-if="note._shouldInsertAd_" :class="[$style.noteWithAd, { '_gaps': !noGap }]">
+					<MkNote :class="$style.note" :note="note" :withHardMute="true"/>
+					<div :class="$style.ad">
+						<MkAd :preferForms="['horizontal', 'horizontal-big']"/>
+					</div>
 				</div>
+				<MkNote v-else :class="$style.note" :note="note" :withHardMute="true"/>
 			</template>
 		</component>
 	</template>
