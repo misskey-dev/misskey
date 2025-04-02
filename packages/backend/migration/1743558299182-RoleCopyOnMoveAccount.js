7e7af5806ef6ca2bcdd47275b54c853dfdc1527f
anatawa12 <anatawa12@icloud.com>
2025-04-02 12:18:14 +0900

    fix: spdx header for migration

--- packages/backend/migration/1743558299182-RoleCopyOnMoveAccount.js
+++ packages/backend/migration/1743558299182-RoleCopyOnMoveAccount.js
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 export class RoleCopyOnMoveAccount1743558299182 {
     name = 'RoleCopyOnMoveAccount1743558299182'
 
