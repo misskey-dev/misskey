5618a3cff4d2f6579352b5d932e7a97965719a63
anatawa12 <anatawa12@icloud.com>
2025-04-04 23:47:21 +0900

    fix: spdx license header

--- packages/frontend/src/utility/virtual.d.ts
+++ packages/frontend/src/utility/virtual.d.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 declare module 'search-index:settings' {
 	export type GeneratedSearchIndexItem = {
 		id: string;
