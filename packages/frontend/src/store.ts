e9aad31f325735fae57f198ddf04e85389baeaa5
Lilia <lirisia@outlook.ie>
2025-04-10 14:05:16 +0900

    Feat: add widget position settings (left/right)

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -266,6 +266,10 @@ export const store = markRaw(new Pizzax('base', {
 		where: 'device',
 		default: DEFAULT_DEVICE_KIND === 'desktop',
 	},
+	widgetsPosition: {
+		where: 'device',
+		default: 'left' as 'left' | 'right',
+	},
 	showFixedPostForm: {
 		where: 'device',
 		default: false,
