b0a36669ecbd7bb3f0d1c66c9abf044912aaafbd
hitalin <hitalin@proton.me>
2025-04-11 12:21:34 +0900

    fix: 500 error when accessing the floater page is resolved.

--- packages/backend/src/server/api/endpoints/notes/floater.ts
+++ packages/backend/src/server/api/endpoints/notes/floater.ts
@@ -88,8 +88,8 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			const query = this.notesRepository.createQueryBuilder('note').innerJoinAndSelect('note.user', 'user');
 
 			this.queryService.generateVisibilityQuery(query, me);
-			this.queryService.generateMutedUserQuery(query, me);
-			this.queryService.generateBlockedUserQuery(query, me);
+			this.queryService.generateMutedUserQueryForNotes(query, me);
+			this.queryService.generateBlockedUserQueryForNotes(query, me);
 			this.queryService.generateMutedUserRenotesQueryForNotes(query, me);
 
 			query.andWhere('note.renoteId IS NULL');
