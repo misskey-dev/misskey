c5f1ce60fa4800d51d3f09b911b85ceff0a69e83
anatawa12 <anatawa12@icloud.com>
2025-04-16 07:51:41 +0900

    fix(sw): type error in sw (#15844)

--- packages/sw/src/scripts/create-notification.ts
+++ packages/sw/src/scripts/create-notification.ts
@@ -271,7 +271,7 @@ async function composeNotification(data: PushNotificationDataMap[keyof PushNotif
 		case 'newChatMessage':
 			if (data.body.toRoom != null) {
 				return [`${data.body.toRoom.name}: ${getUserName(data.body.fromUser)}: ${data.body.text}`, {
-					icon: data.body.fromUser.avatarUrl,
+					icon: data.body.fromUser.avatarUrl ?? undefined,
 					badge: iconUrl('messages'),
 					tag: `chat:room:${data.body.toRoomId}`,
 					data,
@@ -279,7 +279,7 @@ async function composeNotification(data: PushNotificationDataMap[keyof PushNotif
 				}];
 			} else {
 				return [`${getUserName(data.body.fromUser)}: ${data.body.text}`, {
-					icon: data.body.fromUser.avatarUrl,
+					icon: data.body.fromUser.avatarUrl ?? undefined,
 					badge: iconUrl('messages'),
 					tag: `chat:user:${data.body.fromUserId}`,
 					data,
