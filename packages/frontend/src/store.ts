a47466817104376c0efdb95a68971c9ac0116924
miraikumiko <miraikumiko@disroot.org>
2025-04-20 03:49:18 +0200

    feat(frontend): add option Settings->Preferences->Other->When losing connection to the server->Disable

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -212,7 +212,7 @@ export const store = markRaw(new Pizzax('base', {
 	},
 	serverDisconnectedBehavior: {
 		where: 'device',
-		default: 'disabled' as 'quiet' | 'dialog' | 'disabled',
+		default: 'quiet' as 'quiet' | 'reload' | 'dialog' | 'disabled',
 	},
 	nsfw: {
 		where: 'device',
