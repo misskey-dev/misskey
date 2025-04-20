5f47e2ec0bcf2516efde3ec2fec06f15e5e5512c
miraikumiko <miraikumiko@disroot.org>
2025-04-20 21:01:14 +0200

    Fix migration 1745174115062-emailNotificationTypes.js

--- packages/backend/migration/1745174115062-emailNotificationTypes.js
+++ packages/backend/migration/1745174115062-emailNotificationTypes.js
@@ -3,13 +3,13 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-module.exports = class EmailNotificationTypes1745174115062 {
-    name = 'EmailNotificationTypes1745174115062'
-
+export class emailNotificationTypes1745174115062 {
+    constructor() {
+        this.name = 'emailNotificationTypes1745174115062';
+    }
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user_profile" ADD "emailNotificationTypes" jsonb NOT NULL DEFAULT '["login", "follow","receiveFollowRequest","groupInvited"]'`);
     }
-
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user_profile" DROP COLUMN "emailNotificationTypes"`);
     }
