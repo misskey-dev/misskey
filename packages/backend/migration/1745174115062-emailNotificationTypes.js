df9109161c5e0d4383285e521cd897676366262d
miraikumiko <miraikumiko@disroot.org>
2025-04-20 21:06:11 +0200

    Fix migration 1745174115062-emailNotificationTypes.js

--- packages/backend/migration/1745174115062-emailNotificationTypes.js
+++ packages/backend/migration/1745174115062-emailNotificationTypes.js
@@ -8,7 +8,7 @@ export class emailNotificationTypes1745174115062 {
         this.name = 'emailNotificationTypes1745174115062';
     }
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user_profile" ADD "emailNotificationTypes" jsonb NOT NULL DEFAULT '["login", "follow","receiveFollowRequest","groupInvited"]'`);
+        await queryRunner.query(`ALTER TABLE "user_profile" ADD "emailNotificationTypes" jsonb NOT NULL DEFAULT '["login","follow","receiveFollowRequest","groupInvited"]'`);
     }
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user_profile" DROP COLUMN "emailNotificationTypes"`);
