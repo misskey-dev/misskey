31186262fa3745f0f79cf1fe8db80f3414241bc6
syuilo <4439005+syuilo@users.noreply.github.com>
2025-04-23 17:13:09 +0900

    Update cli.ts

--- packages/backend/src/boot/cli.ts
+++ packages/backend/src/boot/cli.ts
@@ -6,34 +6,14 @@
 import 'reflect-metadata';
 import { EventEmitter } from 'node:events';
 import { NestFactory } from '@nestjs/core';
-import Logger from '@/logger.js';
 import { CommandModule } from '@/cli/CommandModule.js';
 import { NestLogger } from '@/NestLogger.js';
 import { CommandService } from '@/cli/CommandService.js';
-import { envOption } from '../env.js';
 
 process.title = 'Misskey Cli';
 
 Error.stackTraceLimit = Infinity;
 EventEmitter.defaultMaxListeners = 128;
-
-const logger = new Logger('core', 'cyan');
-
-//#region Events
-
-// Display detail of unhandled promise rejection
-if (!envOption.quiet) {
-	process.on('unhandledRejection', console.dir);
-}
-
-// Display detail of uncaught exception
-process.on('uncaughtException', err => {
-	try {
-		logger.error(err);
-		console.trace(err);
-	} catch { }
-});
-//#endregion
 
 const app = await NestFactory.createApplicationContext(CommandModule, {
 	logger: new NestLogger(),
