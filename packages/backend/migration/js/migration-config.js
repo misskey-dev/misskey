a577b703022b0b0e465cdfa4d8bb77a0ecd26468
anatawa12 <anatawa12@icloud.com>
2025-05-01 11:47:05 +0900

    fix: spdx license header

--- packages/backend/migration/js/migration-config.js
+++ packages/backend/migration/js/migration-config.js
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 export function isConcurrentIndexMigrationEnabled() {
 	return process.env.MISSKEY_MIGRATION_CREATE_INDEX_CONCURRENTLY === '1';
