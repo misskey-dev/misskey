a4a6e24affcf927d426f2cd46f17b7f3f94e37c2
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-05-04 18:00:59 +0900

    Revert "enhance(frontend): 中クリックでPullToRefreshするように"
    This reverts commit 5aeedf59ff73debbce36aeff4a27f170a2bf1ef4.

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5717,10 +5717,6 @@ export interface Locale extends ILocale {
          * ひっぱって更新
          */
         "enablePullToRefresh": string;
-        /**
-         * マウスでは、ホイールを押し込みながらドラッグします。
-         */
-        "enablePullToRefresh_description": string;
         "_chat": {
             /**
              * 送信者の名前を表示
