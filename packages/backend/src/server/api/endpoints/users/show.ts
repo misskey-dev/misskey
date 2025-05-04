d0d601399bcbccd90da392748c602834a98bd348
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-04 14:52:11 +0900

    Update show.ts

--- packages/backend/src/server/api/endpoints/users/show.ts
+++ packages/backend/src/server/api/endpoints/users/show.ts
@@ -5,7 +5,7 @@
 
 import { In, IsNull } from 'typeorm';
 import { Inject, Injectable } from '@nestjs/common';
-import type { UsersRepository } from '@/models/_.js';
+import type { MiMeta, UsersRepository } from '@/models/_.js';
 import type { MiUser } from '@/models/User.js';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import { UserEntityService } from '@/core/entities/UserEntityService.js';
@@ -82,6 +82,9 @@ export const paramDef = {
 @Injectable()
 export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-disable-line import/no-default-export
 	constructor(
+		@Inject(DI.meta)
+		private serverSettings: MiMeta,
+
 		@Inject(DI.usersRepository)
 		private usersRepository: UsersRepository,
 
@@ -92,6 +95,10 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		private apiLoggerService: ApiLoggerService,
 	) {
 		super(meta, paramDef, async (ps, me, _1, _2, _3, ip) => {
+			if (this.serverSettings.ugcVisibilityForVisitor === 'none' && me == null) {
+				throw new ApiError(meta.errors.noSuchUser);
+			}
+
 			let user;
 
 			const isModerator = await this.roleService.isModerator(me);
@@ -123,6 +130,10 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			} else {
 				// Lookup user
 				if (typeof ps.host === 'string' && typeof ps.username === 'string') {
+					if (this.serverSettings.ugcVisibilityForVisitor === 'local' && me == null) {
+						throw new ApiError(meta.errors.noSuchUser);
+					}
+
 					user = await this.remoteUserResolveService.resolveUser(ps.username, ps.host).catch(err => {
 						this.apiLoggerService.logger.warn(`failed to resolve remote user: ${err}`);
 						throw new ApiError(meta.errors.failedToResolveRemoteUser);
@@ -136,6 +147,10 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				}
 
 				if (user == null || (!isModerator && user.isSuspended)) {
+					throw new ApiError(meta.errors.noSuchUser);
+				}
+
+				if (this.serverSettings.ugcVisibilityForVisitor === 'local' && user.host != null && me == null) {
 					throw new ApiError(meta.errors.noSuchUser);
 				}
 
