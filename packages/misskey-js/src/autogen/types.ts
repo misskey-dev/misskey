a546c258e20a5e8c1d928a1aa672008bb5929df5
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-05-10 19:43:16 +0900

    fix(frontend): ジョブキューインスペクタの型エラー解消

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -4937,6 +4937,32 @@ export type components = {
       failed: number;
       delayed: number;
     };
+    QueueMetrics: {
+      meta: {
+        count: number;
+        prevTS: number;
+        prevCount: number;
+      };
+      data: number[];
+      count: number;
+    };
+    QueueJob: {
+      id: string;
+      name: string;
+      data: Record<string, never>;
+      opts: Record<string, never>;
+      timestamp: number;
+      processedOn?: number;
+      processedBy?: string;
+      finishedOn?: number;
+      progress: Record<string, never>;
+      attempts: number;
+      delay: number;
+      failedReason: string;
+      stacktrace: string[];
+      returnValue: Record<string, never>;
+      isFailed: boolean;
+    };
     Antenna: {
       /** Format: id */
       id: string;
@@ -9037,9 +9063,11 @@ export type operations = {
       };
     };
     responses: {
-      /** @description OK (without any results) */
-      204: {
-        content: never;
+      /** @description OK (with results) */
+      200: {
+        content: {
+          'application/json': components['schemas']['QueueJob'][];
+        };
       };
       /** @description Client error */
       400: {
@@ -9141,9 +9169,43 @@ export type operations = {
       };
     };
     responses: {
-      /** @description OK (without any results) */
-      204: {
-        content: never;
+      /** @description OK (with results) */
+      200: {
+        content: {
+          'application/json': {
+            /** @enum {string} */
+            name: 'system' | 'endedPollNotification' | 'deliver' | 'inbox' | 'db' | 'relationship' | 'objectStorage' | 'userWebhookDeliver' | 'systemWebhookDeliver';
+            qualifiedName: string;
+            counts: {
+              [key: string]: number;
+            };
+            isPaused: boolean;
+            metrics: {
+              completed: components['schemas']['QueueMetrics'];
+              failed: components['schemas']['QueueMetrics'];
+            };
+            db: {
+              version: string;
+              /** @enum {string} */
+              mode: 'cluster' | 'standalone' | 'sentinel';
+              runId: string;
+              processId: string;
+              port: number;
+              os: string;
+              uptime: number;
+              memory: {
+                total: number;
+                used: number;
+                fragmentationRatio: number;
+                peak: number;
+              };
+              clients: {
+                blocked: number;
+                connected: number;
+              };
+            };
+          };
+        };
       };
       /** @description Client error */
       400: {
@@ -9185,9 +9247,22 @@ export type operations = {
    */
   admin___queue___queues: {
     responses: {
-      /** @description OK (without any results) */
-      204: {
-        content: never;
+      /** @description OK (with results) */
+      200: {
+        content: {
+          'application/json': ({
+              /** @enum {string} */
+              name: 'system' | 'endedPollNotification' | 'deliver' | 'inbox' | 'db' | 'relationship' | 'objectStorage' | 'userWebhookDeliver' | 'systemWebhookDeliver';
+              counts: {
+                [key: string]: number;
+              };
+              isPaused: boolean;
+              metrics: {
+                completed: components['schemas']['QueueMetrics'];
+                failed: components['schemas']['QueueMetrics'];
+              };
+            })[];
+        };
       };
       /** @description Client error */
       400: {
@@ -9344,9 +9419,11 @@ export type operations = {
       };
     };
     responses: {
-      /** @description OK (without any results) */
-      204: {
-        content: never;
+      /** @description OK (with results) */
+      200: {
+        content: {
+          'application/json': components['schemas']['QueueJob'];
+        };
       };
       /** @description Client error */
       400: {
