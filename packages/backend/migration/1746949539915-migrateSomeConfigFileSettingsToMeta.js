3dde7f1ac4eb11dd61a4d0513747e22e0ec9bbc0
anatawa12 <anatawa12@icloud.com>
2025-05-12 15:40:40 +0900

    feat: migrate to existing config value (#16030)

--- packages/backend/migration/1746949539915-migrateSomeConfigFileSettingsToMeta.js
+++ packages/backend/migration/1746949539915-migrateSomeConfigFileSettingsToMeta.js
@@ -3,13 +3,17 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import {loadConfig} from "./js/migration-config.js";
+
 export class MigrateSomeConfigFileSettingsToMeta1746949539915 {
     name = 'MigrateSomeConfigFileSettingsToMeta1746949539915'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "meta" ADD "proxyRemoteFiles" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "meta" ADD "signToActivityPubGet" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "meta" ADD "allowExternalApRedirect" boolean NOT NULL DEFAULT true`);
+        const config = loadConfig();
+        // $1 cannot be used in ALTER TABLE queries
+        await queryRunner.query(`ALTER TABLE "meta" ADD "proxyRemoteFiles" boolean NOT NULL DEFAULT ${config.proxyRemoteFiles}`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "signToActivityPubGet" boolean NOT NULL DEFAULT ${config.signToActivityPubGet}`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "allowExternalApRedirect" boolean NOT NULL DEFAULT ${!config.disallowExternalApRedirect}`);
     }
 
     async down(queryRunner) {
