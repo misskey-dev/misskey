c92f1c7b06be38c42d26f117042fa523e6a09cd7
Hazelnoot <acomputerdog@gmail.com>
2025-05-25 12:51:00 -0400

    re-analyze all tables affected by new indexes

--- packages/backend/migration/1748104955717-index_IDX_instance_host_key.js
+++ packages/backend/migration/1748104955717-index_IDX_instance_host_key.js
@@ -8,10 +8,6 @@ export class IndexIDXInstanceHostKey1748104955717 {
 
 	async up(queryRunner) {
 		await queryRunner.query(`CREATE UNIQUE INDEX "IDX_instance_host_key" ON "instance" (((lower(reverse("host")) || '.')::text) text_pattern_ops)`);
-
-		// Flush all cached Linear Scan Plans and redo statistics for expression index
-		// this is important for Postgres to learn that even in highly complex queries, using this index first can reduce the result set significantly
-		await queryRunner.query(`ANALYZE "instance"`);
 	}
 
 	async down(queryRunner) {
