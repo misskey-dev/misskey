e393ac7f97692329f64e238487d0d2782fa6e31f
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-29 19:59:50 +0900

    Update zoomLines.ts

--- packages/frontend/src/utility/image-effector/fxs/zoomLines.ts
+++ packages/frontend/src/utility/image-effector/fxs/zoomLines.ts
@@ -25,7 +25,8 @@ void main() {
 	float angle = atan(-u_pos.y + (in_uv.y), -u_pos.x + (in_uv.x));
 	float t = (1.0 + sin(angle * u_frequency)) / 2.0;
 	if (u_thresholdEnabled) t = t > u_threshold ? 1.0 : 0.0;
-	float mask = distance(in_uv / u_maskSize, u_pos / u_maskSize);
+	float d = distance(in_uv * vec2(2.0, 2.0), u_pos * vec2(2.0, 2.0));
+	float mask = d < u_maskSize ? 0.0 : ((d - u_maskSize) * (1.0 + (u_maskSize * 2.0)));
 	out_color = vec4(
 		mix(in_color.r, u_black ? 0.0 : 1.0, t * mask),
 		mix(in_color.g, u_black ? 0.0 : 1.0, t * mask),
