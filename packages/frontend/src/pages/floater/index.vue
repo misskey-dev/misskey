848412dc3256a13795febecc90bf057ffc36e5eb
hitalin <hitalin@proton.me>
2025-06-02 16:02:39 +0900

    update: floaterページのタブUIをタイムラインページと同様の見た目に
    ※ Misskey本家で廃止された`MkTimeline`コンポーネントをついでに削除

--- packages/frontend/src/pages/floater/index.vue
+++ packages/frontend/src/pages/floater/index.vue
@@ -4,33 +4,36 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<MkStickyContainer>
-	<template #header><MkPageHeader v-model:tab="recent" :actions="headerActions" :tabs="headerTabs"/></template>
+<PageWithHeader v-model:tab="recent" :actions="headerActions" :tabs="headerTabs" :swipable="true">
 	<div class="_spacer" style="--MI_SPACER-w: 1000px;">
 		<Transition name="fade" mode="out-in">
 			<XFloater :key="recent" :anchorDate="anchorDate"/>
 		</Transition>
 	</div>
-</MkStickyContainer>
+</PageWithHeader>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
+import { computed, ref, provide } from 'vue';
 import XFloater from './floater.vue';
 import { definePage } from '@/page.js';
 import { i18n } from '@/i18n.js';
+import PageWithHeader from '@/components/global/PageWithHeader.vue';
+
+// タブUIの表示のためにこのプロバイダーを追加
+provide('shouldOmitHeaderTitle', true);
 
 // 最初から文字列として初期化
 const recent = ref('86400000');
 
 const headerActions = computed(() => []);
 const headerTabs = computed(() => [
-	// タブのkeyも文字列として定義
-	{ key: '3600000', title: i18n.tsx.recentNHours({ n: 1 }) },
-	{ key: '86400000', title: i18n.tsx.recentNDays({ n: 1 }) },
-	{ key: '259200000', title: i18n.tsx.recentNDays({ n: 3 }) },
-	{ key: '604800000', title: i18n.tsx.recentNDays({ n: 7 }) },
-	{ key: '2592000000', title: i18n.tsx.recentNDays({ n: 30 }) },
+	// タブのkeyも文字列として定義、タイムラインのようにiconOnlyを追加
+	{ key: '3600000', title: i18n.tsx.recentNHours({ n: 1 }), icon: 'ti ti-clock', iconOnly: true },
+	{ key: '86400000', title: i18n.tsx.recentNDays({ n: 1 }), icon: 'ti ti-calendar', iconOnly: true },
+	{ key: '259200000', title: i18n.tsx.recentNDays({ n: 3 }), icon: 'ti ti-calendar-event', iconOnly: true },
+	{ key: '604800000', title: i18n.tsx.recentNDays({ n: 7 }), icon: 'ti ti-calendar-week', iconOnly: true },
+	{ key: '2592000000', title: i18n.tsx.recentNDays({ n: 30 }), icon: 'ti ti-calendar-month', iconOnly: true },
 ]);
 
 const anchorDate = computed(() => {
@@ -43,10 +46,3 @@ definePage(() => ({
 	icon: 'ti ti-lifebuoy',
 }));
 </script>
-
-<!-- タブを中央揃えにするスタイルを追加 -->
-<style lang="scss">
-:deep(.mk-page-header-tabs) {
-  justify-content: center !important;
-}
-</style>
