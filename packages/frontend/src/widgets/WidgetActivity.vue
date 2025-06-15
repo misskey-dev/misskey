9f2e3ccb6ccc6b9b4e16951a3f364eef399255ed
hitalin <hitalin@proton.me>
2025-06-15 09:28:24 +0900

    fix: WidgetActivityでユーザー自身のアクティビティを表示できるよう修正
    アクティビティウィジットで「Activity of the user is not public」エラーが発生
    
    - `WidgetActivity.vue`のAPI呼び出しを`misskeyApiGet`から`misskeyApi`に変更

--- packages/frontend/src/widgets/WidgetActivity.vue
+++ packages/frontend/src/widgets/WidgetActivity.vue
@@ -22,11 +22,11 @@ SPDX-License-Identifier: AGPL-3.0-only
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { useWidgetPropsManager } from './widget.js';
-import type { WidgetComponentProps, WidgetComponentEmits, WidgetComponentExpose } from './widget.js';
 import XCalendar from './WidgetActivity.calendar.vue';
 import XChart from './WidgetActivity.chart.vue';
+import type { WidgetComponentProps, WidgetComponentEmits, WidgetComponentExpose } from './widget.js';
 import type { GetFormResultType } from '@/utility/form.js';
-import { misskeyApiGet } from '@/utility/misskey-api.js';
+import { misskeyApi } from '@/utility/misskey-api.js';
 import MkContainer from '@/components/MkContainer.vue';
 import { $i } from '@/i.js';
 import { i18n } from '@/i18n.js';
@@ -77,7 +77,7 @@ const toggleView = () => {
 	save();
 };
 
-misskeyApiGet('charts/user/notes', {
+misskeyApi('charts/user/notes', {
 	userId: $i.id,
 	span: 'day',
 	limit: 7 * 21,
