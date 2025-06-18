59855e6ddaa965cca53f5c62d7677465743c320d
hitalin <hitalin@proton.me>
2025-06-19 04:54:01 +0900

    fix: modify the status indicator to use green instead of the accent color for the active status

--- packages/frontend/src/widgets/WidgetJitsiMeet.vue
+++ packages/frontend/src/widgets/WidgetJitsiMeet.vue
@@ -425,9 +425,9 @@ defineExpose<WidgetComponentExpose>({
 	line-height: 0;
 
 	&.statusActive {
-		background-color: var(--MI_THEME-accent);
+		background-color: rgb(46, 204, 113);
 		opacity: 1;
-		box-shadow: 0 0 8px var(--MI_THEME-accent);
+		box-shadow: 0 0 8px rgb(46, 204, 113);
 	}
 }
 
