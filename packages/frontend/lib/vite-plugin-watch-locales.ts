23d0219c0fb5e9da6902c3c6be978bfddd9c85d0
tai-cha <dev@taichan.site>
2025-06-23 20:15:33 +0900

    SPDX

--- packages/frontend/lib/vite-plugin-watch-locales.ts
+++ packages/frontend/lib/vite-plugin-watch-locales.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import path from 'node:path'
 import locales from '../../../locales/index.js';
 
