bddc00bf475229cafeeb7b2f9666953a51ca5fdd
tai-cha <dev@taichan.site>
2025-06-23 19:53:41 +0900

    chore(frontend): 開発モード時に言語ファイルの変更を自動で反映するように

---
+++ packages/frontend/lib/vite-plugin-watch-locales.ts
@@ -0,0 +1,31 @@
+import path from 'node:path'
+import locales from '../../../locales/index.js';
+
+const localesDir = path.resolve(__dirname, '../../../locales')
+
+/**
+ * 外部ファイルを監視し、必要に応じてサーバを再起動するViteプラグイン
+ * @returns {import('vite').Plugin}
+ */
+export default function pluginWatchLocales() {
+  return {
+    name: 'watch-locales',
+
+    configureServer(server) {
+      const localeYmlPaths = Object.keys(locales).map(locale => path.join(localesDir, `${locale}.yml`));
+
+      // watcherにパスを追加
+      server.watcher.add(localeYmlPaths);
+
+			server.watcher.on('change', (filePath) => {
+				if (localeYmlPaths.includes(filePath)) {
+					server.ws.send({
+						type: 'custom',
+						event: 'language-update',
+						data: filePath.match(/([^\/]+)\.yml$/)?.[1] || null,
+					})
+				}
+			});
+    },
+  };
+}
