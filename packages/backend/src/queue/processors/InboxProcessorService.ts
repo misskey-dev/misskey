8c11fd86cdcdf36dc2f13776a7ec9ca31659f008
lqvp <183242690+lqvp@users.noreply.github.com>
2025-07-06 22:02:37 +0900

    fix(backend): ブロックしたインスタンスのInboxジョブが蓄積し続ける問題を修正

--- packages/backend/src/queue/processors/InboxProcessorService.ts
+++ packages/backend/src/queue/processors/InboxProcessorService.ts
@@ -255,6 +255,9 @@ export class InboxProcessorService implements OnApplicationShutdown {
 				if (e.id === '9f466dab-c856-48cd-9e65-ff90ff750580') {
 					return 'note contains too many mentions';
 				}
+				if (e.id === '09d79f9e-64f1-4316-9cfa-e75c4d091574') { // Instance is blocked
+					return 'skip: blocked instance';
+				}
 			}
 			throw e;
 		}
