e785713523af731ccea819483ef4144b9ba3ea42
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-08-01 20:16:45 +0900

    enhance(misskey-js): テストスイートをVitestに置き換え

---
+++ packages/misskey-js/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, configDefaults } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		include: ['test/**/*.ts'],
+		coverage: {
+			exclude: [
+				...configDefaults.coverage.exclude!,
+				'src/autogen/**/*',
+				'generator/**/*',
+				'built/**/*',
+				'test-d/**/*',
+			],
+		}
+	},
+});
