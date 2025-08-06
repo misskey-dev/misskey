ba6ff314002db9f75f624d60c8b6de1726959d6c
anatawa12 <anatawa12@icloud.com>
2025-08-07 04:35:09 +0900

    fix spex

--- packages/frontend-shared/js/store-boot-errors.ts
+++ packages/frontend-shared/js/store-boot-errors.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import type { Locale } from '../../../locales/index.js';
 
 type BootLoaderLocaleBody = Locale['_bootErrors'] & { reload: Locale['reload'] };
