1402da0c1140ee66355734bad4ec0eef1277f2ea
anatawa12 <anatawa12@icloud.com>
2025-08-06 14:43:08 +0900

    コメント追加

--- packages/frontend/src/i18n.ts
+++ packages/frontend/src/i18n.ts
@@ -8,9 +8,11 @@ import { I18n } from '@@/js/i18n.js';
 import { lang, version } from '@@/js/config.js';
 import type { Locale } from '../../../locales/index.js';
 
+// ここはビルド時に const locale = JSON.parse("...") みたいな感じで置き換えられるので top-level await は消える
 const locale = await window.fetch(`/assets/locales/${lang}.${version}.json`).then(r => r.json());
 export const i18n = markRaw(new I18n<Locale>(locale, _DEV_));
 
+// test 以外では使わないこと。インライン化されてるのでだいたい意味がない
 export function updateI18n(newLocale: Locale) {
 	i18n.locale = newLocale;
 }
