3b4879133c4be16a5e9ed7edd31a210ed3ab89af
syuilo <4439005+syuilo@users.noreply.github.com>
2025-08-18 18:06:32 +0900

    🎨

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5525,6 +5525,10 @@ export interface Locale extends ILocale {
      * セーフモードが有効な間はデフォルトのテーマが使用されます。セーフモードをオフにすると元に戻ります。
      */
     "themeIsDefaultBecauseSafeMode": string;
+    /**
+     * ベータ版の検証にご協力いただきありがとうございます！
+     */
+    "thankYouForTestingBeta": string;
     "_order": {
         /**
          * 新しい順
