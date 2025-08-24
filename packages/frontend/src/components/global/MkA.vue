d044217b9858c5f4748e35988136ef7a049b1789
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-08-24 19:24:52 +0900

    fix: 制御キーの場合を個別ハンドリングするのではなくブラウザ既定の挙動に任せるように

--- packages/frontend/src/components/global/MkA.vue
+++ packages/frontend/src/components/global/MkA.vue
@@ -4,7 +4,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<a ref="el" :href="to" :class="active ? activeClass : null" @click.prevent="nav" @contextmenu.prevent.stop="onContextmenu">
+<a ref="el" :href="to" :class="active ? activeClass : null" @click="nav" @contextmenu.prevent.stop="onContextmenu">
 	<slot></slot>
 </a>
 </template>
@@ -86,6 +86,11 @@ function openWindow() {
 }
 
 function nav(ev: MouseEvent) {
+	// 制御キーとの組み合わせは無視（shiftを除く）
+	if (ev.metaKey || ev.altKey || ev.ctrlKey) return;
+
+	ev.preventDefault();
+
 	if (behavior === 'browser') {
 		window.location.href = props.to;
 		return;
@@ -97,11 +102,6 @@ function nav(ev: MouseEvent) {
 
 	if (ev.shiftKey) {
 		return openWindow();
-	}
-
-	if (ev.ctrlKey) {
-		window.open(props.to, '_blank', 'noopener');
-		return;
 	}
 
 	router.pushByPath(props.to, ev.ctrlKey ? 'forcePage' : null);
