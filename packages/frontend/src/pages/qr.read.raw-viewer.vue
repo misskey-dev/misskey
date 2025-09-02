91245d146da209d67dfbb5173ee655ac32860ea8
tamaina <tamaina@hotmail.co.jp>
2025-09-03 01:06:56 +0900

    add spdx

--- packages/frontend/src/pages/qr.read.raw-viewer.vue
+++ packages/frontend/src/pages/qr.read.raw-viewer.vue
@@ -1,3 +1,8 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
 <template>
 <MkFolder defaultOpen :withSpacer="false">
 	<template #label>{{ data.split('\n')[0] }}</template>
