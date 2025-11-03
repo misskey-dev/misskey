c07632e60ec1ee36cc406509af7d1332ea8ff0f5
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-11-03 21:27:59 +0900

    fix: reorder migration file

--- packages/backend/migration/1762172837314-external-website-warn.js
+++ packages/backend/migration/1762172837314-external-website-warn.js
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export class ExternalWebsiteWarn1711008460816 {
-    name = 'ExternalWebsiteWarn1711008460816'
+export class ExternalWebsiteWarn1762172837314 {
+    name = 'ExternalWebsiteWarn1762172837314'
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "meta" ADD "trustedLinkUrlPatterns" character varying(3072) array NOT NULL DEFAULT '{}'`);
