ad4751918ba70628b59cd7125bb6074d53c25473
syuilo <4439005+syuilo@users.noreply.github.com>
2025-11-07 12:56:28 +0900

    fix type

--- packages/frontend/src/pages/channel.vue
+++ packages/frontend/src/pages/channel.vue
@@ -198,15 +198,15 @@ async function mute() {
 	const { canceled, result: period } = await os.select({
 		title: i18n.ts.mutePeriod,
 		items: [{
-			value: 'indefinitely', text: i18n.ts.indefinitely,
+			value: 'indefinitely', label: i18n.ts.indefinitely,
 		}, {
-			value: 'tenMinutes', text: i18n.ts.tenMinutes,
+			value: 'tenMinutes', label: i18n.ts.tenMinutes,
 		}, {
-			value: 'oneHour', text: i18n.ts.oneHour,
+			value: 'oneHour', label: i18n.ts.oneHour,
 		}, {
-			value: 'oneDay', text: i18n.ts.oneDay,
+			value: 'oneDay', label: i18n.ts.oneDay,
 		}, {
-			value: 'oneWeek', text: i18n.ts.oneWeek,
+			value: 'oneWeek', label: i18n.ts.oneWeek,
 		}],
 		default: 'indefinitely',
 	});
