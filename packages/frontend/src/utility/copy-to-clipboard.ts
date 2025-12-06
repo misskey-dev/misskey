27ff32b3871e3d4b1fb46f51912f19859a3fdd61
FrozenAlex <FrozenAlex@users.noreply.github.com>
2025-12-06 17:11:41 +0000

    Add copy fallback for i2p and tor

--- packages/frontend/src/utility/copy-to-clipboard.ts
+++ packages/frontend/src/utility/copy-to-clipboard.ts
@@ -11,7 +11,20 @@ import { i18n } from '@/i18n.js';
  */
 export function copyToClipboard(input: string | null) {
 	if (input) {
-		navigator.clipboard.writeText(input);
+		// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+		if (navigator.clipboard) {
+			navigator.clipboard.writeText(input);
+		} else {
+			// Fallback for i2p and tor
+			const textarea = window.document.createElement('textarea');
+			textarea.value = input;
+			textarea.style.position = 'fixed';
+			textarea.style.opacity = '0';
+			window.document.body.appendChild(textarea);
+			textarea.select();
+			window.document.execCommand('copy');
+			window.document.body.removeChild(textarea);
+		}
 		os.toast(i18n.ts.copiedToClipboard);
 	}
 };
