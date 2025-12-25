9625f3adb6170cf830d83429b24d38c29824b207
Ebise <lutica@shrimpia.network>
2025-12-25 11:11:53 +0000

    feat(frontend): アバターデコレーションのカテゴリ設定機能

--- packages/frontend/src/pages/avatar-decorations.vue
+++ packages/frontend/src/pages/avatar-decorations.vue
@@ -7,18 +7,21 @@ SPDX-License-Identifier: AGPL-3.0-only
 <PageWithHeader :actions="headerActions" :tabs="headerTabs">
 	<div class="_spacer" style="--MI_SPACER-w: 900px;">
 		<div class="_gaps">
-			<div :class="$style.decorations">
-				<div
-					v-for="avatarDecoration in avatarDecorations"
-					:key="avatarDecoration.id"
-					v-panel
-					:class="$style.decoration"
-					@click="edit(avatarDecoration)"
-				>
-					<div :class="$style.decorationName"><MkCondensedLine :minScale="0.5">{{ avatarDecoration.name }}</MkCondensedLine></div>
-					<MkAvatar style="width: 60px; height: 60px;" :user="$i" :decorations="[{ url: avatarDecoration.url }]" forceShowDecoration/>
+			<MkFoldableSection v-for="category in Object.keys(groupedDecorations)" :key="category" :expanded="true">
+				<template #header>{{ category || i18n.ts.other }}</template>
+				<div :class="$style.decorations">
+					<div
+						v-for="avatarDecoration in groupedDecorations[category]"
+						:key="avatarDecoration.id"
+						v-panel
+						:class="$style.decoration"
+						@click="edit(avatarDecoration)"
+					>
+						<div :class="$style.decorationName"><MkCondensedLine :minScale="0.5">{{ avatarDecoration.name }}</MkCondensedLine></div>
+						<MkAvatar style="width: 60px; height: 60px;" :user="$i" :decorations="[{ url: avatarDecoration.url }]" forceShowDecoration/>
+					</div>
 				</div>
-			</div>
+			</MkFoldableSection>
 		</div>
 	</div>
 </PageWithHeader>
@@ -32,10 +35,20 @@ import * as os from '@/os.js';
 import { misskeyApi } from '@/utility/misskey-api.js';
 import { i18n } from '@/i18n.js';
 import { definePage } from '@/page.js';
+import MkFoldableSection from '@/components/MkFoldableSection.vue';
 
 const $i = ensureSignin();
 
 const avatarDecorations = ref<Misskey.entities.AdminAvatarDecorationsListResponse>([]);
+const groupedDecorations = computed(() => {
+	const groups: Record<string, Misskey.entities.AdminAvatarDecorationsListResponse> = {};
+	for (const decoration of avatarDecorations.value) {
+		const category = decoration.category ?? '';
+		groups[category] ??= [];
+		groups[category].push(decoration);
+	}
+	return groups;
+});
 
 function load() {
 	misskeyApi('admin/avatar-decorations/list').then(_avatarDecorations => {
@@ -47,6 +60,7 @@ load();
 
 async function add(ev: PointerEvent) {
 	const { dispose } = await os.popupAsyncWithDialog(import('./avatar-decoration-edit-dialog.vue').then(x => x.default), {
+		categories: Object.keys(groupedDecorations.value),
 	}, {
 		done: result => {
 			if (result.created) {
@@ -60,6 +74,7 @@ async function add(ev: PointerEvent) {
 async function edit(avatarDecoration: Misskey.entities.AdminAvatarDecorationsListResponse[number]) {
 	const { dispose } = await os.popupAsyncWithDialog(import('./avatar-decoration-edit-dialog.vue').then(x => x.default), {
 		avatarDecoration: avatarDecoration,
+		categories: Object.keys(groupedDecorations.value),
 	}, {
 		done: result => {
 			if (result.updated) {
---
+++ packages/frontend/src/utility/group-avatar-decorations.ts
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import * as Misskey from 'misskey-js';
+
+type AvatarDecoration = Misskey.entities.GetAvatarDecorationsResponse[number];
+
+/**
+ * アバターデコレーションをカテゴリごとにグループ化します。
+ * @param decorations アバターデコレーションの配列
+ * @returns カテゴリごとにグループ化されたアバターデコレーションオブジェクト
+ */
+export const groupAvatarDecorations = (decorations: AvatarDecoration[]) => {
+	const grouped: Record<string, AvatarDecoration[]> = {};
+
+	for (const decoration of decorations) {
+		const category = decoration.category ?? '';
+		if (!(category in grouped)) {
+			grouped[category] = [];
+		}
+		grouped[category].push(decoration);
+	}
+
+	return grouped;
+};
