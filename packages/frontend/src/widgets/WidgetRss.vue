4e17fd26902f939a465c8bf8e154b0523cab5f71
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-12-30 14:48:13 +0900

    fix

--- packages/frontend/src/widgets/WidgetRss.vue
+++ packages/frontend/src/widgets/WidgetRss.vue
@@ -25,6 +25,7 @@ import * as Misskey from 'misskey-js';
 import { url as base } from '@@/js/config.js';
 import { useInterval } from '@@/js/use-interval.js';
 import { useWidgetPropsManager } from './widget.js';
+import { i18n } from '@/i18n.js';
 import type { WidgetComponentEmits, WidgetComponentExpose, WidgetComponentProps } from './widget.js';
 import type { FormWithDefault, GetFormResultType } from '@/utility/form.js';
 import MkContainer from '@/components/MkContainer.vue';
