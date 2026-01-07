ff17b2476e773fd9910220b896794d3ec9869466
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-01-07 16:09:07 +0900

    spdx

--- packages/frontend/src/utility/is-birthday.ts
+++ packages/frontend/src/utility/is-birthday.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import * as Misskey from 'misskey-js';
 
 export function isBirthday(user: Misskey.entities.UserDetailed, now = new Date()): boolean {
