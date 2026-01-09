6c51c4bccb8c8c9092c52125d0698a33bf2f10e8
syuilo <4439005+syuilo@users.noreply.github.com>
2026-01-09 20:02:11 +0900

    wip

--- packages/misskey-js/src/streaming.types.ts
+++ packages/misskey-js/src/streaming.types.ts
@@ -224,7 +224,12 @@ export type Channels = {
 			canceled: (payload: { userId: User['id']; }) => void;
 			changeReadyStates: (payload: { user1: boolean; user2: boolean; }) => void;
 			updateSettings: <K extends ReversiUpdateKey>(payload: { userId: User['id']; key: K; value: ReversiGameDetailed[K]; }) => void;
-			log: (payload: Record<string, unknown>) => void;
+			log: (payload: {
+				time: number;
+				player: boolean;
+				operation: 'put';
+				pos: number;
+			} & { id: string | null }) => void;
 		};
 		receives: {
 			putStone: {
