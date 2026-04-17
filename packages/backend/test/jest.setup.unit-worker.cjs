c2cb46d5179dd30fd8834aee1425c57b8ba5cc06
Acid Chicken <root@acid-chicken.com>
2026-04-18 04:10:07 +0900

    test(backend): delete unused file

--- packages/backend/test/jest.setup.unit-worker.cjs
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: syuilo and misskey-project
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
-const { init } = require('slacc');
-
-// Unit test worker process bootstrap
-init(1);
