b7346843476f46dc6af4ebf03374fa312e46fe46
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-04-19 02:29:43 +0900

    Revert "attempt to fix e2e"
    This reverts commit 9fb86a40769c7809e34fed6dae9027bc5bd26fb7.

--- packages/backend/vitest.config.e2e.ts
+++ packages/backend/vitest.config.e2e.ts
@@ -1,11 +1,9 @@
-import { resolve } from 'node:path';
 import { defineConfig, mergeConfig } from 'vitest/config';
 import { baseConfig } from './vitest.config.js';
 
 export default mergeConfig(
 	baseConfig,
 	defineConfig({
-		root: resolve(__dirname),
 		test: {
 			include: ['test/e2e/**/*.ts'],
 			globalSetup: './built-test/entry.js',
