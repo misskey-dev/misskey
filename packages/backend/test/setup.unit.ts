cd95e1e137b61f8e0cc0202f2827cfb65bf4f20f
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-04-20 15:15:14 +0900

    chore: migrate to vitest

--- packages/backend/test/setup.unit.ts
+++ packages/backend/test/setup.unit.ts
@@ -2,9 +2,11 @@
  * SPDX-FileCopyrightText: syuilo and misskey-project
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import { init } from 'slacc';
 
 export default function setup() {
 	// DBはUTC（っぽい）ので、テスト側も合わせておく
 	process.env.TZ = 'UTC';
 	process.env.NODE_ENV = 'test';
+	init(1);
 }
