4337bff80e6cb44960d94ed5cb4f9ab1b4cb1296
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-04-20 13:57:43 +0900

    attempt to fix e2e

--- packages/backend/test/setup.e2e.ts
+++ packages/backend/test/setup.e2e.ts
@@ -3,9 +3,10 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import { beforeAll } from 'vitest';
 import { initTestDb, sendEnvResetRequest } from './utils.js';
 
-export default async function setup() {
+beforeAll(async () => {
 	await initTestDb(false);
 	await sendEnvResetRequest();
-}
+});
