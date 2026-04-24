0a451ba7400d0ae2d72c3b2cffd81d93ceecfeae
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-04-24 20:16:24 +0900

    spdx

--- packages/frontend/src/directives/autosize.ts
+++ packages/frontend/src/directives/autosize.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import type { Directive } from 'vue';
 import { prefer } from '@/preferences.js';
 
