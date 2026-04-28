d05d7938a420fefb49590c05a405a26df8193c30
syuilo <4439005+syuilo@users.noreply.github.com>
2026-04-28 21:12:05 +0900

    Update engine.ts

--- packages/frontend/src/world/room/engine.ts
+++ packages/frontend/src/world/room/engine.ts
@@ -10,6 +10,7 @@
 // TODO: テクスチャ置き換え時、元のテクスチャをちゃんとdispose
 // TODO: Safariでテクスチャの読み込みに失敗かつ無ハンドリングだとsrが有効にできなくなる現象をbabylonに報告
 // TODO: GridMaterialは今のところwgslにネイティブ対応しておらず、読み込むとwgslへの変換コードが読み込まれるっぽく無駄だしweb worker内ではそのままではインポートエラーになるのでどうにかする
+// TODO: なぜかTVのライトだけmaxRangeによる制限が効かないのを調査
 
 import * as BABYLON from '@babylonjs/core';
 import { registerBuiltInLoaders } from '@babylonjs/loaders/dynamic';
