52e9395fab24b912e735105ee71e562fba156387
syuilo <4439005+syuilo@users.noreply.github.com>
2026-04-29 09:40:12 +0900

    wip

--- packages/frontend/src/world/room/objects/snakeplant.ts
+++ packages/frontend/src/world/room/objects/snakeplant.ts
@@ -3,20 +3,43 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import * as BABYLON from '@babylonjs/core';
 import { defineObject } from '../object.js';
 
 export const snakeplant = defineObject({
 	id: 'snakeplant',
 	name: 'Snake Plant',
 	options: {
-		schema: {},
-		default: {},
+		schema: {
+			potColor: {
+				type: 'color',
+				label: 'potColor',
+			},
+		},
+		default: {
+			potColor: [0.7, 0.7, 0.7],
+		},
 	},
 	placement: 'top',
 	hasCollisions: false,
 	hasTexture: true,
-	createInstance: () => {
+	canPreMeshesMerging: true,
+	createInstance: ({ options, model }) => {
+		const potMaterial = model.findMaterial('__X_POT__');
+
+		const applyPotColor = () => {
+			const [r, g, b] = options.potColor;
+			potMaterial.albedoColor = new BABYLON.Color3(r, g, b);
+		};
+
+		applyPotColor();
+
 		return {
+			onOptionsUpdated: ([k, v]) => {
+				switch (k) {
+					case 'potColor': applyPotColor(); break;
+				}
+			},
 			interactions: {},
 		};
 	},
