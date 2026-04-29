18a4da4ad7fcab0d2057fabbcdb06627a72eeb23
syuilo <4439005+syuilo@users.noreply.github.com>
2026-04-29 09:49:04 +0900

    wip

--- packages/frontend/src/world/room/objects/pachira.ts
+++ packages/frontend/src/world/room/objects/pachira.ts
@@ -3,21 +3,43 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import * as BABYLON from '@babylonjs/core';
 import { defineObject } from '../object.js';
 
 export const pachira = defineObject({
 	id: 'pachira',
 	name: 'Pachira',
 	options: {
-		schema: {},
-		default: {},
+		schema: {
+			potColor: {
+				type: 'color',
+				label: 'potColor',
+			},
+		},
+		default: {
+			potColor: [0.8, 0.8, 0.8],
+		},
 	},
 	placement: 'top',
-	hasCollisions: true,
+	hasCollisions: false,
 	hasTexture: true,
 	canPreMeshesMerging: true,
-	createInstance: () => {
+	createInstance: ({ options, model }) => {
+		const potMaterial = model.findMaterial('__X_POT__');
+
+		const applyPotColor = () => {
+			const [r, g, b] = options.potColor;
+			potMaterial.albedoColor = new BABYLON.Color3(r, g, b);
+		};
+
+		applyPotColor();
+
 		return {
+			onOptionsUpdated: ([k, v]) => {
+				switch (k) {
+					case 'potColor': applyPotColor(); break;
+				}
+			},
 			interactions: {},
 		};
 	},
