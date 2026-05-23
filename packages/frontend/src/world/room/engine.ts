ce72d104913c18a17b49396b78a2e63980d60367
syuilo <4439005+syuilo@users.noreply.github.com>
2026-05-23 19:43:36 +0900

    Update engine.ts

--- packages/frontend/src/world/room/engine.ts
+++ packages/frontend/src/world/room/engine.ts
@@ -10,6 +10,7 @@
 // TODO: テクスチャ置き換え時、元のテクスチャをちゃんとdispose
 // TODO: Safariでテクスチャの読み込みに失敗かつ無ハンドリングだとsrが有効にできなくなる現象をbabylonに報告
 // TODO: 一升瓶を選択するとエラーが出る現象をbabylonに報告
+// TODO: dj-playerを選択するとエラーが出る現象をbabylonに報告
 // TODO: pure barrel importが機能しない問題をbabylonに報告
 // TODO: meshをdiposeした際、scene.meshesやshadowmapのrenderlistからも明示的に削除しないとメモリリークするのかどうかbabylonのforumで尋ねる
 // TODO: 起動時、ひとつでもcustom imageの読み込みに失敗したら「一部の画像を読み込めませんでした」を出す
