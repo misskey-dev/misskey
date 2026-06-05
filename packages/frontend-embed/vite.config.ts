ba3d223ca87ab75ac40828c0faad4f7d51259d6a
anatawa12 <anatawa12@icloud.com>
2026-06-05 09:27:14 +0900

    fix: let rolldown to not hoist i18n modules with other modules

--- packages/frontend-embed/vite.config.ts
+++ packages/frontend-embed/vite.config.ts
@@ -153,9 +153,11 @@ export function getConfig(): UserConfig {
 							name: 'vue',
 							test: /node_modules[\\/]vue/,
 						}, {
-							// dependencies of i18n.ts
-							name: 'config',
-							test: /@@[\\/]js[\\/]config\.js/,
+							// split each i18n related module to each distinct module, deny hoisting
+							name: 'i18n',
+							test: /i18n\.ts/,
+							minSize: 0,
+							maxSize: 1,
 						}],
 					},
 					entryFileNames: `scripts/${localesHash}-[hash:8].js`,
