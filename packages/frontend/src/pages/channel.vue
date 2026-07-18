96409fc61d7a90c16f836c4a7355851e19e8f1e3
Sayamame-beans <61457993+Sayamame-beans@users.noreply.github.com>
2026-07-18 18:43:17 +0900

    fix: incorrect caching item

--- packages/frontend/src/pages/channel.vue
+++ packages/frontend/src/pages/channel.vue
@@ -133,7 +133,7 @@ watch(() => props.channelId, async () => {
 		channelId: props.channelId,
 	});
 
-	miLocalStorage.setItem(`channel:${props.channelId}`, JSON.stringify(channel.value));
+	miLocalStorage.setItem(`channel:${props.channelId}`, JSON.stringify(_channel));
 	favorited.value = _channel.isFavorited ?? false;
 	if (favorited.value || _channel.isFollowing) {
 		tab.value = 'timeline';
