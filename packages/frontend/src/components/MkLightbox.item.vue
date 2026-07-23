cb5d74e4d098eb0507ab43ac6658f6a1dcf4e2f9
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-07-23 20:07:04 +0900

    fix(frontend): Lightbox上で強くスワイプしたときに一時的に画面が操作不能になる問題を修正

--- packages/frontend/src/components/MkLightbox.item.vue
+++ packages/frontend/src/components/MkLightbox.item.vue
@@ -16,7 +16,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 		@pointerup.passive="onPointerup"
 		@pointercancel.passive="cancelPointerGesture"
 		@touchstart.passive="onTouchstart"
-		@touchmove.passive="onTouchmove"
+		@touchmove="onTouchmove"
 		@touchcancel.passive="cancelPointerGesture"
 		@contextmenu="cancelPointerGesture"
 		@wheel="onWheel"
@@ -721,6 +721,11 @@ function onTouchstart(ev: TouchEvent) {
 
 function onTouchmove(ev: TouchEvent) {
 	doubleTapDetector.onTouchmove(ev);
+
+	// スワイプ操作中は、ブラウザがタッチを慣性スクロールとして認識するのを防ぐためにpreventDefaultする必要がある
+	if (currentPointerId != null || pointerEventCache.size > 1) {
+		ev.preventDefault();
+	}
 }
 
 //#region inertia
