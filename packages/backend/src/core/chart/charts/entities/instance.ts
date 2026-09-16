2cd4ea707868682a759ed0d2cbc03f45558f0582
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-09-16 15:35:46 +0900

    fix: カラムは広げない

--- packages/backend/src/core/chart/charts/entities/instance.ts
+++ packages/backend/src/core/chart/charts/entities/instance.ts
@@ -8,9 +8,9 @@ import Chart from '../../core.js';
 export const name = 'instance';
 
 export const schema = {
-	'requests.failed': {},
-	'requests.succeeded': {},
-	'requests.received': {},
+	'requests.failed': { range: 'small' },
+	'requests.succeeded': { range: 'small' },
+	'requests.received': { range: 'small' },
 	'notes.total': { accumulate: true },
 	'notes.inc': {},
 	'notes.dec': {},
