b92a94eb9341f44ad68f5d2c73a489a322875319
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-09-23 14:26:20 +0900

    fix(backend): harden db operations (#17963)
    fix(backend): SQLの取り扱いを強化

--- packages/backend/src/core/ChatService.ts
+++ packages/backend/src/core/ChatService.ts
@@ -898,9 +898,10 @@ export class ChatService {
 
 		await this.chatMessagesRepository.createQueryBuilder().update()
 			.set({
-				reactions: () => `array_append("reactions", '${userId}/${reaction}')`,
+				reactions: () => `array_append("reactions", :pair)`,
 			})
 			.where('id = :id', { id: message.id })
+			.setParameter('pair', `${userId}/${reaction}`)
 			.execute();
 
 		if (room) {
@@ -942,9 +943,10 @@ export class ChatService {
 
 		await this.chatMessagesRepository.createQueryBuilder().update()
 			.set({
-				reactions: () => `array_remove("reactions", '${userId}/${reaction}')`,
+				reactions: () => `array_remove("reactions", :pair)`,
 			})
 			.where('id = :id', { id: message.id })
+			.setParameter('pair', `${userId}/${reaction}`)
 			.execute();
 
 		// TODO: 実際に削除が行われたときのみイベントを発行する
