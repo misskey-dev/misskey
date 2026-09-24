709d758b8136e020c65f0db4501ecb155f0808c8
4sterisk <4sterisk@ymail.ne.jp>
2026-09-25 04:55:52 +0900

    fix(backend): クリップから同じノートを繰り返し削除すると clippedCount が負数になる問題の修正

--- packages/backend/src/core/ClipService.ts
+++ packages/backend/src/core/ClipService.ts
@@ -142,16 +142,14 @@ export class ClipService {
 			throw new ClipService.NoSuchClipError();
 		}
 
-		const note = await this.notesRepository.findOneBy({ id: noteId });
-
-		if (note == null) {
-			throw new ClipService.NoSuchNoteError();
-		}
-
-		await this.clipNotesRepository.delete({
+		const result = await this.clipNotesRepository.delete({
 			noteId: noteId,
 			clipId: clip.id,
 		});
+
+		if (result.affected === 0) {
+			throw new ClipService.NoSuchNoteError();
+		}
 
 		this.notesRepository.decrement({ id: noteId }, 'clippedCount', 1);
 	}
